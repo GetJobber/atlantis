80ed9c1ebed6a44dd2f8fe156a78cff6ec70f3d1
Craig Cairns <craig.c@getjobber.com>
2019-04-17 15:20:44 -0600

    Implement small and large sizes

--- packages/components/src/PlaceholderField.tsx
+++ packages/components/src/PlaceholderField.tsx
@@ -1,16 +1,19 @@
 import React, { useState, ChangeEvent } from "react";
+import classnames from "classnames";
 import styles from "./PlaceholderField.css";
 
 interface PlaceholderFieldProps {
   readonly placeholder?: string;
   readonly disabled?: boolean;
   readonly defaultValue?: string;
+  readonly size: "small" | "normal" | "large";
 }
 
 export default function PlaceholderField({
   placeholder,
   disabled,
-  defaultValue
+  defaultValue,
+  size = "normal",
 }: PlaceholderFieldProps) {
   const [, setValue] = useState();
 
@@ -18,9 +21,10 @@ export default function PlaceholderField({
     setValue(e.target.value);
   }
 
+  const wrapperClass = classnames(styles.inputWrapper, styles[size]);
+
   return (
-    <div className={styles.inputWrapper}>
-      <label>
+    <div className={wrapperClass}>
         <input
           type="text"
           placeholder={placeholder}
@@ -29,7 +33,9 @@ export default function PlaceholderField({
           defaultValue={defaultValue}
           required
         />
+        {size !== "small" && (
         <span className={styles.labelContent}>{placeholder}</span>
+        )}
       </label>
     </div>
   );
