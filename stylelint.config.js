2a6a7fe345a4d48815d0c9b3f46aefe56b94eca8
Craig Cairns <craig.c@getjobber.com>
2019-04-25 09:29:15 -0600

    Add additional stylelint properties

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,18 @@
 /* eslint-env node */
 module.exports = {
   extends: ["stylelint-config-recommended", "stylelint-config-css-modules"],
-  plugins: ["stylelint-order", "stylelint-color-format"],
+  plugins: [
+    "stylelint-prettier",
+    "stylelint-order",
+    "stylelint-color-format",
+    "stylelint-declaration-block-no-ignored-properties",
+  ],
   rules: {
+    "prettier/prettier": true,
     "color-format/format": {
       format: "rgb",
     },
+    "plugin/declaration-block-no-ignored-properties": true,
     "order/properties-order": [
       "position",
       "top",
