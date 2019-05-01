fd1d91e8254571d65389ddd9eb7c3733d210af84
Craig Cairns <craig.c@getjobber.com>
2019-05-01 16:48:50 -0600

    Implement sizes and colours

--- packages/components/src/InlineLabel/InlineLabel.test.tsx
+++ packages/components/src/InlineLabel/InlineLabel.test.tsx
@@ -3,6 +3,12 @@ import { InlineLabel } from ".";
 import renderer from "react-test-renderer";
 
 it("renders correctly", () => {
-  const tree = renderer.create(<InlineLabel />).toJSON();
-  expect(tree).toMatchInlineSnapshot();
+  const tree = renderer.create(<InlineLabel>My Label</InlineLabel>).toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <span
+      className="inlineLabel normal greyBlue"
+    >
+      My Label
+    </span>
+  `);
 });
--- packages/components/src/InlineLabel/InlineLabel.tsx
+++ packages/components/src/InlineLabel/InlineLabel.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import classnames from "classnames";
+import styles from "./InlineLabel.css";
 
-export function InlineLabel() {
-  return <></>;
+interface InlineLabelProps {
+  size?: "normal" | "medium" | "large";
+  color?:
+    | "greyBlue"
+    | "red"
+    | "orange"
+    | "green"
+    | "blue"
+    | "yellow"
+    | "lime"
+    | "purple"
+    | "pink"
+    | "teal"
+    | "yellowGreen"
+    | "blueDark"
+    | "lightBlue"
+    | "indigo";
+  children: ReactNode;
+}
+
+export function InlineLabel({
+  size = "normal",
+  color = "greyBlue",
+  children,
+}: InlineLabelProps) {
+  const className = classnames(styles.inlineLabel, styles[size], styles[color]);
+
+  return <span className={className}>{children}</span>;
 }
