be327d8bffbd3e7eae295464c001e6e22f43be38
Sean Healy <s@xib.ca>
2019-05-01 14:49:03 -0600

    Test one thing at a time.

--- packages/components/src/Icon/Icon.test.tsx
+++ packages/components/src/Icon/Icon.test.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { Icon, IconName } from "./";
 
-it("renders correctly", () => {
-  const tree = renderer
-    .create(
-      <>
-        <Icon iconName={IconName.dashboard} />
-        <Icon iconName={IconName.apple} />
-      </>,
-    )
-    .toJSON();
+it("renders dashboard icon", () => {
+  const tree = renderer.create(<Icon iconName={IconName.dashboard} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders apple icon", () => {
+  const tree = renderer.create(<Icon iconName={IconName.apple} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
