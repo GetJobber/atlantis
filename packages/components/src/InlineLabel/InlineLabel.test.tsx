8f183f355c80669ececf20e8b5906ee1929a7150
Craig Cairns <craig.c@getjobber.com>
2019-05-07 16:56:00 -0600

    Add import linting rules (#31)

--- packages/components/src/InlineLabel/InlineLabel.test.tsx
+++ packages/components/src/InlineLabel/InlineLabel.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { InlineLabel } from "./InlineLabel";
 import renderer from "react-test-renderer";
+import { InlineLabel } from ".";
 
 it("renders correctly", () => {
   const tree = renderer.create(<InlineLabel>My Label</InlineLabel>).toJSON();
