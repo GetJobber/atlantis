7b3cf1e9ab2f4f8f134079b1713d542e649f722e
Sean Healy <s@xib.ca>
2019-05-07 16:49:05 -0600

    Move to inline snapshots.

--- packages/components/src/Table/Cells/Cell.test.tsx
+++ packages/components/src/Table/Cells/Cell.test.tsx
@@ -1,23 +1,47 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { Cell } from "../";
+import { Cell } from "./Cell";
 
 it("renders a simple cell", () => {
   const tree = renderer.create(<Cell>Ship</Cell>).toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(tree).toMatchInlineSnapshot(`
+    <td
+      className="cell left"
+    >
+      Ship
+    </td>
+  `);
 });
 
 it("renders a left aligned cell", () => {
   const tree = renderer.create(<Cell align="left">Ship</Cell>).toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(tree).toMatchInlineSnapshot(`
+    <td
+      className="cell left"
+    >
+      Ship
+    </td>
+  `);
 });
 
 it("renders a right aligned cell", () => {
   const tree = renderer.create(<Cell align="right">Ship</Cell>).toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(tree).toMatchInlineSnapshot(`
+    <td
+      className="cell right"
+    >
+      Ship
+    </td>
+  `);
 });
 
 it("renders a center aligned cell", () => {
   const tree = renderer.create(<Cell align="center">Ship</Cell>).toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(tree).toMatchInlineSnapshot(`
+    <td
+      className="cell center"
+    >
+      Ship
+    </td>
+  `);
 });
--- packages/components/src/Table/Cells/CellNumeric.test.tsx
+++ packages/components/src/Table/Cells/CellNumeric.test.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { CellNumeric } from "../";
+import { CellNumeric } from "./CellNumeric";
 
 it("renders a numeric cell", () => {
   const tree = renderer.create(<CellNumeric value={13} />).toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(tree).toMatchInlineSnapshot(`
+    <td
+      className="cell right"
+    >
+      <span
+        className="numeric"
+      >
+        13
+      </span>
+    </td>
+  `);
 });
