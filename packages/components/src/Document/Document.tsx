8652f50e8c7a2a04c60cddd7c3d5961333768d5a
Craig Cairns <craig.c@getjobber.com>
2019-05-13 16:59:00 -0600

    Place interfaces next to the function that uses them

--- packages/components/src/Document/Document.tsx
+++ packages/components/src/Document/Document.tsx
@@ -2,24 +2,7 @@ import React, { ReactNode } from "react";
 import classnames from "classnames";
 import styles from "./Document.css";
 
-interface DocumentProps {
-  readonly accentColor?: "purple";
-  readonly children: ReactNode;
-}
-
 interface HeaderProps {
-  readonly children: ReactNode;
-}
-
-interface TitleProps {
-  readonly children: ReactNode;
-}
-
-interface DetailProps {
-  readonly children: ReactNode;
-}
-
-interface ContentProps {
   readonly children: ReactNode;
 }
 
@@ -27,16 +10,33 @@ export function Header({ children }: HeaderProps) {
   return <header className={styles.header}>{children}</header>;
 }
 
+interface TitleProps {
+  readonly children: ReactNode;
+}
+
 export function Title({ children }: TitleProps) {
   return <div className={styles.title}>{children}</div>;
+}
+
+interface DetailProps {
+  readonly children: ReactNode;
 }
 
 export function Detail({ children }: DetailProps) {
   return <div className={styles.detail}>{children}</div>;
 }
 
+interface ContentProps {
+  readonly children: ReactNode;
+}
+
 export function Content({ children }: ContentProps) {
   return <div className={styles.content}>{children}</div>;
+}
+
+interface DocumentProps {
+  readonly accentColor?: "purple";
+  readonly children: ReactNode;
 }
 
 export function Document({ accentColor, children }: DocumentProps) {
