2a27444af3086d2594c2f382f8bc2295b71d118b
Craig Cairns <craig.c@getjobber.com>
2019-05-15 10:06:44 -0600

    Wrap title and contactInfo in extra div.

--- packages/components/src/Document/Document.css.d.ts
+++ packages/components/src/Document/Document.css.d.ts
@@ -1,6 +1,7 @@
 export const document: string;
 export const purple: string;
 export const header: string;
+export const flexColumn: string;
 export const title: string;
 export const contactInfo: string;
 export const detail: string;
--- packages/components/src/Document/Document.tsx
+++ packages/components/src/Document/Document.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, ReactElement, FunctionComponent } from "react";
 import classnames from "classnames";
 import styles from "./Document.css";
 
@@ -28,7 +28,7 @@ export function Content({ children }: DocumentSectionProps) {
 
 interface DocumentProps {
   readonly accentColor?: "purple";
-  readonly children: ReactNode;
+  readonly children: ReactElement<DocumentSectionProps, FunctionComponent>[];
 }
 
 export function Document({ accentColor, children }: DocumentProps) {
@@ -37,5 +37,22 @@ export function Document({ accentColor, children }: DocumentProps) {
     accentColor && styles[accentColor],
   );
 
-  return <div className={className}>{children}</div>;
+  const childrenArray = React.Children.toArray(children);
+  function getComponent(component: Function) {
+    return childrenArray.filter(
+      child => child.type.displayName === component.name,
+    );
+  }
+
+  return (
+    <div className={className}>
+      {getComponent(Header)}
+      <div className={styles.flexColumn}>
+        {getComponent(Title)}
+        {getComponent(ContactInfo)}
+      </div>
+      {getComponent(Detail)}
+      {getComponent(Content)}
+    </div>
+  );
 }
