1a7b6365917c60c0df0449668369c973fc6de36a
Sean Healy <s@xib.ca>
2019-05-23 12:55:12 -0600

    Add component status component.

--- doczrc.js
+++ doczrc.js
@@ -8,6 +8,7 @@ const { WatchIgnorePlugin } = require("webpack");
 const modifyBundlerConfig = config => {
   config.resolve.alias = Object.assign({}, config.resolve.alias, {
     "@jobber/components": path.resolve(__dirname, "packages/components/src"),
+    "@jobber/docx": path.resolve(__dirname, "packages/docx/src"),
   });
 
   config.plugins.push(new WatchIgnorePlugin([/css\.d\.ts$/]));
---
+++ packages/docx/src/index.ts
@@ -0,0 +1,1 @@
+export { ComponentStatus } from "./ComponentStatus";
