e50263a93ead595763171c313a3e6faac8c94716
Sean Healy <s@xib.ca>
2019-05-29 17:48:32 -0600

    Leave these names alone.

--- plop/templates/component/{{name}}.test.tsx
+++ plop/templates/component/{{name}}.test.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { {{name}} } from ".";
 
-it("renders a {{lowerCase name}}", () => {
+it("renders a {{name}}", () => {
   const tree = renderer.create(<{{name}} text="Foo" />).toJSON();
   expect(tree).toMatchInlineSnapshot();
 });
 
-it("renders an important {{lowerCase name}}", () => {
+it("renders an important {{name}}", () => {
   const tree = renderer.create(<{{name}} text="Foo" important />).toJSON();
   expect(tree).toMatchInlineSnapshot();
 });
