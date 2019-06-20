bdc084797a5980adae6cb1f0233eaf70f98cfdb4
darryltec <darryl.t@getjobber.com>
2019-06-20 09:51:52 -0600

    add heading and text test

--- packages/components/src/Heading/Heading.test.tsx
+++ packages/components/src/Heading/Heading.test.tsx
@@ -2,12 +2,80 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { Heading } from ".";
 
-it("renders a Heading", () => {
-  const tree = renderer.create(<Heading text="Foo" />).toJSON();
-  expect(tree).toMatchInlineSnapshot();
+it("renders a Heading 1", () => {
+  const tree = renderer
+    .create(<Heading variation="page">Dis be a Heading 1</Heading>)
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <h1
+      className="base jumbo black uppercase"
+    >
+      Dis be a Heading 1
+    </h1>
+  `);
 });
 
-it("renders a loud Heading", () => {
-  const tree = renderer.create(<Heading text="Foo" loud />).toJSON();
-  expect(tree).toMatchInlineSnapshot();
+it("renders a Heading 2", () => {
+  const tree = renderer
+    .create(<Heading variation="subtitle">Dis be a Heading 2</Heading>)
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <h2
+      className="base largest black uppercase"
+    >
+      Dis be a Heading 2
+    </h2>
+  `);
+});
+
+it("renders a Heading 3", () => {
+  const tree = renderer
+    .create(<Heading variation="content">Dis be a Heading 3</Heading>)
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <h3
+      className="base larger bold"
+    >
+      Dis be a Heading 3
+    </h3>
+  `);
+});
+
+it("renders a Heading 4", () => {
+  const tree = renderer
+    .create(<Heading variation="section">Dis be a Heading 4</Heading>)
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <h4
+      className="base large bold"
+    >
+      Dis be a Heading 4
+    </h4>
+  `);
+});
+
+it("renders a Heading 5", () => {
+  const tree = renderer
+    .create(<Heading variation="subsection">Dis be a Heading 5</Heading>)
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <h5
+      className="base base bold"
+    >
+      Dis be a Heading 5
+    </h5>
+  `);
+});
+
+it("renders a Heading 6", () => {
+  const tree = renderer
+    .create(<Heading variation="overline">Dis be a Heading 6</Heading>)
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <h6
+      className="base small bold uppercase"
+    >
+      Dis be a Heading 6
+    </h6>
+  `);
 });
--- packages/components/src/Text/Text.test.tsx
+++ packages/components/src/Text/Text.test.tsx
@@ -2,12 +2,121 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { Text } from ".";
 
-it("renders a Text", () => {
-  const tree = renderer.create(<Text text="Foo" />).toJSON();
-  expect(tree).toMatchInlineSnapshot();
+it("renders a paragraph", () => {
+  const tree = renderer
+    .create(
+      <Text>
+        Ask the information you need upfront from clients and new leads
+      </Text>,
+    )
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <p
+      className="base base regular"
+    >
+      Ask the information you need upfront from clients and new leads
+    </p>
+  `);
 });
 
-it("renders a loud Text", () => {
-  const tree = renderer.create(<Text text="Foo" loud />).toJSON();
-  expect(tree).toMatchInlineSnapshot();
+it("renders a subdued text", () => {
+  const tree = renderer
+    .create(<Text variation="subdued">Job note linked to related invoice</Text>)
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <p
+      className="base base regular greyBlue"
+    >
+      Job note linked to related invoice
+    </p>
+  `);
+});
+
+it("renders a success text", () => {
+  const tree = renderer
+    .create(<Text variation="success">Invoice sent</Text>)
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <p
+      className="base base regular green"
+    >
+      Invoice sent
+    </p>
+  `);
+});
+
+it("renders a error text", () => {
+  const tree = renderer
+    .create(<Text variation="error">Name is required</Text>)
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <p
+      className="base base regular red"
+    >
+      Name is required
+    </p>
+  `);
+});
+
+it("renders a warning text", () => {
+  const tree = renderer
+    .create(
+      <Text variation="warning">Your message is over 160 characters</Text>,
+    )
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <p
+      className="base base regular yellow"
+    >
+      Your message is over 160 characters
+    </p>
+  `);
+});
+
+it("renders a info text", () => {
+  const tree = renderer
+    .create(
+      <Text variation="info">
+        Drag to rearrange the order that fields show up in Jobber
+      </Text>,
+    )
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <p
+      className="base base regular blue"
+    >
+      Drag to rearrange the order that fields show up in Jobber
+    </p>
+  `);
+});
+
+it("renders a button text", () => {
+  const tree = renderer
+    .create(<Text variation="button">Update Quote</Text>)
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <p
+      className="base small extraBold uppercase green"
+    >
+      Update Quote
+    </p>
+  `);
+});
+
+it("renders a subhead text", () => {
+  const tree = renderer
+    .create(
+      <Text variation="subhead">
+        Attract new customers by asking happy clients for a Facebook
+        recommendation
+      </Text>,
+    )
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <p
+      className="base larger regular"
+    >
+      Attract new customers by asking happy clients for a Facebook recommendation
+    </p>
+  `);
 });
