1b9d7056e0221ec0f6e904ef9459a2c288888a5a
Sean Healy <s@xib.ca>
2019-06-27 16:11:49 -0600

    Optional state.

--- packages/components/src/Switch/Switch.tsx
+++ packages/components/src/Switch/Switch.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import classnames from "classnames";
 import styles from "./Switch.css";
 
 interface SwitchProps {
-  readonly on: boolean;
+  readonly value?: boolean;
+  onChange?(newValue: boolean): void;
 }
 
-export function Switch({ on = false }: SwitchProps) {
-  const className = classnames(styles.track, { [styles.isChecked]: on });
+export function Switch({ value: providedValue, onChange }: SwitchProps) {
+  const [statefulValue, setValue] = useState(false);
+  const value = providedValue != undefined ? providedValue : statefulValue;
+
+  const className = classnames(styles.track, { [styles.isChecked]: value });
+
+  const toggle = () => {
+    onChange && onChange(!value);
+
+    if (providedValue == undefined) {
+      setValue(!statefulValue);
+    }
+  };
 
   return (
-    <div className={className}>
-      <input type="checkbox" className={styles.checkbox} checked={on} />
+    <div className={className} onClick={toggle}>
+      <input type="checkbox" className={styles.checkbox} checked={value} />
       <span className={styles.pip} />
     </div>
   );
