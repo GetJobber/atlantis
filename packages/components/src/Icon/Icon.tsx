3fdb3e158c3c87301fca7484e7c0d71312d56370
darryltec <darryl.t@getjobber.com>
2019-07-10 14:52:23 -0600

    add icon size and export icon names

--- packages/components/src/Icon/Icon.tsx
+++ packages/components/src/Icon/Icon.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import classnames from "classnames";
 import styles from "./Icon.css";
 import icons from "./Icons.css";
+import sizes from "./Sizes.css";
+
+export type IconNames = keyof typeof icons;
 
 interface IconProps {
   /** The icon to show.  */
-  readonly iconName: keyof typeof icons;
+  readonly iconName: IconNames;
+  readonly size?: keyof typeof sizes;
 }
 
-export function Icon({ iconName }: IconProps) {
-  return <div className={`${icons[iconName]} ${styles.icon}`} />;
+export function Icon({ iconName, size = "base" }: IconProps) {
+  const iconClasses = classnames(styles.icon, icons[iconName], sizes[size]);
+  return <div className={iconClasses} />;
 }
---
+++ packages/components/src/Icon/Sizes.css.d.ts
@@ -0,0 +1,3 @@
+export const small: string;
+export const base: string;
+export const large: string;
