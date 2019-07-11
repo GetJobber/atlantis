c0a7ba40845069396821127461f4e94459abd231
Greg Bell <51722632+gregb-jobber@users.noreply.github.com>
2019-07-11 16:12:04 -0600

    Updating autogenerated test to include an event testing example. (#69)
    Also added `afterEach(cleanup)` since this caught me out early on and it seems to be required for any React testing with the library we're using.

--- plop/templates/component/{{name}}.test.tsx
+++ plop/templates/component/{{name}}.test.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { cleanup, fireEvent, render } from "@testing-library/react";
 import { {{name}} } from ".";
+
+afterEach(cleanup);
 
 it("renders a {{name}}", () => {
   const tree = renderer.create(<{{name}} text="Foo" />).toJSON();
@@ -10,4 +13,18 @@ it("renders a {{name}}", () => {
 it("renders a loud {{name}}", () => {
   const tree = renderer.create(<{{name}} text="Foo" loud />).toJSON();
   expect(tree).toMatchInlineSnapshot();
+});
+
+test("it should call the handler with the new value", () => {
+  const changeHandler = jest.fn();
+  const newValue = "Bar";
+
+  const { getByLabelText } = render(
+    <{{name}} onChange={changeHandler} placeholder={placeholder} />,
+  );
+
+  fireEvent.change(getByLabelText(placeholder), {
+    target: { value: newValue },
+  });
+  expect(changeHandler).toHaveBeenCalledWith(newValue);
 });
