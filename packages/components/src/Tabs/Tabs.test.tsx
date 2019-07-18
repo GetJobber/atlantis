7606196ccd658ef21ec6206e05dfe846261b64ac
Sean Healy <s@xib.ca>
2019-07-18 17:08:13 -0600

    🕶

---
+++ packages/components/src/Tabs/Tabs.test.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { cleanup, fireEvent, render } from "@testing-library/react";
+import { Tab, Tabs } from ".";
+
+afterEach(cleanup);
+
+const omelet = (
+  <Tabs>
+    <Tab text="Eggs">🍳</Tab>
+    <Tab text="Cheese">🧀</Tab>
+  </Tabs>
+);
+
+it("renders Tabs", () => {
+  const tree = renderer.create(omelet).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("it should switch tabs", () => {
+  const { getByText, queryByText } = render(omelet);
+
+  expect(queryByText("🍳")).toBeTruthy();
+  expect(queryByText("🧀")).toBeFalsy();
+
+  fireEvent.click(getByText("Cheese"));
+  expect(queryByText("🍳")).toBeFalsy();
+  expect(queryByText("🧀")).toBeTruthy();
+
+  fireEvent.click(getByText("Eggs"));
+  expect(queryByText("🍳")).toBeTruthy();
+  expect(queryByText("🧀")).toBeFalsy();
+});
