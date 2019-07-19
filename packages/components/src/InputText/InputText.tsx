0fc0e4a5afae1de896b5d4d9bdb2c18e91343361
Sean Healy <s@xib.ca>
2019-07-19 17:03:45 -0600

    Fix props table.

--- packages/components/src/InputText/InputText.tsx
+++ packages/components/src/InputText/InputText.tsx
@@ -6,8 +6,17 @@ interface InputTextProps extends FormFieldProps {
   readonly rows?: number;
 }
 
-type TypographyOptions = Omit<InputTextProps, "type" | "children">;
+/**
+ * The following is the same as:
+ *   type TypographyOptions = Omit<InputTextProps, "type" | "children">;
+ * Unfortunately Docz doesn't currently support Omit so it has been reduced to
+ * its component parts.
+ */
+type InputTextOptions = Pick<
+  InputTextProps,
+  Exclude<keyof InputTextProps, "type" | "children">
+>;
 
-export function InputText(props: TypographyOptions) {
+export function InputText(props: InputTextOptions) {
   return <FormField type={props.multiline ? "textarea" : "text"} {...props} />;
 }
