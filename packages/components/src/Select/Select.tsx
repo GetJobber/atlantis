e959dd36950c92a1dcd72e1059155271f2dfa77f
Sean Healy <s@xib.ca>
2019-07-19 10:25:42 -0600

    Value the `Select` input. (#74)
    * Select now accepts `value` to go with `onChange`.

--- packages/components/src/Select/Select.tsx
+++ packages/components/src/Select/Select.tsx
@@ -10,11 +10,6 @@ interface SelectProps {
   readonly children?: ReactNode | ReactNode[];
 
   /**
-   * Set the initialy selected option.
-   */
-  readonly defaultValue?: string;
-
-  /**
    * Text size.
    */
   readonly size?: "small" | "large";
@@ -32,6 +27,17 @@ interface SelectProps {
   readonly invalid?: boolean;
 
   /**
+   * Set the initialy selected option.
+   */
+  readonly defaultValue?: string;
+
+  /**
+   * Set the component to the given value.
+   * Must be used with onChange to create a "controlled component".
+   */
+  readonly value?: string;
+
+  /**
    * Simplified onChange handler that only provides the new value.
    * @param newValue
    */
@@ -43,8 +49,9 @@ export function Select({
   size,
   disabled = false,
   invalid = false,
-  onChange,
   defaultValue,
+  value,
+  onChange,
 }: SelectProps) {
   const wrapperClasses = classnames(styles.wrapper, size && styles[size], {
     [styles.disabled]: disabled,
@@ -65,6 +72,7 @@ export function Select({
       <select
         className={styles.select}
         defaultValue={defaultValue}
+        value={value}
         disabled={disabled}
         onChange={onChangeWrapper}
       >
