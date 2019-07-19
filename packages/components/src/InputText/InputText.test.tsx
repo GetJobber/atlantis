16e47087e213bfb308f9a09b5044973ecf425303
darryltec <darryl.t@getjobber.com>
2019-07-19 11:30:12 -0600

    add change handler test

--- packages/components/src/InputText/InputText.test.tsx
+++ packages/components/src/InputText/InputText.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { fireEvent, render } from "@testing-library/react";
 import { InputText } from ".";
 
 it("renders a regular input for text and numbers", () => {
@@ -76,4 +77,31 @@ it("renders a textarea with 4 rows", () => {
       />
     </div>
   `);
+});
+
+test("it should call the handler with the new value", () => {
+  const placeholder = "I hold places.";
+  const newValue =
+    "The snake which cannot cast its skin has to die. As well the minds which are prevented from changing their opinions; they cease to be mind.";
+  const newerValue =
+    "They always say time changes things, but you actually have to change them yourself.";
+  const changeHandler = jest.fn();
+
+  const { getByLabelText } = render(
+    <InputText
+      name="Got milk?"
+      onChange={changeHandler}
+      placeholder={placeholder}
+    />,
+  );
+
+  fireEvent.change(getByLabelText(placeholder), {
+    target: { value: newValue },
+  });
+  expect(changeHandler).toHaveBeenCalledWith(newValue);
+
+  fireEvent.change(getByLabelText(placeholder), {
+    target: { value: newerValue },
+  });
+  expect(changeHandler).toHaveBeenCalledWith(newerValue);
 });
