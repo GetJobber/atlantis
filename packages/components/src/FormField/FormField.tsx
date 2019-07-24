ce38634ccadc523d91b0e3ffd207c6222d1f5ad1
Darryl Tec <darryl.t@getjobber.com>
2019-07-24 11:06:54 -0600

    Id like to get your input on an inline (#73)
    * first pass on master field component
    
    * create master form field component
    
    * replace InputText to use master component
    
    * export formfield props
    
    * adjust inout text default and documentation
    
    * allow textarea and select as a type
    
    * add maxLength
    
    * add alignment
    
    * adjust css to use custom properties
    
    * adjust input text
    
    * add text field component
    
    * reduce default min height for textarea
    
    * add rows and sort props
    
    * don't need to specifcy textarea
    
    * make FormField private
    
    * delete input text
    
    * delete unused file on textfield
    
    * better labels when autofilled
    
    * add form field documentation
    
    * mark textfield as responsive and accessible
    
    * simplify props and make type rquired
    
    * rename TextField to InputText
    
    * add change handler test
    
    * update the docs
    
    * remove accidentally commited import
    
    * add descriptions on form field props
    
    * let CSS handle the width of maxLength
    
    * ensure that miniLabel class gets added if there's placeholder
    
    * omit children too
    
    * allow defaultValue and value and code cleanup
    
    * adjust documentation to make sean happy
    
    * Fix props table.
    
    * Upgrade Docz
    
    * highlight text on readonly
    
    * add better example of controlled inputs
    
    * tighten up usage of multiline and rows
    
    * Handle select on safari

---
+++ packages/components/src/FormField/FormField.tsx
@@ -0,0 +1,188 @@
+import React, { ChangeEvent, ReactNode, useState } from "react";
+import classnames from "classnames";
+import styles from "./FormField.css";
+
+export interface FormFieldProps {
+  /**
+   * Determines the alignment of the text inside the input.
+   */
+  readonly align?: "center" | "right";
+
+  /**
+   * If you need to pass in a children. For example, `<options>` inside
+   * `<select>`.
+   */
+  readonly children?: ReactNode;
+
+  /**
+   * Intial value of the input. Only use this when you need to prepopulate the
+   * field with a data that is not controlled by the components state. If a
+   * state is controlling the value, use the `value` prop instead.
+   */
+  readonly defaultValue?: string;
+
+  /**
+   * Disable the input
+   */
+  readonly disabled?: boolean;
+
+  /**
+   * Adjusts the form field to go inline with a content.
+   */
+  readonly inline?: boolean;
+
+  /**
+   * Highlights the field red to indicate an error.
+   */
+  readonly invalid?: boolean;
+
+  /**
+   * Maximum character length for an input. This also changes the width to
+   * roughly the same size as the max length. This is to communicate that the
+   * user that on certain cases, they can only type a limited amount of
+   * characters.
+   */
+  readonly maxLength?: number;
+
+  /**
+   * Name of the input.
+   */
+  readonly name?: string;
+
+  /**
+   * Hint text that goes above the value once the form is filled out.
+   */
+  readonly placeholder?: string;
+
+  /**
+   * Prevents users from editing the value.
+   */
+  readonly readonly?: boolean;
+
+  /**
+   * Exclusively for textareas. Specifies the visible height of a textarea.
+   */
+  readonly rows?: number;
+
+  /**
+   * Adjusts the interface to either have small or large spacing.
+   */
+  readonly size?: "small" | "large";
+
+  /**
+   * Determines what kind of form field should the component give you.
+   */
+  readonly type?: "text" | "number" | "time" | "textarea" | "select";
+
+  /**
+   * Set the component to the given value.
+   */
+  readonly value?: string;
+
+  /**
+   * Simplified onChange handler that only provides the new value.
+   * @param newValue
+   */
+  onChange?(newValue: string): void;
+}
+
+export function FormField({
+  align,
+  children,
+  defaultValue,
+  disabled,
+  inline,
+  invalid,
+  maxLength,
+  name,
+  onChange,
+  placeholder,
+  readonly,
+  rows,
+  size,
+  type = "text",
+  value,
+}: FormFieldProps) {
+  const [hasMiniLabel, setHasMiniLabel] = useState(
+    defaultValue || value ? true : false,
+  );
+
+  const handleChange = (
+    event:
+      | ChangeEvent<HTMLInputElement>
+      | ChangeEvent<HTMLTextAreaElement>
+      | ChangeEvent<HTMLSelectElement>,
+  ) => {
+    const newValue = event.currentTarget.value;
+    setHasMiniLabel(newValue.length > 0);
+    onChange && onChange(newValue);
+  };
+
+  const handleFocus = (
+    event:
+      | React.FocusEvent<HTMLInputElement>
+      | React.FocusEvent<HTMLTextAreaElement>,
+  ) => {
+    const target = event.currentTarget;
+    setTimeout(() => readonly && target.select());
+  };
+
+  const fieldProps = {
+    id: name,
+    className: styles.formField,
+    name: name,
+    disabled: disabled,
+    readOnly: readonly,
+    onChange: handleChange,
+    value: value,
+    ...(defaultValue && { defaultValue: defaultValue }),
+  };
+
+  const fieldElement = () => {
+    switch (type) {
+      case "select":
+        return <select {...fieldProps}>{children}</select>;
+      case "textarea":
+        return <textarea rows={rows} onFocus={handleFocus} {...fieldProps} />;
+      default:
+        return (
+          <input
+            type={type}
+            maxLength={maxLength}
+            onFocus={handleFocus}
+            {...fieldProps}
+          />
+        );
+    }
+  };
+
+  const wrapperClassNames = classnames(
+    styles.wrapper,
+    inline && styles.inline,
+    size && styles[size],
+    align && styles[align],
+    invalid && styles.invalid,
+    disabled && styles.disabled,
+    maxLength && styles.maxLength,
+    {
+      [styles.miniLabel]:
+        (hasMiniLabel || type === "time" || type === "select") && placeholder,
+    },
+  );
+
+  const Wrapper = inline ? "span" : "div";
+
+  return (
+    <Wrapper
+      className={wrapperClassNames}
+      style={{ ["--formField-maxLength" as string]: maxLength }}
+    >
+      {placeholder && (
+        <label className={styles.label} htmlFor={name}>
+          {placeholder}
+        </label>
+      )}
+      {fieldElement()}
+    </Wrapper>
+  );
+}
--- packages/components/src/InputText/InputText.tsx
+++ packages/components/src/InputText/InputText.tsx
@@ -1,84 +1,32 @@
-import React, { ChangeEvent } from "react";
-import classnames from "classnames";
-import styles from "./InputText.css";
+import React from "react";
+import { XOR } from "ts-xor";
+import { FormField, FormFieldProps } from "../FormField";
 
-interface InputTextProps {
-  /** The input name  */
-  readonly name?: string;
+/**
+ * The following is the same as:
+ *   type BaseProps = Omit<FormFieldProps, "type" | "children">;
+ * Unfortunately Docz doesn't currently support Omit so it has been reduced to
+ * its component parts.
+ */
+type BaseProps = Pick<
+  FormFieldProps,
+  Exclude<keyof FormFieldProps, "type" | "children" | "rows">
+>;
 
-  /** The text that appears when no value is set and displayed as a hover label when a value is present. */
-  readonly placeholder?: string;
-
-  readonly value?: string;
-
-  /** Callback fired when the value is changed. */
-  onChange?(newValue: string): void;
+interface MultilineProps extends BaseProps {
+  /**
+   * Use this when you're expecting a long answer.
+   */
+  readonly multiline: true;
 
   /**
-   * The size of the input
-   * @default normal
+   * Specifies the visible height of a long answer form field.
    */
-  readonly size?: "small" | "normal";
-
-  /**
-   * If true, the input is disabled.
-   * @default false
-   */
-  readonly disabled?: boolean;
-
-  /**
-   * If true, the input cannot be edited.
-   * @default false
-   */
-  readonly readonly?: boolean;
-
-  /**
-   * If true, the input is in an error state.
-   * @default false
-   */
-  readonly error?: boolean;
+  readonly rows?: number;
 }
 
-export function InputText({
-  name,
-  placeholder,
-  value,
-  onChange,
-  size = "normal",
-  disabled = false,
-  readonly = false,
-  error = false,
-}: InputTextProps) {
-  const wrapperClass = classnames(
-    styles.inputWrapper,
-    styles[size],
-    error && styles.error,
-  );
+type InputTextPropOptions = XOR<BaseProps, MultilineProps>;
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const newValue = event.currentTarget.value;
-    if (onChange) {
-      onChange(newValue);
-    }
-  };
-
-  return (
-    <div className={wrapperClass}>
-      <label className={styles.label} htmlFor={name}>
-        <input
-          type="text"
-          className={styles.input}
-          name={name}
-          placeholder={placeholder}
-          disabled={disabled}
-          readOnly={readonly}
-          onChange={handleChange}
-          defaultValue={value}
-        />
-        {size !== "small" && placeholder && (
-          <span className={styles.labelContent}>{placeholder}</span>
-        )}
-      </label>
-    </div>
-  );
+export function InputText(props: InputTextPropOptions) {
+  return <FormField type={props.multiline ? "textarea" : "text"} {...props} />;
 }
