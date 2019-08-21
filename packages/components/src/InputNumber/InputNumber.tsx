0efedc6912295fab5ce26f26a7d1fa6cac45f343
darryltec <darryl.t@getjobber.com>
2019-08-21 11:55:23 -0600

    make sure there's an initial error message if applicable

--- packages/components/src/InputNumber/InputNumber.tsx
+++ packages/components/src/InputNumber/InputNumber.tsx
@@ -7,45 +7,49 @@ import { FormField, FormFieldProps } from "../FormField";
  * Unfortunately Docz doesn't currently support Omit so it has been reduced to
  * its component parts.
  */
-type InputNumberProps = Pick<
-  FormFieldProps,
-  Exclude<keyof FormFieldProps, "type" | "children" | "rows">
->;
+interface InputNumberProps
+  extends Pick<
+    FormFieldProps,
+    Exclude<keyof FormFieldProps, "type" | "children" | "rows">
+  > {
+  value?: number;
+}
 
 export function InputNumber(props: InputNumberProps) {
-  const [overLimit, setOverLimit] = useState("");
-
-  const getOverLimitErrorMessage = (): string => {
-    let message = "";
-    if (props.min != undefined && props.max === undefined) {
-      message = `Enter a number that is greater than or equal to ${props.min}`;
-    } else if (props.max != undefined && props.min === undefined) {
-      message = `Enter a number that is less than or equal to ${props.max}`;
-    } else if (props.min != undefined && props.max != undefined) {
-      message = `Enter a number between ${props.min} and ${props.max}`;
-    }
-    return message;
-  };
-
-  const handleChange = (newValue: number) => {
-    const isOverMax = props.max != undefined && newValue > props.max;
-    const isUnderMin = props.min != undefined && newValue < props.min;
-
-    if (isOverMax || isUnderMin || newValue.toString() === "") {
-      setOverLimit(getOverLimitErrorMessage);
-    } else {
-      setOverLimit("");
-    }
-
-    props.onChange && props.onChange(newValue);
-  };
+  const [overLimitMessage, setOverLimitMessage] = useState(
+    getOverLimitMessage(props.value),
+  );
 
   return (
     <FormField
       type="number"
       {...props}
       onChange={handleChange}
-      errorMessage={props.errorMessage || overLimit}
+      errorMessage={props.errorMessage || overLimitMessage}
     />
   );
+
+  function handleChange(newValue: number) {
+    setOverLimitMessage(getOverLimitMessage(newValue));
+    props.onChange && props.onChange(newValue);
+  }
+
+  function getOverLimitMessage(value: InputNumberProps["value"]): string {
+    let message = "";
+
+    const isOverMax = props.max != undefined && value && value > props.max;
+    const isUnderMin = props.min != undefined && value && value < props.min;
+
+    if (isOverMax || isUnderMin || (value && value.toString() === "")) {
+      if (props.min != undefined && props.max === undefined) {
+        message = `Enter a number that is greater than or equal to ${props.min}`;
+      } else if (props.max != undefined && props.min === undefined) {
+        message = `Enter a number that is less than or equal to ${props.max}`;
+      } else if (props.min != undefined && props.max != undefined) {
+        message = `Enter a number between ${props.min} and ${props.max}`;
+      }
+    }
+
+    return message;
+  }
 }
