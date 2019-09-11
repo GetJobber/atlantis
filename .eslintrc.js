4df82c954200e7fa51e1be50ead63211ac9934ab
Sean Healy <s@xib.ca>
2019-09-11 13:46:23 -0600

    fix: Properly include ESLint Jest plugin. (#123)
    * Add jest to shared plugins
    
    * Remove unneeded line.
    
    * Actually include the jest plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ require("@jobber/eslint-config/patch-eslint-plugin-resolution.js");
 
 module.exports = {
   extends: ["@jobber/eslint-config"],
-  plugins: ["jest"],
   rules: {
     /*
       Need to figure out a good way to enforce intra vs inter module import
