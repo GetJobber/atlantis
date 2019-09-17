acb75a95a475d235f9664c3c6898398551c67eef
darryltec <darryl.t@getjobber.com>
2019-09-17 11:12:04 -0600

    add saved indicator

--- packages/components/src/FeatureSwitch/FeatureSwitch.css.d.ts
+++ packages/components/src/FeatureSwitch/FeatureSwitch.css.d.ts
@@ -1,3 +1,4 @@
 export const container: string;
 export const content: string;
 export const action: string;
+export const savedIndicator: string;
--- packages/components/src/FeatureSwitch/FeatureSwitch.tsx
+++ packages/components/src/FeatureSwitch/FeatureSwitch.tsx
@@ -1,9 +1,11 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { Heading } from "../Heading";
 import { Text } from "../Text";
 import { Content } from "../Content";
 import { Switch } from "../Switch";
 import { Button } from "../Button";
+import { Emphasis } from "../Emphasis";
 import styles from "./FeatureSwitch.css";
 
 interface FeatureSwitchProps {
@@ -22,7 +24,7 @@ interface FeatureSwitchProps {
   /**
    * Feature title.
    */
-  readonly title: string;
+  readonly title?: string;
 
   /**
    * Callback when interacting with the switch.
@@ -45,28 +47,67 @@ export function FeatureSwitch({
   onSwitch,
   title,
 }: FeatureSwitchProps) {
+  const [savedIndicator, setSavedIndicator] = useState(false);
   return (
     <Content>
       <div className={styles.container}>
         <div className={styles.content}>
           <Content>
-            <Heading level={4}>{title}</Heading>
+            {title && <Heading level={4}>{title}</Heading>}
             <Text>{description}</Text>
           </Content>
         </div>
         <div className={styles.action}>
-          <Switch value={enabled} onChange={onSwitch} ariaLabel={description} />
+          <Switch
+            value={enabled}
+            onChange={handleSwitch}
+            ariaLabel={description}
+          />
+          <AnimatePresence>
+            {savedIndicator && (
+              <motion.div
+                className={styles.savedIndicator}
+                initial={{ y: -4, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -4, opacity: 0 }}
+                transition={{
+                  type: "spring",
+                  duration: 0.2,
+                  damping: 20,
+                  stiffness: 300,
+                }}
+              >
+                <Text variation="success">
+                  <Emphasis variation="italic">Saved</Emphasis>
+                </Text>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
 
-      {children && (
+      {(children || onEdit) && (
         <div className={styles.container}>
-          <div className={styles.content}>{children}</div>
-          <div className={styles.action}>
-            <Button label="Edit" type="tertiary" onClick={onEdit} />
-          </div>
+          {children && <div className={styles.content}>{children}</div>}
+          {onEdit && (
+            <div className={styles.action}>
+              <Button label="Edit" type="tertiary" onClick={onEdit} />
+            </div>
+          )}
         </div>
       )}
     </Content>
   );
+
+  function handleSwitch(newValue: boolean) {
+    onSwitch && onSwitch(newValue);
+
+    setTimeout(() => {
+      setSavedIndicator(true);
+
+      setTimeout(() => {
+        setSavedIndicator(false);
+      }, 2000);
+    }, 200);
+  }
 }
