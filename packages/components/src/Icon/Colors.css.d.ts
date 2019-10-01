0beaa6a8541ee403e345c3775217736434a2c919
darryltec <darryl.t@getjobber.com>
2019-10-01 17:08:04 -0600

    adjust css

--- packages/components/src/Icon/Colors.css.d.ts
+++ packages/components/src/Icon/Colors.css.d.ts
@@ -1,4 +1,6 @@
 export const greyBlue: string;
+export const greyBlueDark: string;
+export const blue: string;
 export const lightBlue: string;
 export const green: string;
 export const yellow: string;
