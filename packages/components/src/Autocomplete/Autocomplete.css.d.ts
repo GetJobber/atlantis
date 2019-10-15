cbcb5a36450fbd653fd6da72762789d6964806c8
Sean Healy <s@xib.ca>
2019-10-15 14:14:02 -0600

    Refactor and handle more edge cases.

--- packages/components/src/Autocomplete/Autocomplete.css.d.ts
+++ packages/components/src/Autocomplete/Autocomplete.css.d.ts
@@ -1,4 +1,5 @@
 export const autocomplete: string;
 export const options: string;
+export const visible: string;
 export const option: string;
 export const active: string;
--- packages/components/src/Autocomplete/Autocomplete.tsx
+++ packages/components/src/Autocomplete/Autocomplete.tsx
@@ -4,6 +4,7 @@ import useEventListener from "@use-it/event-listener";
 import styles from "./Autocomplete.css";
 import { InputText, InputTextRef } from "../InputText";
 import { Text } from "../Text";
+import { Icon } from "../Icon";
 
 type OptionValue = string | number;
 
@@ -15,7 +16,7 @@ interface Option {
 interface AutocompleteProps {
   readonly initialOptions?: Option[];
   value: OptionValue;
-  onChange(newValue: OptionValue): void;
+  onChange(newValue?: OptionValue): void;
   getOptions(newValue: string): Promise<Option[]>;
   placeholder: string;
 }
@@ -27,81 +28,132 @@ enum IndexChange {
 
 export function Autocomplete({
   initialOptions = [],
-  value = "",
+  value,
   onChange: onSelectOption,
   getOptions,
   placeholder,
 }: AutocompleteProps) {
-  const [text, setText] = useState(value);
   const [options, setOptions] = useState(initialOptions);
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [optionsMenuVisible, setOptionsMenuVisible] = useState(false);
+
+  const activeOption = options.find(option => option.value === value);
+  const [text, setText] = useState((activeOption && activeOption.label) || "");
+
   const inputRef = useRef() as MutableRefObject<InputTextRef>;
-
-  useEffect(() => {
-    const handleKeyDown = (event: { key: string }) => {
-      const optionsLength = options.length;
-      if (optionsLength === 0) {
-        return;
-      }
-      if (event.key === "ArrowDown" && selectedIndex < optionsLength - 1) {
-        setSelectedIndex(selectedIndex + IndexChange.Next);
-      }
-
-      if (event.key === "ArrowUp" && selectedIndex > -1) {
-        setSelectedIndex(selectedIndex + IndexChange.Previous);
-      }
-    };
-    document.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [options, selectedIndex]);
-
-  useOnKeyDown("Escape", () => inputRef.current.blur());
-  useOnKeyDown("Enter", () => {
-    selectOption(options[selectedIndex])();
-    inputRef.current.blur();
-  });
 
   return (
     <div className={styles.autocomplete}>
       <InputText
         value={text}
-        onChange={handleOnChange}
+        onChange={textChange}
         placeholder={placeholder}
+        onFocus={handleTextFocus}
+        onBlur={handleTextBlur}
         ref={inputRef}
       />
-      <div className={styles.options}>
-        {options.map((option, index) => {
-          const optionClass = classnames(styles.option, {
-            [styles.active]: index === selectedIndex,
-          });
-          return (
-            <button
-              className={optionClass}
-              key={option.value}
-              onMouseDown={selectOption(option)}
-            >
-              <Text>{option.label}</Text>
-            </button>
-          );
-        })}
-      </div>
+      <Menu
+        visible={optionsMenuVisible}
+        options={options}
+        onOptionSelect={selectOption}
+        value={value}
+      />
     </div>
   );
 
   function selectOption(chosenOption: Option) {
     return () => {
       onSelectOption(chosenOption.value);
-      setText(chosenOption.label);
+      textChange(chosenOption.label);
+      setOptionsMenuVisible(false);
     };
   }
 
-  async function handleOnChange(newValue: string) {
+  async function textChange(newValue: string) {
+    setOptionsMenuVisible(true);
     setText(newValue);
     const results = await getOptions(newValue);
     setOptions(results);
+  }
+
+  function handleTextBlur() {
+    setOptionsMenuVisible(false);
+    if (activeOption == undefined || activeOption.label !== text) {
+      onSelectOption(undefined);
+    }
+  }
+
+  function handleTextFocus() {
+    setOptionsMenuVisible(true);
+  }
+}
+
+interface MenuProps {
+  visible: boolean;
+  options: Option[];
+  value: React.ReactText;
+  onOptionSelect: (chosenOption: Option) => () => void;
+}
+
+function Menu({ visible, options, value, onOptionSelect }: MenuProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const optionMenuClass = classnames(styles.options, {
+    [styles.visible]: visible,
+  });
+
+  setupKeyListeners();
+
+  useEffect(() => setSelectedIndex(-1), [options]);
+
+  return (
+    <div className={optionMenuClass}>
+      {options.map((option, index) => {
+        const optionClass = classnames(styles.option, {
+          [styles.active]: index === selectedIndex,
+        });
+        return (
+          <button
+            className={optionClass}
+            key={option.value}
+            onMouseDown={onOptionSelect(option)}
+          >
+            <Text>
+              {option.value === value && (
+                <>
+                  <Icon name="checkmark" size="small" />{" "}
+                </>
+              )}
+              {option.label}
+            </Text>
+          </button>
+        );
+      })}
+    </div>
+  );
+
+  function setupKeyListeners() {
+    useEffect(() => {
+      const handleKeyDown = (event: { key: string }) => {
+        const optionsLength = options.length;
+        if (optionsLength === 0) {
+          return;
+        }
+        if (event.key === "ArrowDown" && selectedIndex < optionsLength - 1) {
+          setSelectedIndex(selectedIndex + IndexChange.Next);
+        }
+        if (event.key === "ArrowUp" && selectedIndex > -1) {
+          setSelectedIndex(selectedIndex + IndexChange.Previous);
+        }
+      };
+      document.addEventListener("keydown", handleKeyDown);
+      return () => {
+        document.removeEventListener("keydown", handleKeyDown);
+      };
+    }, [options, selectedIndex]);
+
+    // useOnKeyDown("Escape", () => inputRef.current.blur());
+    useOnKeyDown("Enter", () => {
+      onOptionSelect(options[selectedIndex])();
+    });
   }
 }
 
