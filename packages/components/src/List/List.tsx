cfb2710aa0bd3f6da3a59c9643e5205ed562452e
darryltec <darryl.t@getjobber.com>
2019-10-16 13:36:59 -0600

    Revert "animate me"
    This reverts commit f4af739f84ce482ba7029e023e2854669a7e588d.

--- packages/components/src/List/List.tsx
+++ packages/components/src/List/List.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { groupBy } from "lodash";
-import { AnimatePresence, motion } from "framer-motion";
 import styles from "./List.css";
 import { ListItem, ListItemProps } from "./ListItem";
 import { Heading } from "../Heading";
@@ -21,27 +20,11 @@ export function List({ items, sectioned }: ListProps) {
 function DisplayList({ items }: Pick<ListProps, "items">) {
   return (
     <ul className={styles.list}>
-      <AnimatePresence initial={false}>
-        {items.map(item => (
-          <motion.li
-            key={item.id}
-            className={styles.item}
-            initial={{ x: 24, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            positionTransition={{
-              duration: 0.2,
-            }}
-            transition={{
-              type: "spring",
-              duration: 0.2,
-              damping: 20,
-              stiffness: 300,
-            }}
-          >
-            <ListItem {...item} />
-          </motion.li>
-        ))}
-      </AnimatePresence>
+      {items.map(item => (
+        <li key={item.id} className={styles.item}>
+          <ListItem {...item} />
+        </li>
+      ))}
     </ul>
   );
 }
