46b6f993fe8bc81e85bde36ae985451d30c43805
darryltec <darryl.t@getjobber.com>
2019-10-24 12:53:11 -0600

    handle change event

--- packages/components/src/Checkbox/Checkbox.tsx
+++ packages/components/src/Checkbox/Checkbox.tsx
@@ -1,32 +1,62 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import styles from "./Checkbox.css";
 import { Icon } from "../Icon";
 import { Text } from "../Text";
 
 interface CheckboxProps {
   /**
-   * Text to display.
+   * Determines if the checkbox is checked or not.
    */
-  readonly label: string;
+  readonly checked?: boolean;
+
+  /**
+   * Initial checked value of the checkbox. Only use this when you need to
+   * pre-populate the checked attribute that is not controlled by the components
+   * state. If a state is controlling it, use the `checked` prop instead.
+   */
+  readonly defaultChecked?: boolean;
+
+  /**
+   * The label that shows up beside the checkbox.
+   */
+  readonly label?: string;
+
+  onChange?(newChecked: boolean): void;
 }
 
-export function Checkbox({ label }: CheckboxProps) {
+export function Checkbox({
+  checked,
+  defaultChecked,
+  label,
+  onChange,
+}: CheckboxProps) {
   return (
     <label className={styles.wrapper}>
       <span className={styles.checkHolder}>
         <input
           type="checkbox"
           name="foo"
+          checked={checked}
+          defaultChecked={defaultChecked}
           className={styles.input}
           aria-label={label}
+          onChange={handleChange}
         />
         <span className={styles.checkBox}>
           <Icon name="checkmark" size="small" color="white" />
         </span>
       </span>
-      <span className={styles.label}>
-        <Text>{label}</Text>
-      </span>
+
+      {label && (
+        <span className={styles.label}>
+          <Text>{label}</Text>
+        </span>
+      )}
     </label>
   );
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    const newChecked = event.currentTarget.checked;
+    onChange && onChange(newChecked);
+  }
 }
