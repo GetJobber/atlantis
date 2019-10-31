913628ed5176ef1d8af8bd4bc9c310e1411813ea
Darryl Tec <tec.darryl@gmail.com>
2019-10-31 15:25:42 -0600

    feat(components): Add List Component (#152)
    * first pass on list component
    
    * add orange
    
    * add is active and sort props
    
    * group dem thangs
    
    * better sections
    
    * don't need no fragment
    
    * support multi content
    
    * group all da things
    
    * animate me
    
    * Revert "animate me"
    
    This reverts commit f4af739f84ce482ba7029e023e2854669a7e588d.
    
    * add documentation
    
    * smarter sections
    
    * add dem testssss
    
    * fix some weird issue with buttons and links
    
    * better examples
    
    * more explanation on what this thang is
    
    * adjust content prop description
    
    * css cleanup
    
    * adjust amount
    
    * add jobber system colours for headings
    
    * document dat colors
    
    * update href to URL for consistency sake
    
    * reorganize CSS y'alls
    
    * rename date and amount to caption and value
    
    * fix safari bug
    
    * Remove auto coloring of section header
    
    * remove unnecessary code
    
    * update case statements around url and btn
    
    * remove extra classes

---
+++ packages/components/src/List/List.css.d.ts
@@ -0,0 +1,3 @@
+export const list: string;
+export const item: string;
+export const section: string;
---
+++ packages/components/src/List/ListItem.tsx
@@ -0,0 +1,156 @@
+import React from "react";
+import classnames from "classnames";
+import styles from "./ListItem.css";
+import { Icon, IconColorNames, IconNames } from "../Icon";
+import { Text } from "../Text";
+import { Heading } from "../Heading";
+import { Typography } from "../Typography";
+import { Markdown } from "../Markdown";
+import { Emphasis } from "../Emphasis";
+
+export interface ListItemProps {
+  /**
+   * Subdued text under the `content` prop.
+   */
+  readonly caption?: string;
+
+  /**
+   * List item content. This accepts a string for a simple content and an array
+   * for a multi line content.
+   * This supports basic markdown node types such as `_italic_` and `**bold**`.
+   */
+  readonly content: string | string[];
+
+  /**
+   * Shows an icon on the left side of the contents.
+   */
+  readonly icon?: IconNames;
+
+  /**
+   * Changes the color of the icons.
+   */
+  readonly iconColor?: IconColorNames;
+
+  /**
+   * The ID of the list item. This will be helpful to know the selected list
+   * items when a batch action is implemented.
+   */
+  readonly id: number;
+
+  /**
+   * Highlights the list item with the lightest green icon. This communicates
+   * that the line item needs attention.
+   */
+  readonly isActive?: boolean;
+
+  /**
+   * This determines how and when to group the list items
+   */
+  readonly section?: string;
+
+  /**
+   * Adds a heading.
+   */
+  readonly title?: string;
+
+  /**
+   * This turns the list item into a clickable link.
+   */
+  readonly url?: string;
+
+  /**
+   * A numeric value of a row. This always shows up in the right side of the row
+   * with a text aligned to the right.
+   */
+  readonly value?: string;
+
+  /**
+   * Callback when a list item gets clicked.
+   */
+  onClick?(): void;
+}
+
+export function ListItem({
+  caption,
+  content,
+  icon,
+  iconColor,
+  id,
+  isActive,
+  onClick,
+  title,
+  url,
+  value,
+}: ListItemProps) {
+  const actionClasses = classnames(
+    styles.action,
+    isActive && styles.isActive,
+    (onClick || url) && styles.hoverable,
+  );
+  const Wrapper = url ? "a" : "button";
+
+  const buttonProps = {
+    ...(Wrapper === "button" && { role: "button", type: "button" }),
+  };
+
+  return (
+    <Wrapper
+      id={id.toString()}
+      className={actionClasses}
+      href={url}
+      onClick={onClick}
+      {...buttonProps}
+    >
+      {icon && (
+        <div className={styles.icon}>
+          <Icon name={icon} color={iconColor} />
+        </div>
+      )}
+
+      <div className={styles.info}>
+        {title && <Heading level={5}>{title}</Heading>}
+        <Description content={content} />
+
+        {caption && (
+          <Text variation="subdued">
+            <Typography element="span" size="small" emphasisType="italic">
+              {caption}
+            </Typography>
+          </Text>
+        )}
+      </div>
+
+      {value && (
+        <div className={styles.amount}>
+          <Text>
+            <Emphasis variation="bold">{value}</Emphasis>
+          </Text>
+        </div>
+      )}
+    </Wrapper>
+  );
+}
+
+function Description({ content }: Pick<ListItemProps, "content">) {
+  if (content instanceof Array) {
+    return (
+      <>
+        {content.map((item, i) => (
+          <Text key={i}>
+            <span className={styles.truncate}>
+              <Markdown content={item} basicUsage={true} />
+            </span>
+          </Text>
+        ))}
+      </>
+    );
+  } else {
+    return (
+      <Text>
+        <span className={styles.truncate}>
+          <Markdown content={content} basicUsage={true} />
+        </span>
+      </Text>
+    );
+  }
+}
