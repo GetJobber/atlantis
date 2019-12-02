a99ebd0c55efb479c588196afb3a12e8fad8b440
Dustin McRorie <dustin.m@getjobber.com>
2019-12-02 09:11:29 -0700

    deleted some unused stuff

--- packages/components/src/Spinner/Spinner.tsx
+++ packages/components/src/Spinner/Spinner.tsx
@@ -2,26 +2,8 @@ import React from "react";
 import classnames from "classnames";
 import styles from "./Spinner.css";
 
-interface SpinnerProps {
-  /**
-   * Styles the text bold and uppercased
-   * @default false
-   */
-  readonly loud?: boolean;
-
-  /**
-   * Text to display.
-   */
-  readonly text: string;
-
-  /**
-   * Click handler.
-   */
-  onClick?(): void;
-}
-
 // TODO: area label (accessibility)
-export function Spinner({}: SpinnerProps) {
+export function Spinner() {
   const className = classnames(styles.spinner);
 
   return <div className={className}></div>;
