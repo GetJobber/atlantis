fa077e97f430810ac7a43ad6fdac07b51408b7d0
Dustin McRorie <dustin.m@getjobber.com>
2019-12-20 14:13:24 -0700

    switched the order of the docs descriptions

--- packages/components/src/Spinner/Spinner.tsx
+++ packages/components/src/Spinner/Spinner.tsx
@@ -3,31 +3,34 @@ import classnames from "classnames";
 import styles from "./Spinner.css";
 
 export interface SpinnerProps {
-  readonly size?: "small" | "base";
   /**
    * Specifies the size of the spinner
    *
    * @default "base"
    */
-  readonly inline?: boolean;
+  readonly size?: "small" | "base";
+
   /**
    * Controls whether the spinner is rendered in its own space
    * or in line
    */
-  readonly ariaBusy?: "true" | "false";
+  readonly inline?: boolean;
+
   /**
    * Prop to be set when a relevant portion of the page is
    * currently getting updated
    *
    * @default "false"
    */
-  readonly role?: "alert" | "none";
+  readonly ariaBusy?: "true" | "false";
+
   /**
    * When set, sends out accessible alert event to assistive
    * technology products
    *
    * @default "none"
    */
+  readonly role?: "alert" | "none";
 }
 
 export function Spinner({
