d3245d45818354a5776b0b5792fd1c888e55f2f9
Dustin McRorie <dustin.m@getjobber.com>
2019-12-20 14:08:41 -0700

    added docs lines to props for explanations of functionality and defaults

--- packages/components/src/Spinner/Spinner.tsx
+++ packages/components/src/Spinner/Spinner.tsx
@@ -4,9 +4,30 @@ import styles from "./Spinner.css";
 
 export interface SpinnerProps {
   readonly size?: "small" | "base";
+  /**
+   * Specifies the size of the spinner
+   *
+   * @default "base"
+   */
   readonly inline?: boolean;
+  /**
+   * Controls whether the spinner is rendered in its own space
+   * or in line
+   */
   readonly ariaBusy?: "true" | "false";
+  /**
+   * Prop to be set when a relevant portion of the page is
+   * currently getting updated
+   *
+   * @default "false"
+   */
   readonly role?: "alert" | "none";
+  /**
+   * When set, sends out accessible alert event to assistive
+   * technology products
+   *
+   * @default "none"
+   */
 }
 
 export function Spinner({
