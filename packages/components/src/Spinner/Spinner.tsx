77471facbf7afcd2c7f7e61217f4ef41de6ceafe
Dustin McRorie <dustin.m@getjobber.com>
2019-12-20 13:57:54 -0700

    added accessibility props to spinner

--- packages/components/src/Spinner/Spinner.tsx
+++ packages/components/src/Spinner/Spinner.tsx
@@ -5,13 +5,20 @@ import styles from "./Spinner.css";
 export interface SpinnerProps {
   readonly size?: "small" | "base";
   readonly inline?: boolean;
+  readonly ariaBusy?: "true" | "false";
+  readonly role?: "alert" | "none";
 }
 
-export function Spinner({ size = "base", inline }: SpinnerProps) {
+export function Spinner({
+  size = "base",
+  inline,
+  ariaBusy = "false",
+  role = "none",
+}: SpinnerProps) {
   const spinnerStyles = classnames(styles.spinner, {
     [styles.small]: size === "small",
     [styles.inline]: inline,
   });
 
-  return <div className={spinnerStyles}></div>;
+  return <div className={spinnerStyles} role={role} aria-busy={ariaBusy}></div>;
 }
