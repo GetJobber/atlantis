54dae3775001a2a71db487c30f43a73c78a565a1
Sean Healy <s@xib.ca>
2020-02-11 15:54:18 -0700

    Working-ish

--- packages/components/src/Tooltip/Tooltip.tsx
+++ packages/components/src/Tooltip/Tooltip.tsx
@@ -13,6 +13,11 @@ import { createPopper } from "@popperjs/core";
 import styles from "./Tooltip.css";
 import { Text } from "../Text";
 
+const variation = {
+  startOrStop: { scale: 0.6, opacity: 0 },
+  done: { scale: 1, opacity: 1 },
+};
+
 interface TooltipProps {
   readonly children: ReactElement;
   /**
@@ -23,57 +28,78 @@ interface TooltipProps {
 
 type Direction = "above" | "below";
 
+// eslint-disable-next-line max-statements
 export function Tooltip({ message, children }: TooltipProps) {
-  const [direction, setDirection] = useState("above" as Direction);
-  const [position, setPosition] = useState({
-    transform: "translate(0px, 0px)",
-  });
+  const [placement, setPlacement] = useState("above" as Direction);
+  const [tooltipStyles, setTooltipStyles] = useState({});
+  const [arrowStyles, setArrowStyles] = useState({});
   const [show, setShow] = useState(true);
+  const [popperInstance, setPopperInstance] = useState<
+    ReturnType<typeof createPopper> | undefined
+  >(undefined);
   const tooltipRef = createRef<HTMLDivElement>();
+  const arrowRef = createRef<HTMLDivElement>();
   const shadowRef = createRef<HTMLSpanElement>();
 
-  // showOnHover();
-  // positionTooltip();
+  showOnHover();
 
   useLayoutEffect(() => {
-    let popperInstance: ReturnType<typeof createPopper>;
     if (
       shadowRef.current &&
       shadowRef.current.nextElementSibling &&
       tooltipRef.current
     ) {
-      popperInstance = createPopper(
-        shadowRef.current.nextElementSibling,
-        tooltipRef.current,
-        {
+      setPopperInstance(
+        createPopper(shadowRef.current.nextElementSibling, tooltipRef.current, {
+          placement: "top",
           modifiers: [
+            {
+              name: "preventOverflow",
+              options: {
+                padding: 5,
+              },
+            },
+            {
+              name: "flip",
+              options: { padding: 50, fallbackPlacements: ["bottom"] },
+            },
+            {
+              name: "arrow",
+              options: { element: arrowRef.current, padding: 5 },
+            },
             {
               name: "applyStyles",
               fn: data => {
-                console.log({ data });
-                setPosition({ transform: data.state.styles.popper.transform });
+                setTooltipStyles(data.state.styles.popper);
+                setArrowStyles(data.state.styles.arrow);
+
+                if (data.state.placement === "top") {
+                  setPlacement("above");
+                } else {
+                  setPlacement("below");
+                }
               },
             },
           ],
-        },
+        }),
       );
+
+      setShow(false);
     }
 
     return () => {
-      popperInstance.destroy();
+      if (popperInstance) {
+        popperInstance.destroy();
+        setPopperInstance(undefined);
+      }
     };
   }, []);
 
   const toolTipClassNames = classnames(
     styles.tooltipWrapper,
-    direction === "below" && styles.below,
-    direction === "above" && styles.above,
+    placement === "below" && styles.below,
+    placement === "above" && styles.above,
   );
-
-  const variation = {
-    startOrStop: { scale: 0.6, opacity: 0 },
-    done: { scale: 1, opacity: 1 },
-  };
 
   return (
     <>
@@ -81,12 +107,12 @@ export function Tooltip({ message, children }: TooltipProps) {
       {children}
       <TooltipPortal>
         <AnimatePresence>
-          {show && (
-            <div
-              className={toolTipClassNames}
-              style={position}
-              ref={tooltipRef}
-            >
+          <div
+            className={toolTipClassNames}
+            style={tooltipStyles}
+            ref={tooltipRef}
+          >
+            {show && (
               <motion.div
                 className={styles.tooltip}
                 variants={variation}
@@ -101,70 +127,42 @@ export function Tooltip({ message, children }: TooltipProps) {
                 }}
               >
                 <Text>{message}</Text>
+                <div
+                  ref={arrowRef}
+                  style={arrowStyles}
+                  className={styles.arrow}
+                ></div>
               </motion.div>
-            </div>
-          )}
+            )}
+          </div>
         </AnimatePresence>
       </TooltipPortal>
     </>
   );
 
-  // function positionTooltip() {
-  //   useEffect(() => {
-  //     if (
-  //       tooltipRef.current &&
-  //       shadowRef.current &&
-  //       shadowRef.current.nextElementSibling
-  //     ) {
-  //       const { bounds, positionStyle } = getPosition(
-  //         shadowRef.current.nextElementSibling,
-  //         tooltipRef.current,
-  //       );
-  //       if (bounds.top <= 100) {
-  //         setDirection("below");
-  //         setPosition(positionStyle.below);
-  //       } else {
-  //         setDirection("above");
-  //         setPosition(positionStyle.above);
-  //       }
-  //     }
-  //   }, [show]);
-  // }
+  function showOnHover() {
+    useEffect(() => {
+      if (popperInstance) {
+        setTimeout(popperInstance.update);
+      }
+    }, [show]);
 
-  // function showOnHover() {
-  //   useEffect(() => {
-  //     const showTooltip = () => {
-  //       setShow(true);
-  //     };
+    useEffect(() => {
+      const showTooltip = () => {
+        setShow(true);
+      };
 
-  //     const hideTooltip = () => {
-  //       setShow(false);
-  //     };
+      const hideTooltip = () => {
+        setShow(false);
+      };
 
-  //     if (shadowRef.current && shadowRef.current.nextElementSibling) {
-  //       const activator = shadowRef.current.nextElementSibling;
-  //       activator.addEventListener("mouseenter", showTooltip);
-  //       activator.addEventListener("mouseleave", hideTooltip);
-  //     }
-  //   }, []);
-  // }
-}
-
-function getPosition(activator: Element, tooltip: HTMLDivElement) {
-  const bounds = activator.getBoundingClientRect();
-  const xOffset = bounds.right - bounds.width / 2 - tooltip.clientWidth / 2;
-  const positionStyle = {
-    above: {
-      top: `${bounds.top + window.scrollY - tooltip.clientHeight}px`,
-      left: `${xOffset}px`,
-    },
-    below: {
-      top: `${bounds.bottom + window.scrollY}px`,
-      left: `${xOffset}px`,
-    },
-  };
-
-  return { bounds, positionStyle };
+      if (shadowRef.current && shadowRef.current.nextElementSibling) {
+        const activator = shadowRef.current.nextElementSibling;
+        activator.addEventListener("mouseenter", showTooltip);
+        activator.addEventListener("mouseleave", hideTooltip);
+      }
+    }, []);
+  }
 }
 
 interface TooltipPortalProps {
