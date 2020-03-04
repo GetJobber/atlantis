bc1e42c159377e1f15a5cfb7ceda84a01b58132f
Sean Healy <s@xib.ca>
2020-03-04 13:55:01 -0700

    Things are mostly working again.

--- doczrc.js
+++ doczrc.js
@@ -1,138 +1,34 @@
 /* eslint-env node */
 import path from "path";
+import { createPlugin } from "docz-core";
 import glob from "glob";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { css } from "styled-components";
-// eslint-disable-next-line @typescript-eslint/no-var-requires, import/no-extraneous-dependencies
-const { WatchIgnorePlugin } = require("webpack");
+// import { WatchIgnorePlugin } from "webpack";
 
-const modifyBundlerConfig = config => {
-  config.resolve.alias = Object.assign({}, config.resolve.alias, {
-    "@jobber/components": path.resolve(__dirname, "packages/components/src"),
-    "@jobber/docx": path.resolve(__dirname, "packages/docx/src"),
+const projectPlugin = () =>
+  createPlugin({
+    onCreateWebpackConfig: ({ rules, actions, getConfig }) => {
+      // 😢 https://github.com/gatsbyjs/gatsby/issues/16129
+
+      const config = getConfig();
+
+      const cssRule = {
+        ...rules.cssModules(),
+        test: rules.css().test,
+      };
+
+      config.module.rules = [
+        ...config.module.rules.filter(rule => {
+          const areCssRules =
+            rule.oneOf && rule.oneOf.some(r => r.test.test("style.css"));
+
+          return !areCssRules;
+        }),
+        cssRule,
+      ];
+
+      actions.replaceWebpackConfig(config);
+    },
   });
-
-  config.plugins.push(new WatchIgnorePlugin([/css\.d\.ts$/]));
-
-  config.module.rules.push({
-    enforce: "pre",
-    test: /\.css$/,
-    exclude: /node_modules/,
-    loader: require.resolve("typed-css-modules-loader"),
-  });
-
-  config.module.rules.push({
-    test: /\.css$/,
-    use: [
-      require.resolve("style-loader"),
-      {
-        loader: require.resolve("css-loader"),
-        options: {
-          modules: {
-            localIdentName: "[name]__[local]__[hash:base64]",
-          },
-          importLoaders: 1,
-        },
-      },
-      {
-        loader: require.resolve("postcss-loader"),
-        options: {
-          modules: true,
-          plugins: [
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            require("postcss-preset-env")({
-              stage: 1,
-              preserve: true,
-              importFrom: [
-                require.resolve("@jobber/design"),
-                require.resolve("@jobber/design/src/responsiveBreakpoints.css"),
-              ],
-            }),
-          ],
-        },
-      },
-    ],
-  });
-
-  return config;
-};
-
-const fonts = {
-  // Used for headings larger than 20px.
-  display: '"Poppins", sans-serif',
-  // Used for code and sometimes numbers in tables.
-  mono: '"Anonymous Pro", monospace',
-  // Used for text and UI (which includes almost anything).
-  ui: '"Source Sans Pro", sans-serif',
-};
-
-const themeConfig = {
-  showPlaygroundEditor: true,
-  fonts,
-  styles: {
-    body: css`
-      font-family: ${fonts.ui};
-      font-size: 18px;
-      line-height: 1.8;
-    `,
-    h1: css`
-      margin: 48px 0 16px;
-      font-family: ${fonts.display};
-      font-size: 48px;
-      line-height: 1.33;
-      font-weight: 800;
-      text-transform: uppercase;
-    `,
-    h2: css`
-      margin: 48px 0 16px;
-      font-family: ${fonts.display};
-      font-size: 24px;
-      line-height: 1.33em;
-      font-weight: 600;
-      text-transform: uppercase;
-    `,
-    h3: css`
-      margin: 32px 0 16px;
-      font-family: ${fonts.display};
-      font-size: 20px;
-      line-height: 1.2em;
-      font-weight: 600;
-    `,
-    h4: css`
-      margin: 0 0 16px;
-      font-family: ${fonts.display};
-      font-size: 18px;
-      line-height: 1.2em;
-      font-weight: 600;
-    `,
-  },
-};
-
-const htmlContext = {
-  head: {
-    links: [
-      {
-        rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700,700i",
-      },
-      {
-        rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css?family=Anonymous+Pro:400,400i,700,700i",
-      },
-      {
-        rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i",
-      },
-      {
-        rel: "stylesheet",
-        href: "/public/foundation.css",
-      },
-    ],
-  },
-};
 
 function privateComponentReadmies() {
   if (process.env.PRIVATE_COMPONENTS === "visible") {
@@ -156,9 +52,8 @@ export default {
     ...privateComponentReadmies(),
     "./packages/generators/templates/**/*",
   ],
-  codeSandbox: false,
-  public: "public",
-  themeConfig,
-  htmlContext,
-  modifyBundlerConfig,
+  plugins: [projectPlugin()],
+  themeConfig: {
+    showDarkModeSwitch: false,
+  },
 };
---
+++ src/gatsby-theme-docz/wrapper.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet-async";
+
+// Note: We are using react-helmet-async here instead of react-helmet
+// https://github.com/nfl/react-helmet/issues/426
+// https://www.docz.site/docs/add-favicon-and-metadata
+
+// eslint-disable-next-line import/no-internal-modules,no-restricted-imports
+import "../../../packages/design/src/foundation.css";
+
+// The doc prop contains some metadata about the page being rendered that you can use.
+const Wrapper = ({ children }) => (
+  <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <link
+        type="text/css"
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700,700i"
+      ></link>
+      <link
+        type="text/css"
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Anonymous+Pro:400,400i,700,700i"
+      ></link>
+      <link
+        type="text/css"
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+      ></link>
+    </Helmet>
+    {children}
+  </>
+);
+
+Wrapper.propTypes = {
+  children: PropTypes.children,
+};
+
+// eslint-disable-next-line import/no-default-export
+export default Wrapper;
