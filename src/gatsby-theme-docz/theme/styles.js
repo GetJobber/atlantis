5a4da9066f2133352788495cadcec3d8268d2afb
eddysims <eddy.s@getjobber.com>
2020-03-10 14:53:54 -0600

    Styles the inline code

---
+++ src/gatsby-theme-docz/theme/styles.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line
+import themeStyles from "gatsby-theme-docz/src/theme/styles";
+
+const styles = {
+  ...themeStyles,
+  inlineCode: {
+    color: "rgb(125, 137, 156)",
+    background: "rgb(245, 246, 247)",
+    fontSize: 2,
+    mx: "3px",
+    borderRadius: "radius",
+    p: "2px 5px",
+  },
+};
+
+// eslint-disable-next-line import/no-default-export
+export default styles;
