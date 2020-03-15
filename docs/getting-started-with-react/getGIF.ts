ab0bb7687da96a34b6d5a29070fe93199f84b674
jcw- <jc.w@getjobber.com>
2020-03-15 01:24:26 -0600

    fix(docs): Remove scheme from search URL

--- docs/getting-started-with-react/getGIF.ts
+++ docs/getting-started-with-react/getGIF.ts
@@ -4,7 +4,7 @@ export const loadingGIF =
   "https://media.giphy.com/media/l3nWhI38IWDofyDrW/giphy.gif";
 
 export async function getGIF(topic: string) {
-  const searchURL = `http://api.giphy.com/v1/gifs/random?tag=${topic}&rating=PG&api_key=${gifKey}`;
+  const searchURL = `//api.giphy.com/v1/gifs/random?tag=${topic}&rating=PG&api_key=${gifKey}`;
   const response = await fetch(searchURL);
   const json = await response.json();
   return json.data.images.original.url;
