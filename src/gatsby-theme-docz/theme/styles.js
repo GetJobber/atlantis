63507aa65646d685625ba3a67ffbead2e8674dd1
Eddy Sims <46460840+eddysims@users.noreply.github.com>
2020-03-16 11:44:19 -0600

    chore: Upgrade docz - Style inline code (#207)
    * Styles the inline code
    
    * Changes styles to use jobber css vars

---
+++ src/gatsby-theme-docz/theme/styles.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line
+import themeStyles from "gatsby-theme-docz/src/theme/styles";
+
+const styles = {
+  ...themeStyles,
+  inlineCode: {
+    color: "var(--color-grey--dark)",
+    background: "var(--color-grey--lightest)",
+    fontSize: 2,
+    marginX: "2px",
+    borderRadius: "radius",
+    padding: "2px 5px",
+  },
+};
+
+// eslint-disable-next-line import/no-default-export
+export default styles;
