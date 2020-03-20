2b4fe75716e956c437db53712db5164045341bb4
Sean Healy <s@xib.ca>
2020-03-20 15:14:20 -0600

    chore: Keep our privates private once again. (#220)
    * chore: Keep our privates private once again.
    
    * Cleanup PR comments.

--- doczrc.js
+++ doczrc.js
@@ -52,15 +52,38 @@ const projectPlugin = () =>
     },
   });
 
-function privateComponentReadmies() {
+/**
+ * Return an array of private components to hide from the user.
+ *
+ * Atlantis contains some private components. These are indicated by an
+ * empty `.private` file in the component's directory.
+ *
+ * @return {string[]} Private components.
+ */
+function privateComponentReadmes() {
+  // If specified at run time show documentation for privates components.
   if (process.env.PRIVATE_COMPONENTS === "visible") {
     return [];
   }
-  return glob.sync("packages/components/src/**/.private").map(file => {
-    const directory = path.dirname(file);
-    const componentName = path.basename(directory);
-    return path.join(directory, `${componentName}.mdx`);
-  });
+
+  // Ensure we're on atlantis root. Gatsby can also execute this file
+  // from within the build directory.
+  const atlantisPath = __dirname.replace(/\/\.docz$/, "");
+
+  return glob
+    .sync(path.join(atlantisPath, "packages/components/src/**/.private"))
+    .map(file => {
+      const directory = path.dirname(file);
+      const componentName = path.basename(directory);
+
+      return (
+        path
+          // Construct the absolute path to ignored component.
+          .join(directory, `${componentName}.mdx`)
+          // Convert to relative path.
+          .slice(atlantisPath.length + 1)
+      );
+    });
 }
 
 // eslint-disable-next-line import/no-default-export
@@ -73,7 +96,7 @@ export default {
   menu: ["Atlantis", "Patterns", "Components"],
   files: "{README.md,CONTRIBUTING.md,**/*.mdx}",
   ignore: [
-    ...privateComponentReadmies(),
+    ...privateComponentReadmes(),
     "./packages/generators/templates/**/*",
   ],
   plugins: [projectPlugin()],
