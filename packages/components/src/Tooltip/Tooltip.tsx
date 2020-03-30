373ffcb23fa8e565b5fb7fdbf09bcd9743c4527a
Sean Healy <s@xib.ca>
2020-03-30 14:05:23 -0600

    fix(components): Improve tooltip positioning. (#201)
    * initial popper working
    
    * Working-ish
    
    * Cleanup popper initialization
    
    * Cleanup PopperInstance type.
    
    * Cleanup wip code.
    
    * Clarify usage and example.

--- packages/components/src/Tooltip/Tooltip.tsx
+++ packages/components/src/Tooltip/Tooltip.tsx
@@ -3,13 +3,20 @@ import React, {
   ReactNode,
   createRef,
   useEffect,
+  useLayoutEffect,
   useState,
 } from "react";
 import classnames from "classnames";
 import ReactDOM from "react-dom";
 import { AnimatePresence, motion } from "framer-motion";
+import { Instance as PopperInstance, createPopper } from "@popperjs/core";
 import styles from "./Tooltip.css";
 import { Text } from "../Text";
+
+const variation = {
+  startOrStop: { scale: 0.6, opacity: 0 },
+  done: { scale: 1, opacity: 1 },
+};
 
 interface TooltipProps {
   readonly children: ReactElement;
@@ -22,25 +29,22 @@ interface TooltipProps {
 type Direction = "above" | "below";
 
 export function Tooltip({ message, children }: TooltipProps) {
-  const [direction, setDirection] = useState("above" as Direction);
-  const [position, setPosition] = useState({ top: "0px", left: "0px" });
-  const [show, setShow] = useState(false);
+  const [placement, setPlacement] = useState("above" as Direction);
+  const [tooltipStyles, setTooltipStyles] = useState({});
+  const [arrowStyles, setArrowStyles] = useState({});
+  const [show, setShow] = useState(true);
+
   const tooltipRef = createRef<HTMLDivElement>();
+  const arrowRef = createRef<HTMLDivElement>();
   const shadowRef = createRef<HTMLSpanElement>();
 
   showOnHover();
-  positionTooltip();
 
   const toolTipClassNames = classnames(
     styles.tooltipWrapper,
-    direction === "below" && styles.below,
-    direction === "above" && styles.above,
+    placement === "below" && styles.below,
+    placement === "above" && styles.above,
   );
-
-  const variation = {
-    startOrStop: { scale: 0.6, opacity: 0 },
-    done: { scale: 1, opacity: 1 },
-  };
 
   return (
     <>
@@ -48,12 +52,12 @@ export function Tooltip({ message, children }: TooltipProps) {
       {children}
       <TooltipPortal>
         <AnimatePresence>
-          {show && (
-            <div
-              className={toolTipClassNames}
-              style={position}
-              ref={tooltipRef}
-            >
+          <div
+            className={toolTipClassNames}
+            style={tooltipStyles}
+            ref={tooltipRef}
+          >
+            {show && (
               <motion.div
                 className={styles.tooltip}
                 variants={variation}
@@ -68,38 +72,33 @@ export function Tooltip({ message, children }: TooltipProps) {
                 }}
               >
                 <Text>{message}</Text>
+                <div
+                  ref={arrowRef}
+                  style={arrowStyles}
+                  className={styles.arrow}
+                ></div>
               </motion.div>
-            </div>
-          )}
+            )}
+          </div>
         </AnimatePresence>
       </TooltipPortal>
     </>
   );
 
-  function positionTooltip() {
-    useEffect(() => {
-      if (
-        tooltipRef.current &&
-        shadowRef.current &&
-        shadowRef.current.nextElementSibling
-      ) {
-        const { bounds, positionStyle } = getPosition(
-          shadowRef.current.nextElementSibling,
-          tooltipRef.current,
-        );
-        if (bounds.top <= 100) {
-          setDirection("below");
-          setPosition(positionStyle.below);
-        } else {
-          setDirection("above");
-          setPosition(positionStyle.above);
-        }
-      }
-    }, [show]);
-  }
-
   function showOnHover() {
+    const [popperInstance, setPopperInstance] = useState<
+      PopperInstance | undefined
+    >(undefined);
+
     useEffect(() => {
+      if (popperInstance) popperInstance.update();
+    }, [show]);
+
+    useLayoutEffect(() => {
+      const popper = setupPopper();
+      setPopperInstance(popper);
+      setShow(false);
+
       const showTooltip = () => {
         setShow(true);
       };
@@ -113,25 +112,66 @@ export function Tooltip({ message, children }: TooltipProps) {
         activator.addEventListener("mouseenter", showTooltip);
         activator.addEventListener("mouseleave", hideTooltip);
       }
+
+      return () => {
+        if (popper) popper.destroy();
+
+        if (shadowRef.current && shadowRef.current.nextElementSibling) {
+          const activator = shadowRef.current.nextElementSibling;
+          activator.removeEventListener("mouseenter", showTooltip);
+          activator.removeEventListener("mouseleave", hideTooltip);
+        }
+      };
     }, []);
   }
-}
 
-function getPosition(activator: Element, tooltip: HTMLDivElement) {
-  const bounds = activator.getBoundingClientRect();
-  const xOffset = bounds.right - bounds.width / 2 - tooltip.clientWidth / 2;
-  const positionStyle = {
-    above: {
-      top: `${bounds.top + window.scrollY - tooltip.clientHeight}px`,
-      left: `${xOffset}px`,
-    },
-    below: {
-      top: `${bounds.bottom + window.scrollY}px`,
-      left: `${xOffset}px`,
-    },
-  };
+  function setupPopper() {
+    if (
+      shadowRef.current &&
+      shadowRef.current.nextElementSibling &&
+      tooltipRef.current
+    ) {
+      const popper = createPopper(
+        shadowRef.current.nextElementSibling,
+        tooltipRef.current,
+        {
+          placement: "top",
+          modifiers: [
+            {
+              name: "preventOverflow",
+              options: {
+                padding: 5,
+              },
+            },
+            {
+              name: "flip",
+              options: { padding: 50, fallbackPlacements: ["bottom"] },
+            },
+            {
+              name: "arrow",
+              options: { element: arrowRef.current, padding: 5 },
+            },
+            {
+              name: "applyStyles",
+              fn: data => {
+                setTooltipStyles(data.state.styles.popper);
+                setArrowStyles(data.state.styles.arrow);
+                if (data.state.placement === "top") {
+                  setPlacement("above");
+                } else {
+                  setPlacement("below");
+                }
+              },
+            },
+          ],
+        },
+      );
 
-  return { bounds, positionStyle };
+      return popper;
+    } else {
+      return undefined;
+    }
+  }
 }
 
 interface TooltipPortalProps {
