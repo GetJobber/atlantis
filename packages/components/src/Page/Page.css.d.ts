183bbf482a470f69ef5e9388f001f3cb6f1de6f3
Jeff Marvin <jeff.m@getjobber.com>
2020-05-06 12:55:16 -0600

    improve CSS in title

--- packages/components/src/Page/Page.css.d.ts
+++ packages/components/src/Page/Page.css.d.ts
@@ -2,5 +2,5 @@ export const page: string;
 export const fill: string;
 export const standard: string;
 export const narrow: string;
-export const actionGroup: string;
 export const titleBar: string;
+export const actionGroup: string;
