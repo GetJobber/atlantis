71e3e68508fc98f4951864e9e3d3d16950a74d52
eddysims <eddy.s@getjobber.com>
2020-05-21 11:49:42 -0600

    Tests for routing a button

--- packages/components/src/Button/Button.test.tsx
+++ packages/components/src/Button/Button.test.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { fireEvent, render } from "@testing-library/react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { Button } from ".";
 
 it("renders a Button", () => {
   const tree = renderer.create(<Button label="Submit" />).toJSON();
   expect(tree).toMatchInlineSnapshot(`
-                <button
-                  className="button base work primary"
-                  disabled={false}
-                  type="button"
-                >
-                  <span
-                    className="base extraBold small uppercase white"
-                  >
-                    Submit
-                  </span>
-                </button>
-        `);
+                    <button
+                      className="button base work primary"
+                      disabled={false}
+                      type="button"
+                    >
+                      <span
+                        className="base extraBold small uppercase white"
+                      >
+                        Submit
+                      </span>
+                    </button>
+          `);
 });
 
 it("renders a secondary Button", () => {
@@ -25,18 +26,18 @@ it("renders a secondary Button", () => {
     .create(<Button label="Submit" type="secondary" />)
     .toJSON();
   expect(tree).toMatchInlineSnapshot(`
-                <button
-                  className="button base work secondary"
-                  disabled={false}
-                  type="button"
-                >
-                  <span
-                    className="base extraBold small uppercase green"
-                  >
-                    Submit
-                  </span>
-                </button>
-        `);
+                    <button
+                      className="button base work secondary"
+                      disabled={false}
+                      type="button"
+                    >
+                      <span
+                        className="base extraBold small uppercase green"
+                      >
+                        Submit
+                      </span>
+                    </button>
+          `);
 });
 
 it("renders a tertiary Button", () => {
@@ -44,18 +45,18 @@ it("renders a tertiary Button", () => {
     .create(<Button label="Submit" type="tertiary" />)
     .toJSON();
   expect(tree).toMatchInlineSnapshot(`
-                <button
-                  className="button base work tertiary"
-                  disabled={false}
-                  type="button"
-                >
-                  <span
-                    className="base extraBold small uppercase green"
-                  >
-                    Submit
-                  </span>
-                </button>
-        `);
+                    <button
+                      className="button base work tertiary"
+                      disabled={false}
+                      type="button"
+                    >
+                      <span
+                        className="base extraBold small uppercase green"
+                      >
+                        Submit
+                      </span>
+                    </button>
+          `);
 });
 
 it("renders a destructuve Button", () => {
@@ -63,18 +64,18 @@ it("renders a destructuve Button", () => {
     .create(<Button label="Submit" variation="destructive" />)
     .toJSON();
   expect(tree).toMatchInlineSnapshot(`
-                <button
-                  className="button base destructive primary"
-                  disabled={false}
-                  type="button"
-                >
-                  <span
-                    className="base extraBold small uppercase white"
-                  >
-                    Submit
-                  </span>
-                </button>
-        `);
+                    <button
+                      className="button base destructive primary"
+                      disabled={false}
+                      type="button"
+                    >
+                      <span
+                        className="base extraBold small uppercase white"
+                      >
+                        Submit
+                      </span>
+                    </button>
+          `);
 });
 
 it("renders a learning Button", () => {
@@ -82,18 +83,18 @@ it("renders a learning Button", () => {
     .create(<Button label="Submit" variation="learning" />)
     .toJSON();
   expect(tree).toMatchInlineSnapshot(`
-                <button
-                  className="button base learning primary"
-                  disabled={false}
-                  type="button"
-                >
-                  <span
-                    className="base extraBold small uppercase white"
-                  >
-                    Submit
-                  </span>
-                </button>
-        `);
+                    <button
+                      className="button base learning primary"
+                      disabled={false}
+                      type="button"
+                    >
+                      <span
+                        className="base extraBold small uppercase white"
+                      >
+                        Submit
+                      </span>
+                    </button>
+          `);
 });
 
 it("renders a cancel Button", () => {
@@ -101,18 +102,18 @@ it("renders a cancel Button", () => {
     .create(<Button label="Submit" variation="cancel" />)
     .toJSON();
   expect(tree).toMatchInlineSnapshot(`
-                <button
-                  className="button base cancel primary"
-                  disabled={false}
-                  type="button"
-                >
-                  <span
-                    className="base extraBold small uppercase greyBlue"
-                  >
-                    Submit
-                  </span>
-                </button>
-        `);
+                    <button
+                      className="button base cancel primary"
+                      disabled={false}
+                      type="button"
+                    >
+                      <span
+                        className="base extraBold small uppercase greyBlue"
+                      >
+                        Submit
+                      </span>
+                    </button>
+          `);
 });
 
 it("renders a disabled Button", () => {
@@ -120,18 +121,18 @@ it("renders a disabled Button", () => {
     .create(<Button label="Submit" disabled={true} />)
     .toJSON();
   expect(tree).toMatchInlineSnapshot(`
-                <button
-                  className="button base work primary disabled"
-                  disabled={true}
-                  type="button"
-                >
-                  <span
-                    className="base extraBold small uppercase grey"
-                  >
-                    Submit
-                  </span>
-                </button>
-        `);
+                    <button
+                      className="button base work primary disabled"
+                      disabled={true}
+                      type="button"
+                    >
+                      <span
+                        className="base extraBold small uppercase grey"
+                      >
+                        Submit
+                      </span>
+                    </button>
+          `);
 });
 
 it("renders a Button with a link and opens in new tab", () => {
@@ -139,46 +140,46 @@ it("renders a Button with a link and opens in new tab", () => {
     .create(<Button label="Submit" url="💩.com" external={true} />)
     .toJSON();
   expect(tree).toMatchInlineSnapshot(`
-                    <a
-                      className="button base work primary"
-                      disabled={false}
-                      href="💩.com"
-                      target="_blank"
-                    >
-                      <span
-                        className="base extraBold small uppercase white"
-                      >
-                        Submit
-                      </span>
-                    </a>
-          `);
+                        <a
+                          className="button base work primary"
+                          disabled={false}
+                          href="💩.com"
+                          target="_blank"
+                        >
+                          <span
+                            className="base extraBold small uppercase white"
+                          >
+                            Submit
+                          </span>
+                        </a>
+            `);
 });
 
 it("renders a Button with an icon", () => {
   const tree = renderer.create(<Button label="Add" icon="add" />).toJSON();
   expect(tree).toMatchInlineSnapshot(`
-    <button
-      className="button base hasIcon work primary"
-      disabled={false}
-      type="button"
-    >
-      <svg
-        className="icon base"
-        viewBox="0 0 1024 1024"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          className="white"
-          d="M512 128c-23.565 0-42.667 19.103-42.667 42.667v298.667h-298.667c-23.564 0-42.667 19.102-42.667 42.667s19.103 42.667 42.667 42.667h298.667v298.667c0 23.565 19.102 42.667 42.667 42.667s42.667-19.102 42.667-42.667v-298.667h298.667c23.565 0 42.667-19.102 42.667-42.667s-19.102-42.667-42.667-42.667h-298.667v-298.667c0-23.564-19.102-42.667-42.667-42.667z"
-        />
-      </svg>
-      <span
-        className="base extraBold small uppercase white"
-      >
-        Add
-      </span>
-    </button>
-  `);
+        <button
+          className="button base hasIcon work primary"
+          disabled={false}
+          type="button"
+        >
+          <svg
+            className="icon base"
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              className="white"
+              d="M512 128c-23.565 0-42.667 19.103-42.667 42.667v298.667h-298.667c-23.564 0-42.667 19.102-42.667 42.667s19.103 42.667 42.667 42.667h298.667v298.667c0 23.565 19.102 42.667 42.667 42.667s42.667-19.102 42.667-42.667v-298.667h298.667c23.565 0 42.667-19.102 42.667-42.667s-19.102-42.667-42.667-42.667h-298.667v-298.667c0-23.564-19.102-42.667-42.667-42.667z"
+            />
+          </svg>
+          <span
+            className="base extraBold small uppercase white"
+          >
+            Add
+          </span>
+        </button>
+    `);
 });
 
 it("renders a Button with an icon on the right", () => {
@@ -186,62 +187,62 @@ it("renders a Button with an icon on the right", () => {
     .create(<Button label="Add" icon="add" iconOnRight={true} />)
     .toJSON();
   expect(tree).toMatchInlineSnapshot(`
-    <button
-      className="button base hasIcon iconOnRight work primary"
-      disabled={false}
-      type="button"
-    >
-      <svg
-        className="icon base"
-        viewBox="0 0 1024 1024"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          className="white"
-          d="M512 128c-23.565 0-42.667 19.103-42.667 42.667v298.667h-298.667c-23.564 0-42.667 19.102-42.667 42.667s19.103 42.667 42.667 42.667h298.667v298.667c0 23.565 19.102 42.667 42.667 42.667s42.667-19.102 42.667-42.667v-298.667h298.667c23.565 0 42.667-19.102 42.667-42.667s-19.102-42.667-42.667-42.667h-298.667v-298.667c0-23.564-19.102-42.667-42.667-42.667z"
-        />
-      </svg>
-      <span
-        className="base extraBold small uppercase white"
-      >
-        Add
-      </span>
-    </button>
-  `);
+        <button
+          className="button base hasIcon iconOnRight work primary"
+          disabled={false}
+          type="button"
+        >
+          <svg
+            className="icon base"
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              className="white"
+              d="M512 128c-23.565 0-42.667 19.103-42.667 42.667v298.667h-298.667c-23.564 0-42.667 19.102-42.667 42.667s19.103 42.667 42.667 42.667h298.667v298.667c0 23.565 19.102 42.667 42.667 42.667s42.667-19.102 42.667-42.667v-298.667h298.667c23.565 0 42.667-19.102 42.667-42.667s-19.102-42.667-42.667-42.667h-298.667v-298.667c0-23.564-19.102-42.667-42.667-42.667z"
+            />
+          </svg>
+          <span
+            className="base extraBold small uppercase white"
+          >
+            Add
+          </span>
+        </button>
+    `);
 });
 
 it("renders a small Button", () => {
   const tree = renderer.create(<Button label="Add" size="small" />).toJSON();
   expect(tree).toMatchInlineSnapshot(`
-                <button
-                  className="button small work primary"
-                  disabled={false}
-                  type="button"
-                >
-                  <span
-                    className="base extraBold smaller uppercase white"
-                  >
-                    Add
-                  </span>
-                </button>
-        `);
+                    <button
+                      className="button small work primary"
+                      disabled={false}
+                      type="button"
+                    >
+                      <span
+                        className="base extraBold smaller uppercase white"
+                      >
+                        Add
+                      </span>
+                    </button>
+          `);
 });
 
 it("renders a large Button", () => {
   const tree = renderer.create(<Button label="Add" size="large" />).toJSON();
   expect(tree).toMatchInlineSnapshot(`
-                <button
-                  className="button large work primary"
-                  disabled={false}
-                  type="button"
-                >
-                  <span
-                    className="base extraBold base uppercase white"
-                  >
-                    Add
-                  </span>
-                </button>
-        `);
+                    <button
+                      className="button large work primary"
+                      disabled={false}
+                      type="button"
+                    >
+                      <span
+                        className="base extraBold base uppercase white"
+                      >
+                        Add
+                      </span>
+                    </button>
+          `);
 });
 
 it("renders a Button with a loading state", () => {
@@ -249,18 +250,18 @@ it("renders a Button with a loading state", () => {
     .create(<Button label="Adding" loading={true} />)
     .toJSON();
   expect(tree).toMatchInlineSnapshot(`
-                <button
-                  className="button base work primary loading"
-                  disabled={false}
-                  type="button"
-                >
-                  <span
-                    className="base extraBold small uppercase white"
-                  >
-                    Adding
-                  </span>
-                </button>
-        `);
+                    <button
+                      className="button base work primary loading"
+                      disabled={false}
+                      type="button"
+                    >
+                      <span
+                        className="base extraBold small uppercase white"
+                      >
+                        Adding
+                      </span>
+                    </button>
+          `);
 });
 
 test("it should call the handler on click", () => {
@@ -281,4 +282,65 @@ test("it shouldn't call the handler on click when disabled", () => {
 
   fireEvent.click(getByText(label));
   expect(clickHandler).toHaveBeenCalledTimes(0);
+});
+
+it("renders a Link as a Button for routing", () => {
+  const tree = renderer
+    .create(
+      <Router>
+        <Button label="Adding" to="/jobber" />
+      </Router>,
+    )
+    .toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <a
+      className="button base work primary"
+      disabled={false}
+      href="/jobber"
+      onClick={[Function]}
+    >
+      <span
+        className="base extraBold small uppercase white"
+      >
+        Adding
+      </span>
+    </a>
+  `);
+});
+
+it("routes when buttons are clicked", () => {
+  const { queryByText } = render(
+    <Router>
+      <Button label="One" to="/" />
+      <Button label="Two" to="/two" />
+      <Button label="Three" to="/three" />
+      <Switch>
+        <Route exact path="/">
+          Uno
+        </Route>
+        <Route exact path="/two">
+          Dos
+        </Route>
+        <Route exact path="/three">
+          Tres
+        </Route>
+      </Switch>
+    </Router>,
+  );
+
+  expect(queryByText("Uno")).toBeInstanceOf(HTMLElement);
+  expect(queryByText("Dos")).not.toBeInstanceOf(HTMLElement);
+  expect(queryByText("Tres")).not.toBeInstanceOf(HTMLElement);
+
+  fireEvent.click(queryByText("Two"));
+
+  expect(queryByText("Uno")).not.toBeInstanceOf(HTMLElement);
+  expect(queryByText("Dos")).toBeInstanceOf(HTMLElement);
+  expect(queryByText("Tres")).not.toBeInstanceOf(HTMLElement);
+
+  fireEvent.click(queryByText("Three"));
+
+  expect(queryByText("Uno")).not.toBeInstanceOf(HTMLElement);
+  expect(queryByText("Dos")).not.toBeInstanceOf(HTMLElement);
+  expect(queryByText("Tres")).toBeInstanceOf(HTMLElement);
 });
