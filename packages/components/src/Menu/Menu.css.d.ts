1bf10e96ddfabf972c4a02d7e0ad2d828f2b2bdc
Chris Murray <chris.m@getjobber.com>
2020-06-12 17:39:53 -0600

    CSS updates for mobile responsive behaviour
    - not using resizeObserver because this behaviour is specific to browser window size
    - converts menu into an action-sheet-esque drawer on small screens

--- packages/components/src/Menu/Menu.css.d.ts
+++ packages/components/src/Menu/Menu.css.d.ts
@@ -1,7 +1,5 @@
 export const wrapper: string;
 export const menu: string;
-export const above: string;
-export const below: string;
 export const section: string;
 export const sectionHeader: string;
 export const action: string;
