b3480ba5e134edac3dc82af875188e5de69ca5df
Chris Murray <chris.m@getjobber.com>
2020-06-12 17:45:04 -0600

    move med+ screen styling within breakpoint

--- packages/components/src/Menu/Menu.css.d.ts
+++ packages/components/src/Menu/Menu.css.d.ts
@@ -1,5 +1,7 @@
 export const wrapper: string;
 export const menu: string;
+export const above: string;
+export const below: string;
 export const section: string;
 export const sectionHeader: string;
 export const action: string;
