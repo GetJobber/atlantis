7d3a362c2aa192cf07799762a9562cb04c763d73
hosseintalebi <hossein.t@getjobber.com>
2020-06-22 14:25:55 -0600

    simple change to make the release happen

--- packages/design/jobberStyle.js
+++ packages/design/jobberStyle.js
@@ -15,7 +15,7 @@ const jsonContent =
 
 fs.writeFile("./foundation.js", jsonContent, "utf8", function(err) {
   if (err) {
-    console.log("An error occured while writing JSON object to File.");
+    console.log("An error occured while writing JSON Object to File.");
     return console.log(err);
   }
   console.log("JSON file has been saved.");
