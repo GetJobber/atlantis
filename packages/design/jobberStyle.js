97cfd61af38c2590f89224351128a4089a97d46d
afroholic78 <mickael.zerihoun@gmail.com>
2020-06-30 14:30:41 -0600

    define regex object before its needed

--- packages/design/jobberStyle.js
+++ packages/design/jobberStyle.js
@@ -3,6 +3,13 @@ const fs = require("fs");
 // eslint-disable-next-line import/no-internal-modules
 const customPropertiesObject = require("./foundation.js");
 
+const regexExpressions = {
+  cssVars: /var\((.*)\)/,
+  calculations: /calc\((.*)\)/,
+  removeAllNonNumerals: /[^0-9.+\-/*]/gi,
+  extractAllVarGroups: /var\(.*?\)/g,
+};
+
 const customProperties = customPropertiesObject.customProperties;
 
 const resolvedCssVars = getResolvedCSSVars(customProperties);
@@ -18,25 +25,16 @@ fs.writeFile("./foundation.js", jsonContent, "utf8", function(err) {
   console.log("JSON file has been saved.");
 });
 
-function regexExpressions() {
-  return {
-    cssVars: /var\((.*)\)/,
-    calculations: /calc\((.*)\)/,
-    removeAllNonNumerals: /[^0-9.+\-/*]/gi,
-    extractAllVarGroups: /var\(.*?\)/g,
-  };
-}
-
 function jobberStyle(styling) {
   const styleValue = customProperties[styling];
-  const varRegexResult = regexExpressions().cssVars.exec(styleValue);
-  const calcRegexResult = regexExpressions().calculations.exec(styleValue);
+  const varRegexResult = regexExpressions.cssVars.exec(styleValue);
+  const calcRegexResult = regexExpressions.calculations.exec(styleValue);
   if (calcRegexResult) {
     const finalExpression = handleExpressionsInCalc(calcRegexResult);
     // eslint-disable-next-line no-new-func
     const calculatedValue = new Function(
       "return " +
-        finalExpression.replace(regexExpressions().removeAllNonNumerals, ""),
+        finalExpression.replace(regexExpressions.removeAllNonNumerals, ""),
     )();
     return isSpacingValue(calculatedValue)
       ? parseFloat(calculatedValue)
@@ -50,11 +48,11 @@ function jobberStyle(styling) {
 
 function handleExpressionsInCalc(calcRegexResult) {
   const calcExtract = calcRegexResult[1];
-  const varGroups = calcExtract.match(regexExpressions().extractAllVarGroups);
+  const varGroups = calcExtract.match(regexExpressions.extractAllVarGroups);
   let finalExpression = calcExtract;
   varGroups &&
     varGroups.forEach(group => {
-      const cssVariableRegexResult = regexExpressions().cssVars.exec(group);
+      const cssVariableRegexResult = regexExpressions.cssVars.exec(group);
       if (cssVariableRegexResult) {
         finalExpression = resolveCssVarsInExpression({
           group,
