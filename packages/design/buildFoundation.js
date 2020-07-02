4edc9c8d181cb9e38ecba01cb4c843199b4a3cad
Mickael Zerihoun <mickael.zerihoun@gmail.com>
2020-07-02 11:26:38 -0600

    feat: export foundation as a js file with the corresponding types (#280)
    * move icons to jobber design
    
    * use icons from jobber design
    
    * update readme's
    
    * update files for tarball payload
    
    * update readme
    
    * add test for getIcon
    
    * define IconColorNames and IconSizes independent of the css file
    Since we have a module declaration for all our css files on react native, we lost the types on the css files. This change helps React Native have access to the actual types
    
    * add sourcemap config to rollup
    
    * update jobber design readme to include icons
    
    * export classNameMaps
    
    * invert classNames mapping
    
    * add height to icon sizes
    
    * refactor get icon color
    
    * add buildfoundation.js to package.json
    
    * export foundations.css as a js file
    
    * add buildFoundation.js to build method
    
    * remove redundant code
    
    * add foundation.js to files that get served
    
    * remove un-needed package
    
    * remove redundant encoding
    
    * create and export ts types
    
    * ignore all foundation files
    
    * resolve all css vars
    
    * use es6 module export
    
    * emit declration file only
    
    * fix the css script
    
    * change to update package version
    
    * package up foundation using rollup
    
    * simple change to make the release happen
    
    * revert back to top level export
    
    * fix input file for postcss
    
    * let tsc create foundation.js
    
    * undo changes
    
    * undoing package-lock changes
    
    * merge master
    
    * extract regex and logic
    
    * add generated file to prettierignore
    
    * commit package-lock
    
    * define regex object before its needed
    
    * add comments, add temp file and cleanup for it and add linter exclusion to typescript types
    
    * add lint exception to generated files
    
    * change comments and rename npm task
    
    * allow for rem measurements and fixup package.json
    
    * account for all relative css units
    
    Co-authored-by: hosseintalebi <hossein.t@getjobber.com>

---
+++ packages/design/buildFoundation.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require("fs");
+const postcss = require("postcss");
+const postcssCustomProperties = require("postcss-custom-properties");
+
+const foundation = fs.readFileSync("foundation.css");
+
+postcss([
+  postcssCustomProperties({
+    exportTo: ["src/foundation.js"],
+  }),
+])
+  .process(foundation, { from: undefined })
+  .then();
---
+++ packages/design/jobberStyle.js
@@ -0,0 +1,114 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require("fs");
+// eslint-disable-next-line import/no-internal-modules
+const customPropertiesObject = require("./src/foundation.js");
+
+const regexExpressions = {
+  cssVars: /var\((.*)\)/,
+  calculations: /calc\((.*)\)/,
+  removeAllNonNumerals: /[^0-9.+\-/*]/gi,
+  extractAllVarGroups: /var\(.*?\)/g,
+};
+
+const customProperties = customPropertiesObject.customProperties;
+
+const resolvedCssVars = getResolvedCSSVars(customProperties);
+
+const jsonContent =
+  "export const JobberStyle = " + JSON.stringify(resolvedCssVars, undefined, 2);
+
+fs.writeFile("./foundation.js", jsonContent, "utf8", function(err) {
+  if (err) {
+    console.log("An error occured while writing JSON object to File.");
+    return console.log(err);
+  }
+  console.log("JSON file has been saved.");
+});
+
+/**
+ * Recursively resolve css custom properties.
+ *
+ * eg:
+ * ```
+ * jobberStyle(`
+ *   "--base": "5px",
+ *   "--foo": "calc(var(--base) * 2),
+ * `);
+ * ```
+ * =>
+ * ```
+ * {
+ *   "--base": "5",
+ *   "--foo": "10",
+ * }
+ * ```
+ */
+
+function jobberStyle(styling) {
+  const styleValue = customProperties[styling];
+
+  //varRegexResult returns --base-unit from var(--base-unit)
+  const varRegexResult = regexExpressions.cssVars.exec(styleValue);
+
+  //calcRegexResult returns var(--base-unit) / 16 from calc(var(--base-unit) / 16)
+  const calcRegexResult = regexExpressions.calculations.exec(styleValue);
+  if (calcRegexResult) {
+    const finalExpression = handleExpressionsInCalc(calcRegexResult);
+    // eslint-disable-next-line no-new-func
+    const calculatedValue = new Function(
+      "return " +
+        finalExpression.replace(regexExpressions.removeAllNonNumerals, ""),
+    )();
+    return isSpacingValue(calculatedValue)
+      ? parseFloat(calculatedValue)
+      : calculatedValue;
+  } else if (varRegexResult) {
+    return jobberStyle(varRegexResult[1]);
+  } else {
+    return isSpacingValue(styleValue) ? parseFloat(styleValue) : styleValue;
+  }
+}
+
+function handleExpressionsInCalc(calcRegexResult) {
+  const calcExtract = calcRegexResult[1];
+  const varGroups = calcExtract.match(regexExpressions.extractAllVarGroups);
+  let finalExpression = calcExtract;
+  varGroups &&
+    varGroups.forEach(group => {
+      const cssVariableRegexResult = regexExpressions.cssVars.exec(group);
+      if (cssVariableRegexResult) {
+        finalExpression = resolveCssVarsInExpression({
+          group,
+          cssVariableRegexResult,
+          finalExpression,
+        });
+      }
+    });
+  return finalExpression;
+}
+
+function resolveCssVarsInExpression({
+  group,
+  cssVariableRegexResult,
+  finalExpression,
+}) {
+  const cssVariable = cssVariableRegexResult[1];
+  const realValue = jobberStyle(cssVariable);
+  const unresolvedCssVariable = group;
+  return finalExpression.replace(unresolvedCssVariable, realValue);
+}
+
+function isSpacingValue(value) {
+  return !!String(value).match(
+    /(^\d+(px|%|rem|em|ex|ch|vw|vh|vmin|vmax)$)|(^\d+$)/,
+  );
+}
+
+function getResolvedCSSVars(cssProperties) {
+  const allKeys = Object.keys(cssProperties);
+  return allKeys.reduce((acc, key) => {
+    const newKey = key.replace("--", "");
+    acc[newKey] = jobberStyle(key);
+    return acc;
+  }, {});
+}
