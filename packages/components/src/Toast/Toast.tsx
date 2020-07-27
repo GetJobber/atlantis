c78de58968f19eb4ea233941ab133da7385338d8
eddysims <eddy.s@getjobber.com>
2020-07-27 16:15:57 -0600

    Gets a text string added

--- packages/components/src/Toast/Toast.tsx
+++ packages/components/src/Toast/Toast.tsx
@@ -1,31 +1,32 @@
-import React from "react";
-import classnames from "classnames";
-import styles from "./Toast.css";
+import React, {
+  ReactNode,
+  Ref,
+  forwardRef,
+  useImperativeHandle,
+  useState,
+} from "react";
 
 interface ToastProps {
-  /**
-   * Styles the text bold and uppercased
-   * @default false
-   */
-  readonly loud?: boolean;
-
-  /**
-   * Text to display.
-   */
-  readonly text: string;
-
-  /**
-   * Click handler.
-   */
-  onClick?(): void;
+  message: string;
 }
 
-export function Toast({ loud = false, text, onClick }: ToastProps) {
-  const className = classnames(styles.toast, { [styles.bold]: loud });
+interface ToastRef {
+  add(props: ToastProps): void;
+}
 
-  return (
-    <div className={className} onClick={onClick}>
-      {text}
-    </div>
-  );
+export const Toast = forwardRef(ToastInternal);
+
+function ToastInternal(_props: any, ref: Ref<ToastRef>) {
+  const [toasts, setToasts] = useState<ReactNode[]>([]);
+
+  useImperativeHandle(ref, () => ({
+    add: ({ message }) => {
+      setToasts([...toasts, <Slice key="hello" message={message} />]);
+    },
+  }));
+  return <>{toasts}</>;
+}
+
+function Slice({ message }: ToastProps) {
+  return <div>{message}</div>;
 }
