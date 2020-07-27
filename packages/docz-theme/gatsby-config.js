07f544fe7c75a6a9be6c155033be3865358c9c43
eddysims <eddy.s@getjobber.com>
2020-07-26 22:53:42 -0600

    Add fake link color

---
+++ packages/docz-theme/gatsby-config.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-theme-docz",
+      options: {},
+    },
+  ],
+};
---
+++ packages/docz-theme/src/gatsby-theme-docz/theme/styles.js
@@ -0,0 +1,15 @@
+import themeStyles from "gatsby-theme-docz/src/theme/styles";
+import { merge } from "lodash/fp";
+
+// eslint-disable-next-line import/no-default-export
+export default merge(themeStyles.default, {
+  Container: {
+    maxWidth: 300,
+  },
+  a: {
+    color: "rgb(239, 87, 51)",
+  },
+  inlineCode: {
+    background: "rgb(239, 87, 51)",
+  },
+});
