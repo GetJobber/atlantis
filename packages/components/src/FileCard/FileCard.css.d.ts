326e2dc4771e305f440df353a0ccaef76f6dd1bb
Jeff Marvin <jeff.m@getjobber.com>
2020-08-12 19:23:21 -0600

    crashing

---
+++ packages/components/src/FileCard/FileCard.css.d.ts
@@ -0,0 +1,2 @@
+export const fileCard: string;
+export const bold: string;
--- packages/components/src/FileCard/FileCard.tsx
+++ packages/components/src/FileCard/FileCard.tsx
@@ -1,31 +1,63 @@
 import React from "react";
 import classnames from "classnames";
 import styles from "./FileCard.css";
+import { Button } from "../Button";
+
+interface AtFile {
+  /**
+   * File Identifier
+   */
+  readonly id: string;
+  /**
+   * The name of the file.
+   */
+  readonly name: string;
+  /**
+   * The [MIME](https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types) type of the file
+   */
+  readonly type: string;
+  /**
+   * The size of the file in bytes.
+   */
+  readonly size: number;
+  /**
+   * The progress of a file upload between 0 and 1.
+   * - `0` represents Upload just started.
+   * - `1` represents a complete upload.
+   */
+  readonly progress: number;
+  /**
+   * The url of the file.
+   */
+  readonly src: string;
+  /**
+   * If the file is an image the url of the thumbnail.
+   */
+  readonly thumbnailSrc?: string;
+}
 
 interface FileCardProps {
   /**
-   * Styles the text bold and uppercased
-   * @default false
+   * File details object.
    */
-  readonly loud?: boolean;
+  readonly fileDetails: AtFile;
 
   /**
-   * Text to display.
+   * onDelete callback - this function will be called when the delete action is triggered
    */
-  readonly text: string;
-
-  /**
-   * Click handler.
-   */
-  onClick?(): void;
+  onDelete?(): void;
 }
 
-export function FileCard({ loud = false, text, onClick }: FileCardProps) {
-  const className = classnames(styles.fileCard, { [styles.bold]: loud });
+export function FileCard({ fileDetails, onDelete }: FileCardProps) {
+  const className = classnames(styles.fileCard);
 
   return (
-    <div className={className} onClick={onClick}>
-      {text}
+    <div className={className}>
+      <p>{JSON.stringify(fileDetails)}</p>
+      <h2>{fileDetails.src}</h2>
+      {onDelete && (
+        <Button onClick={onDelete} type="destructive" icon="trash" />
+      )}
     </div>
   );
 }
