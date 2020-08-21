132b485d06043b10567a55df0c5976c074e4371c
eddysims <eddy.s@getjobber.com>
2020-08-21 12:13:35 -0600

    Adds a favicon config option

---
+++ packages/docz-theme/src/gatsby-theme-docz/wrapper.js
@@ -0,0 +1,41 @@
+import React from "react";
+import t from "prop-types";
+import { Helmet } from "react-helmet";
+import { useConfig, useCurrentDoc } from "docz";
+
+const Wrapper = ({ children }) => {
+  const {
+    themeConfig: { favicon },
+    title,
+    description,
+  } = useConfig();
+  const { name, route } = useCurrentDoc();
+  const isHomePage = route === "/";
+  const hasName = name !== "";
+  const showName = hasName && !isHomePage;
+
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        {showName ? (
+          <title>
+            {name} | {title}
+          </title>
+        ) : (
+          <title>{title}</title>
+        )}
+        <meta name="description" content={description} />
+        {favicon && <link rel="icon" type="image/png" href={favicon} />}
+      </Helmet>
+      {children}
+    </>
+  );
+};
+
+Wrapper.propTypes = {
+  children: t.element,
+};
+
+// eslint-disable-next-line import/no-default-export
+export default Wrapper;
