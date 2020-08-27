9807543398f4d10ad944a3a796f346cdc02305fe
Eddy Sims <46460840+eddysims@users.noreply.github.com>
2020-08-27 10:17:31 -0600

    feat(components): Divider component (#345)
    * Divider component
    
    * PR changes to divider
    
    * Update the prop description

---
+++ packages/components/src/Divider/Divider.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import classnames from "classnames";
+import styles from "./Divider.css";
+
+interface DividerProps {
+  /**
+   * The weight of the divider.
+   *
+   * @default "base"
+   */
+  readonly size?: "base" | "large";
+}
+
+export function Divider({ size = "base" }: DividerProps) {
+  const className = classnames(styles.divider, {
+    [styles.large]: size === "large",
+  });
+
+  return <hr className={className} />;
+}
