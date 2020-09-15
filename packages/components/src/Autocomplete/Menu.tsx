2eada6cbafc69624e1c7f56c9974132b0e12bb6c
Dustin McRorie <dustin.m@getjobber.com>
2020-09-15 12:33:31 -0700

    address first round of feedback

--- packages/components/src/Autocomplete/Menu.tsx
+++ packages/components/src/Autocomplete/Menu.tsx
@@ -19,7 +19,6 @@ interface MenuProps {
   onOptionSelect(chosenOption: Option): void;
 }
 
-// eslint-disable-next-line max-statements
 export function Menu({
   visible,
   options,
@@ -30,7 +29,6 @@ export function Menu({
   const optionMenuClass = classnames(styles.options, {
     [styles.visible]: visible,
   });
-  const menuItemRefs = useRef<HTMLDivElement[]>([]);
   const menuDiv = useRef<HTMLDivElement>();
 
   const detectSeparatorCondition = (option: Option) =>
@@ -50,7 +48,7 @@ export function Menu({
     <div
       className={optionMenuClass}
       ref={element => {
-        setRef(element, menuDiv);
+        menuDiv.current = element as HTMLDivElement;
       }}
     >
       {options.map((option, index) => {
@@ -71,13 +69,8 @@ export function Menu({
             key={option.value}
             onMouseDown={onOptionSelect.bind(undefined, option)}
           >
-            <div
-              className={styles.icon}
-              ref={element => {
-                setRef(element, undefined, menuItemRefs, index);
-              }}
-            >
-              {isOptionSelected(option) && (
+            <div className={styles.icon}>
+              {isOptionSelected(selectedOption, option) && (
                 <Icon name="checkmark" size="small" />
               )}
             </div>
@@ -100,28 +93,6 @@ export function Menu({
     </div>
   );
 
-  function setRef(
-    element: HTMLDivElement | null,
-    ref: React.MutableRefObject<HTMLDivElement | undefined> | undefined,
-    refArray?: React.MutableRefObject<HTMLDivElement[]>,
-    index?: number,
-  ) {
-    const newElement = element as HTMLDivElement;
-    if (index && refArray) {
-      refArray.current[index] = newElement;
-      return refArray.current[index];
-    } else if (ref) {
-      ref.current = newElement;
-      return ref.current;
-    } else {
-      return undefined;
-    }
-  }
-
-  function isOptionSelected(option: Option) {
-    return selectedOption && selectedOption.value === option.value;
-  }
-
   function setupKeyListeners() {
     useOnKeyDown("ArrowDown", (event: KeyboardEvent) => {
       const indexChange = arrowKeyPress(event, IndexChange.Next);
@@ -130,50 +101,41 @@ export function Menu({
           Math.min(options.length - 1, highlightedIndex + indexChange),
         );
       }
-      if (menuDiv.current && menuItemRefs.current[highlightedIndex]) {
-        scrollDownIfTooLow(
-          menuDiv.current,
-          menuItemRefs.current[highlightedIndex],
-        );
+      if (menuDiv.current) {
+        scrollMenuIfItemNotInView(menuDiv.current, "down");
       }
     });
-
-    function scrollDownIfTooLow(
-      menuDivElement: HTMLDivElement,
-      itemDivElement: HTMLDivElement,
-    ) {
-      const tempDiv = itemDivElement.parentElement as HTMLDivElement;
-      const itemHeight = tempDiv.getBoundingClientRect().height;
-      const itemTrueBottom =
-        tempDiv.getBoundingClientRect().bottom + itemHeight;
-      if (itemTrueBottom > menuDivElement.getBoundingClientRect().bottom) {
-        menuDivElement.scrollTop += itemHeight;
-      }
-    }
 
     useOnKeyDown("ArrowUp", (event: KeyboardEvent) => {
       const indexChange = arrowKeyPress(event, IndexChange.Previous);
       if (indexChange) {
         setHighlightedIndex(Math.max(0, highlightedIndex + indexChange));
       }
-      if (menuDiv.current && menuItemRefs.current[highlightedIndex]) {
-        scrollUpIfTooHigh(
-          menuDiv.current,
-          menuItemRefs.current[highlightedIndex],
-        );
+      if (menuDiv.current) {
+        scrollMenuIfItemNotInView(menuDiv.current, "up");
       }
     });
 
-    function scrollUpIfTooHigh(
+    function scrollMenuIfItemNotInView(
       menuDivElement: HTMLDivElement,
-      itemDivElement: HTMLDivElement,
+      direction: string,
     ) {
-      const tempDiv = itemDivElement.parentElement as HTMLDivElement;
+      const tempDiv = menuDivElement.getElementsByClassName(
+        classnames(styles.option),
+      )[highlightedIndex] as HTMLButtonElement;
+      if (!tempDiv) {
+        return;
+      }
       const menuTop = menuDivElement.getBoundingClientRect().top;
       const itemTop = tempDiv.getBoundingClientRect().top;
       const itemHeight = tempDiv.getBoundingClientRect().height;
-      if (itemTop - itemHeight < menuTop) {
+      const itemTrueBottom =
+        tempDiv.getBoundingClientRect().bottom + itemHeight;
+      const menuBottom = menuDivElement.getBoundingClientRect().bottom;
+      if (direction == "up" && itemTop - itemHeight < menuTop) {
         menuDivElement.scrollTop -= itemHeight;
+      } else if (direction == "down" && itemTrueBottom > menuBottom) {
+        menuDivElement.scrollTop += itemHeight;
       }
     }
 
@@ -194,6 +156,10 @@ export function Menu({
       ? direction + direction
       : direction;
   }
+}
+
+function isOptionSelected(selectedOption: Option | undefined, option: Option) {
+  return selectedOption && selectedOption.value === option.value;
 }
 
 // Split this out into a hooks package.
