0888953cc658efaa4e40826f2ca0a3ba90af04d0
Jeff Marvin <jeff.m@getjobber.com>
2020-09-21 16:16:01 -0600

    update script to write whole file
    appearance still wonly

--- packages/components/scripts/writeComponentIndex.ts
+++ packages/components/scripts/writeComponentIndex.ts
@@ -9,18 +9,67 @@ const pGlob = promisify(glob);
 build();
 
 async function build() {
-  writeFileSync(`./src/index.ts`, await getEntryPointsContent());
+  writeFileSync(`../../docs/Playground.mdx`, await getPlaygroundContent());
 }
 
-async function getEntryPointsContent() {
+async function getComponentList() {
   const indices = await pGlob("./src/*/index.ts");
   const content = "";
 
   return indices.reduce((listContent, entryPoint) => {
     const pathParts = entryPoint.split(sep);
     const moduleName = pathParts[2];
-    listContent += `export { ${moduleName} } from "./${moduleName}";\n`;
+    listContent += `import { ${moduleName} } from "@jobber/components/${moduleName}";\n`;
 
     return listContent;
   }, content);
+}
+
+async function getPlaygroundContent() {
+  const list = await getComponentList();
+  const result = `---
+name: Playground
+route: /playground
+fullpage: true
+---
+
+import { useRef, useState } from "react";
+import { Playground, Props } from "docz";
+${list}
+
+
+# Playground
+
+Use All the Components!! Maybe this playground needs a button?
+
+
+## Coming soon, probably:
+
+- "Quick Add" UI to copy in the component example from another MDX file!
+
+---
+
+<Playground>
+  {() => {
+    const [bannerIsVisible, setBannerIsVisible] = useState(true);
+    return (
+      <Content>
+        {bannerIsVisible && (
+          <Banner type="notice">
+            ☀️ Play nicely with everything in the sandbox. 🏖
+          </Banner>
+        )}
+        <Card>
+          <Checkbox
+            checked={bannerIsVisible}
+            label="Dark Mode?"
+            onChange={setBannerIsVisible}
+          />
+        </Card>
+      </Content>
+    );
+  }}
+</Playground>
+`;
+  return result;
 }
