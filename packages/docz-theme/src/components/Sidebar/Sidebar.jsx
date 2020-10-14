35a67c2cc1906482a25ca0a26519b1c79dc1a712
eddysims <eddy.s@getjobber.com>
2020-10-14 13:11:04 -0600

    Adds search to sidebar

--- packages/docz-theme/src/components/Sidebar/Sidebar.jsx
+++ packages/docz-theme/src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,21 @@
 /** @jsx jsx */
 import { Box, jsx } from "theme-ui";
+import { useState } from "react";
 import { Logo } from "@jobber/docz-theme/components/Logo";
+import { InputText } from "@jobber/components/InputText";
 
 export function Sidebar() {
+  const [query, setQuery] = useState("");
+
   return (
     <Box>
       <Box>
         <Logo />
       </Box>
-      <Box>Search</Box>
-      <Box>Navigation</Box>
+      <Box>
+        <InputText onChange={setQuery} placeholder="Search Atlantis" />
+      </Box>
+      <Box>Navigation - query: {query}</Box>
     </Box>
   );
 }
