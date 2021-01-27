339f8853602e498c8b69be1445ff9bc537d68e68
eddysims <eddy.s@getjobber.com>
2021-01-27 14:06:01 -0700

    move postcss to docz-tools

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,44 +5,6 @@ const path = require("path");
 exports.onCreateWebpackConfig = ({ actions, stage, getConfig }) => {
   const config = getConfig();
 
-  /**
-   * Generate css types on `.css` file save,
-   * as well as handle PostCss
-   */
-  config.module.rules.push({
-    enforce: "pre",
-    test: /\.css$/,
-    exclude: /node_modules/,
-    use: [
-      require.resolve("typed-css-modules-loader"),
-      {
-        loader: "postcss-loader",
-        options: {
-          ident: "postcss",
-          plugins: () => [
-            require("postcss-import"),
-            require("autoprefixer"),
-            require("postcss-preset-env")({
-              stage: 1,
-              preserve: true,
-              importFrom: [
-                require.resolve(
-                  path.join(__dirname, "../packages/design/foundation.css"),
-                ),
-                require.resolve(
-                  path.join(
-                    __dirname,
-                    "../packages/design/src/responsiveBreakpoints.css",
-                  ),
-                ),
-              ],
-            }),
-          ],
-        },
-      },
-    ],
-  });
-
   if (stage.includes("html")) {
     config.module.rules.push({
       test: /.*\.(?:md|mdx)$/,
