f0febe4787787c1dd2b823eaf3f02747058cdebf
Robby Tiu <54861184+robbyjobber@users.noreply.github.com>
2021-04-09 15:26:44 -0400

    feat(components): Add new popover component (#494)
    * feature add new popover component
    
    * feat: add new popper-like props for flexibility
    
    * feat: adding more props for flexibility
    
    * fix package dependencies
    
    * feat: updated prop types
    
    * updated proptype
    
    * fix the popover example
    
    * revert checkbox ref changes
    
    * reverted ref addition to button props
    
    * feat: popover updated test specs
    
    * fix: remove useless eslint disable
    
    * updated popper docs
    
    * fix: clean up test specs
    
    * fix: cleaned up css
    
    * fix: removed title prop
    
    * get rid of visibility css
    
    * feat: updated docs and css
    
    * fix: feedback fixes on the mdx descriptions
    
    * feat: updated mdx and lint issue fixes
    
    * feat: fixed mdx descriptions and dismiss button glitch
    
    * feat: add accessibility descriptors
    
    * feat: updated mdx example and prop descriptor

---
+++ packages/components/src/Popover/Popover.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { usePopper } from "react-popper";
+import classes from "./Popover.css";
+import { ButtonDismiss } from "../ButtonDismiss";
+
+export interface PopoverProps {
+  /**
+   * Element the popover will attach to and point at. A `useRef` must be attached to an html element
+   * and passed as an attachTo prop in order for the popover to function properly
+   */
+  readonly attachTo: Element | React.RefObject<Element | null>;
+
+  /**
+   * Pop-over content.
+   */
+  readonly children: React.ReactNode;
+
+  /**
+   * Control popover viability.
+   */
+  readonly open: boolean;
+
+  /**
+   * Callback executed when the user wants to close/dismiss the popover
+   */
+  readonly onRequestClose?: () => void;
+
+  /**
+   * Describes the preferred placement of the popper.
+   * @default 'auto'
+   */
+  readonly preferredPlacement?: "top" | "bottom" | "left" | "right" | "auto";
+}
+
+export function Popover({
+  onRequestClose,
+  children,
+  attachTo,
+  open,
+  preferredPlacement = "auto",
+}: PopoverProps) {
+  const [popperElement, setPopperElement] = useState<HTMLElement | null>();
+  const [arrowElement, setArrowElement] = useState<HTMLElement | null>();
+  const { styles: popperStyles, attributes } = usePopper(
+    attachTo instanceof Element ? attachTo : attachTo.current,
+    popperElement,
+    {
+      modifiers: buildModifiers(arrowElement),
+      placement: preferredPlacement,
+    },
+  );
+
+  return (
+    open && (
+      <div
+        role="dialog"
+        ref={setPopperElement}
+        style={popperStyles.popper}
+        className={classes.popover}
+        {...attributes.popper}
+      >
+        <div className={classes.dismissButton}>
+          <ButtonDismiss onClick={onRequestClose} ariaLabel="Close dialog" />
+        </div>
+        {children}
+        <div
+          ref={setArrowElement}
+          className={classes.arrow}
+          style={popperStyles.arrow}
+        />
+      </div>
+    )
+  );
+}
+
+function buildModifiers(arrowElement: HTMLElement | undefined | null) {
+  const modifiers = [
+    {
+      name: "arrow",
+      options: { element: arrowElement, padding: 10 },
+    },
+    {
+      name: "offset",
+      options: {
+        offset: [0, 10],
+      },
+    },
+    {
+      name: "flip",
+      options: {
+        fallbackPlacements: ["auto"],
+      },
+    },
+  ];
+  return modifiers;
+}
