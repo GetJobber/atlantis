8839b82a8cbc34bcde8caa769aa4bf7eb5374a51
Chris Murray <chris.m@getjobber.com>
2021-05-05 18:16:24 -0600

    did a second pass

--- packages/components/src/Menu/Menu.tsx
+++ packages/components/src/Menu/Menu.tsx
@@ -223,7 +223,7 @@ function Action({ label, icon, onClick }: ActionProps) {
           <Icon name={icon} />
         </span>
       )}
-      <Typography element="span" size="base" textColor="greyBlueDark">
+      <Typography element="span" size="base" textColor="text">
         {label}
       </Typography>
     </button>
