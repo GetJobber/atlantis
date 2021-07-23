46a2542092b24699c2bd71dff7e52bd3d3a207b2
deepvishwas <deep.v@getjobber.com>
2021-07-23 15:22:38 -0400

    Affix Input WIP

--- packages/components/src/AffixInput/AffixInput.tsx
+++ packages/components/src/AffixInput/AffixInput.tsx
@@ -1,8 +1,52 @@
 import React from "react";
 import classnames from "classnames";
 import styles from "./AffixInput.css";
+import { Icon } from "../Icon";
+import { FormField, FormFieldProps } from "../FormField";
 
 interface AffixInputProps {
+  /**
+   * Does component have a prefix field
+   * @default false
+   */
+  prefix?: boolean;
+
+  /**
+   * Does component have a postfix field
+   * @default false
+   */
+  postfix?: boolean;
+
+  /**
+   *  Prefix field text if any
+   * @default null
+   */
+  prefixText?: string;
+
+  /**
+   * Postfix fext if any
+   * @default null
+   */
+  postfixText?: string;
+
+  /**
+   * prefix icon name if any
+   * @default null
+   */
+  prefixIcon?: string;
+
+  /**
+   * POstfix icon if any
+   * @default null
+   */
+  postfixIcon?: string;
+
+  /**
+   * Tooltip text if any
+   * @default null
+   */
+  tooltipText?: string;
+
   /**
    * Styles the text bold and uppercased
    * @default false
@@ -10,22 +54,62 @@ interface AffixInputProps {
   readonly loud?: boolean;
 
   /**
-   * Text to display.
+   * MouseOver handler.
    */
-  readonly text: string;
+  onMouseOver?(): void;
 
   /**
-   * Click handler.
+   * Focus handler.
    */
-  onClick?(): void;
+  onFocus?(): void;
+
+  formFieldProps: FormFieldProps;
 }
 
-export function AffixInput({ loud = false, text, onClick }: AffixInputProps) {
-  const className = classnames(styles.affixInput, { [styles.bold]: loud });
+export function AffixInput({
+  prefix,
+  postfix,
+  prefixText,
+  postfixText,
+  prefixIcon,
+  postfixIcon,
+  onMouseOver,
+  onFocus,
+  formFieldProps,
+}: AffixInputProps) {
+  const className = classnames(styles.affixInput, { [styles.bold]: true });
+
+  function isPrefix() {
+    if (prefix) {
+      return (
+        <>
+          <span>{prefixText}</span>
+          <Icon name={prefixIcon} />
+        </>
+      );
+    }
+  }
+
+  function isPostfix() {
+    if (postfix) {
+      return (
+        <>
+          <Icon
+            name={postfixIcon}
+            onMouseOver={onMouseOver}
+            onFocus={onFocus}
+          />
+          <span>{postfixText}</span>
+        </>
+      );
+    }
+  }
 
   return (
-    <div className={className} onClick={onClick}>
-      {text}
+    <div className={className}>
+      {isPrefix}
+      <FormField {...formFieldProps} />
+      {isPostfix}
     </div>
   );
 }
