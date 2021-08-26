401bcb919cc8162849df3a279233e5e119cf536e
Joel Warrington <joel.w@getjobber.com>
2021-08-26 16:24:32 -0600

    refactored tests for input password, added visibility toggle for passwords

--- packages/components/src/InputPassword/InputPassword.test.tsx
+++ packages/components/src/InputPassword/InputPassword.test.tsx
@@ -1,93 +1,57 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import { cleanup, fireEvent, render } from "@testing-library/react";
 import { InputPassword } from ".";
 
 afterEach(cleanup);
 
-it("renders an input type number", () => {
-  const tree = renderer.create(<InputPassword value="123" />).toJSON();
-  expect(tree).toMatchInlineSnapshot(`
-    <div
-      className="wrapper"
-      style={
-        Object {
-          "--formField-maxLength": undefined,
-        }
-      }
-    >
-      <div
-        className="inputWrapper"
-      >
-        <label
-          className="label"
-          htmlFor="123e4567-e89b-12d3-a456-426655440001"
-        />
-        <input
-          className="input"
-          id="123e4567-e89b-12d3-a456-426655440001"
-          onBlur={[Function]}
-          onChange={[Function]}
-          onFocus={[Function]}
-          onKeyDown={[Function]}
-          type="password"
-          value="123"
-        />
-      </div>
-    </div>
-  `);
-});
-
-it("renders an error", () => {
-  const tree = renderer
-    .create(<InputPassword value="p" errorMessage="Not long enough" />)
-    .toJSON();
-  expect(tree).toMatchInlineSnapshot(`
-    <div
-      className="wrapper"
-      style={
-        Object {
-          "--formField-maxLength": undefined,
-        }
-      }
-    >
-      <div
-        className="inputWrapper"
-      >
-        <label
-          className="label"
-          htmlFor="123e4567-e89b-12d3-a456-426655440004"
-        />
-        <input
-          className="input"
-          id="123e4567-e89b-12d3-a456-426655440004"
-          onBlur={[Function]}
-          onChange={[Function]}
-          onFocus={[Function]}
-          onKeyDown={[Function]}
-          type="password"
-          value="p"
-        />
-      </div>
-    </div>
-  `);
-});
-
-test("it should call the handler with a value", () => {
-  const changeHandler = jest.fn();
-  const newValue = "password";
-  const placeholder = "Count";
-
-  const { getByLabelText } = render(
-    <InputPassword
-      onChange={changeHandler}
-      placeholder={placeholder}
-      name={placeholder}
-    />,
-  );
-
-  fireEvent.change(getByLabelText(placeholder), {
-    target: { value: newValue },
+describe("<InputPassword />", () => {
+  it("renders an input type password", () => {
+    const { container } = render(<InputPassword value="123" />);
+    expect(container).toMatchSnapshot();
   });
-  expect(changeHandler).toHaveBeenCalledWith(newValue);
+
+  it("renders a show/hide password button", () => {
+    const { getByLabelText } = render(
+      <InputPassword value="123" hasVisibilityToggle />,
+    );
+    expect(getByLabelText("Input action")).toBeInTheDocument();
+  });
+
+  it("should call the handler with a value", () => {
+    const changeHandler = jest.fn();
+    const newValue = "password";
+    const placeholder = "Count";
+
+    const { getByLabelText } = render(
+      <InputPassword
+        onChange={changeHandler}
+        placeholder={placeholder}
+        name={placeholder}
+      />,
+    );
+
+    fireEvent.change(getByLabelText(placeholder), {
+      target: { value: newValue },
+    });
+    expect(changeHandler).toHaveBeenCalledWith(newValue);
+  });
+
+  describe("hasVisibilityToggle", () => {
+    it("should toggle visible on click", async () => {
+      const { getByLabelText, container } = render(
+        <InputPassword value="password" hasVisibilityToggle />,
+      );
+      // initially should display password input
+      expect(
+        container.querySelector("input[type='password']"),
+      ).toBeInTheDocument();
+
+      // clicking on visibility toggle, should see password
+      fireEvent.click(getByLabelText("Input action"));
+      expect(container.querySelector("input[type='text']")).toBeInTheDocument();
+      expect(
+        container.querySelector("input[type='password']"),
+      ).not.toBeInTheDocument();
+    });
+  });
 });
--- packages/components/src/InputPassword/InputPassword.tsx
+++ packages/components/src/InputPassword/InputPassword.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormField, FormFieldProps } from "../FormField";
 
 /**
@@ -25,10 +25,29 @@ interface InputPasswordProps
   > {
   value?: string;
   onChange?(newValue: string): void;
+  /**
+   * Display toggle to change the visibility of the password input
+   * @default false
+   */
+  hasVisibilityToggle?: boolean;
 }
 
 export function InputPassword(props: InputPasswordProps) {
-  return <FormField {...props} type="password" onChange={handleChange} />;
+  const { hasVisibilityToggle = false } = props;
+  const [visible, setVisibility] = useState(false);
+  return (
+    <FormField
+      {...props}
+      {...(hasVisibilityToggle && {
+        suffix: {
+          icon: visible ? "eye" : "eyeCrossed",
+          onClick: () => setVisibility(!visible),
+        },
+      })}
+      type={visible ? "text" : "password"}
+      onChange={handleChange}
+    />
+  );
 
   function handleChange(newValue: string) {
     props.onChange && props.onChange(newValue);
