169dce7b88d087da4f5d10b2dacd51d76ac1eb39
Darryl Tec <darryl.t@getjobber.com>
2021-09-03 16:06:42 -0600

    code cleanup

--- packages/components/src/Modal/useFocusTrap.ts
+++ packages/components/src/Modal/useFocusTrap.ts
@@ -1,30 +1,26 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * Traps the focus within the children of the ref element.
+ *
+ * @param active - Turns the focus trapping on or off. Also adds aria-hidden on the
+ * body but not the dialog.
+ *
+ * @returns ref
+ */
 export function useFocusTrap<T extends HTMLElement>(active: boolean) {
   // There's an ongoing issue with useRef return type clashing with an element's
   // ref prop type. TLDR: Use null because useRef doesn't expect undefined.
-  // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/35572#issuecomment-498242139
+  // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/35572
   // eslint-disable-next-line no-null/no-null
   const ref = useRef<T>(null);
-  const focusables = [
-    "button",
-    "[href]",
-    "input",
-    "select",
-    "textarea",
-    '[tabindex]:not([tabindex="-1"])',
-  ];
 
   function handleKeyDown(event: KeyboardEvent) {
     if (!(active && ref.current) || event.key !== "Tab") {
       return;
     }
 
-    const elements = ref.current.querySelectorAll<HTMLElement>(
-      focusables.join(", "),
-    );
-    const firstElement = ref.current;
-    const lastElement = elements[elements.length - 1];
+    const { firstElement, lastElement } = getElements(ref.current);
 
     if (event.shiftKey) {
       if (document.activeElement === firstElement) {
@@ -55,4 +51,19 @@ export function useFocusTrap<T extends HTMLElement>(active: boolean) {
   });
 
   return ref;
+}
+
+function getElements<T extends HTMLElement>(ref: T) {
+  const focusables = [
+    "button",
+    "[href]",
+    "input",
+    "select",
+    "textarea",
+    '[tabindex]:not([tabindex="-1"])',
+  ];
+  const elements = ref.querySelectorAll<HTMLElement>(focusables.join(", "));
+  const firstElement = ref;
+  const lastElement = elements[elements.length - 1];
+  return { firstElement, lastElement };
 }
