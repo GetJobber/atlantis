182d0731c674ad3ac7398dd20510e598aea06d94
Darryl Tec <darryl.t@getjobber.com>
2021-09-03 15:50:01 -0600

    add more test

--- packages/components/src/Card/tests/CardClickable.test.tsx
+++ packages/components/src/Card/tests/CardClickable.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { fireEvent, render } from "@testing-library/react";
 import { CardClickable } from "../CardClickable";
 
-test("it should trigger the click event after enter press", () => {
+it("should trigger the click event only after enter press", () => {
   const clickHandler = jest.fn();
 
   const { getByTestId } = render(
@@ -11,11 +11,15 @@ test("it should trigger the click event after enter press", () => {
     </CardClickable>,
   );
 
+  getByTestId("clickable-card").focus();
+  fireEvent.keyUp(getByTestId("clickable-card"), { key: "Tab" });
+  expect(clickHandler).toHaveBeenCalledTimes(0);
+
   fireEvent.keyUp(getByTestId("clickable-card"), { key: "Enter" });
   expect(clickHandler).toHaveBeenCalledTimes(1);
 });
 
-test("it should trigger the click event after spacebar press", () => {
+it("should trigger the click event only after spacebar press", () => {
   const clickHandler = jest.fn();
 
   const { getByTestId } = render(
@@ -24,6 +28,38 @@ test("it should trigger the click event after spacebar press", () => {
     </CardClickable>,
   );
 
+  getByTestId("clickable-card").focus();
+  fireEvent.keyUp(getByTestId("clickable-card"), { key: "Shift" });
+  expect(clickHandler).toHaveBeenCalledTimes(0);
+
   fireEvent.keyUp(getByTestId("clickable-card"), { key: " " });
   expect(clickHandler).toHaveBeenCalledTimes(1);
+});
+
+it("should not trigger the click eventon eneter press when the card is not focused", () => {
+  const clickHandler = jest.fn();
+
+  const { getByTestId } = render(
+    <CardClickable className="boink" onClick={clickHandler}>
+      <p>Press enter</p>
+    </CardClickable>,
+  );
+
+  getByTestId("clickable-card").blur();
+  fireEvent.keyUp(getByTestId("clickable-card"), { key: "Enter" });
+  expect(clickHandler).toHaveBeenCalledTimes(0);
+});
+
+it("should not trigger the click event on spacebar tap when the card is not focused", () => {
+  const clickHandler = jest.fn();
+
+  const { getByTestId } = render(
+    <CardClickable className="boink" onClick={clickHandler}>
+      <p>Press spacebar</p>
+    </CardClickable>,
+  );
+
+  getByTestId("clickable-card").blur();
+  fireEvent.keyUp(getByTestId("clickable-card"), { key: " " });
+  expect(clickHandler).toHaveBeenCalledTimes(0);
 });
