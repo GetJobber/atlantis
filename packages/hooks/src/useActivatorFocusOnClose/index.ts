943981e33c569e5c680ced28e763d48d8261dd6a
Darryl Tec <darryl.t@getjobber.com>
2021-09-07 12:06:09 -0600

    create da hook

---
+++ packages/hooks/src/useActivatorFocusOnClose/index.ts
@@ -0,0 +1,1 @@
+export { useActivatorFocusOnClose } from "./useActivatorFocusOnClose";
---
+++ packages/hooks/src/useActivatorFocusOnClose/useActivatorFocusOnClose.ts
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+let activator: Element | null | undefined;
+
+/**
+ * Brings back the focus to the element that opened an overlaid element once
+ * said overlaid element is dismissed.
+ *
+ * @param active - Determines if it should focus or not
+ */
+export function useActivatorFocusOnClose(active: boolean) {
+  useEffect(() => {
+    return () => {
+      if (!activator) {
+        activator = document.activeElement;
+      }
+
+      if (active) {
+        // Wait for other DOM changes before focusing on the activator
+        setTimeout(() => {
+          if (activator instanceof HTMLElement) {
+            activator.focus();
+          }
+          activator = undefined;
+        }, 0);
+      }
+    };
+  }, [active]);
+}
