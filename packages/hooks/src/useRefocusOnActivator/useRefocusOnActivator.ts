a9424ea8687f6c714191bcfc25ac0ec3fd57ac7e
Darryl Tec <darryl.t@getjobber.com>
2021-09-09 11:37:57 -0600

    allow hook to work on nested component

--- packages/hooks/src/useRefocusOnActivator/useRefocusOnActivator.ts
+++ packages/hooks/src/useRefocusOnActivator/useRefocusOnActivator.ts
@@ -1,7 +1,5 @@
 import { useEffect } from "react";
 
-let activator: Element | null | undefined;
-
 /**
  * Brings back the focus to the element that opened an overlaid element once
  * said overlaid element is dismissed.
@@ -10,16 +8,22 @@ let activator: Element | null | undefined;
  */
 export function useRefocusOnActivator(active: boolean) {
   useEffect(() => {
+    let activator: Element | null | undefined;
+
     if (active && !activator) {
       activator = document.activeElement;
-    } else {
-      // Wait for other DOM changes before focusing on the activator
-      setTimeout(() => {
-        if (activator instanceof HTMLElement) {
-          activator.focus();
-        }
-        activator = undefined;
-      }, 0);
     }
+
+    return () => {
+      if (active) {
+        // Wait for other DOM changes before focusing on the activator
+        setTimeout(() => {
+          if (activator instanceof HTMLElement) {
+            activator.focus();
+          }
+          activator = undefined;
+        }, 0);
+      }
+    };
   }, [active]);
 }
