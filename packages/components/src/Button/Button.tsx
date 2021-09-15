7e64549a07e9a58048590a33f3f6dbab34118c5f
eddysims <eddy.s@getjobber.com>
2021-09-15 17:15:24 -0600

    test a prerelease

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -157,6 +157,8 @@ export function Button(props: ButtonProps) {
   return <Tag {...tagProps}>{buttonInternals}</Tag>;
 }
 
+console.log("button");
+
 function ButtonInternals({ label, icon, size = "base" }: ButtonProps) {
   return (
     <>
