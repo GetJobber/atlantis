640e00dbed1f0747d7f2bab79612d8a410095dfd
Darryl Tec <darryl.t@getjobber.com>
2021-09-21 16:38:12 -0600

    add icon chip
    change color of icon when it's active
    
    add chip icon props on mdx

--- packages/components/src/Chip/Chip.tsx
+++ packages/components/src/Chip/Chip.tsx
@@ -1,11 +1,12 @@
 import React, { ReactElement } from "react";
 import classnames from "classnames";
+import { XOR } from "ts-xor";
 import styles from "./Chip.css";
 import { ChipAvatar, ChipAvatarProps } from "./ChipAvatar";
+import { ChipIcon, ChipIconProps } from "./ChipIcon";
 import { Typography } from "../Typography";
 
-interface ChipProps {
-  readonly avatar?: ReactElement<ChipAvatarProps>;
+interface ChipBaseProps {
   /**
    * Label of the chip.
    */
@@ -27,16 +28,29 @@ interface ChipProps {
   onClick?(event: React.MouseEvent<HTMLDivElement>): void;
 }
 
+interface ChipWithAvatarProps extends ChipBaseProps {
+  readonly avatar?: ReactElement<ChipAvatarProps>;
+}
+
+interface ChipWithIconProps extends ChipBaseProps {
+  readonly icon?: ReactElement<ChipIconProps>;
+}
+
+type ChipProps = XOR<ChipWithAvatarProps, ChipWithIconProps>;
+
 export function Chip({
   avatar,
+  icon,
   label,
   active = false,
   disabled = false,
   onClick,
 }: ChipProps) {
-  console.log(avatar?.type);
   if (avatar && avatar.type !== ChipAvatar) {
     throw new Error("`avatar` prop only accepts `<ChipAvatar />` component");
+  }
+  if (icon && icon.type !== ChipIcon) {
+    throw new Error("`icon` prop only accepts `<ChipIconProps />` component");
   }
 
   const isClickable = onClick && !disabled;
@@ -44,7 +58,7 @@ export function Chip({
     [styles.clickable]: isClickable,
     [styles.active]: active,
     [styles.disabled]: disabled,
-    [styles.hasPrefix]: avatar,
+    [styles.hasPrefix]: avatar || icon,
   });
 
   const props = {
@@ -59,6 +73,7 @@ export function Chip({
 
   return (
     <div {...props}>
+      {icon && active ? React.cloneElement(icon, { color: "white" }) : icon}
       {avatar}
       <Typography element="span" size="base">
         {label}
