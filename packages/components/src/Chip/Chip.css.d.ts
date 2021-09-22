99b80da17327f592b98e5ed5f213ffd703557bee
Darryl Tec <darryl.t@getjobber.com>
2021-09-22 13:47:43 -0600

    allow suffix and limit it to ChipIcon

--- packages/components/src/Chip/Chip.css.d.ts
+++ packages/components/src/Chip/Chip.css.d.ts
@@ -1,5 +1,6 @@
 export const chip: string;
 export const hasPrefix: string;
+export const hasSuffix: string;
 export const clickable: string;
 export const active: string;
 export const invalid: string;
