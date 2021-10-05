884e33b51233fd657d3120f6f2d75398185b4b82
Shreya Vijay <shreyavi@usc.edu>
2021-10-05 11:23:09 -0700

    remove screen from test

--- packages/components/src/Card/tests/Card.test.tsx
+++ packages/components/src/Card/tests/Card.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { Card } from "..";
 
 it("renders a simple card", () => {
