80f1d25f0479375b73784f21fd4877fa94e3fc99
Eddy Sims <46460840+eddysims@users.noreply.github.com>
2021-11-16 14:28:11 -0700

    feat(components): DatePicker component (#774)
    * installs react-datepicker
    
    * generates the DatePicker
    
    * Adds the DatePicker component
    
    * adds a custom header to the DatePicker
    
    * Apply datepicker patch, fix dependencies
    
    * Added CSS for dates
    
    * Add transform/styling to weekdays
    
    * Add more styling
    
    * fix the package-lock
    
    * run patch from root
    
    * run the bootstrap after the patch
    
    * clean up the patch
    
    * change cache version
    
    * npx patch package
    
    * make types package a dep
    
    * move patch to prebuild
    
    * remove some unneeded stuffs
    
    * update datepicker docs
    
    * update related components
    
    * Address initial comments
    
    * Add snapshot
    
    * Make button tertiary and work
    
    * Focuses on the selected date
    
    * Add ref to control focus
    
    * Change selected date check
    
    * Add inline-block styling for parent wrappern
    
    * swap global for styles in wrapper css
    
    * Rename wrapper div
    
    * Add blurb on date object
    
    * Add blurb on date object
    
    * Make user handle state of component
    
    * Fix docstring
    
    * Remove snapshot
    
    Co-authored-by: Adam Lefaivre <adam.le@getjobber.com>
    Co-authored-by: Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>

---
+++ packages/components/src/DatePicker/DatePicker.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "datePickerWrapper": string;
+  readonly "datePicker": string;
+  readonly "inline": string;
+  readonly "header": string;
+  readonly "month": string;
+};
+export = styles;
+
---
+++ packages/components/src/DatePicker/DatePicker.tsx
@@ -0,0 +1,93 @@
+import React, { ReactElement, RefObject, useRef } from "react";
+import classnames from "classnames";
+import ReactDatePicker from "react-datepicker";
+/**
+ * Disabling no-internal-modules here because we need
+ * to reach into the package to get the css file.
+ */
+// eslint-disable-next-line import/no-internal-modules
+import "react-datepicker/dist/react-datepicker.css";
+import { XOR } from "ts-xor";
+import styles from "./DatePicker.css";
+import { DatePickerCustomHeader } from "./DatePickerCustomHeader";
+import { Button } from "../Button";
+
+interface BaseDatePickerProps {
+  /**
+   * The selected Date object
+   */
+  readonly selected: Date;
+
+  /**
+   * Change handler that will return the date selected.
+   */
+  onChange(val: Date): void;
+}
+
+interface DatePickerModalProps extends BaseDatePickerProps {
+  /**
+   * Use a custom activator to trigger the DatePicker
+   */
+  readonly activator?: ReactElement;
+}
+
+interface DatePickerInlineProps extends BaseDatePickerProps {
+  readonly inline?: boolean;
+}
+
+type DatePickerProps = XOR<DatePickerModalProps, DatePickerInlineProps>;
+
+export function DatePicker({
+  onChange,
+  activator,
+  inline,
+  selected,
+}: DatePickerProps) {
+  const datePickerClassNames = classnames(styles.datePicker, {
+    [styles.inline]: inline,
+  });
+
+  const datePickerRef = useRef() as RefObject<HTMLDivElement>;
+
+  return (
+    <div className={styles.datePickerWrapper} ref={datePickerRef}>
+      <ReactDatePicker
+        calendarClassName={datePickerClassNames}
+        showPopperArrow={false}
+        selected={selected}
+        inline={inline}
+        onChange={handleChange}
+        formatWeekDay={date => date.substr(0, 3)}
+        customInput={
+          activator ? (
+            activator
+          ) : (
+            <Button
+              variation="work"
+              type="tertiary"
+              icon="calendar"
+              ariaLabel="Open Datepicker"
+            />
+          )
+        }
+        renderCustomHeader={props => <DatePickerCustomHeader {...props} />}
+        onCalendarOpen={focusSelectedDate}
+      />
+    </div>
+  );
+
+  function handleChange(val: Date) {
+    onChange && onChange(val);
+  }
+
+  function focusSelectedDate() {
+    const selectedDateClass = ".react-datepicker__day--selected";
+
+    const selectedDate =
+      datePickerRef.current?.querySelector(selectedDateClass);
+
+    if (selectedDate instanceof HTMLDivElement) {
+      selectedDate.focus();
+    }
+  }
+}
