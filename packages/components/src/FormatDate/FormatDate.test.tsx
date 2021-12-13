831fcf6d09f1272c180227997395248b4d9a73c8
Darryl Tec <darryl.t@getjobber.com>
2021-12-13 11:57:45 -0700

    update FormatDate test to include locale

--- packages/components/src/FormatDate/FormatDate.test.tsx
+++ packages/components/src/FormatDate/FormatDate.test.tsx
@@ -2,17 +2,51 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { cleanup } from "@testing-library/react";
 import { CivilDate } from "@std-proposal/temporal";
-import { FormatDate } from "./FormatDate";
+import { FormatDate, strFormatDate } from "./FormatDate";
 
 afterEach(cleanup);
 
-Object.entries({
-  CivilDate: new CivilDate(2020, 2, 26),
-  ISO8601DateString: "2019-03-30T00:45",
-  Date: new Date("2019-03-30T00:45"),
-}).forEach(([inputType, value]) => {
-  it(`renders a FormatDate from ${inputType}`, () => {
-    const tree = renderer.create(<FormatDate date={value} />).toJSON();
-    expect(tree).toMatchSnapshot();
+describe("Different date values", () => {
+  const dates = {
+    CivilDate: new CivilDate(2020, 2, 26),
+    ISO8601DateString: "2019-03-30T00:45",
+    Date: new Date("2019-03-30T00:45"),
+  };
+
+  const mockDateResult = [`Feb 26, 2020`, `Mar 30, 2019`, `Mar 30, 2019`];
+
+  Object.entries(dates).forEach(([inputType, value], index) => {
+    it(`renders a formatted date from ${inputType}`, () => {
+      const tree = renderer.create(<FormatDate date={value} />).toJSON();
+      expect(tree).toMatchInlineSnapshot(
+        `"${strFormatDate(new Date(mockDateResult[index]))}"`,
+      );
+    });
+  });
+});
+
+describe("strFormatDate", () => {
+  const expectedDateString = "Jan 4, 1992";
+  const expectedDate = new Date(expectedDateString);
+  const mockLocale = undefined;
+  const mockLocaleDateFormat: Intl.DateTimeFormatOptions = {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  };
+
+  it("should return the right locale date format", () => {
+    const mockDateSpy = jest.spyOn(Date.prototype, "toLocaleDateString");
+    strFormatDate(new Date(expectedDate));
+    expect(mockDateSpy).toHaveBeenCalledWith(mockLocale, mockLocaleDateFormat);
+  });
+
+  it("should return the proper date format", () => {
+    expect(strFormatDate(new Date(expectedDate))).toBe(
+      new Date(expectedDateString).toLocaleDateString(
+        mockLocale,
+        mockLocaleDateFormat,
+      ),
+    );
   });
 });
