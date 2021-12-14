70e79619cb363f8cd85255cc362a0d84c5489b06
Darryl Tec <darryl.t@getjobber.com>
2021-12-14 14:07:17 -0700

    chore(deps): Upgrade atlantis to node 14 (#796)
    * bump nvmrc to 14
    
    * update package lock
    
    * Update README.md
    
    * bump node in circle
    
    * Update package-lock.json
    
    * update FormatDate test to include locale
    
    * update FormatRelativeTime tests
    
    * set system time instead of mocking what date returns
    
    * code cleanup
    
    * Update FormatDate.test.tsx

--- packages/components/src/FormatRelativeDateTime/FormatRelativeDateTime.test.tsx
+++ packages/components/src/FormatRelativeDateTime/FormatRelativeDateTime.test.tsx
@@ -4,141 +4,129 @@ import { cleanup } from "@testing-library/react";
 import { CivilDateTime } from "@std-proposal/temporal";
 import { FormatRelativeDateTime } from "./FormatRelativeDateTime";
 
-afterEach(cleanup);
 beforeEach(() => {
-  Date.now = jest.fn(() => 1593115122000);
+  jest.useFakeTimers("modern");
+  jest.setSystemTime(new Date(1593115122000));
+});
+
+afterEach(() => {
+  jest.useRealTimers();
+  cleanup();
 });
 
 it("renders x minutes ago when less than an hour ago", () => {
-  const testDate = new Date(Date.now());
+  const testDate = new Date();
   testDate.setMinutes(testDate.getMinutes() - 5);
+  const dates = getMockDates(testDate);
 
-  const civilTestDate = getCivilTime(testDate);
-  const isoTestDate = getISOString(testDate);
-
-  expect(
-    renderer.create(<FormatRelativeDateTime date={civilTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot(`"5 minutes ago"`);
-  expect(
-    renderer.create(<FormatRelativeDateTime date={isoTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot(`"5 minutes ago"`);
-  expect(
-    renderer.create(<FormatRelativeDateTime date={testDate} />).toJSON(),
-  ).toMatchInlineSnapshot(`"5 minutes ago"`);
+  Object.values(dates).forEach(value => {
+    expect(
+      renderer.create(<FormatRelativeDateTime date={value} />).toJSON(),
+    ).toEqual("5 minutes ago");
+  });
 });
 
 it("renders 1 minute ago when less than a minute ago", () => {
-  const testDate = new Date(Date.now());
+  const testDate = new Date();
   testDate.setSeconds(testDate.getSeconds() - 25);
+  const dates = getMockDates(testDate);
 
-  const civilTestDate = getCivilTime(testDate);
-  const isoTestDate = getISOString(testDate);
-
-  expect(
-    renderer.create(<FormatRelativeDateTime date={civilTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot(`"1 minute ago"`);
-  expect(
-    renderer.create(<FormatRelativeDateTime date={isoTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot(`"1 minute ago"`);
-  expect(
-    renderer.create(<FormatRelativeDateTime date={testDate} />).toJSON(),
-  ).toMatchInlineSnapshot(`"1 minute ago"`);
+  Object.values(dates).forEach(value => {
+    expect(
+      renderer.create(<FormatRelativeDateTime date={value} />).toJSON(),
+    ).toEqual("1 minute ago");
+  });
 });
 
 it("renders the time when less than a day ago", () => {
-  const testDate = new Date(Date.now());
+  const testDate = new Date();
   testDate.setHours(testDate.getHours() - 9);
+  const dates = getMockDates(testDate);
 
-  const hours = testDate.getHours();
-  const minutes = testDate.getMinutes();
-  const expectedStr = hours + ":" + minutes + " AM";
-
-  const civilTestDate = getCivilTime(testDate);
-  const isoTestDate = getISOString(testDate);
-
-  expect(
-    renderer.create(<FormatRelativeDateTime date={civilTestDate} />).toJSON(),
-  ).toEqual(expectedStr);
-  expect(
-    renderer.create(<FormatRelativeDateTime date={isoTestDate} />).toJSON(),
-  ).toEqual(expectedStr);
-  expect(
-    renderer.create(<FormatRelativeDateTime date={testDate} />).toJSON(),
-  ).toEqual(expectedStr);
+  Object.values(dates).forEach(value => {
+    expect(
+      renderer.create(<FormatRelativeDateTime date={value} />).toJSON(),
+    ).toEqual(
+      testDate.toLocaleTimeString(undefined, {
+        hour: "numeric",
+        minute: "numeric",
+      }),
+    );
+  });
 });
 
 it("renders the day when less than 7 days ago", () => {
-  const testDate = new Date(Date.now());
+  const testDate = new Date();
   testDate.setDate(testDate.getDate() - 3);
+  const dates = getMockDates(testDate);
 
-  const civilTestDate = getCivilTime(testDate);
-  const isoTestDate = getISOString(testDate);
-
-  expect(
-    renderer.create(<FormatRelativeDateTime date={civilTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Mon"');
-  expect(
-    renderer.create(<FormatRelativeDateTime date={isoTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Mon"');
-  expect(
-    renderer.create(<FormatRelativeDateTime date={testDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Mon"');
+  Object.values(dates).forEach(value => {
+    expect(
+      renderer.create(<FormatRelativeDateTime date={value} />).toJSON(),
+    ).toEqual(testDate.toLocaleDateString(undefined, { weekday: "short" }));
+  });
 });
 
 it("renders the month and date when less than 1 year ago", () => {
-  const testDate = new Date(Date.now());
+  const testDate = new Date();
   testDate.setDate(testDate.getDate() - 60);
+  const dates = getMockDates(testDate);
 
-  const civilTestDate = getCivilTime(testDate);
-  const isoTestDate = getISOString(testDate);
-
-  expect(
-    renderer.create(<FormatRelativeDateTime date={civilTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Apr 26"');
-  expect(
-    renderer.create(<FormatRelativeDateTime date={isoTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Apr 26"');
-  expect(
-    renderer.create(<FormatRelativeDateTime date={testDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Apr 26"');
+  Object.values(dates).forEach(value => {
+    expect(
+      renderer.create(<FormatRelativeDateTime date={value} />).toJSON(),
+    ).toEqual(
+      new Date("Apr 26").toLocaleDateString(undefined, {
+        month: "short",
+        day: "numeric",
+      }),
+    );
+  });
 });
 
 it("renders the month and date when yesterday's date 1 year previous (border case)", () => {
-  const testDate = new Date(Date.now());
+  const testDate = new Date();
   testDate.setFullYear(testDate.getFullYear() - 1);
   testDate.setDate(testDate.getDate() + 2); //The +2 vs. +1 is a fudge for leap year
+  const dates = getMockDates(testDate);
 
-  const civilTestDate = getCivilTime(testDate);
-  const isoTestDate = getISOString(testDate);
-
-  expect(
-    renderer.create(<FormatRelativeDateTime date={civilTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Jun 27"');
-  expect(
-    renderer.create(<FormatRelativeDateTime date={isoTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Jun 27"');
-  expect(
-    renderer.create(<FormatRelativeDateTime date={testDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Jun 27"');
+  Object.values(dates).forEach(value => {
+    expect(
+      renderer.create(<FormatRelativeDateTime date={value} />).toJSON(),
+    ).toEqual(
+      new Date("Jun 27").toLocaleDateString(undefined, {
+        month: "short",
+        day: "numeric",
+      }),
+    );
+  });
 });
 
 it("renders the month day, year when over a year ago", () => {
-  const testDate = new Date(Date.now());
+  const testDate = new Date();
   testDate.setFullYear(testDate.getFullYear() - 2);
+  const dates = getMockDates(testDate);
 
-  const civilTestDate = getCivilTime(testDate);
-  const isoTestDate = getISOString(testDate);
-
-  expect(
-    renderer.create(<FormatRelativeDateTime date={civilTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Jun 25, 2018"');
-  expect(
-    renderer.create(<FormatRelativeDateTime date={isoTestDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Jun 25, 2018"');
-  expect(
-    renderer.create(<FormatRelativeDateTime date={testDate} />).toJSON(),
-  ).toMatchInlineSnapshot('"Jun 25, 2018"');
+  Object.values(dates).forEach(value => {
+    expect(
+      renderer.create(<FormatRelativeDateTime date={value} />).toJSON(),
+    ).toEqual(
+      new Date("Jun 25, 2018").toLocaleDateString(undefined, {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+      }),
+    );
+  });
 });
+
+function getMockDates(date: Date) {
+  return {
+    Date: date,
+    CivilDate: getCivilTime(date),
+    ISO8601DateString: getISOString(date),
+  };
+}
 
 function getISOString(date: Date) {
   return date.toISOString();
