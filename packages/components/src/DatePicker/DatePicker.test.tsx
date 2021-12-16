cf28a7073e7a10670612046e4b2eb8012503643b
Darryl Tec <darryl.t@getjobber.com>
2021-12-16 14:38:32 -0700

    chore(components): Add test for datepicker to make sure custom css doesnt fail (#808)
    * chore(components): add tests on the class names we're overwriting
    
    * ensure popper gets popped
    
    * match pattern with other awaits
    
    * do some 1 liner pro move

--- packages/components/src/DatePicker/DatePicker.test.tsx
+++ packages/components/src/DatePicker/DatePicker.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { cleanup, fireEvent, render } from "@testing-library/react";
+import { act, cleanup, fireEvent, render } from "@testing-library/react";
+import ReactDatePicker from "react-datepicker";
 import { DatePicker } from "./DatePicker";
 
 afterEach(cleanup);
@@ -33,17 +34,13 @@ it("returns the dates from onChange", async () => {
   const { getByTestId, getByText } = render(
     <DatePicker selected={new Date()} onChange={changeHandler} />,
   );
-
-  const button = getByTestId("calendar");
-  fireEvent.click(button);
-
-  const date = getByText("15");
-  fireEvent.click(date);
+  await popperUpdate(() => fireEvent.click(getByTestId("calendar")));
+  await popperUpdate(() => fireEvent.click(getByText("15")));
 
   expect(changeHandler).toHaveBeenCalledWith(expect.any(Date));
 });
 
-it("allows for a custom activator to open the DatePicker", () => {
+it("allows for a custom activator to open the DatePicker", async () => {
   const { getByText } = render(
     <DatePicker
       selected={new Date()}
@@ -51,9 +48,7 @@ it("allows for a custom activator to open the DatePicker", () => {
       activator={<div>Activate me</div>}
     />,
   );
-
-  const button = getByText("Activate me");
-  fireEvent.click(button);
+  await popperUpdate(() => fireEvent.click(getByText("Activate me")));
 
   expect(getByText("15")).toBeInstanceOf(HTMLDivElement);
 });
@@ -77,3 +72,48 @@ it("should not add the `react-datepicker-ignore-onclickoutside` when inline", ()
 
   expect(target).not.toHaveClass(className);
 });
+
+describe("Ensure ReactDatePicker CSS class names exists", () => {
+  it("should have the click outside class", async () => {
+    const { getByRole } = render(<ReactDatePicker onChange={jest.fn} />);
+    const input = getByRole("textbox");
+    const className = "react-datepicker-ignore-onclickoutside";
+
+    expect(input).not.toHaveClass(className);
+    await popperUpdate(() => fireEvent.focus(input));
+    expect(input).toHaveClass(className);
+  });
+
+  describe("Overwritten CSS class name", () => {
+    const classNames = [
+      ".react-datepicker",
+      ".react-datepicker__input-container",
+      ".react-datepicker-wrapper",
+      ".react-datepicker-popper",
+      ".react-datepicker__header",
+      ".react-datepicker__month",
+      ".react-datepicker__day",
+      ".react-datepicker__day--outside-month",
+      ".react-datepicker__day--selected",
+      ".react-datepicker__day-names",
+      ".react-datepicker__day-name",
+    ];
+
+    classNames.forEach(className => {
+      it(`should have ${className}`, async () => {
+        const { getByRole, container } = render(
+          <ReactDatePicker selected={new Date()} onChange={jest.fn} />,
+        );
+        await popperUpdate(() => fireEvent.focus(getByRole("textbox")));
+        expect(container.querySelector(className)).toBeTruthy();
+      });
+    });
+  });
+});
+
+async function popperUpdate(event: Function) {
+  event();
+  // Wait for the Popper update() so jest doesn't throw an act warning
+  // https://github.com/popperjs/react-popper/issues/350
+  await act(async () => undefined);
+}
