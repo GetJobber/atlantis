fb76f55f78e8535b10bf96dc272c469cc9e0954a
Darryl Tec <darryl.t@getjobber.com>
2022-01-20 15:02:30 -0700

    Revert "example with useCollectionQuery"
    This reverts commit 3ea93de30d4559e8c0c4921a0f6d3f601cbaf1a0.

--- packages/components/src/Chips/TestChip.tsx
+++ packages/components/src/Chips/TestChip.tsx
@@ -2,15 +2,8 @@
 // TODO: Move to MDX playground as an example
 
 import React, { useState } from "react";
-import { useCollectionQuery } from "@jobber/hooks";
-import {
-  LIST_QUERY,
-  ListQueryType,
-  apolloClient,
-  getLoadingState,
-} from "./utils";
-import { Chip, Chips } from "..";
-import { Avatar } from "../../Avatar";
+import { Chip, Chips } from ".";
+import { Avatar } from "../Avatar";
 
 export function TestChip() {
   const [options, setOptions] = useState([
@@ -34,46 +27,6 @@ export function TestChip() {
   ]);
   const [selected, setSelected] = useState([options[0]]);
 
-  const {
-    data,
-    error,
-    refresh,
-    nextPage,
-    loadingRefresh,
-    loadingNextPage,
-    loadingInitialContent,
-  } = useCollectionQuery<ListQueryType>({
-    // useCollectionQuery should be called with the query type, and
-    // optionally, the subscription type. The playground errors with
-    // typing included, so typing has been removed in this example.
-    // Please see the first example for appropriate typing.
-    query: LIST_QUERY,
-    queryOptions: {
-      fetchPolicy: "network-only",
-      nextFetchPolicy: "cache-first",
-      // eslint-disable-next-line
-      // @ts-ignore
-      client: apolloClient,
-    },
-    getCollectionByPath(items) {
-      return items && items.allPlanets;
-    },
-  });
-
-  const { loadingStatus, loading } = getLoadingState(
-    loadingInitialContent,
-    loadingRefresh,
-    loadingNextPage,
-  );
-
-  let items: string[] = [];
-
-  if (data) {
-    items = data.allPlanets.edges.map(edge => {
-      return edge.node.name;
-    });
-  }
-
   return (
     <Chips
       type="dismissible"
@@ -82,9 +35,9 @@ export function TestChip() {
       onCustomAdd={handleCustomAdd}
       onClick={(_, v) => alert(v)}
       onSearch={v => console.log(v)}
-      onLoadMore={() => nextPage()}
+      onLoadMore={v => console.log(v)}
     >
-      {items.map(name => (
+      {options.map(name => (
         <Chip
           key={name}
           prefix={<Avatar initials={getInitials(name)} />}
