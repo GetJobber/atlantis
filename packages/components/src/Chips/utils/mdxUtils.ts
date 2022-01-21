ef6377719be969de55872e2ce9d56c5024bf414a
Darryl Tec <darryl.t@getjobber.com>
2022-01-21 08:52:11 -0700

    first pass on search callback example

--- packages/components/src/Chips/utils/mdxUtils.ts
+++ packages/components/src/Chips/utils/mdxUtils.ts
@@ -1,30 +1,46 @@
+import { uniq } from "lodash";
 import { useEffect, useState } from "react";
 
 export function useFakeOptionQuery() {
   const [options, setOptions] = useState<string[]>([]);
-  const [next, setNext] = useState(
-    "https://swapi.dev/api/planets/?format=json",
-  );
+  const initialDataUrl = "https://swapi.dev/api/planets/?format=json";
+  const [next, setNext] = useState(initialDataUrl);
   const [loading, setLoading] = useState(false);
+  const [selected, setSelected] = useState(["Tatooine"]);
 
   const actions = {
-    loadMore: () => {
-      if (loading) return;
+    handleLoadMore: () => {
+      if (loading || !next) return;
 
       setLoading(true);
       fetchPlanets(next).then(result => {
-        setOptions([...options, ...result.options]);
+        const newOptions = uniq([...selected, ...options, ...result.options]);
+        setOptions(newOptions);
         setNext(result.next);
         setLoading(false);
       });
     },
+    handleSearch: (searchValue: string) => {
+      setNext(initialDataUrl + `&search=${searchValue}`);
+      setOptions(selected);
+    },
+    handleSelect: (value: string[]) => {
+      setSelected(value);
+    },
+    handleCustomAdd: (value: string) => {
+      setSelected([...selected, value]);
+    },
   };
 
   useEffect(() => {
-    actions.loadMore();
+    actions.handleLoadMore();
+  }, [next]);
+
+  useEffect(() => {
+    actions.handleLoadMore();
   }, []);
 
-  return { loading, options, ...actions };
+  return { selected, options, loading, ...actions };
 }
 
 async function fetchPlanets(url: string) {
