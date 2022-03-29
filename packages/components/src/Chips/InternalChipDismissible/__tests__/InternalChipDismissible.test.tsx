77ffd87f13a7bed5eeea15c5b3021c002d6c3ed7
Adam Lefaivre <adam.le@getjobber.com>
2022-03-29 15:41:03 -0600

    Wrap current chip tests in separate describe

--- packages/components/src/Chips/InternalChipDismissible/__tests__/InternalChipDismissible.test.tsx
+++ packages/components/src/Chips/InternalChipDismissible/__tests__/InternalChipDismissible.test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   act,
   cleanup,
@@ -22,185 +22,229 @@ const handleClickChip = jest.fn((_, value) => value);
 const handleCustomAdd = jest.fn(value => value);
 const handleSearch = jest.fn(value => value);
 const handleLoadMore = jest.fn(value => value);
-
-const chips = ["Amazing", "Fabulous", "Magical"];
-const selectedChips = ["Amazing"];
 const voiceOver = " Press delete to remove chip";
 
+describe("Basic interaction", () => {
+  const chips = ["Amazing", "Fabulous", "Magical"];
+  const selectedChips = ["Amazing"];
 
-beforeEach(async () => {
-  await popperUpdate();
+  beforeEach(async () => {
+    await popperUpdate();
 
-  render(
-    <InternalChipDismissible
-      selected={selectedChips}
-      onChange={handleChange}
-      onCustomAdd={handleCustomAdd}
-      onClick={handleClickChip}
-      onSearch={handleSearch}
-      onLoadMore={handleLoadMore}
-    >
-      {chips.map(chip => (
-        <Chip key={chip} label={chip} value={chip} />
-      ))}
-    </InternalChipDismissible>,
-  );
+    render(
+      <InternalChipDismissible
+        selected={selectedChips}
+        onChange={handleChange}
+        onCustomAdd={handleCustomAdd}
+        onClick={handleClickChip}
+        onSearch={handleSearch}
+        onLoadMore={handleLoadMore}
+      >
+        {chips.map(chip => (
+          <Chip key={chip} label={chip} value={chip} />
+        ))}
+      </InternalChipDismissible>,
+    );
 
-  await popperUpdate();
-});
-
-afterEach(cleanup);
-
-it("should only render the selected chip on the UI", () => {
-  const wrapperEl = screen.getByTestId("dismissible-chips");
-
-  selectedChips.forEach(chip => {
-    expect(within(wrapperEl).queryByText(chip)).toBeInTheDocument();
+    await popperUpdate();
   });
 
-  chips
-    .filter(chip => !selectedChips.includes(chip))
-    .forEach(chip => {
-      expect(within(wrapperEl).queryByText(chip)).not.toBeInTheDocument();
-    });
-});
+  afterEach(cleanup);
 
-describe("Open Menu", () => {
-  let wrapperEl: HTMLElement;
+  it("should only render the selected chip on the UI", () => {
+    const wrapperEl = screen.getByTestId("dismissible-chips");
 
-  beforeEach(() => {
-    fireEvent.click(screen.getByLabelText("Add"));
-    wrapperEl = screen.getByRole("listbox");
-  });
-
-  it("should not render the selected chips on the menu", () => {
     selectedChips.forEach(chip => {
-      expect(within(wrapperEl).queryByText(chip)).not.toBeInTheDocument();
+      expect(within(wrapperEl).queryByText(chip)).toBeInTheDocument();
     });
 
     chips
       .filter(chip => !selectedChips.includes(chip))
       .forEach(chip => {
-        expect(within(wrapperEl).queryByText(chip)).toBeInTheDocument();
+        expect(within(wrapperEl).queryByText(chip)).not.toBeInTheDocument();
       });
   });
 
-  it("should trigger the onChange callback when you select from available options", () => {
-    const target = chips[chips.length - 1];
-    fireEvent.click(screen.getByText(target));
+  describe("Open Menu", () => {
+    let wrapperEl: HTMLElement;
 
-    expect(handleChange).toHaveBeenCalledWith([...selectedChips, target]);
-    expect(handleCustomAdd).not.toHaveBeenCalled();
-  });
-
-  it("should trigger the onSearch callback when you type on the input", async () => {
-    const value = "🌮";
-    fireEvent.change(screen.getByRole("combobox"), {
-      target: { value: value },
+    beforeEach(() => {
+      fireEvent.click(screen.getByLabelText("Add"));
+      wrapperEl = screen.getByRole("listbox");
     });
 
-    await waitFor(() => expect(handleSearch).toHaveBeenCalledWith(value));
+    it("should not render the selected chips on the menu", () => {
+      selectedChips.forEach(chip => {
+        expect(within(wrapperEl).queryByText(chip)).not.toBeInTheDocument();
+      });
+
+      chips
+        .filter(chip => !selectedChips.includes(chip))
+        .forEach(chip => {
+          expect(within(wrapperEl).queryByText(chip)).toBeInTheDocument();
+        });
+    });
+
+    it("should trigger the onChange callback when you select from available options", () => {
+      const target = chips[chips.length - 1];
+      fireEvent.click(screen.getByText(target));
+
+      expect(handleChange).toHaveBeenCalledWith([...selectedChips, target]);
+      expect(handleCustomAdd).not.toHaveBeenCalled();
+    });
+
+    it("should trigger the onSearch callback when you type on the input", async () => {
+      const value = "🌮";
+      fireEvent.change(screen.getByRole("combobox"), {
+        target: { value: value },
+      });
+
+      await waitFor(() => expect(handleSearch).toHaveBeenCalledWith(value));
+    });
+
+    it("should only trigger the onCustomAdd callback when you select the custom option", async () => {
+      const value = "🌮";
+      fireEvent.change(screen.getByRole("combobox"), {
+        target: { value: value },
+      });
+
+      await waitFor(() => {
+        fireEvent.click(screen.getByText(value));
+        expect(handleCustomAdd).toHaveBeenCalledWith(value);
+        expect(handleChange).not.toHaveBeenCalled();
+      });
+    });
+
+    it("should not trigger the onLoadMore callback", () => {
+      expect(handleLoadMore).not.toHaveBeenCalled();
+    });
+
+    describe("Load More", () => {
+      beforeAll(() => {
+        mockIsInView.mockReturnValueOnce(true);
+      });
+
+      it("should trigger the onLoadMore callback", () => {
+        expect(handleLoadMore).toHaveBeenCalled();
+      });
+    });
   });
 
-  it("should only trigger the onCustomAdd callback when you select the custom option", async () => {
-    const value = "🌮";
-    fireEvent.change(screen.getByRole("combobox"), {
-      target: { value: value },
+  it("should trigger the onClick callback when a chip gets clicked", () => {
+    const targetChip = selectedChips[0];
+    fireEvent.click(
+      screen.getByRole("button", {
+        name: targetChip + voiceOver,
+      }),
+    );
+    expect(handleClickChip).toHaveBeenCalledWith(
+      expect.any(Object),
+      selectedChips[0],
+    );
+  });
+
+  it("should trigger the onChange callback when removing a chip", () => {
+    const targetChip = selectedChips[0];
+    const wrapperEl = screen.getByRole("button", {
+      name: targetChip + voiceOver,
     });
+    fireEvent.click(within(wrapperEl).getByTestId("remove-chip-button"));
+
+    expect(handleChange).toHaveBeenCalledWith([]);
+  });
+
+  describe("delete via keyboard", () => {
+    beforeEach(() => {
+      const addButton = screen.getByRole("button", { name: "Add" });
+      fireEvent.click(addButton);
+    });
+
+    it("should add the highlighted option on enter", () => {
+      fireEvent.keyDown(screen.queryByRole("combobox"), { key: "Enter" });
+      expect(handleChange).toHaveBeenCalledWith([...selectedChips, chips[1]]);
+      expect(handleCustomAdd).not.toHaveBeenCalled();
+    });
+
+    it("should add the highlighted option on tab", () => {
+      fireEvent.keyDown(screen.queryByRole("combobox"), { key: "Tab" });
+      expect(handleChange).toHaveBeenCalledWith([...selectedChips, chips[1]]);
+      expect(handleCustomAdd).not.toHaveBeenCalled();
+    });
+
+    it("should focus on the last selected chip on backspace", () => {
+      fireEvent.keyDown(screen.queryByRole("combobox"), { key: "Backspace" });
+      const wrapperEl = screen.getByRole("button", {
+        name: selectedChips[0] + voiceOver,
+      });
+      expect(wrapperEl).toHaveFocus();
+    });
+  });
+
+  describe("left and right arrow keys via keyboard", () => {
+    it("should focus on the correct element when left or right arrow down", () => {
+      const chipWrappers = screen.getAllByTestId("chip-wrapper");
+      const first = chipWrappers[0];
+
+      fireEvent.select(first);
+      expect(first).toHaveFocus();
+
+      fireEvent.keyDown(first, {
+        key: "ArrowRight",
+      });
+      expect(first).not.toHaveFocus();
+
+      const addButton = screen.getByRole("button", { name: "Add" });
+      expect(addButton).toHaveFocus();
+
+      fireEvent.keyDown(addButton, {
+        key: "ArrowLeft",
+      });
+      expect(addButton).not.toHaveFocus();
+      expect(first).toHaveFocus();
+    });
+  });
+});
+
+describe("Deleting a chip", () => {
+  function MockChips() {
+    const [selected, setSelected] = useState(["chip", "chip2"]);
+
+    return (
+      <InternalChipDismissible
+        selected={selected}
+        onChange={setSelected}
+        onClick={jest.fn}
+      >
+        {["chip", "chip2"].map(chip => (
+          <Chip key={chip} label={chip} value={chip} />
+        ))}
+      </InternalChipDismissible>
+    );
+  }
+
+  // There is a quick focus on the body before focusing on the next chip,
+  // this makes the the component difficult to test comprehensively.
+  // Strategies tried: userEvent library (typing backspace),
+  // and waitFor on the first chip's removal.
+  // eslint-disable-next-line jest/no-disabled-tests
+  it.skip("should focus on the next chip", async () => {
+    render(<MockChips />);
+
+    const first = screen.getByRole("button", { name: "chip" + voiceOver });
+    first.focus();
+    expect(first).toHaveFocus();
+
+    fireEvent.keyDown(document.body, { key: "Backspace" });
 
     await waitFor(() => {
-      fireEvent.click(screen.getByText(value));
-      expect(handleCustomAdd).toHaveBeenCalledWith(value);
-      expect(handleChange).not.toHaveBeenCalled();
-    });
-  });
-
-  it("should not trigger the onLoadMore callback", () => {
-    expect(handleLoadMore).not.toHaveBeenCalled();
-  });
-
-  describe("Load More", () => {
-    beforeAll(() => {
-      mockIsInView.mockReturnValueOnce(true);
+      expect(first).not.toBeInTheDocument();
     });
 
-    it("should trigger the onLoadMore callback", () => {
-      expect(handleLoadMore).toHaveBeenCalled();
+    const second = screen.getByRole("button", {
+      name: "chip2" + voiceOver,
     });
-  });
-});
 
-it("should trigger the onClick callback when a chip gets clicked", () => {
-  const targetChip = selectedChips[0];
-  fireEvent.click(
-    screen.getByRole("button", {
-      name: targetChip + voiceOver,
-    }),
-  );
-  expect(handleClickChip).toHaveBeenCalledWith(
-    expect.any(Object),
-    selectedChips[0],
-  );
-});
-
-it("should trigger the onChange callback when removing a chip", () => {
-  const targetChip = selectedChips[0];
-  const wrapperEl = screen.getByRole("button", {
-    name: targetChip + voiceOver,
-  });
-  fireEvent.click(within(wrapperEl).getByTestId("remove-chip-button"));
-
-  expect(handleChange).toHaveBeenCalledWith([]);
-});
-
-describe("delete via keyboard", () => {
-  beforeEach(() => {
-    const addButton = screen.getByRole("button", { name: "Add" });
-    fireEvent.click(addButton);
-  });
-
-  it("should add the highlighted option on enter", () => {
-    fireEvent.keyDown(screen.queryByRole("combobox"), { key: "Enter" });
-    expect(handleChange).toHaveBeenCalledWith([...selectedChips, chips[1]]);
-    expect(handleCustomAdd).not.toHaveBeenCalled();
-  });
-
-  it("should add the highlighted option on tab", () => {
-    fireEvent.keyDown(screen.queryByRole("combobox"), { key: "Tab" });
-    expect(handleChange).toHaveBeenCalledWith([...selectedChips, chips[1]]);
-    expect(handleCustomAdd).not.toHaveBeenCalled();
-  });
-
-  it("should focus on the last selected chip on backspace", () => {
-    fireEvent.keyDown(screen.queryByRole("combobox"), { key: "Backspace" });
-    const wrapperEl = screen.getByRole("button", {
-      name: selectedChips[0] + voiceOver,
-    });
-    expect(wrapperEl).toHaveFocus();
-  });
-});
-
-describe("left and right arrow keys via keyboard", () => {
-  it("should focus on the correct element when left or right arrow down", () => {
-    const chipWrappers = screen.getAllByTestId("chip-wrapper");
-    const first = chipWrappers[0];
-
-    fireEvent.select(first);
-    expect(first).toHaveFocus();
-
-    fireEvent.keyDown(first, {
-      key: "ArrowRight",
-    });
-    expect(first).not.toHaveFocus();
-
-    const addButton = screen.getByRole("button", { name: "Add" });
-    expect(addButton).toHaveFocus();
-
-    fireEvent.keyDown(addButton, {
-      key: "ArrowLeft",
-    });
-    expect(addButton).not.toHaveFocus();
-    expect(first).toHaveFocus();
+    expect(second).toHaveFocus();
   });
 });
 
