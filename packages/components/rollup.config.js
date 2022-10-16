497a619bbbc4d9aaa5736a2ded73c2f748a55c98
jcw- <jc.w@getjobber.com>
2022-10-16 03:51:42 -0600

    Upgrade rollup in components package

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-default-export */
 import multiInput from "rollup-plugin-multi-input";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: `src/*/index.ts`,
