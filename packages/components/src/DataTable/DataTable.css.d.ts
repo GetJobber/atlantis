777f4dce695e0c28880db77483e6d77903b7b484
Guilherme de Moraes <guilherme.dm@getjobber.com>
2022-11-02 20:29:42 -0300

    feat(components): Data table component (#925)
    * chore: add @tanstack/react-table package
    
    * feat: generate DataTable Component
    
    * wip: implement data-table draft with pagination
    
    * feat: finish pagination
    
    * feat: add sorting
    
    * fix: select and buttons on the footer
    
    * fix: use Atlantis icons for the sorting
    
    * docs: move DataTable to patterns
    
    * fix: add onClick cb to pagination buttons
    
    * added initial test for datatable
    
    * fix: use css variables for styling
    
    * feat: add itemsPerPage prop
    
    * wip: fix footer css
    
    * added basic jest test
    
    * wip: fix various bugs and add custom pagination example
    
    * update @tanstack/react-table
    
    * refactor: update tableSettings methods
    
    * refactor: move createTableSettings to its own file
    
    * refactor: improve pagination and sorting typing and update createTableSettings method
    
    * fix: some border issues and text wraping on the header
    
    * refactor: fix css issues
    
    * fix: items per page not working properly
    
    * docs: add examples
    
    * docs: add example with actions
    
    * docs: update manual sorting wording
    
    * addded documentation to the data table coponent
    
    * Added Types to paginantion and sorting to jest test
    
    * docs: improve manual sorting example
    
    * fix: add box shadow to the sticky header
    
    * feat: add enableSortingRemoval prop
    
    * Revert "Added Types to paginantion and sorting to jest test"
    
    This reverts commit 8001e0378c98eafc23a2873af310622874eac992.
    
    * test: update test and snapshot
    
    * feat: add clickable rows and hover effects
    
    * fix: stickyHeader box-shadow styling and sorting icon
    
    * fix: pinFirstColumn drop-shadow effect
    
    * fix: pagination controls wrapping behavior
    
    * fix: firstColumn hover effect when is clickable
    
    * docs: update horizontal scrolling example
    
    * docs: move DataTable to components
    
    * fix: types description
    
    * fix: pagination info and control increase of space-between when width is too large
    
    * chore: freeze tanstack version to 8.5.13 to mitigate npm start error
    
    * fix: typo in classname
    
    * docs: move Custom Pagination example to the playground
    
    * feat: export types for better usability
    
    * update typos and add accessorFn example to resurrect jon snow
    
    * docs: use descriptive variable instead of hardcoded number on custom patination example
    
    * refactor: move body and header to its own file
    
    * fix: use atlantis token on pin first column
    
    * re-organize jest tests
    
    * fix: set border-radius to border-base
    
    * docs: update custom header example
    
    * docs: update custom header example with green color
    
    * fix: use color-heading for the table head
    
    * fix: items per page not working
    
    * refactor: move default value for items per page into an use memo
    
    * test: remove snapshot testing, add tests for basic table, sorting, and pagination
    
    * test: add tests for manual pagination, manual sorting, and row click
    
    * fix: items per page initial state
    
    Co-authored-by: Juan Suazo <juan.s@getjobber.com>

---
+++ packages/components/src/DataTable/DataTable.css.d.ts
@@ -0,0 +1,12 @@
+declare const styles: {
+  readonly "dataTableContainer": string;
+  readonly "tableContainer": string;
+  readonly "table": string;
+  readonly "pinFirstColumn": string;
+  readonly "stickyHeader": string;
+  readonly "pinFirstHeaderSortable": string;
+  readonly "sortableColumn": string;
+  readonly "clickableRow": string;
+};
+export = styles;
+
---
+++ packages/components/src/DataTable/Footer.css.d.ts
@@ -0,0 +1,10 @@
+declare const styles: {
+  readonly "pagination": string;
+  readonly "paginationInfo": string;
+  readonly "paginationNav": string;
+  readonly "paginationSelect": string;
+  readonly "paginationSelectLabel": string;
+  readonly "paginationButtons": string;
+};
+export = styles;
+
