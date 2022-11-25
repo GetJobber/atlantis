7a4fee4b51c07deaec07c85b0a399194e12f3f6f
Guilherme Vieira de Moraes <guilherme.dm@getjobber.com>
2022-11-25 14:40:53 -0300

    remove svg placeholder

--- packages/components/src/DataTable/Header.tsx
+++ packages/components/src/DataTable/Header.tsx
@@ -2,13 +2,13 @@ import { Row, Table, flexRender } from "@tanstack/react-table";
 import classNames from "classnames";
 import React from "react";
 import styles from "./DataTable.css";
-import { Sorting } from "./types";
+import { SortingType } from "./types";
 import { Icon } from "../Icon";
 
 interface HeaderProps<T> {
   table: Table<T>;
   stickyHeader?: boolean;
-  sorting?: Sorting;
+  sorting?: SortingType;
   onRowClick?: (row: Row<T>) => void;
 }
 
@@ -45,17 +45,12 @@ export function Header<T extends object>({
                       header.column.columnDef.header,
                       header.getContext(),
                     )}
-                    {{
-                      asc: <Icon name="longArrowUp" color="green" />,
-                      desc: <Icon name="longArrowDown" color="green" />,
-                    }[header.column.getIsSorted() as string] ?? (
-                      <svg
-                        style={{
-                          width: "var(--space-large)",
-                          height: "var(--space-large)",
-                        }}
-                      />
-                    )}
+                    {
+                      {
+                        asc: <Icon name="longArrowUp" color="green" />,
+                        desc: <Icon name="longArrowDown" color="green" />,
+                      }[header.column.getIsSorted() as string]
+                    }
                   </div>
                 )}
               </th>
