caff38f7ef2a080fd5e16ec28fcb9a253f9eb664
danlaurent <daniel.s.laurent@gmail.com>
2022-12-23 17:29:29 -0300

    Remove duplicated code

--- packages/components/src/DataTable/DataTable.test.tsx
+++ packages/components/src/DataTable/DataTable.test.tsx
@@ -1,28 +1,16 @@
 import React from "react";
 import { fireEvent, render, screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import * as jobberHooks from "@jobber/hooks";
 import { DataTable } from "./DataTable";
 import {
   columSizeColumns,
   columnSizeData,
   columns,
   data,
+  mockContainerWidth,
   royaltyReportColumns,
   royaltyReportData,
 } from "./test-utilities";
-
-const mockContainerWidth = (exactWidth?: number) => {
-  jest.spyOn(jobberHooks, "useResizeObserver").mockReturnValue([
-    { current: null },
-    {
-      width: 1200,
-      height: 800,
-      exactWidth: exactWidth || 1200,
-      exactHeight: 800,
-    },
-  ]);
-};
 
 describe("when rendering a Basic Table", () => {
   beforeEach(() => {
