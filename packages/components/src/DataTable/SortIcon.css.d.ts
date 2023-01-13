0cadb1c82c07a9ebd592f8378cea4d8e7b5d7a87
Kyle Herrmann <kyle.h@getjobber.com>
2023-01-12 16:27:00 -0800

    Styling

--- packages/components/src/DataTable/SortIcon.css.d.ts
+++ packages/components/src/DataTable/SortIcon.css.d.ts
@@ -1,7 +1,7 @@
 declare const styles: {
   readonly "sortIcon": string;
-  readonly "topArrow": string;
-  readonly "bottomArrow": string;
+  readonly "active": string;
+  readonly "inactive": string;
 };
 export = styles;
 
