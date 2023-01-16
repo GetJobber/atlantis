32a9c476d1842d05381227ae41aa0bdf471b15cc
Michael Paradis <michael.p@getjobber.com>
2023-01-16 12:25:31 -0800

    Removed unneed code

--- packages/design/buildFoundation.js
+++ packages/design/buildFoundation.js
@@ -3,41 +3,10 @@
 const fs = require("fs");
 const postcss = require("postcss");
 const postcssCustomProperties = require("postcss-custom-properties");
-const postcssImport = require("postcss-import");
-// eslint-disable-next-line import/no-internal-modules
-const removeRule = require("./foundation_config/postcss-filter-rules");
-
-let hasJobberFonts = false;
-try {
-  require("@jobber/fonts");
-  hasJobberFonts = true;
-} catch (e) {
-  hasJobberFonts = false;
-}
-function removeFontImport(rule) {
-  if (rule.params?.includes("@jobber/fonts")) {
-    if (!hasJobberFonts) {
-      rule.remove();
-    }
-  }
-}
 
 const foundation = fs.readFileSync("./foundation.css");
 
-const postcssImportInstance = postcssImport({
-  basedir: ["node_modules", "./"],
-  filter: path => {
-    if (path.includes("@jobber/fonts")) {
-      return hasJobberFonts;
-    } else {
-      return true;
-    }
-  },
-});
-
 postcss([
-  removeRule({ ruleFilters: [removeFontImport] }),
-  postcssImportInstance,
   postcssCustomProperties({
     exportTo: ["src/foundation.js"],
   }),
