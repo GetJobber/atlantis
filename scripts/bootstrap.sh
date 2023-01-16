1ac68d5570e1779bdb5513d8ab303928e9efc431
Michael Paradis <michael.p@getjobber.com>
2023-01-16 08:10:51 -0800

    Hopefully fix

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if test -z "$IGNORE_OPTIONAL_DEPENDENCIES"; then
-  echo npm whoami
+  npm whoami
   npx lerna bootstrap
-  echo $(find -type d -path "**/@jobber/fonts")
+  find -type d -path "**/@jobber/fonts"
 else
   echo IGNORE_OPTIONAL_DEPENDENCIES set, not installing optional dependencies;
   npx lerna bootstrap -- --no-optional;
