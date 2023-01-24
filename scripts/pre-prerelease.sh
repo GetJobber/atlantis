b2799dd3845a10c0f3cd483990322d850d200ec0
Michael Paradis <michael.p@getjobber.com>
2023-01-24 11:55:27 -0800

    build: Fix lerna package json (#1001)
    * Update check for pre-release script to be more readable
    
    * chore(release): Publish
    
     - @jobber/components@3.0.0-alpha.0
     - @jobber/design@1.0.0-alpha.0
     - @jobber/docx@0.2.2-alpha.0
     - @jobber/docz-tools@2.0.0-alpha.0

--- scripts/pre-prerelease.sh
+++ scripts/pre-prerelease.sh
@@ -1,4 +1,4 @@
-if ! npm list -la 2>/dev/null | grep  -p "UNMET OPTIONAL DEPENDENCY @jobber\/fonts@1.0.0-pre-release.0"  ; then
+if ls node_modules/@jobber/fonts || ls **/node_modules/@jobber/fonts  ; then
   echo "Jobber Fonts in installed packaged. Please install without @jobber/fonts"
   echo "Please clear node_modules and packages with: npm run lerna:clean"
   echo "Then install dependencies with: export IGNORE_OPTIONAL_DEPENDENCIES=true; npm ci --no-optional"
