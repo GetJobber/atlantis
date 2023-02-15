eb5a56750295f3b56d2c9e84f3584eef8240ef52
Darryl Tec <darryl.t@getjobber.com>
2023-02-15 10:26:51 -0700

    set viewmode to docs by default and annotate button

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,5 @@
 import "@jobber/design/foundation.css";
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+  viewMode: "docs",
 }
