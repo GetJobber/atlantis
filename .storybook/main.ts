12b7ac1a74790578ec7aaba6090e11df810c5fa5
Darryl Tec <darryl.t@getjobber.com>
2023-02-15 08:25:26 -0700

    update storybook config

---
+++ .storybook/main.ts
@@ -0,0 +1,73 @@
+import type { StorybookConfig } from "@storybook/react/types";
+import * as webpack from "webpack";
+const path = require("path");
+
+const config: StorybookConfig = {
+  "stories": [
+    "../packages/**/*.stories.mdx",
+    "../packages/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  "framework": "@storybook/react",
+  webpackFinal: async (config) => {
+
+    /**
+     * Generate css types on `.css` file save,
+     * as well as handle PostCss
+     */
+    config.module?.rules.push({
+      enforce: "pre",
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: [
+        require.resolve("typed-css-modules-loader"),
+        {
+          loader: "postcss-loader",
+          options: {
+            ident: "postcss",
+            plugins: () => [
+              require("postcss-import"),
+              require("autoprefixer"),
+              require("postcss-preset-env")({
+                stage: 1,
+                preserve: true,
+                importFrom: [
+                  require.resolve(
+                    path.join(__dirname, "../packages/design/foundation.css"),
+                  ),
+                ],
+              }),
+            ],
+          },
+        },
+      ],
+    });
+
+    /**
+     * Since we don't use .module.css, we'll have to enable CSS modules for
+     * all CSS files.
+     */
+    const ruleCssIndex = config.module?.rules.findIndex(rule => rule.test?.toString() === '/\\.css$/');
+
+    if (ruleCssIndex) {
+      (config.module?.rules[ruleCssIndex]?.use as webpack.RuleSetLoader[])?.map(item => {
+        if (item.loader && item.loader.includes('/css-loader/')) {
+          (item.options as Record<any, any>).modules = {
+            localIdentName: "[name]__[local]--[hash:base64:5]",
+          };
+        }
+
+        return item;
+      })
+    }
+
+    // Return the altered config
+    return config;
+  }
+}
+
+module.exports = config
