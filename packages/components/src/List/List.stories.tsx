1faecee6cd462e94cb560c3548c918ae983ce51f
Taylor Noj <104797704+taylorvnoj@users.noreply.github.com>
2023-02-22 12:31:29 -0500

    docs(components): Convert component package bundle two (#1050)

---
+++ packages/components/src/List/List.stories.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { ListItemProps } from "./ListItem";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const StorybookListPropsInterface = (_: ListItemProps): JSX.Element => (
+  <></>
+);
