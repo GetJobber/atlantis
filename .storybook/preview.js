cd34ef78330c7030108163dd1024a679a92366bd
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2023-02-22 15:00:12 -0300

    Improve Atlantis branding on Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,11 @@
+import React from "react";
+import { Heading } from "@jobber/components/Heading";
+import { Text } from "@jobber/components/Text";
+import { Content } from "@jobber/components/Content";
+import { InlineCode } from "./components/InlineCode";
+
 import "@jobber/design/foundation.css";
+import "@jobber/fonts";
 
 export const parameters = {
   viewMode: "docs",
@@ -8,4 +15,14 @@ export const parameters = {
       order: ["Components", "Design", "*"],
     },
   },
-}
+  docs: {
+    components: {
+      wrapper: props => <Content>{props.children}</Content>,
+      h1: props => <Heading {...props} level={1} />,
+      h2: props => <Heading {...props} level={2} />,
+      h3: props => <Heading {...props} level={3} />,
+      p: props => <Text {...props} />,
+      inlineCode: props => <InlineCode {...props} />,
+    },
+  },
+};
