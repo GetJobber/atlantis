e32c9ed063fcf8ccbc2ca159e0810f29e3e120ea
Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>
2023-03-03 15:42:11 -0700

    use gap, banner name vs flash, use semantic color

--- packages/components/src/Banner/Banner.css.d.ts
+++ packages/components/src/Banner/Banner.css.d.ts
@@ -1,8 +1,7 @@
 declare const styles: {
-  readonly "flash": string;
+  readonly "banner": string;
   readonly "bannerContent": string;
   readonly "bannerChildren": string;
-  readonly "dismissibleSpacing": string;
   readonly "medium": string;
   readonly "bannerAction": string;
   readonly "closeButton": string;
