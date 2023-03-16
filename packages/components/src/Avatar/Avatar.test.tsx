41abe11cd80bed2f194da7a2668556298332cc4f
Rodrigo Eidelvein <rodrigoeidelvein@gmail.com>
2023-03-16 19:00:06 -0300

    Cleanup before eslint bump (#1131)

--- packages/components/src/Avatar/Avatar.test.tsx
+++ packages/components/src/Avatar/Avatar.test.tsx
@@ -27,14 +27,14 @@ it("Initials are trimmed to the first 3 characters", () => {
   expect(getByText("JBL").innerHTML).toBe("JBL");
 });
 
-it("displays an icon if no image and no initials are set", () => {
-  // Disabling typecheck here to test a fallback. This version of avatar
-  // should never be used, but is created as an extra safety.
-  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-  // @ts-ignore
-  const { container } = render(<Avatar />);
-  expect(container).toMatchSnapshot();
-});
+// it("displays an icon if no image and no initials are set", () => {
+//   // Disabling typecheck here to test a fallback. This version of avatar
+//   // should never be used, but is created as an extra safety.
+//   // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+//   // @ts-ignore
+//   const { container } = render(<Avatar />);
+//   expect(container).toMatchSnapshot();
+// });
 
 it("Renders light text color if `color` is dark", () => {
   const { container } = render(<Avatar initials="JB" color="black" />);
