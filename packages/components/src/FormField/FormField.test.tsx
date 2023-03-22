fad5448485d43d783a96c2cd1006dd980549bf7e
Taylor Noj <104797704+taylorvnoj@users.noreply.github.com>
2023-03-22 11:43:00 -0400

    build(deps): Revert "Update react-hook-form to v7.43.7" [JOB-65564] (#1145)
    Revert "build(deps): Update react-hook-form to v7.43.7 [JOB-65458] (#1140)"
    
    This reverts commit cdf6b9b458a4bb7114245dc536b4677666946a8d.

--- packages/components/src/FormField/FormField.test.tsx
+++ packages/components/src/FormField/FormField.test.tsx
@@ -189,7 +189,7 @@ describe("FormField", () => {
     });
 
     describe("without validation errors", () => {
-      it("should trigger onValidation with an empty string", () => {
+      it("should trigger onValidation with undefined", () => {
         const validationHandler = jest.fn();
 
         render(
@@ -201,7 +201,7 @@ describe("FormField", () => {
         );
 
         expect(validationHandler).toHaveBeenCalled();
-        expect(validationHandler).toHaveBeenCalledWith("");
+        expect(validationHandler).toHaveBeenCalledWith(undefined);
       });
     });
 
