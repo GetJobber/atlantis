2f96d3e2816c6a82d237c24cab1742776622d78a
Darryl Tec <darryl.t@getjobber.com>
2023-03-28 10:00:43 -0600

    setup rollup

--- packages/mobile/rollup.config.js
+++ packages/mobile/rollup.config.js
@@ -1,34 +1,28 @@
 /* eslint-disable import/no-default-export */
+import multiInput from "rollup-plugin-multi-input";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
-import packageJson from "./package.json";
 
 export default {
-  input: "src/index.ts",
-  external: ["react", "react-native"],
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true,
-      name: "react-lib",
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
+  input: `src/*/index.ts`,
   plugins: [
+    multiInput.default(),
+    typescript({ tsconfig: "./tsconfig.json", declarationDir: "dist" }),
     external(),
     resolve(),
     commonjs(),
-    typescript({ tsconfig: "./tsconfig.json" }),
     postcss(),
     terser(),
   ],
+  output: [
+    {
+      dir: "dist",
+      format: "cjs",
+    },
+  ],
+  external: ["react", "react-native"],
 };
