c841836fdfc051b14e871453567a21214a3474b3
Darryl Tec <darryl.t@getjobber.com>
2023-03-30 11:41:01 -0600

    fix: add babel-preset-expo

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,12 @@
 /* eslint-env node */
-module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-    "@babel/preset-react",
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["@babel/preset-env", { targets: { node: "current" } }],
+      "@babel/preset-typescript",
+      "@babel/preset-react",
+      "babel-preset-expo",
+    ],
+  };
 };
