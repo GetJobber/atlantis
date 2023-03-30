4f4507240d6afc3e8cd37ec781bde9784349d1fb
Darryl Tec <darryl.t@getjobber.com>
2023-03-30 08:17:27 -0600

    chore: move expo to root

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 // eslint-disable-next-line import/no-internal-modules
-import { Button } from "./src/Button";
+import { Button } from "./packages/mobile/src/Button";
 
 // eslint-disable-next-line import/no-default-export
 export default function App() {
