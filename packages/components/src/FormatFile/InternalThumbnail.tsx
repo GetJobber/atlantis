d333cd0e3e5aaa78113a514016f5edb1b61fa97d
Darryl Tec <darryl.t@getjobber.com>
2023-04-06 09:33:33 -0600

    chore: revert to original check

--- packages/components/src/FormatFile/InternalThumbnail.tsx
+++ packages/components/src/FormatFile/InternalThumbnail.tsx
@@ -22,7 +22,7 @@ export function InternalThumbnail({
   const iconName = getIconNameFromType(type);
   const hasName = Boolean(name);
 
-  if (!imageLoaded && type.startsWith("image/")) {
+  if (!imageSource && type.startsWith("image/")) {
     src().then(url => setImageSource(url));
   }
 
