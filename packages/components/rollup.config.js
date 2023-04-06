91f1aabe68514e860e44d0920447dfc5953ee114
Michael Paradis <michael.p@getjobber.com>
2023-04-06 08:57:40 -0700

    Fixed rollup import

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -21,12 +21,12 @@ export default {
       plugins: [
         require("postcss-import"),
         require("autoprefixer"),
-        // require("@csstools/postcss-global-data")({
-        //   files: [
-        //     require.resolve("@jobber/design/foundation.css"),
-        //     require.resolve("@jobber/design/src/responsiveBreakpoints.css"),
-        //   ],
-        // }),
+        require("@csstools/postcss-global-data")({
+          files: [
+            require.resolve("@jobber/design/foundation.css"),
+            require.resolve("@jobber/design/src/responsiveBreakpoints.css"),
+          ],
+        }),
         // eslint-disable-next-line @typescript-eslint/no-var-requires
         require("postcss-preset-env")({
           debug: true,
