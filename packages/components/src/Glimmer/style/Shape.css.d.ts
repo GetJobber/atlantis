f625af76f7fab28a5e06700f2c0bdf589bc7b13d
Darryl Tec <darryl.t@getjobber.com>
2023-04-12 08:48:40 -0600

    feat: add extra shapes

--- packages/components/src/Glimmer/style/Shape.css.d.ts
+++ packages/components/src/Glimmer/style/Shape.css.d.ts
@@ -1,5 +1,7 @@
 declare const styles: {
   readonly "rectangle": string;
+  readonly "rectangleShort": string;
+  readonly "rectangleShorter": string;
   readonly "square": string;
   readonly "circle": string;
 };
