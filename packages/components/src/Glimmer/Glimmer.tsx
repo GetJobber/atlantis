1970450395ca89b8d224aca98c1fc8a93cae6696
Darryl Tec <darryl.t@getjobber.com>
2023-04-12 07:43:56 -0600

    feat: enable timing

--- packages/components/src/Glimmer/Glimmer.tsx
+++ packages/components/src/Glimmer/Glimmer.tsx
@@ -5,16 +5,27 @@ import classnames from "classnames";
 import styles from "./Glimmer.css";
 import sizes from "./style/Sizes.css";
 import shapes from "./style/Shape.css";
+import timings from "./style/Timing.css";
 
 interface GlimmerProps {
   readonly size?: keyof typeof sizes;
   readonly shape?: keyof typeof shapes;
+  readonly timing?: keyof typeof timings;
 }
 
 export const GLIMMER_TEST_ID = "ATL-Glimmer";
 
-export function Glimmer({ size = "base", shape = "rectangle" }: GlimmerProps) {
-  const className = classnames(styles.glimmer, sizes[size], shapes[shape]);
+export function Glimmer({
+  size = "base",
+  shape = "rectangle",
+  timing = "base",
+}: GlimmerProps) {
+  const className = classnames(
+    styles.glimmer,
+    sizes[size],
+    shapes[shape],
+    timings[timing],
+  );
 
   return (
     <div aria-busy="true" className={className} data-testid={GLIMMER_TEST_ID} />
---
+++ packages/components/src/Glimmer/style/Timing.css.d.ts
@@ -0,0 +1,6 @@
+declare const styles: {
+  readonly "base": string;
+  readonly "fast": string;
+};
+export = styles;
+
