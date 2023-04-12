1cab8dc726a3535d8ba53ceb81ca8cce02c1eced
Darryl Tec <darryl.t@getjobber.com>
2023-04-12 07:41:39 -0600

    chore: delete corners styles

--- packages/components/src/Glimmer/Glimmer.tsx
+++ packages/components/src/Glimmer/Glimmer.tsx
@@ -4,28 +4,17 @@ import React from "react";
 import classnames from "classnames";
 import styles from "./Glimmer.css";
 import sizes from "./style/Sizes.css";
-import corners from "./style/Corner.css";
 import shapes from "./style/Shape.css";
 
 interface GlimmerProps {
   readonly size?: keyof typeof sizes;
-  readonly corner?: keyof typeof corners;
   readonly shape?: keyof typeof shapes;
 }
 
 export const GLIMMER_TEST_ID = "ATL-Glimmer";
 
-export function Glimmer({
-  size = "base",
-  corner = "base",
-  shape = "rectangle",
-}: GlimmerProps) {
-  const className = classnames(
-    styles.glimmer,
-    sizes[size],
-    corners[corner],
-    shapes[shape],
-  );
+export function Glimmer({ size = "base", shape = "rectangle" }: GlimmerProps) {
+  const className = classnames(styles.glimmer, sizes[size], shapes[shape]);
 
   return (
     <div aria-busy="true" className={className} data-testid={GLIMMER_TEST_ID} />
--- packages/components/src/Glimmer/style/Corner.css.d.ts
+++
@@ -1,8 +0,0 @@
-declare const styles: {
-  readonly "none": string;
-  readonly "base": string;
-  readonly "rounder": string;
-  readonly "roundest": string;
-};
-export = styles;
-
