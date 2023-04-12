3f70d56253d40dbc98d87496f7e3533bcf286dc6
Darryl Tec <darryl.t@getjobber.com>
2023-04-12 08:43:36 -0600

    docs: add a more elaborate example

--- packages/components/src/Glimmer/style/Sizes.css.d.ts
+++ packages/components/src/Glimmer/style/Sizes.css.d.ts
@@ -2,6 +2,7 @@ declare const styles: {
   readonly "small": string;
   readonly "base": string;
   readonly "large": string;
+  readonly "larger": string;
   readonly "largest": string;
   readonly "auto": string;
 };
