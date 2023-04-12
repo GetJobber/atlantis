8f9c82cfbb7966c5468a2287441e31e78d4dcb18
Darryl Tec <darryl.t@getjobber.com>
2023-04-12 09:04:03 -0600

    docs: add jsdocs

--- packages/components/src/Glimmer/Glimmer.tsx
+++ packages/components/src/Glimmer/Glimmer.tsx
@@ -1,20 +1,39 @@
-// TODO FIX ME
-/* eslint-disable import/no-internal-modules */
 import React from "react";
 import classnames from "classnames";
 import styles from "./Glimmer.css";
+/* eslint-disable import/no-internal-modules */
 import sizes from "./style/Sizes.css";
 import shapes from "./style/Shape.css";
 import timings from "./style/Timing.css";
+/* eslint-enable import/no-internal-modules */
 
 interface GlimmerProps {
+  /**
+   * Sets the size of the glimmer.
+   */
   readonly size?: keyof typeof sizes;
+
+  /**
+   * Sets the shape of the glimmer.
+   */
   readonly shape?: keyof typeof shapes;
+
+  /**
+   * Control how fast the shine moves from left to right. This is useful when
+   * the glimmer is used on smaller spaces.
+   */
   readonly timing?: keyof typeof timings;
 }
 
 export const GLIMMER_TEST_ID = "ATL-Glimmer";
 
+/**
+ * **Experimental component! Use at your own risk.**
+ *
+ * Foundational component used to build loading states such as Skeleton UI
+ *
+ @experimental
+ */
 export function Glimmer({
   size = "base",
   shape = "rectangle",
