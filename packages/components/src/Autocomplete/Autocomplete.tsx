defd8be49414ff9fcaa9ad4569e7d634feedcaf8
Michael Paradis <michael.p@getjobber.com>
2023-04-28 07:28:42 -0700

    Fix menu positioning in Autcomplete when used in a portal

--- packages/components/src/Autocomplete/Autocomplete.tsx
+++ packages/components/src/Autocomplete/Autocomplete.tsx
@@ -105,7 +105,7 @@ export function Autocomplete({
       {menuVisible && (
         <Menu
           attachTo={autocompleteRef}
-          visible={true}
+          visible={menuVisible && options.length > 0}
           options={options}
           selectedOption={value}
           onOptionSelect={handleMenuChange}
