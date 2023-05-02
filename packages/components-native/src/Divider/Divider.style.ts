2c44563712f362fb71867544a7a862614df9e9a8
Rodrigo Eidelvein <rodrigoeidelvein@gmail.com>
2023-05-02 13:51:24 -0300

    feat: Create package for mobile components (#1218)
    * feat: initial mobile package
    
    * feat: move icon
    
    * fix: change lib to dist
    
    * feat: try exports module
    
    * Revert "feat: try exports module"
    
    This reverts commit c1b63a99e1876f0b530cf21ca6348f38821328fa.
    
    * feat: add index.ts
    
    * chore: ignore dist instead of lib
    
    * chore: don't mark package as private
    
    * build: let npm decide the best version to use
    
    * chore: rename package to components-native
    
    * feat: use typescript to compile everything
    
    * fix: use esm for main
    
    * Match react-native-svg version with jobber-mobile
    
    * Remove bob library
    
    * Remove commonjs typescript build
    
    * Add react native as a devDependency
    
    * Add divider
    
    * Remove prepare package json script
    
    * Empty commit
    
    * Add README
    
    * Add LICENSE
    
    * Address PR review comments
    
    * Match RN version with jobber-mobile
    
    * Remove ts config files
    
    * Fix peerDependency versions
    
    * Rollback unnecessary change
    
    * Specify versions for types and typescript
    
    * Update package.json main and module properties
    
    * Add documentation to set it up with Jest
    
    * Match RN version with jobber-mobile
    
    ---------
    
    Co-authored-by: Darryl Tec <darryl.t@getjobber.com>

---
+++ packages/components-native/src/Divider/Divider.style.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+// eslint-disable-next-line import/no-internal-modules
+import { JobberStyle } from "@jobber/design/foundation";
+
+export const styles = StyleSheet.create({
+  base: {
+    height: JobberStyle["space-minuscule"],
+    margin: 0,
+    borderBottomWidth: JobberStyle["border-base"],
+    borderBottomColor: JobberStyle["color-border"],
+  },
+  large: {
+    borderBottomWidth: JobberStyle["border-thick"],
+    opacity: 0.875,
+  },
+  largest: {
+    borderBottomWidth: JobberStyle["space-small"],
+    opacity: 0.375,
+  },
+});
