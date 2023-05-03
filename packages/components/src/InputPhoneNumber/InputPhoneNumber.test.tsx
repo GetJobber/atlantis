c3a70074111d287788cb6d6ed2f5e56878406fa6
Rodrigo Eidelvein <rodrigoeidelvein@gmail.com>
2023-05-03 14:02:36 -0300

    Update packages/components/src/InputPhoneNumber/InputPhoneNumber.test.tsx
    Co-authored-by: Taylor Noj <104797704+taylorvnoj@users.noreply.github.com>

--- packages/components/src/InputPhoneNumber/InputPhoneNumber.test.tsx
+++ packages/components/src/InputPhoneNumber/InputPhoneNumber.test.tsx
@@ -60,60 +60,66 @@ describe("InputPhoneNumber", () => {
     expect(await screen.findByText("This is required")).toBeInTheDocument();
   });
 
-  it("should show the validation error message when the user doesn't fill a complete phone number", async () => {
-    render(<InputPhoneNumber value="123123" onChange={jest.fn()} />);
-    const input = screen.getByRole("textbox");
-    input.focus();
-    input.blur();
-    expect(await screen.findByText("Enter a phone number")).toBeInTheDocument();
-  });
-
-  it("should not show the validation error message when the user fills a complete phone number", async () => {
-    const validationHandler = jest.fn();
-    render(
-      <InputPhoneNumber
-        value="6135551232"
-        onChange={jest.fn()}
-        onValidation={validationHandler}
-      />,
-    );
-    const input = screen.getByRole("textbox");
-    input.focus();
-    input.blur();
-
-    await waitFor(() => {
-      expect(validationHandler).toHaveBeenCalledWith(undefined);
-    });
-  });
-
-  it("should remove the validation error message when the user changes to a valid phone number", async () => {
-    const validationHandler = jest.fn();
-    const changeHandler = jest.fn();
-
-    render(
-      <InputPhoneNumber
-        value="613555"
-        onChange={changeHandler}
-        onValidation={validationHandler}
-      />,
-    );
-    const input = screen.getByRole("textbox");
-
-    input.focus();
-    input.blur();
-
-    expect(await screen.findByText("Enter a phone number")).toBeInTheDocument();
-    waitFor(() => {
-      expect(validationHandler).toHaveBeenCalledWith("Enter a phone number");
+describe("The validation error message", () => {
+    it("should appear when the user doesn't enter ten or more digits", async () => {
+      render(<InputPhoneNumber value="123123" onChange={jest.fn()} />);
+      const input = screen.getByRole("textbox");
+      input.focus();
+      input.blur();
+      expect(
+        await screen.findByText("Enter a phone number"),
+      ).toBeInTheDocument();
     });
 
-    input.focus();
-    fireEvent.change(input, { target: { value: "6135551232" } });
-    input.blur();
+    it("should not appear with a valid phone number", async () => {
+      const validationHandler = jest.fn();
+      render(
+        <InputPhoneNumber
+          value="6135551232"
+          onChange={jest.fn()}
+          onValidation={validationHandler}
+        />,
+      );
+      const input = screen.getByRole("textbox");
+      input.focus();
+      input.blur();
 
-    await waitFor(() => {
-      expect(changeHandler).toHaveBeenCalledWith("(613) 555-1232");
-      expect(validationHandler).toHaveBeenCalledWith(undefined);
+      await waitFor(() => {
+        expect(validationHandler).toHaveBeenCalledWith(undefined);
+      });
+    });
+
+    it("should disappear once the input has been changed back to a valid phone number", async () => {
+      const validationHandler = jest.fn();
+      const changeHandler = jest.fn();
+
+      render(
+        <InputPhoneNumber
+          value="613555"
+          onChange={changeHandler}
+          onValidation={validationHandler}
+        />,
+      );
+      const input = screen.getByRole("textbox");
+
+      input.focus();
+      input.blur();
+
+      expect(
+        await screen.findByText("Enter a phone number"),
+      ).toBeInTheDocument();
+      waitFor(() => {
+        expect(validationHandler).toHaveBeenCalledWith("Enter a phone number");
+      });
+
+      input.focus();
+      fireEvent.change(input, { target: { value: "6135551232" } });
+      input.blur();
+
+      await waitFor(() => {
+        expect(changeHandler).toHaveBeenCalledWith("(613) 555-1232");
+        expect(validationHandler).toHaveBeenCalledWith(undefined);
+      });
     });
   });
 });
