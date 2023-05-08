6c79338727c6ff73f1ad9c556b220315a41b223a
Darryl Tec <darryl.t@getjobber.com>
2023-05-08 09:16:34 -0600

    refactor: autogenerate breakpoints

--- packages/components/src/Grid/GridCell/GridCell.tsx
+++ packages/components/src/Grid/GridCell/GridCell.tsx
@@ -1,18 +1,29 @@
-import React, { CSSProperties, PropsWithChildren } from "react";
+import React, { PropsWithChildren } from "react";
 import styles from "./GridCell.css";
 
+/**
+ * Breakpoints that we support
+ */
+const breakpoints = ["xs", "sm", "md", "lg", "xl"] as const;
+
+/**
+ *  Grab type from breakpoints const
+ */
+type ValuesOf<T extends readonly unknown[]> = T[number];
+type Breakpoints = ValuesOf<typeof breakpoints>;
+
+type Sizes = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
 export interface GridCellProps {
-  readonly size: Record<string, number>;
+  readonly size: Record<Breakpoints, Sizes>;
 }
 
 export function GridCell({ size, children }: PropsWithChildren<GridCellProps>) {
-  const cssVariables = {
-    "--gridCell--size-xs": size.xs,
-    "--gridCell--size-sm": size.sm,
-    "--gridCell--size-md": size.md,
-    "--gridCell--size-lg": size.lg,
-    "--gridCell--size-xl": size.xl,
-  } as CSSProperties;
+  const cssVariables = breakpoints.reduce((values, currentSize) => {
+    return Object.assign(values, {
+      [`--gridCell--size-${currentSize}`]: size[currentSize],
+    });
+  }, {});
 
   return (
     <div className={styles.gridCell} style={cssVariables}>
