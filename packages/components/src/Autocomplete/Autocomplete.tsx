ac727d99ef6ac3327212ed9f1514fd0b84ede776
Vanika Dhamija <32287738+vanikadhamija@users.noreply.github.com>
2023-05-10 06:51:45 -0600

    feat(components): Add validation support to Autocomplete component (#1233)
    Add validations support on autocomplete component that uses input text component and add the validation to an exisiting example to showcase it

--- packages/components/src/Autocomplete/Autocomplete.tsx
+++ packages/components/src/Autocomplete/Autocomplete.tsx
@@ -10,7 +10,10 @@ import { FormFieldProps } from "../FormField";
 type OptionCollection = XOR<Option[], GroupOption[]>;
 
 interface AutocompleteProps
-  extends Pick<FormFieldProps, "size" | "onBlur" | "onFocus" | "invalid"> {
+  extends Pick<
+    FormFieldProps,
+    "size" | "onBlur" | "onFocus" | "invalid" | "name" | "validations"
+  > {
   /**
    * Initial options to show when user first focuses the Autocomplete
    */
@@ -73,6 +76,8 @@ export function Autocomplete({
   placeholder,
   onBlur,
   onFocus,
+  name,
+  validations,
 }: AutocompleteProps) {
   const [options, setOptions] = useState(initialOptions);
   const [menuVisible, setMenuVisible] = useState(false);
@@ -101,6 +106,8 @@ export function Autocomplete({
         placeholder={placeholder}
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
+        name={name}
+        validations={validations}
       />
       {menuVisible && (
         <Menu
