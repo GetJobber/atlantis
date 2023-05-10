8ec633156f7dbf9f8160bf14d455c2b8992f050b
Darryl Tec <darryl.t@getjobber.com>
2023-05-10 13:56:56 -0600

    fix: remove parameters for now

--- packages/components-native/src/Divider/Divider.stories.tsx
+++ packages/components-native/src/Divider/Divider.stories.tsx
@@ -5,14 +5,6 @@ import { Divider } from "./Divider";
 export default {
   title: "Components/Divider",
   component: Divider,
-  parameters: {
-    viewMode: "story",
-    previewTabs: {
-      "storybook/docs/panel": {
-        hidden: true,
-      },
-    },
-  },
 } as ComponentMeta<typeof Divider>;
 
 const Template: ComponentStory<typeof Divider> = args => <Divider {...args} />;
