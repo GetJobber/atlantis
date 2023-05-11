d2761e47ef570ebafc802940e86dc46eba60070c
Darryl Tec <darryl.t@getjobber.com>
2023-05-11 07:31:40 -0600

    docs: expand on that

--- packages/components/src/TextList/TextList.tsx
+++ packages/components/src/TextList/TextList.tsx
@@ -3,10 +3,24 @@ import classNames from "classnames";
 import { TextListItem } from "./TextListItem";
 import styles from "./TextList.css";
 
-type Data = Array<string | ReactElement | Data>;
-
 interface TextListProps {
-  readonly data: Data;
+  /**
+   * Array of string, elements, or a tuple of `[string, <TextList />]` that
+   * creates the list.
+   *
+   * The tuple type allows you to nest the list and keeps the option of it being
+   * open to a nested number or bullet list.
+   */
+  readonly data: Array<
+    string | ReactElement | [string, ReactElement<TextListProps>]
+  >;
+
+  /**
+   * Changes the list from bullet to numbers. Semantically, this also changes
+   * the tag from UL to OL.
+   *
+   * @default "bullets"
+   */
   readonly type?: "bullets" | "numbers";
 }
 
@@ -15,7 +29,7 @@ export function TextList({ data, type = "bullets" }: TextListProps) {
   const Tag = isBulleted ? "ul" : "ol";
   const tagClassNames = classNames(styles.textList, {
     [styles.bulleted]: isBulleted,
-    [styles.numbered]: !isBulleted,
+    [styles.numbered]: type === "numbers",
   });
 
   return (
