8328d6c718c533c20853e4fcc281c9dcf36218b2
Darryl Tec <darryl.t@getjobber.com>
2023-05-11 15:57:37 -0600

    feat: Grid component (#1229)

---
+++ packages/components/src/Grid/Grid.tsx
@@ -0,0 +1,44 @@
+import React, { ReactElement } from "react";
+import classNames from "classnames";
+import { GridCell, GridCellProps } from "./GridCell";
+import styles from "./Grid.css";
+import alignments from "./GridAlign.css";
+
+interface GridProps {
+  /**
+   * Add spacing between elements.
+   */
+  readonly gap?: boolean;
+
+  /**
+   * Adjust the alignment of columns. We only support a few select properties
+   * from `align-items` due to the nature of the other properties acting the
+   * same. Read more about [align-items](https://developer.mozilla.org/en-US/docs/Web/CSS/align-items) property values.
+   */
+  readonly alignItems?: keyof typeof alignments;
+
+  /**
+   * Array of `Grid.Cell` children
+   */
+  readonly children: Array<ReactElement<GridCellProps>>;
+}
+
+export const GRID_TEST_ID = "ATL-Grid";
+
+export function Grid({
+  alignItems = "start",
+  gap = true,
+  children,
+}: GridProps) {
+  const classnames = classNames(styles.grid, alignments[alignItems], {
+    [styles.gap]: gap,
+  });
+
+  return (
+    <div data-testid={GRID_TEST_ID} className={classnames}>
+      {children}
+    </div>
+  );
+}
+
+Grid.Cell = GridCell;
