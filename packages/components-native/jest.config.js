5785d4af98347977669e4b17bfb11766f4e5edce
Rodrigo Eidelvein <rodrigoeidelvein@gmail.com>
2023-05-12 17:18:32 -0300

    refactor(components-native): Add tests to components-native's Typography (#1247)
    Add tests to components-native's Typography

--- packages/components-native/jest.config.js
+++ packages/components-native/jest.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   displayName: "Atlantis Mobile",
   preset: "react-native",
-  setupFilesAfterEnv: ["./jestMobileSetupConfig.js"],
+  setupFilesAfterEnv: [
+    "./jestMobileSetupConfig.js",
+    "./node_modules/react-native-gesture-handler/jestSetup.js",
+  ],
   testPathIgnorePatterns: ["/node_modules/"],
   transformIgnorePatterns: [
     "node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-native)",
