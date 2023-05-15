192bcc8dce0ccfc8d085125a192522ef4bb36eb3
Darryl Tec <darryl.t@getjobber.com>
2023-05-15 09:03:42 -0600

    fix: documentation that gotborked

--- packages/components/src/List/List.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ListItemProps } from "./ListItem";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const StorybookListPropsInterface = (_: ListItemProps): JSX.Element => (
-  <></>
-);
