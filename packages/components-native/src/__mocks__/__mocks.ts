a7764a2f5139817695219df00bc78479d48aeb6a
Michael Paradis <michael.p@getjobber.com>
2023-05-16 13:50:54 -0700

    fix tests

--- packages/components-native/src/__mocks__/__mocks.ts
+++ packages/components-native/src/__mocks__/__mocks.ts
@@ -1,7 +1,28 @@
 // eslint-disable-next-line import/no-internal-modules
+import { MissingTranslationError } from "react-intl";
 import mockRNLocalize from "react-native-localize/mock";
 
 jest.mock("react-native-localize", () => ({
   ...mockRNLocalize,
   getTimeZone: () => "UTC",
 }));
+
+export const MOCK_INTL_CONFIG = {
+  locale: "en-US",
+  onError: (error: MissingTranslationError): void => {
+    if (error.code === "MISSING_TRANSLATION") {
+      return;
+    }
+    throw error;
+  },
+};
+
+jest.mock("react-intl", () => {
+  const reactIntl = jest.requireActual("react-intl");
+  const intl = reactIntl.createIntl(MOCK_INTL_CONFIG);
+
+  return {
+    ...reactIntl,
+    useIntl: () => intl,
+  };
+});
