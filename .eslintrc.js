9524e793c2da00ad8c775df63242cea39c97158b
Darryl Tec <darryl.t@getjobber.com>
2023-05-17 13:49:23 -0600

    docs: Set a pattern for how we document web and mobile (#1260)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ require("@jobber/eslint-config/patch-eslint-plugin-resolution.js");
 
 const packageAliases = [
   ["@jobber/components", "./packages/components/src"],
+  ["@jobber/components-native", "./packages/components-native/src"],
   ["@jobber/hooks", "./packages/hooks/src"],
 ];
 
@@ -30,6 +31,7 @@ module.exports = {
       {
         allow: [
           "@jobber/components/*",
+          "@jobber/components-native",
           "@jobber/hooks/*",
           "@jobber/design/*",
           "lodash/*",
@@ -43,11 +45,20 @@ module.exports = {
       files: ["*.stories.mdx"],
       extends: "plugin:mdx/recommended",
       rules: {
-        "react-native/no-inline-styles": "off",
         "no-alert": "off",
         "@typescript-eslint/naming-convention": "off",
         "@typescript-eslint/no-unused-expressions": "off",
         "import/no-extraneous-dependencies": "off",
+      },
+    },
+    {
+      files: ["*.stories.tsx"],
+      rules: {
+        "import/no-relative-parent-imports": "off",
+        "no-alert": "off",
+        "@typescript-eslint/naming-convention": "off",
+        "@typescript-eslint/no-unused-expressions": "off",
+        "import/no-default-export": "off",
       },
     },
   ],
