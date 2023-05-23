8f36bb9d13163911d7c20a2332c5aefa8ec65b1c
Darryl Tec <darryl.t@getjobber.com>
2023-05-23 11:30:39 -0600

    docs: replace text stories with tsx

---
+++ docs/components/Text/Mobile.stories.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Text } from "@jobber/components-native";
+import { JobberStyle } from "@jobber/design/foundation";
+
+export default {
+  title: "Components/Text/Mobile",
+  component: Text,
+  parameters: {
+    viewMode: "story",
+    previewTabs: { "storybook/docs/panel": { hidden: true } },
+  },
+} as ComponentMeta<typeof Text>;
+
+const Template: ComponentStory<typeof Text> = args => (
+  <Text {...args}>{args.children}</Text>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "Ask the information you need upfront from clients and new leads",
+};
+
+export const ReverseTheme = Template.bind({});
+ReverseTheme.parameters = {
+  backgrounds: {
+    default: "dark",
+    values: [
+      {
+        name: "dark",
+        value: JobberStyle["color-surface--reverse"],
+      },
+      {
+        name: "light",
+        value: JobberStyle["color-surface"],
+      },
+    ],
+  },
+};
+ReverseTheme.args = {
+  children: "Changes Text color for display on a dark background",
+  reverseTheme: true,
+};
+
+export const TextSupporting = Template.bind({});
+TextSupporting.args = {
+  children:
+    "Supporting Text should be used to supplement additional, more important content",
+  level: "textSupporting",
+};
