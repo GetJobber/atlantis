0586afb2950861b473c98a85d2c7fa2f5a7aa9b6
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2023-05-25 14:50:22 -0300

    Migrate EmptyState

---
+++ packages/components-native/src/EmptyState/EmptyState.tsx
@@ -0,0 +1,91 @@
+import { IconColorNames, IconNames } from "@jobber/design";
+import React from "react";
+import { View } from "react-native";
+import { styles } from "./EmptyState.style";
+import { Text } from "../Text";
+import { Content } from "../Content";
+import { Icon } from "../Icon";
+import { Heading } from "../Heading";
+import { Button, ButtonType } from "../Button";
+
+interface Action {
+  label: string;
+  onPress: () => void;
+  disabled?: boolean;
+}
+
+interface ButtonAction {
+  action?: Action;
+  type: ButtonType;
+}
+
+function ButtonAction({ action, type }: ButtonAction) {
+  if (!action) return <></>;
+
+  return <Button fullWidth={false} type={type} {...action} />;
+}
+
+export interface EmptyStateProps {
+  /**
+   * Icon to display.
+   */
+  readonly icon?: IconNames;
+
+  /**
+   * Color of Icon to display.
+   */
+  readonly iconColor?: IconColorNames;
+
+  /**
+   * Title of the empty state.
+   */
+  readonly title?: string;
+
+  /**
+   * Description of the empty state.
+   */
+  readonly description?: string;
+
+  /**
+   * Handler for the primary action.
+   */
+  readonly primaryAction?: Action;
+
+  /**
+   * Handler for the secondary action.
+   */
+  readonly secondaryAction?: Action;
+}
+
+export function EmptyState({
+  icon,
+  title,
+  description,
+  primaryAction,
+  secondaryAction,
+  iconColor = "blue",
+}: EmptyStateProps): JSX.Element {
+  return (
+    <Content>
+      {icon && (
+        <View style={styles.icon}>
+          <Icon name={icon} size="large" color={iconColor} />
+        </View>
+      )}
+      <Content spacing="none" childSpacing="small">
+        {title && (
+          <Heading level="subHeading" align="center">
+            {title}
+          </Heading>
+        )}
+        {description && <Text align="center">{description}</Text>}
+        {(primaryAction || secondaryAction) && (
+          <Content spacing="none" childSpacing="small">
+            <ButtonAction action={primaryAction} type="primary" />
+            <ButtonAction action={secondaryAction} type="tertiary" />
+          </Content>
+        )}
+      </Content>
+    </Content>
+  );
+}
