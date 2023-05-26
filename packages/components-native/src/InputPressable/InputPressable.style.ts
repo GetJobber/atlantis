2a4b6febed51a033cf34de30b2d0a80c6a24c919
Darryl Tec <darryl.t@getjobber.com>
2023-05-26 09:35:00 -0600

    fix: change use tokens instead of JobberStyle

--- packages/components-native/src/InputPressable/InputPressable.style.ts
+++ packages/components-native/src/InputPressable/InputPressable.style.ts
@@ -1,9 +1,9 @@
-import { JobberStyle } from "@jobber/design/foundation";
+import { tokens } from "@jobber/design/foundation";
 import { StyleSheet } from "react-native";
 import { typographyStyles } from "../Typography/Typography.style";
 
 const miniLabelFontSize = typographyStyles.smallSize.fontSize || 0;
-const miniLabelPadding = JobberStyle["space-small"];
+const miniLabelPadding = tokens["space-small"];
 
 export const styles = StyleSheet.create({
   pressable: {
@@ -24,6 +24,6 @@ export const styles = StyleSheet.create({
   },
 
   inputInvalid: {
-    borderColor: JobberStyle["color-critical"],
+    borderColor: tokens["color-critical"],
   },
 });
