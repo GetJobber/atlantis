2668df489cb3dd348ad62bfd069715700c346876
Taylor Noj <104797704+taylorvnoj@users.noreply.github.com>
2023-05-31 11:55:36 -0400

    feat(components-native): Import Switch to components-native (#1295)
    * import switch
    
    * update hook import
    
    * remove comment
    
    Co-authored-by: Michael Paradis <michael.p@getjobber.com>
    
    ---------
    
    Co-authored-by: Michael Paradis <michael.p@getjobber.com>

---
+++ packages/components-native/src/Switch/components/BaseSwitch/BaseSwitch.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { Platform } from "react-native";
+import { Switch } from "react-native-gesture-handler";
+import { useFormController } from "../../../hooks";
+import { tokens } from "../../../utils/design";
+
+export interface BaseSwitchProps {
+  /**
+   * Value of the switch
+   */
+  readonly value?: boolean;
+
+  /**
+   * Default value of the switch when uncontrolled
+   */
+  readonly defaultValue?: boolean;
+
+  /**
+   * Callback to handle value changes
+   */
+  onValueChange?(val: boolean): void;
+
+  /**
+   * When true, the switch cannot be toggled
+   */
+  readonly disabled?: boolean;
+
+  /**
+   * Accessibility label for this switch
+   */
+  readonly accessibilityLabel?: string;
+
+  /**
+   * Name of the input.
+   */
+  readonly name?: string;
+}
+
+export function BaseSwitch({
+  value,
+  defaultValue,
+  onValueChange,
+  disabled = false,
+  accessibilityLabel,
+  name,
+}: BaseSwitchProps): JSX.Element {
+  const { field } = useFormController({
+    name,
+    value: value ?? defaultValue,
+  });
+
+  const internalValue = value ?? field.value;
+
+  function getThumbColor() {
+    if (Platform.OS === "android") {
+      if (disabled) {
+        return tokens["color-disabled"];
+      } else if (internalValue) {
+        return tokens["color-interactive"];
+      } else {
+        return tokens["color-surface--background"];
+      }
+    }
+    return undefined; //use default iOS
+  }
+
+  function getTrackColors() {
+    if (Platform.OS === "android") {
+      return {
+        true: disabled
+          ? tokens["color-disabled--secondary"]
+          : tokens["color-green--lighter"],
+        false: disabled
+          ? tokens["color-disabled--secondary"]
+          : tokens["color-disabled"],
+      };
+    } else {
+      //iOS
+      return {
+        true: tokens["color-interactive"],
+        false: tokens["color-surface--background"],
+      };
+    }
+  }
+
+  return (
+    <Switch
+      value={internalValue}
+      onValueChange={(val: boolean) => {
+        if (!disabled) {
+          onValueChange?.(val);
+          field.onChange(val);
+        }
+      }}
+      disabled={disabled}
+      thumbColor={getThumbColor()}
+      trackColor={getTrackColors()}
+      ios_backgroundColor={tokens["color-surface--background"]}
+      accessibilityLabel={accessibilityLabel}
+      accessibilityRole={"switch"}
+      accessibilityState={{
+        disabled: disabled,
+        checked: internalValue,
+      }}
+    />
+  );
+}
