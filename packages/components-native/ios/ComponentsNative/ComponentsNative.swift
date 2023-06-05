966ac413813f26a6c00ad134ae949d176f061a0c
Michael Paradis <michael.p@getjobber.com>
2023-06-05 09:05:11 -0700

    Might work

---
+++ packages/components-native/ios/ComponentsNative/ComponentsNative.swift
@@ -0,0 +1,8 @@
+@objc(ComponentsNative)
+class ComponentsNative: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
