74dc507119384eb52e25709d34f7a3ebd7e39563
Michael Paradis <michael.p@getjobber.com>
2023-06-05 14:14:46 -0700

    Clean up random files

--- packages/components-native/ios/ComponentsNative/ComponentsNative.swift
+++
@@ -1,8 +0,0 @@
-@objc(ComponentsNative)
-class ComponentsNative: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
