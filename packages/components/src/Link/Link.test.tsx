4074296afb0e17d9a6c495cd2aad2f59cfc435c7
Devin Serink <devin.s@getjobber.com>
2023-06-15 17:01:11 -0600

    Replaced snapshot tests

--- packages/components/src/Link/Link.test.tsx
+++ packages/components/src/Link/Link.test.tsx
@@ -4,14 +4,21 @@ import { Link } from ".";
 
 afterEach(cleanup);
 
-it("renders a Link", () => {
-  const { container } = render(<Link text="Foo" url="https://getjobber.com" />);
-  expect(container).toMatchSnapshot();
-});
-
-it("renders an external Link", () => {
-  const { container } = render(
-    <Link text="Foo" url="https://example.com" external={true} />,
-  );
-  expect(container).toMatchSnapshot();
+describe("when a Link is rendered", () => {
+  it("opens the link in the same tab if external is left at default value of false", () => {
+    const { getByText } = render(
+      <Link url="https://getjobber.com">This is a link</Link>,
+    );
+    const link = getByText("This is a link");
+    expect(link.getAttribute("target")).toBeNull();
+  });
+  it("opens in a new tab if external is set to true", () => {
+    const { getByText } = render(
+      <Link url="https://getjobber.com" external={true}>
+        This is a link
+      </Link>,
+    );
+    const link = getByText("This is a link");
+    expect(link.getAttribute("target")).toBe("_blank");
+  });
 });
