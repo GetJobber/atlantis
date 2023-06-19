8b3e8e17b72d162187e36c94e11f295d5befd51b
Michael Paradis <michael.p@getjobber.com>
2023-06-19 12:30:25 -0700

    Remove commented out exports

--- packages/components-native/src/InputFile/index.ts
+++ packages/components-native/src/InputFile/index.ts
@@ -1,6 +1,2 @@
 export { InputFile } from "./InputFile";
 export type { FileUpload, UploadParams } from "./types";
-// export {
-//   fetchInternalUploadParams,
-//   fetchPublicUploadParams,
-// } from "./utils/fetchUploadParams";
