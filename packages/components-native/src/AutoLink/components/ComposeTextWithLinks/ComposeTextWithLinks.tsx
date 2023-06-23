55a17310882499002be15e814a181e7dbe6465e0
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2023-06-23 10:56:22 -0300

    Migrate Autolink

---
+++ packages/components-native/src/AutoLink/components/ComposeTextWithLinks/ComposeTextWithLinks.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useIntl } from "react-intl";
+import { Platform } from "react-native";
+import { ComposeTextWithLinksProps } from "../../types";
+import { onLongPressLink, onPressLink } from "../../utils";
+import { Link } from "../Link/Link";
+import { Text } from "../../../Text";
+
+export function ComposeTextWithLinks({
+  part,
+  index,
+  match,
+  bottomTabsVisible,
+  selectable = true,
+}: ComposeTextWithLinksProps): JSX.Element {
+  const { formatMessage } = useIntl();
+
+  const isLink = match.getType();
+
+  if (isLink) {
+    return (
+      <Link
+        key={index}
+        onPress={() => onPressLink(match)}
+        onLongPress={() => {
+          if (selectable && Platform.OS === "android") {
+            return;
+          }
+          onLongPressLink(match, bottomTabsVisible, formatMessage);
+        }}
+      >
+        {match.getAnchorText()}
+      </Link>
+    );
+  }
+
+  return <Text key={index}>{part}</Text>;
+}
