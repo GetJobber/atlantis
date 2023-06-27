b80ed9cf4d556e7c0cc4fb61b87adfcfebe65f2c
Darryl Tec <darryl.t@getjobber.com>
2023-06-27 09:02:40 -0600

    chore: update other snaps

--- packages/components/src/InputValidation/InputValidation.test.tsx
+++ packages/components/src/InputValidation/InputValidation.test.tsx
@@ -15,21 +15,26 @@ it("renders the input validation messages", () => {
           role="alert"
           tabindex="0"
         >
-          <svg
+          <span
             class="Z6OfUI2sH34- qm8vYTlCGG4-"
-            data-testid="alert"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+            style="transform: none;"
           >
-            <path
-              class="avM4gudjYeg-"
-              d="M12 7a1 1 0 0 0-1 1v5a1 1 0 1 0 2 0V8a1 1 0 0 0-1-1Zm-1 10a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
-            />
-            <path
-              class="avM4gudjYeg-"
-              d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2s10 4.477 10 10Zm-2 0a8 8 0 1 0-16 0 8 8 0 0 0 16 0Z"
-            />
-          </svg>
+            <svg
+              class="Z6OfUI2sH34- qm8vYTlCGG4-"
+              data-testid="alert"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                class="avM4gudjYeg-"
+                d="M12 7a1 1 0 0 0-1 1v5a1 1 0 1 0 2 0V8a1 1 0 0 0-1-1Zm-1 10a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
+              />
+              <path
+                class="avM4gudjYeg-"
+                d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2s10 4.477 10 10Zm-2 0a8 8 0 1 0-16 0 8 8 0 0 0 16 0Z"
+              />
+            </svg>
+          </span>
           <p
             class="base regular small critical"
           >
