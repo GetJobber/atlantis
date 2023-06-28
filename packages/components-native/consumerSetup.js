082a5e83f4d60337a9029fb19458c030040bc8bf
Michael Paradis <michael.p@getjobber.com>
2023-06-28 14:18:20 -0700

    Possible fix

--- packages/components-native/consumerSetup.js
+++ packages/components-native/consumerSetup.js
@@ -1,8 +1,41 @@
 // Not used right now but something like this may be needed
-jest.mock("react-native-localize", () => {
-  const mockRNLocalize = jest.requireActual("react-native-localize");
-  return {
-    ...mockRNLocalize,
-    getTimeZone: () => "UTC",
-  };
-});
+// jest.mock("react-native-localize", () => {
+//   const mockRNLocalize = jest.requireActual("react-native-localize");
+//   return {
+//     ...mockRNLocalize,
+//     getTimeZone: () => "UTC",
+//   };
+// });
+
+// require("react-native-reanimated/lib/reanimated2/jestUtils").setUpTests();
+
+// jest.mock("react-native-reanimated", () => {
+//   const reanimated = require("react-native-reanimated/mock");
+//   const timing = () => ({ start: () => undefined });
+//   return {
+//     ...reanimated,
+//     default: {
+//       ...reanimated.default,
+//       timing,
+//       // The mock for `call` immediately calls the callback which is incorrect
+//       // So we override it with a no-op
+//       call: () => undefined,
+//     },
+//     timing,
+//     FadeIn: {
+//       duration: () => undefined,
+//     },
+//   };
+// });
+jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
+
+// jest.mock("react-native-uuid", () => {
+//   let count = 0;
+//   const finalSection = 426655440000;
+
+//   function fakeUUID() {
+//     count += 1;
+//     return `123e4567-e89b-12d3-a456-${finalSection + count}`;
+//   }
+//   return { v1: fakeUUID, v4: fakeUUID };
+// });
