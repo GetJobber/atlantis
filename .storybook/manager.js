fef377199fac13bff96904f8ace83c4a42d3c65a
Darryl Tec <darryl.t@getjobber.com>
2023-06-29 10:36:05 -0600

    feat: overwrite sidebar styling

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,6 +2,8 @@ import { addons } from "@storybook/addons";
 import theme from "./theme";
 import favicon from "./assets/favicon.svg";
 import React, { useEffect } from "react";
+import "./assets/css/manager.css";
+import "@jobber/design/foundation.css";
 
 const link = document.createElement("link");
 link.setAttribute("rel", "shortcut icon");
@@ -11,6 +13,7 @@ document.head.appendChild(link);
 addons.setConfig({
   theme,
   sidebar: {
+    collapsedRoots: ["components", "design", "content", "guides", "hooks", "packages", "changelog"],
     renderLabel: (api) => {
       const ref = React.useRef();
         useEffect(() => {
