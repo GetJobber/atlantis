f89b17b50adf9f44842829c48797d640a80fee2d
Darryl Tec <darryl.t@getjobber.com>
2023-06-29 14:53:59 -0600

    fix: componentize the label workaround

---
+++ .storybook/components/SidebarLabel/SidebarLabel.tsx
@@ -0,0 +1,50 @@
+import React, { MouseEvent, useEffect, useRef } from "react";
+import { useStorybookApi } from "@storybook/api";
+
+export function SidebarLabel(label: Record<string, any>) {
+  const ref = useRef<HTMLSpanElement>(null);
+  const { selectStory, getCurrentStoryData } = useStorybookApi();
+  const currentStory = getCurrentStoryData();
+
+  useEffect(() => {
+    const parentElement = ref.current?.parentElement;
+
+    // Automatically expand the first level components group by initiating a
+    // click that relies on event bubbling on component mount.
+    if (
+      label.id.startsWith("components") &&
+      label.depth === 1 &&
+      parentElement?.getAttribute("aria-expanded") === "false"
+    ) {
+      parentElement?.click();
+    }
+  }, []);
+
+  return (
+    <span ref={ref} onClick={handleClick}>
+      {label.name}
+    </span>
+  );
+
+  function handleClick(event: MouseEvent<HTMLSpanElement>) {
+    const targetID = label.children[0];
+
+    if (
+      label.type === "group" &&
+      // Ensure we only trigger the selection when it's a second level group
+      label.depth > 1 &&
+      // Ensure we only trigger the selection when it's a doc page. This should
+      // still allow Storybook's auto selecting the first story in a group.
+      targetID?.endsWith("page") &&
+      // Don't select the already selected story. This allows the collapsing of
+      // the whole group again.
+      currentStory.id !== targetID
+    ) {
+      // Don't trigger the parent click handler which stops the collapsing and
+      // expanding again
+      event.stopPropagation();
+
+      selectStory(label.children[0]);
+    }
+  }
+}
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,5 @@
 import { addons } from "@storybook/addons";
-import React, { useEffect, useRef } from "react";
-import { useStorybookApi } from "@storybook/api";
+import { SidebarLabel } from "./components/SidebarLabel";
 import theme from "./theme";
 import favicon from "./assets/favicon.svg";
 import "./assets/css/manager.css";
@@ -23,35 +22,6 @@ addons.setConfig({
       "packages",
       "changelog",
     ],
-    renderLabel: label => {
-      const { selectStory } = useStorybookApi();
-      const ref = useRef();
-      useEffect(() => {
-        if (
-          label.id.startsWith("components") &&
-          label.depth === 1 &&
-          ref.current?.parentElement?.getAttribute("aria-expanded") !== "true"
-        ) {
-          ref.current?.parentElement?.click();
-        }
-      }, []);
-
-      return (
-        <span ref={ref} onClick={handleClick}>
-          {label.name}
-        </span>
-      );
-
-      function handleClick(event) {
-        if (
-          label.type === "group" &&
-          label.depth > 1 &&
-          label.children?.length
-        ) {
-          event.stopPropagation();
-          selectStory(label.children[0]);
-        }
-      }
-    },
+    renderLabel: SidebarLabel,
   },
 });
