778e9de3869a063a737a057b7f30e081c0479a7c
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2023-07-04 09:23:43 -0300

    Remove a mock from consumerSetup

--- packages/components-native/consumerSetup.js
+++ packages/components-native/consumerSetup.js
@@ -9,24 +9,6 @@
 
 require("react-native-reanimated/lib/reanimated2/jestUtils").setUpTests();
 
-jest.mock("react-native-reanimated", () => {
-  const reanimated = require("react-native-reanimated/mock");
-  const timing = () => ({ start: () => undefined });
-  return {
-    ...reanimated,
-    default: {
-      ...reanimated.default,
-      timing,
-      // The mock for `call` immediately calls the callback which is incorrect
-      // So we override it with a no-op
-      call: () => undefined,
-    },
-    timing,
-    FadeIn: {
-      duration: () => undefined,
-    },
-  };
-});
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 
 // jest.mock("react-native-uuid", () => {
