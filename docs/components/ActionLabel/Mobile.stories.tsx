600a6614132c10b658da244e014aa788a88be733
Darryl Tec <darryl.t@getjobber.com>
2023-07-10 15:25:44 -0600

    docs: update action label stories (#1375)
    * docs: replace action label mobile mdx with tsx
    
    * docs: move action label figma to docs
    
    * docs: grammar
    
    * docs: add example for dark backgrounds
    
    * Update docs/components/ActionLabel/ActionLabel.stories.mdx
    
    * docs: bring back disabled callout
    
    * docs: un code the ActionLabel name

---
+++ docs/components/ActionLabel/Mobile.stories.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ActionLabel } from "@jobber/components-native";
+
+export default {
+  title: "Components/Actions/ActionLabel/Mobile",
+  parameters: {
+    viewMode: "story",
+  },
+  component: ActionLabel,
+} as ComponentMeta<typeof ActionLabel>;
+
+const BasicTemplate: ComponentStory<typeof ActionLabel> = args => (
+  <ActionLabel {...args}>{args.children}</ActionLabel>
+);
+
+export const Basic = BasicTemplate.bind({});
+Basic.args = { children: "I am a label text" };
+
+export const Color = BasicTemplate.bind({});
+Color.args = { variation: "learning", children: "Learning" };
+
+export const DarkBackground = BasicTemplate.bind({});
+DarkBackground.args = { variation: "onPrimary", children: "Light Me Up" };
+DarkBackground.parameters = {
+  backgrounds: { default: "dark" },
+};
+
+export const Disabled = BasicTemplate.bind({});
+Disabled.args = { disabled: true, children: "Disabled" };
