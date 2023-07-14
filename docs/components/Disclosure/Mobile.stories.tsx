e0e521e13cefafb26d65af2824aad67994ed63d4
Taylor Noj <104797704+taylorvnoj@users.noreply.github.com>
2023-07-14 10:47:39 -0400

    docs(components): Update Disclosure stories (#1364)
    * update docs page for Disclosure
    
    * add Disclosure web story
    
    * add code sandbox
    
    * add mobile story but doesn't open
    
    * add babel plugin to check console
    
    * Attempting to configure reanimated with storybook
    
    * Attempting to configure reanimated with storybook, added missed change
    
    * update react-native-web version
    
    * clean up
    
    * add background color in mobile story
    
    * remove background color
    
    ---------
    
    Co-authored-by: Michael Paradis <michael.p@getjobber.com>

---
+++ docs/components/Disclosure/Mobile.stories.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Disclosure, Text } from "@jobber/components-native";
+
+export default {
+  title: "Components/Layouts and Structure/Disclosure/Mobile",
+  component: Disclosure,
+  parameters: {
+    viewMode: "story",
+  },
+} as ComponentMeta<typeof Disclosure>;
+
+const BasicTemplate: ComponentStory<typeof Disclosure> = args => {
+  const [open, setOpen] = useState(false);
+  return <Disclosure {...args} open={open} onToggle={() => setOpen(!open)} />;
+};
+
+export const Basic = BasicTemplate.bind({});
+Basic.args = {
+  header: <Text>Advanced Instructions</Text>,
+  content: "For every 2 team members you add, your profits will triple.",
+  isEmpty: false,
+};
