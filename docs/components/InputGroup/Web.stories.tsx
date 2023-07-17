a5db316d5e87b4bbf38a4118ef05caf1253f3c10
Taylor Noj <taylor.n@getjobber.com>
2023-07-17 15:47:02 -0400

    add extraImports to web stories

--- docs/components/InputGroup/Web.stories.tsx
+++ docs/components/InputGroup/Web.stories.tsx
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { CivilTime } from "@std-proposal/temporal";
 import { InputGroup } from "@jobber/components/InputGroup";
 import { InputTime } from "@jobber/components/InputTime";
 import { InputText } from "@jobber/components/InputText";
-import { Autocomplete } from "@jobber/components/Autocomplete";
 
 export default {
   title: "Components/Forms and Inputs/InputGroup/Web",
   component: InputGroup,
   parameters: {
     viewMode: "story",
-    previewTabs: { code: { hidden: false } },
+    previewTabs: {
+      code: {
+        hidden: false,
+        extraImports: {
+          "@std-proposal/temporal": ["CivilTime"],
+        },
+      },
+    },
   },
 } as ComponentMeta<typeof InputGroup>;
 
@@ -41,48 +47,6 @@ const NestedTemplate: ComponentStory<typeof InputGroup> = args => {
   );
 };
 
-const options = [
-  { value: 1, label: "Nostromo" },
-  { value: 2, label: "Rodger Young" },
-  { value: 3, label: "Serenity" },
-  { value: 4, label: "Sleeper Service" },
-  { value: 5, label: "Enterprise" },
-  { value: 6, label: "Enterprise-D" },
-];
-
-const NestedWithAutocompleteTemplate: ComponentStory<
-  typeof InputGroup
-> = args => {
-  const [value, setValue] = useState<Option: any | undefined>();
-  return (
-    <InputGroup {...args}>
-      <InputText placeholder="Street 1" />
-      <InputText placeholder="Street 2" />
-      <InputGroup flowDirection="horizontal">
-        <InputText placeholder="City" />
-        <Autocomplete
-          value={value}
-          initialOptions={options}
-          onChange={newValue => setValue(newValue)}
-          getOptions={getOptions}
-          placeholder="Autocomplete this!"
-        />
-      </InputGroup>
-      <InputGroup flowDirection="horizontal">
-        <InputText placeholder="Zip code" />
-        <InputText placeholder="Country" />
-      </InputGroup>
-    </InputGroup>
-  );
-  function getOptions(text: string) {
-    if (text === "") {
-      return options;
-    }
-    const filterRegex = new RegExp(text, "i");
-    return options.filter(option => option.label.match(filterRegex));
-  }
-};
-
 export const Basic = BasicTemplate.bind({});
 Basic.args = {
   flowDirection: "vertical",
@@ -90,10 +54,5 @@ Basic.args = {
 
 export const Nested = NestedTemplate.bind({});
 Nested.args = {
-  flowDirection: "vertical",
-};
-
-export const NestedWithAutocomplete = NestedWithAutocompleteTemplate.bind({});
-NestedWithAutocomplete.args = {
   flowDirection: "vertical",
 };
