4cbf2d1931d3e2a13ee8751086aaa089cf81803b
Darryl Tec <darryl.t@getjobber.com>
2023-07-17 08:28:44 -0600

    docs: add button group docs and mobile example (#1380)
    * docs: add initial button group docs
    
    * docs: add mobile examples
    
    * docs: force story to show as mobile
    
    * docs: allow adding disclaimer on stories
    
    * update contributing guide
    
    * Update .storybook/components/StoryDetails/StoryDetails.tsx
    
    * Update .storybook/components/StoryDetails/StoryDetails.css
    
    Co-authored-by: Michael Paradis <michael.p@getjobber.com>
    
    ---------
    
    Co-authored-by: Michael Paradis <michael.p@getjobber.com>

---
+++ docs/components/ButtonGroup/Mobile.SecondaryAction.stories.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ButtonGroup } from "@jobber/components-native";
+
+export default {
+  title: "Components/Actions/ButtonGroup/Mobile/ButtonGroup.SecondaryAction",
+  parameters: {
+    viewMode: "story",
+    viewport: { defaultViewport: "mobile1" },
+    showNativeOnWebDisclaimer: true,
+  },
+  component: ButtonGroup.SecondaryAction,
+} as ComponentMeta<typeof ButtonGroup>;
+
+const Template: ComponentStory<typeof ButtonGroup.SecondaryAction> = args => (
+  <ButtonGroup
+    bottomSheetHeading="What would you like to do"
+    showCancelInBottomSheet={true}
+  >
+    <ButtonGroup.PrimaryAction
+      label={"Click that ellipsis >>>"}
+      buttonType={"secondary"}
+      onPress={() => alert("No, not me. The ellipsis!")}
+    />
+    <ButtonGroup.SecondaryAction {...args} />
+  </ButtonGroup>
+);
+
+export const Primary = Template.bind({});
+Primary.storyName = "ButtonGroup.SecondaryAction";
+Primary.args = {
+  label: "Create",
+  icon: "plus",
+  onPress: () => console.log("create"),
+};
