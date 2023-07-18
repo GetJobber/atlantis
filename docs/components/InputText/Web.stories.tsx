581670f079e2d195a294a614ba2f67a7cf42ff35
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2023-07-18 17:37:17 -0300

    Finish docs for InputText

--- docs/components/InputText/Web.stories.tsx
+++ docs/components/InputText/Web.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { InputText } from "@jobber/components/InputText";
 
@@ -11,8 +11,30 @@ export default {
 } as ComponentMeta<typeof InputText>;
 
 const BasicTemplate: ComponentStory<typeof InputText> = args => {
-  return <InputText name="age" placeholder="Age in words" />;
+  return <InputText {...args} />;
 };
 
 export const Basic = BasicTemplate.bind({});
-Basic.args = {};
+Basic.args = {
+  name: "age",
+  placeholder: "Age in words",
+};
+
+export const Multiline = BasicTemplate.bind({});
+Multiline.args = {
+  defaultValue: "Rocinante",
+  multiline: true,
+  rows: { min: 1, max: 5 },
+};
+
+export const Readonly = BasicTemplate.bind({});
+Readonly.args = {
+  defaultValue: "Rocinante",
+  readonly: true,
+};
+
+export const Loading = BasicTemplate.bind({});
+Loading.args = {
+  name: "phoneNumber",
+  loading: true,
+};
