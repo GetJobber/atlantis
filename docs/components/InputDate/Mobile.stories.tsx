144370408d808b84c9f1b09097693eabf859430d
Michael Paradis <michael.p@getjobber.com>
2023-07-19 14:41:52 -0700

    Fixed issues with code sandbox not working

--- docs/components/InputDate/Mobile.stories.tsx
+++ docs/components/InputDate/Mobile.stories.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import omit from "lodash/omit";
 import { InputDate } from "@jobber/components-native";
 
 export default {
@@ -8,36 +7,32 @@ export default {
   component: InputDate,
   parameters: {
     viewMode: "story",
-    previewTabs: { code: { hidden: false } },
+    previewTabs: {
+      code: {
+        hidden: false,
+      },
+    },
     viewport: { defaultViewport: "mobile1" },
     showNativeOnWebDisclaimer: true,
   },
 } as ComponentMeta<typeof InputDate>;
 
-const BasicTemplate: ComponentStory<typeof InputDate> = args => {
-  const [date, setDate] = useState<Date | undefined>(new Date("11/11/2011"));
-  const props = omit({ ...args }, "name", "validations", "defaultValue");
-  return <InputDate {...props} value={date} onChange={setDate} />;
+const BasicTemplate: ComponentStory<any> = args => {
+  const [date, setDate] = useState(new Date("11/11/2011"));
+  return <InputDate {...args} value={date} onChange={setDate} />;
 };
 
 export const Basic = BasicTemplate.bind({});
 
 Basic.args = {};
 
-const EmptyValueLabelTemplate: ComponentStory<typeof InputDate> = args => {
-  return <InputDate {...args} />;
-};
-
-export const EmptyValueLabel = EmptyValueLabelTemplate.bind({});
+export const EmptyValueLabel = BasicTemplate.bind({});
 EmptyValueLabel.args = {
   placeholder: "Start date",
   emptyValueLabel: "Unscheduled",
 };
 
-const InvalidTemplate: ComponentStory<typeof InputDate> = args => {
-  return <InputDate {...args} />;
-};
-export const Invalid = InvalidTemplate.bind({});
+export const Invalid = BasicTemplate.bind({});
 Invalid.args = {
   placeholder: "Start date",
   invalid: "Start Date is required",
