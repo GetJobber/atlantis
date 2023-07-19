46d13951510cdf79b3c911a6e19207a2b6d9ff86
Darryl Tec <darryl.t@getjobber.com>
2023-07-19 09:32:07 -0600

    docs: allow code tab with no preview for mobile and other improvements (#1393)
    * docs: initial setup for code only
    
    * docs: prettify code
    
    * docs: add more playground tests
    
    * docs: add loading state and ensure docs stays
    
    * docs: update text and typography
    
    * docs: turn off preview again
    
    * replace logic on determining playground
    
    * enable code tab on actions mobile stories

--- .storybook/components/Playground/Playground.test.tsx
+++ .storybook/components/Playground/Playground.test.tsx
@@ -13,12 +13,24 @@ afterEach(() => {
 });
 
 describe("Playground", () => {
+  it("should render a playground", () => {
+    mockStoryData({ sourceCode: "args => <Button />" });
+    const { container, getByTitle } = render(<Playground />);
+
+    expect(container).toHaveTextContent(
+      "export function Example() { return <Button />; }",
+    );
+
+    expect(getByTitle("Sandpack Preview")).toBeInTheDocument();
+    expect(container).not.toHaveTextContent("Read-only");
+  });
+
   it("should render a basic read only source code", () => {
     mockStoryData({ sourceCode: "() => <p></p>" });
     const { container } = render(<Playground />);
 
     expect(container).toHaveTextContent(
-      "export function Example() { return <p></p> }",
+      "export function Example() { return <p></p>; }",
     );
     expect(container).toHaveTextContent("Read-only");
   });
@@ -33,6 +45,13 @@ describe("Playground", () => {
     expect(container).toMatchInlineSnapshot(`<div />`);
   });
 
+  it("should render the code unavailable div when it's not a story", () => {
+    mockStoryData({ type: "docs", title: "Design/Foundation" });
+    const { getByTestId } = render(<Playground />);
+
+    expect(getByTestId("code-unavailable")).toBeInTheDocument();
+  });
+
   describe("Props and arg values", () => {
     it("should render the spread arg values", () => {
       mockStoryData({
@@ -42,7 +61,7 @@ describe("Playground", () => {
       const { container } = render(<Playground />);
 
       expect(container).toHaveTextContent(
-        `export function Example() { return <Button size={"large"} /> }`,
+        `export function Example() { return <Button size={"large"} />; }`,
       );
     });
 
@@ -84,9 +103,7 @@ describe("Playground", () => {
       mockStoryData({ args: { data: ["fee", "fi", "fo", "fum"] } });
       const { container } = render(<Playground />);
 
-      expect(container).toHaveTextContent(
-        `data={["fee", "fi", "fo", "fum", ]}`,
-      );
+      expect(container).toHaveTextContent(`data={["fee", "fi", "fo", "fum"]}`);
     });
 
     it("should render an object arg value", () => {
@@ -96,7 +113,7 @@ describe("Playground", () => {
       const { container } = render(<Playground />);
 
       expect(container).toHaveTextContent(
-        `action={{label: "click me", onClick: () => undefined, }}`,
+        `action={{ label: "click me", onClick: () => undefined }}`,
       );
     });
   });
@@ -124,7 +141,7 @@ describe("Playground", () => {
       'import { Button } from "@jobber/components/Button";',
     );
     expect(container).toHaveTextContent(
-      "export function Example() { return <Button /> }",
+      "export function Example() { return <Button />; }",
     );
     expect(container).not.toHaveTextContent("Read-only");
   });
@@ -155,70 +172,71 @@ describe("Playground", () => {
     );
   });
 
-  it("should include the parameter extra imports when it's available", () => {
-    mockStoryData({
-      // @ts-expect-error - Storybook API types does allow this
-      parameters: {
-        previewTabs: {
-          code: {
-            extraImports: { "@jobber/components/Tab": ["Tabs", "Tab"] },
-          },
-        },
-      },
-      sourceCode: `args => <Content>Sup!</Content>`,
-    });
-    const { container } = render(<Playground />);
-
-    expect(container).toHaveTextContent(
-      'import { Content } from "@jobber/components/Content";',
-    );
-    expect(container).toHaveTextContent(
-      'import { Tabs, Tab } from "@jobber/components/Tab";',
-    );
-  });
-
-  it("should allow aliases for extra imports parameter", () => {
-    mockStoryData({
-      // @ts-expect-error - Storybook API types does allow this
-      parameters: {
-        previewTabs: {
-          code: {
-            extraImports: {
-              "react-router-dom": ["Router"],
+  describe("Extra imports", () => {
+    it("should include the parameter extra imports when it's available", () => {
+      mockStoryData({
+        // @ts-expect-error - Storybook API types does allow this
+        parameters: {
+          previewTabs: {
+            code: {
+              extraImports: { "@jobber/components/Tab": ["Tabs", "Tab"] },
             },
           },
         },
-      },
-      sourceCode: `args => (
+        sourceCode: `args => <Content>Sup!</Content>`,
+      });
+      const { container } = render(<Playground />);
+
+      expect(container).toHaveTextContent(
+        'import { Content } from "@jobber/components/Content";',
+      );
+      expect(container).toHaveTextContent(
+        'import { Tabs, Tab } from "@jobber/components/Tab";',
+      );
+    });
+
+    it("should allow aliases for extra imports parameter", () => {
+      mockStoryData({
+        // @ts-expect-error - Storybook API types does allow this
+        parameters: {
+          previewTabs: {
+            code: {
+              extraImports: {
+                "react-router-dom": ["Router"],
+              },
+            },
+          },
+        },
+        sourceCode: `args => (
         <Router basename="/components/button">
           <Content>Sup!</Content>
         </Router>)`,
+      });
+      const { container } = render(<Playground />);
+
+      expect(container).toHaveTextContent(
+        'import { Router } from "react-router-dom";',
+      );
+
+      expect(container).not.toHaveTextContent(
+        `import { Router } from "@jobber/components/Router`,
+      );
     });
-    const { container } = render(<Playground />);
 
-    expect(container).toHaveTextContent(
-      'import { Router } from "react-router-dom";',
-    );
-
-    expect(container).not.toHaveTextContent(
-      `import { Router } from "@jobber/components/Router`,
-    );
-  });
-
-  it("should allow imports of subcomponents of @jobber/components", () => {
-    mockStoryData({
-      // @ts-expect-error - Storybook API types does allow this
-      parameters: {
-        previewTabs: {
-          code: {
-            extraImports: {
-              "react-router-dom": ["Router"],
-              "@jobber/components/Drawer": ["DrawerGrid"],
+    it("should allow imports of subcomponents of @jobber/components", () => {
+      mockStoryData({
+        // @ts-expect-error - Storybook API types does allow this
+        parameters: {
+          previewTabs: {
+            code: {
+              extraImports: {
+                "react-router-dom": ["Router"],
+                "@jobber/components/Drawer": ["DrawerGrid"],
+              },
             },
           },
         },
-      },
-      sourceCode: `args => (
+        sourceCode: `args => (
         <Router basename="/components/button">
         <DrawerGrid>
           <Content>Sup!</Content>
@@ -228,22 +246,40 @@ describe("Playground", () => {
         </DrawerGrid>
         </Router>
         )`,
-    });
-    const { container } = render(<Playground />);
+      });
+      const { container } = render(<Playground />);
 
-    expect(container).toHaveTextContent(
-      'import { DrawerGrid } from "@jobber/components/Drawer";',
-    );
-    expect(container).toHaveTextContent(
-      'import { Drawer } from "@jobber/components/Drawer";',
-    );
-    expect(container).not.toHaveTextContent(
-      'import { DrawerGrid } from "@jobber/components/DrawerGrid";',
-    );
+      expect(container).toHaveTextContent(
+        'import { DrawerGrid } from "@jobber/components/Drawer";',
+      );
+      expect(container).toHaveTextContent(
+        'import { Drawer } from "@jobber/components/Drawer";',
+      );
+      expect(container).not.toHaveTextContent(
+        'import { DrawerGrid } from "@jobber/components/DrawerGrid";',
+      );
+    });
+  });
+
+  describe("Mobile stories", () => {
+    beforeEach(() => {
+      mockStoryData({
+        title: "Components/Mobile",
+        sourceCode: "args => <Button />",
+      });
+    });
+
+    it("should only render the code", () => {
+      const { container, queryByTitle } = render(<Playground />);
+
+      expect(container).toHaveTextContent("Read-only");
+      expect(queryByTitle("Sandpack Preview")).not.toBeInTheDocument();
+    });
   });
 });
 
 interface MockStoryDataType extends Partial<sbAPI.Story> {
+  title?: string;
   sourceCode?: string;
 }
 
@@ -254,6 +290,8 @@ function mockStoryData({
 }: MockStoryDataType) {
   sbAPISpy.mockImplementation(() => ({
     getCurrentStoryData: () => ({
+      type: "story",
+      title: "Components/Web",
       ...rest,
       parameters: { ...parameters, storySource: { source: sourceCode } },
     }),
