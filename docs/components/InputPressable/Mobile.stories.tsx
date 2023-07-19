c2d2fc535b7a48486dedac8085f228a302b0e938
Taylor Noj <taylor.n@getjobber.com>
2023-07-19 10:22:27 -0400

    export props and update mobile story

---
+++ docs/components/InputPressable/Mobile.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { InputPressable } from "@jobber/components-native";
+
+export default {
+  title: "Components/Forms and Inputs/InputPressable/Mobile",
+  component: InputPressable,
+  parameters: {
+    viewMode: "story",
+  },
+} as ComponentMeta<typeof InputPressable>;
