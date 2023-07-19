a8b0212a5b8d85906aa8ba91b12d32dda8c70d3f
Darryl Tec <darryl.t@getjobber.com>
2023-07-19 08:16:30 -0600

    docs: add Google Analytics tracking (#1388)
    * install react-ga4
    
    * implement google analytics

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { addons, types } from "@storybook/addons";
+import { STORY_CHANGED, STORY_ERRORED, STORY_MISSING } from '@storybook/core-events';
 import { SidebarLabel } from "./components/SidebarLabel";
 import theme from "./theme";
 import favicon from "./assets/favicon.svg";
 import { Playground } from "./components/Playground";
+import ReactGA from "react-ga4";
 
 import "./assets/css/manager.css";
 import "@jobber/design/foundation.css";
@@ -37,5 +39,29 @@ addons.register("code/tab", () => {
       `/code/${[refId, storyId].filter(Boolean).join("_")}`,
     match: ({ viewMode }) => viewMode === "code",
     render: ({ key, active }) => <>{active && <Playground key={key} />}</>,
+  });
+});
+
+addons.register('google-analytics', (api) => {
+  ReactGA.initialize("G-V1N3TQVQB5");
+
+  api.on(STORY_CHANGED, () => {
+    const { title, name } = api.getCurrentStoryData();
+    const { path } = api.getUrlState();
+    ReactGA.send({ hitType: "pageview", page: path, title: `${title} - ${name}` });
+  });
+
+  api.on(STORY_ERRORED, ({ description }) => {
+    ReactGA.event({
+      category: "story error",
+      action: description,
+    });
+  });
+
+  api.on(STORY_MISSING, (id) => {
+    ReactGA.event({
+      action: `attempted to render ${id}, but it is missing`,
+      category: "story missing",
+    });
   });
 });
