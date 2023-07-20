53d19b49a3043f186d277b454f04fcfcd879b97f
Taylor Noj <taylor.n@getjobber.com>
2023-07-20 10:11:07 -0400

    create inputValidation story

---
+++ docs/components/InputValidation/Web.stories.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { InputValidation } from "@jobber/components/InputValidation";
+import { InputText } from "@jobber/components/InputText";
+import { Text } from "@jobber/components/Text";
+
+export default {
+  title: "Components/Forms and Inputs/InputValidation/Web",
+  components: InputValidation,
+  parameters: {
+    viewMode: "story",
+    previewTabs: { code: { hidden: false } },
+  },
+} as ComponentMeta<typeof InputValidation>;
+
+const BasicTemplate: ComponentStory<typeof InputValidation> = () => {
+  const [validationMessages, setValidationMessages] = useState();
+  return (
+    <>
+      <Text>
+        My name is
+        <InputText
+          validations={{
+            required: {
+              value: true,
+              message: "Please tell me your name",
+            },
+            pattern: {
+              value: /Jeff/,
+              message: "Have you considered a better name, like Jeff?",
+            },
+          }}
+          onValidation={message => setValidationMessages(message)}
+          size="small"
+          inline={true}
+          maxLength={4}
+        />
+      </Text>
+      {validationMessages && <InputValidation message={validationMessages} />}
+    </>
+  );
+};
+
+export const Basic = BasicTemplate.bind({});
