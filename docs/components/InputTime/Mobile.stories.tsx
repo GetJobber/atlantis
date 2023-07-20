057e9e796208e644a4654912f9c0adff834ef3f5
Taylor Noj <taylor.n@getjobber.com>
2023-07-20 16:57:04 -0400

    update docs page and set up mobile file

---
+++ docs/components/InputTime/Mobile.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { InputTime } from '@jobber/components-native';
+
+export default {
+  title: 'Components/Forms and Inputs/InputTime/Mobile',
+  component: InputTime,
+  parameters: {
+    viewMode: 'story',
+    previewTabs: {
+      code: {
+        hidden: false,
+      },
+    },
+    viewport: { defaultViewport: 'mobile1' },
+  },
+} as ComponentMeta<typeof InputTime>;
