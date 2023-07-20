70e855c92b71f4e864e1e133055fd3e914151fb0
Michael Paradis <michael.p@getjobber.com>
2023-07-20 07:26:04 -0700

    Fix minDate story

--- docs/components/InputDate/Web.stories.tsx
+++ docs/components/InputDate/Web.stories.tsx
@@ -22,13 +22,21 @@ Basic.args = {
 };
 
 const MinMaxTemplate: ComponentStory<typeof InputDate> = args => {
+  const minDate = new Date("11/06/2011");
+  const maxDate = new Date("11/25/2011");
   const [date, setDate] = useState(new Date("11/11/2011"));
-  return <InputDate {...args} value={date} onChange={setDate} />;
+  return (
+    <InputDate
+      {...args}
+      minDate={minDate}
+      maxDate={maxDate}
+      value={date}
+      onChange={setDate}
+    />
+  );
 };
 
 export const MinMax = MinMaxTemplate.bind({});
 MinMax.args = {
-  minDate: new Date("11/06/2011"),
-  maxDate: new Date("11/25/2011"),
   placeholder: "Start Date",
 };
