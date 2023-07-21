3d7495b676af3ec2ae610e8fb03e7b1a0592dc25
Taylor Noj <taylor.n@getjobber.com>
2023-07-21 16:43:34 -0400

    add final examples for mobile

--- docs/components/InputTime/Mobile.stories.tsx
+++ docs/components/InputTime/Mobile.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { InputTime } from "@jobber/components-native";
+import { Content, Form, InputTime } from "@jobber/components-native";
 
 export default {
   title: "Components/Forms and Inputs/InputTime/Mobile",
@@ -22,6 +22,18 @@ const BasicTemplate: ComponentStory<any> = args => {
   return <InputTime {...args} value={time} onChange={setTime} />;
 };
 
+const EmptyValueTemplate: ComponentStory<typeof InputTime> = args => {
+  return <InputTime {...args} />;
+};
+
+const FormControlledTemplate: ComponentStory<any> = args => (
+  <Form initialValues={{ startTime: new Date("2023-07-21T16:36:34.873Z") }}>
+    <Content>
+      <InputTime {...args} />
+    </Content>
+  </Form>
+);
+
 export const Basic = BasicTemplate.bind({});
 Basic.args = {
   placeholder: "Start time",
@@ -37,4 +49,16 @@ export const Invalid = BasicTemplate.bind({});
 Invalid.args = {
   placeholder: "Start time",
   invalid: "Start time is required",
+};
+
+export const FormControlled = FormControlledTemplate.bind({});
+FormControlled.args = {
+  name: "startTime",
+  placeholder: "Created time",
+};
+
+export const EmptyValue = EmptyValueTemplate.bind({});
+EmptyValue.args = {
+  placeholder: "Start time",
+  emptyValueLabel: "Unscheduled",
 };
