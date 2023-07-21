ba97edd02294be3162acf0e9ebae597b5b2bd0fb
Taylor Noj <104797704+taylorvnoj@users.noreply.github.com>
2023-07-21 09:07:31 -0400

    docs(components-native): Update mobile stories to include small viewport and add code tab (#1398)
    * add mobile viewport to ActionLabel
    
    * adjust mobile viewport and little fixes
    
    * change viewport on Card and add code tab
    
    * update viewport for Content and add code
    
    * update Disclosure view, code and error message
    
    * update divider viewport and add code
    
    * change viewport for flex and add code tab
    
    * fix viewport for FormatFile and add code tab
    
    * update icon viewport for mobile, add code
    
    * might as well keep distinction with viewport
    
    * update inputCurrency viewport and code tab
    
    * fix up thumbnailList with code, disclaimer and viewport
    
    * update some links
    
    * make flex prettier in mobile view

--- docs/components/Disclosure/Mobile.stories.tsx
+++ docs/components/Disclosure/Mobile.stories.tsx
@@ -7,6 +7,9 @@ export default {
   component: Disclosure,
   parameters: {
     viewMode: "story",
+    previewTabs: { code: { hidden: false } },
+    viewport: { defaultViewport: "mobile1" },
+    showNativeOnWebDisclaimer: true,
   },
 } as ComponentMeta<typeof Disclosure>;
 
