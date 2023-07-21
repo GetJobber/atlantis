7c431c2fe93292b73e501272d7c48369616001cd
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2023-07-21 18:59:09 -0300

    Add examples for Web

--- docs/components/InputNumber/Web.stories.tsx
+++ docs/components/InputNumber/Web.stories.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { Content } from "@jobber/components/Content";
-import { Divider, InputNumber } from "@jobber/components-native";
+import { InputNumber } from "@jobber/components/InputNumber";
+import { Text } from "@jobber/components/Text";
 
 export default {
   title: "Components/Forms and Inputs/InputNumber/Web",
@@ -9,24 +9,79 @@ export default {
   parameters: {
     viewMode: "story",
     previewTabs: { code: { hidden: false } },
-    viewport: { defaultViewport: "mobile1" },
   },
 } as ComponentMeta<typeof InputNumber>;
 
-const BasicTemplate: ComponentStory<typeof InputNumber> = args => (
-  <div
-    style={{
-      display: "grid",
-      gap: "var(--space-base)",
-    }}
-  >
-    <Content>Some amazing content</Content>
-    <Divider {...args} />
-    <Content>Even more amazing content</Content>
-  </div>
-);
+const BasicTemplate: ComponentStory<typeof InputNumber> = args => {
+  const [value, setValue] = useState(args.value);
+  return (
+    <InputNumber
+      {...args}
+      value={value}
+      onChange={(newValue: number) => setValue(newValue)}
+    />
+  );
+};
 
 export const Basic = BasicTemplate.bind({});
 Basic.args = {
-  size: "base",
+  value: 3,
+};
+
+export const Controlled = BasicTemplate.bind({});
+Controlled.args = {
+  value: 30,
+  min: 0,
+  max: 32,
+  placeholder: "Age",
+};
+
+export const Disabled = BasicTemplate.bind({});
+Disabled.args = {
+  value: 1234,
+  disabled: true,
+  placeholder: "SIN Number",
+};
+
+export const ReadOnly = BasicTemplate.bind({});
+ReadOnly.args = {
+  value: 1234,
+  readonly: true,
+  placeholder: "Your PIN number",
+};
+
+export const Invalid = BasicTemplate.bind({});
+Invalid.args = {
+  value: 1.1,
+  invalid: true,
+  placeholder: "Give a whole number",
+};
+
+export const Sizes = BasicTemplate.bind({});
+Sizes.args = {
+  size: "small",
+};
+
+const InlineTemplate: ComponentStory<typeof InputNumber> = args => {
+  const [value, setValue] = useState(args.value);
+  return (
+    <Text>
+      Follow-up after
+      <InputNumber
+        {...args}
+        value={value}
+        onChange={(newValue: number) => setValue(newValue)}
+      />
+      days
+    </Text>
+  );
+};
+
+export const Inline = InlineTemplate.bind({});
+Inline.args = {
+  size: "small",
+  value: 2,
+  inline: true,
+  maxLength: 2,
+  align: "center",
 };
