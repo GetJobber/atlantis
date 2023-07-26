ec1d2b0002d29d248a0da01ce028ecf37c1c3418
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2023-07-26 17:34:51 -0300

    Update docs for Chips

---
+++ docs/components/Chips/Web.stories.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Content } from "@jobber/components/Content";
+import { Chip, Chips } from "@jobber/components/Chips";
+import { Icon } from "@jobber/components/Icon";
+import { Text } from "@jobber/components/Text";
+import { Avatar } from "@jobber/components/Avatar";
+import { useFakeOptionQuery } from "./utils/mdxUtils";
+
+export default {
+  title: "Components/Selections/Chips/Web",
+  component: Chips,
+  subcomponents: { Chip },
+  parameters: {
+    viewMode: "story",
+    previewTabs: { code: { hidden: false } },
+  },
+} as ComponentMeta<typeof Chips>;
+
+const BasicTemplate: ComponentStory<typeof Chips> = args => {
+  const [selected, setSelected] = useState();
+  return (
+    <Content>
+      <Text>
+        You are <u>{selected ? selected : "_______"}</u>
+      </Text>
+      <Chips {...args} selected={selected} onChange={setSelected}>
+        <Chip
+          prefix={<Avatar initials="AZ" />}
+          label="Amazing"
+          value="Amazing"
+        />
+        <Chip
+          prefix={<Icon name="video" />}
+          label="Wonderful"
+          value="Wonderful"
+        />
+        <Chip
+          prefix={<Icon name="starFill" />}
+          label="Brilliant"
+          value="Brilliant"
+        />
+        <Chip label="Magnificent" value="Magnificent" />
+      </Chips>
+    </Content>
+  );
+};
+
+export const Basic = BasicTemplate.bind({});
+Basic.args = {};
+
+const MultiSelectTemplate: ComponentStory<typeof Chips> = args => {
+  const [selected, setSelected] = useState([]);
+  return (
+    <Content>
+      <Text>
+        You are <u>{selected.length ? selected.join(" ") : "_______"}</u>
+      </Text>
+      <Chips
+        {...args}
+        type="multiselect"
+        selected={selected}
+        onChange={setSelected}
+      >
+        <Chip label="Amazing" value="Amazing" />
+        <Chip label="Wonderful" value="Wonderful" />
+        <Chip label="Brilliant" value="Brilliant" />
+        <Chip label="Magnificent" value="Magnificent" />
+      </Chips>
+    </Content>
+  );
+};
+
+export const MultiSelect = MultiSelectTemplate.bind({});
+MultiSelect.args = {};
+
+const SelectionTemplate: ComponentStory<typeof Chips> = args => {
+  const {
+    selected,
+    options,
+    loading,
+    handleLoadMore,
+    handleSearch,
+    handleSelect,
+    handleCustomAdd,
+  } = useFakeOptionQuery();
+  return (
+    <Chips
+      {...args}
+      selected={selected}
+      onChange={handleSelect}
+      onCustomAdd={handleCustomAdd}
+      isLoadingMore={loading}
+      onSearch={handleSearch}
+      onLoadMore={handleLoadMore}
+    >
+      {options.map(name => (
+        <Chip key={name} label={name} value={name} />
+      ))}
+    </Chips>
+  );
+};
+
+export const Selection = SelectionTemplate.bind({});
+Selection.args = {
+  type: "dismissible",
+};
