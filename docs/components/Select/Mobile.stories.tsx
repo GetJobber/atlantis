6a60ed53f1aace3cd70f25bc1b619381713629ce
Taylor Noj <taylor.n@getjobber.com>
2023-07-26 17:34:48 -0400

    add extra mobile story and finalize docs page

--- docs/components/Select/Mobile.stories.tsx
+++ docs/components/Select/Mobile.stories.tsx
@@ -7,26 +7,25 @@ export default {
   component: Select,
   parameters: {
     viewMode: "story",
-    previewTabs: {
-      code: {
-        hidden: false,
-        extraImports: {
-          "@jobber/components-native": ["Select", "Option"],
-        },
-      },
-    },
+    previewTabs: { code: { hidden: false } },
     viewport: { defaultViewport: "mobile1" },
   },
 } as ComponentMeta<typeof Select>;
 
 const BasicTemplate: ComponentStory<typeof Select> = args => (
   <Select {...args}>
-    <Option value={"1"}>1</Option>
-    <Option value={"2"}>2</Option>
+    <Option value="1">1</Option>
+    <Option value="2">2</Option>
   </Select>
 );
 
 export const Basic = BasicTemplate.bind({});
 Basic.args = {
   label: "Favorite number",
+};
+
+export const InitialValue = BasicTemplate.bind({});
+InitialValue.args = {
+  label: "Favorite number",
+  defaultValue: "2",
 };
