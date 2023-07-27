7a7765f5d47625f57430f4ae531bab27b37627dc
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2023-07-27 15:25:14 -0300

    Fix Code example

--- docs/components/InlineLabel/Web.stories.tsx
+++ docs/components/InlineLabel/Web.stories.tsx
@@ -12,10 +12,8 @@ export default {
 } as ComponentMeta<typeof InlineLabel>;
 
 const BasicTemplate: ComponentStory<typeof InlineLabel> = args => (
-  <InlineLabel {...args} />
+  <InlineLabel {...args}>Draft</InlineLabel>
 );
 
 export const Basic = BasicTemplate.bind({});
-Basic.args = {
-  children: "draft",
-};
+Basic.args = {};
