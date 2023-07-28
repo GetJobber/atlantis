bf935aa85480cf4e9e9d060c21c6ea1f70af906e
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2023-07-28 17:40:29 -0300

    Update Link docs

---
+++ docs/components/Link/Web.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Link } from "@jobber/components/Link";
+
+export default {
+  title: "Components/Text and Typography/Link/Web",
+  component: Link,
+  parameters: {
+    viewMode: "story",
+    previewTabs: { code: { hidden: false } },
+  },
+} as ComponentMeta<typeof Link>;
+
+const BasicTemplate: ComponentStory<typeof Link> = args => (
+  <Link {...args}>What is a Link anyway?</Link>
+);
+
+export const Horizontal = BasicTemplate.bind({});
+Horizontal.args = {
+  url: "https://en.wikipedia.org/wiki/Hyperlink",
+  external: true,
+};
