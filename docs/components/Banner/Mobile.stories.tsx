47e0fb469e90199a2f96239593edb94368b2ad2e
Jeff Marvin <jeff.m@getjobber.com>
2023-07-28 11:54:53 -0600

    Unify web and mobile docs and add examples

---
+++ docs/components/Banner/Mobile.stories.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Banner } from "@jobber/components-native";
+
+export default {
+  title: "Components/Status and Feedback/Banner/Mobile",
+  component: Banner,
+  parameters: {
+    viewMode: "story",
+    previewTabs: { code: { hidden: false } },
+    viewport: { defaultViewport: "mobile1" },
+  },
+} as ComponentMeta<typeof Banner>;
+
+const BasicTemplate: ComponentStory<typeof Banner> = args => (
+  <>
+    <Banner
+      type="warning"
+      text="Changing line items will remove approval on this quote"
+      {...args}
+    ></Banner>
+  </>
+);
+export const Basic = BasicTemplate.bind({});
+Basic.args = {
+  type: "notice",
+  text: "Your import is in progress",
+};
+
+const ActionTemplate: ComponentStory<typeof Banner> = () => (
+  <Banner
+    type="notice"
+    action={{
+      label: "View Plans",
+      onPress: () => alert("Plans"),
+    }}
+    text="Your trial has been extended!"
+  ></Banner>
+);
+export const ActionInBanners = ActionTemplate.bind({});
+
+const ErrorTemplate: ComponentStory<typeof Banner> = args => (
+  <Banner {...args}></Banner>
+);
+export const Error = ErrorTemplate.bind({});
+Error.args = {
+  type: "error",
+  text: "Something has gone wrong. Please retry in a few minutes.",
+};
+
+const FormErrorsTemplate: ComponentStory<typeof Banner> = args => (
+  <Banner {...args}></Banner>
+);
+export const FormErrors = FormErrorsTemplate.bind({});
+FormErrors.args = {
+  type: "error",
+  text: "There was an error submitting your form",
+  details: [
+    "This client already exists",
+    "This phone number doesn't receive SMS",
+  ],
+};
---
+++ docs/components/Banner/Web.stories.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Banner } from "@jobber/components/Banner";
+
+export default {
+  title: "Components/Status and Feedback/Banner/Web",
+  component: Banner,
+  parameters: {
+    viewMode: "story",
+    previewTabs: { code: { hidden: false } },
+  },
+} as ComponentMeta<typeof Banner>;
+
+const BasicTemplate: ComponentStory<typeof Banner> = args => (
+  <Banner {...args} />
+);
+export const Basic = BasicTemplate.bind({});
+Basic.args = {
+  type: "notice",
+  children: "Your splines are being reticulated",
+};
+
+const ActionsTemplate: ComponentStory<typeof Banner> = () => (
+  <>
+    <Banner
+      type="notice"
+      primaryAction={{
+        label: "View Plans",
+        onClick: () => alert("Plans"),
+      }}
+      dismissible={false}
+    >
+      Your trial has been extended!
+    </Banner>
+    <Banner
+      type="error"
+      primaryAction={{
+        label: "Refresh",
+        onClick: () => alert("Refreshing"),
+      }}
+    >
+      Network is unavailable. Please check your internet connection.
+    </Banner>
+  </>
+);
+export const ActionsInBanners = ActionsTemplate.bind({});
+
+const SuccessTemplate: ComponentStory<typeof Banner> = args => (
+  <Banner
+    primaryAction={{
+      label: "View clients",
+      onClick: () => alert("🎉 Woo hoo"),
+    }}
+    {...args}
+  >
+    Your client import is complete
+  </Banner>
+);
+export const Success = SuccessTemplate.bind({});
+Success.args = {
+  type: "success",
+  dismissible: false,
+};
