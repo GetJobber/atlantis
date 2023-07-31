346efcdd4cd2c59ddd3472bb7ee3bc3ecb3ab561
Taylor Noj <taylor.n@getjobber.com>
2023-07-31 11:09:00 -0400

    add header exaple

--- docs/components/ProgressBar/Mobile.stories.tsx
+++ docs/components/ProgressBar/Mobile.stories.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react-native";
-import { ProgressBar } from "@jobber/components-native";
+import { Button, Heading, ProgressBar } from "@jobber/components-native";
 
 export default {
   title: "Components/Status and Feedback/ProgressBar/Mobile",
@@ -16,9 +16,38 @@ const BasicTemplate: ComponentStory<typeof ProgressBar> = args => (
   <ProgressBar {...args} />
 );
 
+const WithHeaderTemplate: ComponentStory<typeof ProgressBar> = args => {
+  const [step, setStep] = useState(1);
+  const totalSteps = 6;
+  return (
+    <ProgressBar
+      {...args}
+      current={step}
+      total={totalSteps}
+      header={
+        <>
+          <Heading level="heading">Upload progress</Heading>
+          <br />
+          <Button
+            label="Step Back"
+            onPress={() => setStep(Math.max(0, step - 1))}
+          />
+          <Button
+            label="Step Forward"
+            onPress={() => setStep(Math.min(totalSteps, step + 1))}
+          />
+        </>
+      }
+    />
+  );
+};
+
 export const Basic = BasicTemplate.bind({});
 Basic.args = {
   total: 5,
   current: 1,
   inProgress: 2,
 };
+
+export const WithHeader = WithHeaderTemplate.bind({});
+WithHeader.args = {};
