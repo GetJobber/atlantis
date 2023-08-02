a939d6163666ebb89b3e1c2e2283427bcce55126
Taylor Noj <taylor.n@getjobber.com>
2023-08-01 20:04:36 -0400

    add mobile examples and info on docs page

---
+++ docs/components/FormField/Mobile.stories.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { FormField, InputText, Text } from "@jobber/components-native";
+
+export default {
+  title: "Components/Private/FormField/Mobile",
+  component: FormField,
+  parameters: {
+    viewMode: "story",
+    previewTabs: { code: { hidden: false } },
+    viewport: { defaultViewport: "mobile1" },
+    showNativeOnWebDisclaimer: true,
+  },
+} as ComponentMeta<typeof FormField>;
+
+const BasicTemplate: ComponentStory<typeof FormField> = args => {
+  return (
+    <FormField {...args}>
+      {field => {
+        return <InputText value={field.value} placeholder="Enter name here" />;
+      }}
+    </FormField>
+  );
+};
+
+const WithValidationsTemplate: ComponentStory<typeof FormField> = args => {
+  return (
+    <FormField
+      {...args}
+      validations={{
+        maxLength: {
+          value: 5,
+          message: "Should not exceed 5 characters",
+        },
+      }}
+    >
+      {(field, error) => {
+        return (
+          <>
+            {error && <Text variation="error">{error.message}</Text>}
+            <InputText
+              value={field.value}
+              placeholder="Should not exceed 5 characters"
+            />
+          </>
+        );
+      }}
+    </FormField>
+  );
+};
+
+export const Basic = BasicTemplate.bind({});
+Basic.args = {
+  name: "name",
+};
+
+export const WithValidations = WithValidationsTemplate.bind({});
+WithValidations.args = {
+  name: "validations",
+};
