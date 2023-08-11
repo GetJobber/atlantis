258424d88e1dd419ed8353b725e6f40db9f30667
Darryl Tec <darryl.t@getjobber.com>
2023-08-11 08:36:15 -0600

    docs: fix custom radio option code tab (#1482)
    docs: fix custom radio option

--- docs/components/RadioGroup/Web.stories.tsx
+++ docs/components/RadioGroup/Web.stories.tsx
@@ -18,6 +18,7 @@ export default {
         hidden: false,
         extraImports: {
           "@jobber/components/RadioGroup": ["RadioGroup", "RadioOption"],
+          "@jobber/components/Chips": ["Chips", "Chip"],
         },
       },
     },
@@ -96,59 +97,55 @@ const DescriptionTemplate: ComponentStory<typeof RadioGroup> = args => {
 
 const CustomRadioOptionContentTemplate: ComponentStory<
   typeof RadioGroup
-> = () => {
+> = args => {
   const [reminderSetting, setReminderSetting] = useState("fixed");
   const [fixedTimeSetting, setFixedTimeSetting] = useState("MINUTES_30");
 
   return (
-    <>
-      <Content>
-        <RadioGroup
-          onChange={(value: string) => setReminderSetting(value)}
-          value={reminderSetting}
-          ariaLabel="Reminder Settings"
+    <RadioGroup
+      {...args}
+      onChange={(value: string) => setReminderSetting(value)}
+      value={reminderSetting}
+    >
+      <RadioOption
+        value={"fixed"}
+        label="Fixed Time"
+        description="Select from the 15 mins interval"
+      >
+        <Chips
+          onChange={setting => setFixedTimeSetting(setting ?? "MINUTES_30")}
+          selected={fixedTimeSetting}
         >
-          <RadioOption
-            value={"fixed"}
-            label="Fixed Time"
-            description="Select from the 15 mins interval"
-          >
-            <Chips
-              onChange={setting => setFixedTimeSetting(setting ?? "MINUTES_30")}
-              selected={fixedTimeSetting}
-            >
-              <Chip label="15 min" value="MINUTES_15" />
-              <Chip label="30 min" value="MINUTES_30" />
-              <Chip label="45 min" value="MINUTES_45" />
-              <Chip label="60 min" value="MINUTES_60" />
-            </Chips>
-          </RadioOption>
-          <RadioOption
-            value={"specific"}
-            label="Specific Time"
-            description="Set your reminder down to the seconds"
-          >
-            Remind me at{" "}
-            <InputNumber
-              size="small"
-              suffix={{ label: "mins" }}
-              defaultValue={"42"}
-              maxLength={5}
-              inline
-            />{" "}
-            and{" "}
-            <InputNumber
-              size="small"
-              suffix={{ label: "sec" }}
-              defaultValue={"3"}
-              maxLength={5}
-              inline
-            />{" "}
-            interval
-          </RadioOption>
-        </RadioGroup>
-      </Content>
-    </>
+          <Chip label="15 min" value="MINUTES_15" />
+          <Chip label="30 min" value="MINUTES_30" />
+          <Chip label="45 min" value="MINUTES_45" />
+          <Chip label="60 min" value="MINUTES_60" />
+        </Chips>
+      </RadioOption>
+      <RadioOption
+        value={"specific"}
+        label="Specific Time"
+        description="Set your reminder down to the seconds"
+      >
+        Remind me at{" "}
+        <InputNumber
+          size="small"
+          suffix={{ label: "mins" }}
+          defaultValue={"42"}
+          maxLength={5}
+          inline
+        />{" "}
+        and{" "}
+        <InputNumber
+          size="small"
+          suffix={{ label: "sec" }}
+          defaultValue={"3"}
+          maxLength={5}
+          inline
+        />{" "}
+        interval
+      </RadioOption>
+    </RadioGroup>
   );
 };
 
@@ -161,3 +158,7 @@ export const Disabled = DisabledTemplate.bind({});
 export const CustomRadioOptionContent = CustomRadioOptionContentTemplate.bind(
   {},
 );
+
+CustomRadioOptionContent.args = {
+  ariaLabel: "Reminder Settings",
+};
