19bdbb5b2f9685bbfd198df3f4be10691486ded3
Michael Paradis <michael.p@getjobber.com>
2023-08-21 12:10:18 -0700

    Added storybook for multiple breakpoints

--- packages/components/src/DataList/DataList.stories.tsx
+++ packages/components/src/DataList/DataList.stories.tsx
@@ -13,6 +13,8 @@ import {
   // getLoadingState,
 } from "./storyUtils";
 import { Grid } from "../Grid";
+import { Card } from "../Card";
+import { Content } from "../Content";
 
 export default {
   title: "Components/Lists and Tables/DataList/Web",
@@ -98,7 +100,7 @@ const Template: ComponentStory<typeof DataList> = args => {
       }}
       loading={loadingInitialContent}
     >
-      <DataList.Layout size={"lg"}>
+      <DataList.Layout>
         {(item: DataListItemType<typeof mappedData>) => (
           <Grid alignItems="center">
             <Grid.Cell size={{ xs: 5 }}>
@@ -113,7 +115,6 @@ const Template: ComponentStory<typeof DataList> = args => {
           </Grid>
         )}
       </DataList.Layout>
-
       <DataList.EmptyState
         message="This is empty"
         action={{
@@ -131,3 +132,110 @@ export const EmptyState = Template.bind({});
 EmptyState.args = {
   data: [],
 };
+
+const BreakpointTemplate: ComponentStory<typeof DataList> = args => {
+  const {
+    data,
+    /* See useCollectionQuery for example on how to load more */
+    // refresh,
+    // nextPage,
+    // loadingRefresh,
+    // loadingNextPage,
+    loadingInitialContent,
+  } = useCollectionQuery<ListQueryType>({
+    query: LIST_QUERY,
+    queryOptions: {
+      fetchPolicy: "network-only",
+      nextFetchPolicy: "cache-first",
+      client: apolloClient,
+    },
+    getCollectionByPath(items) {
+      return items?.allPeople;
+    },
+  });
+
+  /* For when we start creating a loading state */
+  // const { loading } = getLoadingState(
+  //   loadingInitialContent,
+  //   loadingRefresh,
+  //   loadingNextPage,
+  // );
+
+  const items = data?.allPeople.edges || [];
+  const mappedData = items.map(({ node }) => ({
+    label: node.name,
+    home: node.homeworld.name,
+    tags: [
+      node.gender,
+      node.hairColor?.split(", "),
+      node.skinColor?.split(", "),
+    ],
+    homePopulation: node.homeworld.population?.toLocaleString(),
+    created: new Date(node.created),
+  }));
+
+  return (
+    <DataList
+      {...args}
+      data={(args.data as typeof mappedData) || mappedData}
+      headers={{
+        label: "Name",
+        home: "Home world",
+        tags: "Attributes",
+        homePopulation: "Home world population",
+        created: "Created",
+      }}
+      loading={loadingInitialContent}
+    >
+      <DataList.Layout size="lg">
+        {(item: DataListItemType<typeof mappedData>) => (
+          <Grid alignItems="center">
+            <Grid.Cell size={{ xs: 5 }}>
+              <Grid alignItems="center">
+                <Grid.Cell size={{ xs: 6 }}>{item.label}</Grid.Cell>
+                <Grid.Cell size={{ xs: 6 }}>{item.home}</Grid.Cell>
+              </Grid>
+            </Grid.Cell>
+            <Grid.Cell size={{ xs: 2 }}>{item.homePopulation}</Grid.Cell>
+            <Grid.Cell size={{ xs: 3 }}>{item.tags}</Grid.Cell>
+            <Grid.Cell size={{ xs: 2 }}>{item.created}</Grid.Cell>
+          </Grid>
+        )}
+      </DataList.Layout>
+      <DataList.Layout size={"xs"}>
+        {(item: DataListItemType<typeof mappedData>) => (
+          <Card>
+            <Content>
+              <Grid>
+                <Grid.Cell size={{ xs: 8 }}>
+                  <Grid>
+                    <Grid.Cell size={{ xs: 12 }}>{item.label}</Grid.Cell>
+                    <Grid.Cell size={{ xs: 12 }}>{item.tags}</Grid.Cell>
+                  </Grid>
+                </Grid.Cell>
+                <Grid.Cell size={{ xs: 4 }}>
+                  <Grid>
+                    <Grid.Cell size={{ xs: 12 }}>{item.home}</Grid.Cell>
+                    <Grid.Cell size={{ xs: 12 }}>
+                      {item.homePopulation}
+                    </Grid.Cell>
+                  </Grid>
+                </Grid.Cell>
+                <Grid.Cell size={{ xs: 12 }}>{item.created}</Grid.Cell>
+              </Grid>
+            </Content>
+          </Card>
+        )}
+      </DataList.Layout>
+      <DataList.EmptyState
+        message="This is empty"
+        action={{
+          label: "Do a thing",
+          onClick: () => alert("A thing has been done"),
+        }}
+      />
+    </DataList>
+  );
+};
+
+export const Breakpoints = BreakpointTemplate.bind({});
