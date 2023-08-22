fc3e62d9d251dba76b6ebefe4c30bbff0d07668a
Michael Paradis <michael.p@getjobber.com>
2023-08-22 08:23:19 -0700

    Delete test file that isn't needed

--- packages/components/src/DataList/DataList.utils.test.tsx
+++
@@ -1,8 +0,0 @@
-import { BREAKPOINTS } from "./DataList.const";
-import { sortSizeProp } from "./DataList.utils";
-
-describe("Sort Size Prop", () => {
-  it("sorts correctly", () => {
-    expect(sortSizeProp(["xl", "lg", "md", "sm", "xs"])).toEqual(BREAKPOINTS);
-  });
-});
