19d4372c8f51b289cce3eb9d09f82b81a4291492
Darryl Tec <darryl.t@getjobber.com>
2023-08-24 14:15:59 -0600

    update Menu

--- packages/components-native/src/Menu/messages.ts
+++
@@ -1,9 +0,0 @@
-import { defineMessages } from "react-intl";
-
-export const messages = defineMessages({
-  more: {
-    id: "more",
-    defaultMessage: "Menu",
-    description: "Accessibility label to open the menu",
-  },
-});
