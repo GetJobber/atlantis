20acaadb1647e0acf244e2de75dd27281fc1bd6c
Zev Permack <zev.p@getjobber.com>
2023-08-24 10:20:51 -0400

    add tests for datatable loading state

--- packages/components/src/DataTable/DataTable.test.tsx
+++ packages/components/src/DataTable/DataTable.test.tsx
@@ -11,6 +11,7 @@ import {
   royaltyReportColumns,
   royaltyReportData,
 } from "./test-utilities";
+import { GLIMMER_TEST_ID } from "../Glimmer";
 
 // Allow us to mock and replace the value of useResizeObserver would return via
 // a spy
@@ -288,5 +289,26 @@ describe("when the table has no data", () => {
 
   it("renders the provided empty state", () => {
     expect(screen.getByText("No data")).toBeDefined();
+  });
+});
+
+describe("when the table has a loading prop set to true", () => {
+  it("Shows the default amount of loaders for each column", async () => {
+    render(
+      <DataTable data={[]} columns={royaltyReportColumns} loading={true} />,
+    );
+    expect(await screen.findAllByTestId(GLIMMER_TEST_ID)).toHaveLength(40);
+  });
+
+  it("Shows the amount of loaders based on smallest items per page value", async () => {
+    render(
+      <DataTable
+        data={[]}
+        pagination={{ manualPagination: false, itemsPerPage: [20, 30, 40] }}
+        columns={royaltyReportColumns}
+        loading={true}
+      />,
+    );
+    expect(await screen.findAllByTestId(GLIMMER_TEST_ID)).toHaveLength(80);
   });
 });
