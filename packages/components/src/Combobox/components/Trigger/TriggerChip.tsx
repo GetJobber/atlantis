3dbf423a0a6f560274539e8f362eb060025a44e5
Taylor Noj <taylor.n@getjobber.com>
2023-09-01 15:31:44 -0400

    make triggerChip a button, update css and add tests

--- packages/components/src/Combobox/components/Trigger/TriggerChip.tsx
+++ packages/components/src/Combobox/components/Trigger/TriggerChip.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { TriggerProps } from "./types";
 import styles from "./TriggerChip.css";
-// import { Text } from "../../../Text";
 import { Icon } from "../../../Icon";
 import { ComboboxContext } from "../../ComboboxProvider";
 import { Typography } from "../../../Typography";
@@ -10,18 +9,18 @@ export function TriggerChip(props: TriggerProps): JSX.Element {
   const { open, setOpen } = React.useContext(ComboboxContext);
 
   return (
-    <div
+    <button
       className={styles.trigger}
       onClick={() => {
         setOpen(!open);
       }}
     >
-      <Typography fontWeight="heavy" textColor="heading">
+      <Typography fontWeight="semiBold" textColor="heading">
         {props.label}
       </Typography>
       <div className={styles.triggerIcon}>
         <Icon color="surfaceReverse" name="add" size="small" />
       </div>
-    </div>
+    </button>
   );
 }
