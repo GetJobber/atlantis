d81b87c5b7349de728b56b7d452c24770d4e9075
Taylor Noj <taylor.n@getjobber.com>
2023-09-01 10:06:58 -0400

    match level 600 weight name to mobile equivalent

--- packages/components/src/Typography/css/FontWeights.css.d.ts
+++ packages/components/src/Typography/css/FontWeights.css.d.ts
@@ -1,6 +1,6 @@
 declare const styles: {
   readonly "regular": string;
-  readonly "heavy": string;
+  readonly "semiBold": string;
   readonly "bold": string;
   readonly "extraBold": string;
   readonly "black": string;
