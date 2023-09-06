9aa2d571439bc10c7473750abd3f522ef05ddba2
Darryl Tec <darryl.t@getjobber.com>
2023-09-06 11:10:00 -0600

    feat(components): add search ability for data list (#1514)
    * Setup initial search on DataList
    
    * layout adjustment
    
    * add debounce
    
    * add toggleable search on medium below
    
    * pure CSS fancy transition
    
    * autofocus on show
    
    * adjust the transition a wee bit
    
    * transition improvement
    
    * search styling improvement
    
    * adjust search when filters aren't defined
    
    * refactor how we grab filters and search components
    
    * add test
    
    * adjust alignment for filters when scrollbar shows
    
    * use tokens instead of magic number
    
    Co-Authored-By: Rodrigo Eidelvein <rodrigoeidelvein@gmail.com>
    
    * adjust alignment of input
    
    ---------
    
    Co-authored-by: Rodrigo Eidelvein <rodrigo.e@getjobber.com>
    Co-authored-by: Rodrigo Eidelvein <rodrigoeidelvein@gmail.com>

--- packages/components/src/DataList/context/DataListContext/DataListContext.tsx
+++ packages/components/src/DataList/context/DataListContext/DataListContext.tsx
@@ -1,14 +1,15 @@
 import { createContext, useContext } from "react";
-import { DataListObject, DataListProps } from "../../DataList.types";
+import { DataListContextProps, DataListObject } from "../../DataList.types";
 
 export const defaultValues = {
+  title: "",
   data: [],
   headers: {},
   children: [],
 };
 
 export const DataListContext =
-  createContext<DataListProps<DataListObject>>(defaultValues);
+  createContext<DataListContextProps<DataListObject>>(defaultValues);
 
 export function useDataListContext() {
   return useContext(DataListContext);
