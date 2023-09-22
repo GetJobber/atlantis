6c4f0b172ea351a5810d21d6146287213c19448b
Darryl Tec <darryl.t@getjobber.com>
2023-09-22 15:27:35 -0600

    feat(components): add context menu on data list (#1542)
    * first pass and cleanup code
    
    * sync up tag overflow with the active color
    
    * add tests
    
    * semantic colors and multi pop over
    
    * memoize generator and use existing const
    
    * add mo test!

--- packages/components/src/DataList/components/DataListLayoutInternal/__tests__/DataListItems.test.tsx
+++ packages/components/src/DataList/components/DataListLayoutInternal/__tests__/DataListItems.test.tsx
@@ -1,4 +1,11 @@
-import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  within,
+} from "@testing-library/react";
 import React from "react";
 import omit from "lodash/omit";
 import userEvent from "@testing-library/user-event";
@@ -7,6 +14,8 @@ import { defaultValues } from "../../../context/DataListContext";
 import * as dataListContext from "../../../context/DataListContext/DataListContext";
 import { DataListLayout } from "../../DataListLayout";
 import { DataListItemType } from "../../../DataList.types";
+import { DataListItemActions } from "../../DataListItemActions";
+import { DataListAction } from "../../DataListAction";
 
 const spy = jest.spyOn(dataListContext, "useDataListContext");
 const mockData = [
@@ -14,6 +23,7 @@ const mockData = [
   { id: 2, label: "Anakin Skywalker" },
 ];
 const mockHeader = { label: "Name" };
+const mockEditClick = jest.fn();
 const contextValueWithRenderableChildren = {
   ...defaultValues,
   data: mockData,
@@ -28,6 +38,12 @@ const contextValueWithRenderableChildren = {
         </div>
       )}
     </DataListLayout>
+  ),
+  itemActionComponent: (
+    <DataListItemActions>
+      <DataListAction label="Edit" onClick={mockEditClick} />
+      <DataListAction label="Delete" onClick={jest.fn()} />
+    </DataListItemActions>
   ),
 };
 
@@ -117,6 +133,84 @@ describe("DataListItems", () => {
 
       expect(onSelectMock).toHaveBeenCalledTimes(1);
       expect(onSelectMock).toHaveBeenCalledWith([mockData[1].id]);
+    });
+  });
+
+  describe("Context menu", () => {
+    it("should render context menu when right clicking on a list item", () => {
+      spy.mockReturnValue({ ...contextValueWithRenderableChildren });
+      renderItems();
+
+      expect(screen.queryByRole("menu")).not.toBeInTheDocument();
+
+      const listItem = screen.getByText(mockData[0].label);
+      userEvent.hover(listItem);
+
+      fireEvent.contextMenu(listItem);
+
+      const menuElement = screen.getByRole("menu");
+      expect(menuElement).toBeInTheDocument();
+      expect(within(menuElement).getAllByRole("button")).toHaveLength(2);
+    });
+
+    it("should render context menu in the right position when right clicking on a list item", () => {
+      spy.mockReturnValue({ ...contextValueWithRenderableChildren });
+      renderItems();
+
+      const listItem = screen.getByText(mockData[0].label);
+      userEvent.hover(listItem);
+
+      const clientX = 20;
+      const clientY = 30;
+      fireEvent.contextMenu(listItem, { clientX, clientY });
+
+      const menuElement = screen.getByRole("menu");
+      expect(menuElement).toHaveStyle({
+        "--actions-menu-x": `${clientX}px`,
+        "--actions-menu-y": `${clientY}px`,
+      });
+    });
+
+    it("should have the correct item when clicking the edit button", () => {
+      spy.mockReturnValue({ ...contextValueWithRenderableChildren });
+      renderItems();
+
+      const selectedItem = mockData[1];
+      const listItem = screen.getByText(selectedItem.label);
+      userEvent.hover(listItem);
+      fireEvent.contextMenu(listItem);
+
+      const menuElement = screen.getByRole("menu");
+      const editButton = within(menuElement).getByText("Edit");
+      userEvent.click(editButton);
+      expect(mockEditClick).toHaveBeenCalledWith(selectedItem);
+    });
+
+    it("should not render context menu when right clicking on a list item and itemActionComponent is not provided", () => {
+      spy.mockReturnValue({
+        ...contextValueWithRenderableChildren,
+        itemActionComponent: undefined,
+      });
+      renderItems();
+
+      const listItem = screen.getByText(mockData[0].label);
+      userEvent.hover(listItem);
+      fireEvent.contextMenu(listItem);
+
+      expect(screen.queryByRole("menu")).not.toBeInTheDocument();
+    });
+
+    it("should not close the menu when hovering out of the target", () => {
+      spy.mockReturnValue({ ...contextValueWithRenderableChildren });
+      renderItems();
+
+      const listItem = screen.getByText(mockData[0].label);
+      userEvent.hover(listItem);
+      fireEvent.contextMenu(listItem);
+
+      userEvent.unhover(listItem);
+
+      expect(screen.getByRole("menu")).toBeInTheDocument();
     });
   });
 });
