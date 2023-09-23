2d67b0eac9a63d560537035f8cbba7f150394f2a
Taylor Noj <taylor.n@getjobber.com>
2023-09-23 16:43:49 -0400

    add gaps between each options and actions, add shadow focus

--- packages/components/src/Combobox/components/ComboboxContent/ComboboxContent.css.d.ts
+++ packages/components/src/Combobox/components/ComboboxContent/ComboboxContent.css.d.ts
@@ -1,12 +1,12 @@
 declare const styles: {
   readonly "content": string;
   readonly "hidden": string;
+  readonly "clearSearch": string;
   readonly "optionsList": string;
   readonly "option": string;
   readonly "selectedOption": string;
   readonly "search": string;
   readonly "searchInput": string;
-  readonly "clearSearch": string;
   readonly "filterMessage": string;
   readonly "emptyStateMessage": string;
   readonly "actions": string;
