2415dca526c21898ffc544d723b422e7be0a19cb
Darryl Tec <darryl.t@getjobber.com>
2023-09-29 08:32:10 -0600

    add data list action test

---
+++ packages/components/src/DataList/components/DataListAction/DataListAction.test.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { DataListAction } from "./DataListAction";
+import { DataListLayoutActionsContext } from "../DataListLayoutActions/DataListLayoutContext";
+
+const name = "Hello";
+const handleClick = jest.fn();
+const mockIconValue = jest.fn().mockReturnValue(undefined);
+const mockDestructiveValue = jest.fn().mockReturnValue(undefined);
+const mockActiveItemValue = jest.fn().mockReturnValue(undefined);
+
+beforeEach(() => {
+  handleClick.mockClear();
+  mockIconValue.mockClear();
+  mockDestructiveValue.mockClear();
+  mockActiveItemValue.mockClear();
+});
+
+describe("DataListAction", () => {
+  it("should render an action with no icon", () => {
+    const { button } = renderComponent();
+
+    expect(button).toBeInTheDocument();
+    expect(button.querySelector("svg")).not.toBeInTheDocument();
+  });
+
+  it("should render an action with an icon", () => {
+    const iconName = "add";
+    mockIconValue.mockReturnValue(iconName);
+    const { button } = renderComponent();
+
+    expect(button.querySelector("svg")).toBeInTheDocument();
+    expect(screen.getByTestId(iconName)).toBeInstanceOf(SVGSVGElement);
+  });
+
+  it("should render a destructive action", () => {
+    mockDestructiveValue.mockReturnValue(true);
+    const { button } = renderComponent();
+
+    expect(button.querySelector("p")).toHaveClass("critical");
+  });
+
+  it("should not fire the onClick when the item from the context is undefined", () => {
+    const { button } = renderComponent();
+
+    userEvent.click(button);
+    expect(handleClick).not.toHaveBeenCalled();
+  });
+
+  it("should fire the onClick when the item from the context is defined", () => {
+    const mockItem = { id: "1" };
+    mockActiveItemValue.mockReturnValue(mockItem);
+    const { button } = renderComponent();
+
+    userEvent.click(button);
+    expect(handleClick).toHaveBeenCalledWith(mockItem);
+  });
+});
+
+function renderComponent() {
+  const component = render(
+    <DataListLayoutActionsContext.Provider
+      value={{ activeItem: mockActiveItemValue() }}
+    >
+      <DataListAction
+        label={name}
+        onClick={handleClick}
+        icon={mockIconValue()}
+        destructive={mockDestructiveValue()}
+      />
+    </DataListLayoutActionsContext.Provider>,
+  );
+
+  return { ...component, button: screen.getByRole("button", { name }) };
+}
