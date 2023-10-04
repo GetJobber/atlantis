5f57defc45abcf5c2495de56542788bf389c890f
Scott Thompson <scott.t@getjobber.com>
2023-10-04 10:50:12 -0300

    updating code locations within atlantis to better support jobber-mobile

---
+++ packages/components-native/src/InputEmail/InputEmail.tsx
@@ -0,0 +1,25 @@
+import React, { Ref, forwardRef } from "react";
+import { InputText, InputTextProps, InputTextRef } from "../InputText";
+
+export const InputEmail = forwardRef(InputEmailInternal);
+type InputEmailProps = Omit<InputTextProps, "keyboard">;
+
+function InputEmailInternal(props: InputEmailProps, ref: Ref<InputTextRef>) {
+  return (
+    <InputText
+      {...props}
+      ref={ref}
+      autoCapitalize="none"
+      autoCorrect={false}
+      keyboard={"email-address"}
+      validations={{
+        pattern: {
+          value:
+            /[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?/,
+          message: "Enter a valid email address (email@example.com)",
+        },
+        ...props.validations,
+      }}
+    />
+  );
+}
