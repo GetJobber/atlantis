4c5118cb2ba71390fd7a066a8915c9ea8f5f1343
Taylor Noj <taylor.n@getjobber.com>
2023-10-04 11:58:41 -0400

    change header action to Button small

--- packages/components/src/Combobox/components/ComboboxContent/ComboboxHeader.css.d.ts
+++ packages/components/src/Combobox/components/ComboboxContent/ComboboxHeader.css.d.ts
@@ -1,6 +1,5 @@
 declare const styles: {
   readonly "header": string;
-  readonly "action": string;
 };
 export = styles;
 
--- packages/components/src/Combobox/components/ComboboxContent/ComboboxHeader.tsx
+++ packages/components/src/Combobox/components/ComboboxContent/ComboboxHeader.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Typography } from "@jobber/components/Typography";
+import { Button } from "@jobber/components/Button";
 import styles from "./ComboboxHeader.css";
 
 // interface ComboboxHeaderProps {
@@ -13,14 +14,18 @@ export function ComboboxHeader(): JSX.Element {
       <Typography textColor="heading" fontWeight="semiBold">
         Select
       </Typography>
-      <button className={styles.action}>
-        <Typography size="base" textColor="green" fontWeight="semiBold">
-          Clear
-        </Typography>
-      </button>
+      <Button size="small" label="Select all" type="tertiary" />
     </div>
   );
 }
 
-// when multiselect is enabled, the header should show the number of selected item and a "Clear" button
-// when multiselect is disabled, the header should not exist
+// when multiselect is true, the header should be present
+//  the label should:
+//  read "Select" || "Select {subjectNoun}" when no options are selected
+//  read "{count} selected" when one or more options is selected
+
+// the action should:
+// read "Select all" when no options are selected
+// read "Clear" when one or more options is selected
+
+// when multiselect is false, the header should not exist
