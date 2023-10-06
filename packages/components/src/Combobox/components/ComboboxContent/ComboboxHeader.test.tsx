b334b8167f0b3d6df6e5a2064baa0be8e9de135e
Taylor Noj <taylor.n@getjobber.com>
2023-10-06 13:32:42 -0400

    remove MockComboboxProvider from Header tests

--- packages/components/src/Combobox/components/ComboboxContent/ComboboxHeader.test.tsx
+++ packages/components/src/Combobox/components/ComboboxContent/ComboboxHeader.test.tsx
@@ -1,25 +1,18 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { ComboboxHeader } from "./ComboboxHeader";
-import { ComboboxContext } from "../../ComboboxProvider";
 
 describe("ComboboxHeader", () => {
   describe("When no options have been selected", () => {
     it("should render a button with the correct label", () => {
-      const { getByText } = render(
-        <MockComboboxProvider>
-          <ComboboxHeader selectedCount={0} />
-        </MockComboboxProvider>,
-      );
+      const { getByText } = render(<ComboboxHeader selectedCount={0} />);
 
       expect(getByText("Select all")).toBeInTheDocument();
     });
 
     it("should render the correct label when subjectNoun is provided", () => {
       const { getByText } = render(
-        <MockComboboxProvider>
-          <ComboboxHeader selectedCount={0} subjectNoun="tax rates" />
-        </MockComboboxProvider>,
+        <ComboboxHeader selectedCount={0} subjectNoun="tax rates" />,
       );
 
       expect(getByText("Select tax rates")).toBeInTheDocument();
@@ -28,40 +21,15 @@ describe("ComboboxHeader", () => {
 
   describe("When one or more options has been selected", () => {
     it("should render a button with the correct label", () => {
-      const { getByText } = render(
-        <MockComboboxProvider>
-          <ComboboxHeader selectedCount={1} />
-        </MockComboboxProvider>,
-      );
+      const { getByText } = render(<ComboboxHeader selectedCount={1} />);
 
       expect(getByText("Clear")).toBeInTheDocument();
     });
 
     it("should render the correct label", () => {
-      const { getByText } = render(
-        <MockComboboxProvider>
-          <ComboboxHeader selectedCount={1} />
-        </MockComboboxProvider>,
-      );
+      const { getByText } = render(<ComboboxHeader selectedCount={1} />);
 
       expect(getByText("1 selected")).toBeInTheDocument();
     });
   });
 });
-
-function MockComboboxProvider({ children }: { children: React.ReactNode }) {
-  const [open, setOpen] = React.useState(false);
-
-  return (
-    <ComboboxContext.Provider
-      value={{
-        multiselect: true,
-        open,
-        setOpen,
-        wrapperRef: { current: null },
-      }}
-    >
-      {children}
-    </ComboboxContext.Provider>
-  );
-}
