0b03c0d04507be5352189cbdad39912349d5ed52
Scott Thompson <scott.t@getjobber.com>
2023-10-10 13:04:58 -0300

    adding a comment for useChildComponent

--- packages/components/src/hooks/useChildComponent.ts
+++ packages/components/src/hooks/useChildComponent.ts
@@ -1,5 +1,11 @@
 import React, { ReactNode, isValidElement, useMemo } from "react";
 
+/**
+ * Returns the first React Child that receives `true` from isCorrectComponent
+ * @param children React Children
+ * @param isCorrectComponent a function that accepts a JSX Element and returns a boolean.
+ * @returns The first child component that returns true on isCorrectComponent
+ */
 export function useChildComponent(
   children: ReactNode | undefined,
   isCorrectComponent: (toCheck: JSX.Element) => boolean,
