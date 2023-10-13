a2eed9a82263d20284a4fb961655762f9b450e6e
Darryl Tec <darryl.t@getjobber.com>
2023-10-13 13:28:34 -0600

    chore: reduce the amount of mocking a consumer would have to do when using DataList (#1584)
    guard match media

--- packages/components/src/DataList/hooks/useMediaQuery.ts
+++ packages/components/src/DataList/hooks/useMediaQuery.ts
@@ -1,6 +1,19 @@
 import { useEffect, useState } from "react";
 
 export function useMediaQuery(CSSMediaQuery: string) {
+  /**
+   * matchMedia have had full support for browsers since 2012 but jest, being a
+   * lite version of a DOM, doesn't support it.
+   *
+   * Instead of the consumers mocking matchMedia on every usage of DataList,
+   * we can just return true to mimic the largest screen we support.
+   *
+   * In the event that the consumer wants to test the DataList on different
+   * screen sizes, they can use the `mockViewportWidth` function from
+   * `@jobber/components/useBreakpoints`.
+   */
+  if (window.matchMedia === undefined) return true;
+
   const [matches, setMatches] = useState(
     window.matchMedia(CSSMediaQuery).matches,
   );
