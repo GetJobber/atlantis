73e89c9b4d55d7a4ca2492d82a57d9b573769114
Matt Anderson <matt@mattdev.ca>
2023-10-19 08:29:02 -0400

    feat(components): add Flex component for web (#1556)

---
+++ packages/components/src/Flex/Flex.css.d.ts
@@ -0,0 +1,14 @@
+declare const styles: {
+  readonly "flexible": string;
+  readonly "smallestGap": string;
+  readonly "smallerGap": string;
+  readonly "smallGap": string;
+  readonly "baseGap": string;
+  readonly "largeGap": string;
+  readonly "noneGap": string;
+  readonly "startAlign": string;
+  readonly "centerAlign": string;
+  readonly "endAlign": string;
+};
+export = styles;
+
