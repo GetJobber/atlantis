4bc6f8e6f9ebf2922e0fbd12df85b43966d8ba34
Matt Anderson <matt@mattdev.ca>
2023-10-20 13:54:23 -0400

    feat(components): Tabs supports caching active tab between page loads (#1591)
    * add tabs feature allowing preset active tab
    
    * check that default tab is within bounds
    
    * set defaultTab to 0 by default to avoid type guarding
    
    * make defaultTab comment more specific
    
    * make tabs tests more user-centric
    
    * add web stories for default tab and callback
    
    * remove prev tab from tab change callback
    
    * simplify callback example

--- packages/components/src/Tabs/Tabs.test.tsx
+++ packages/components/src/Tabs/Tabs.test.tsx
@@ -17,32 +17,86 @@ const omelet = (
   </Tabs>
 );
 
-it("renders Tabs", () => {
-  const { container } = render(omelet);
-  expect(container).toMatchSnapshot();
-});
+describe("Tabs", () => {
+  it("renders Tabs", () => {
+    const { container } = render(omelet);
+    expect(container).toMatchSnapshot();
+  });
 
-test("it should switch tabs", () => {
-  const { getByText, queryByText } = render(omelet);
+  it("should switch tabs", () => {
+    const { getByText, queryByText } = render(omelet);
 
-  expect(queryByText("🍳")).toBeTruthy();
-  expect(queryByText("🧀")).toBeFalsy();
+    expect(queryByText("🍳")).toBeInTheDocument();
+    expect(queryByText("🧀")).not.toBeInTheDocument();
 
-  fireEvent.click(getByText("Cheese"));
-  expect(queryByText("🍳")).toBeFalsy();
-  expect(queryByText("🧀")).toBeTruthy();
+    fireEvent.click(getByText("Cheese"));
+    expect(queryByText("🍳")).not.toBeInTheDocument();
+    expect(queryByText("🧀")).toBeInTheDocument();
 
-  fireEvent.click(getByText("Eggs"));
-  expect(queryByText("🍳")).toBeTruthy();
-  expect(queryByText("🧀")).toBeFalsy();
-});
+    fireEvent.click(getByText("Eggs"));
+    expect(queryByText("🍳")).toBeInTheDocument();
+    expect(queryByText("🧀")).not.toBeInTheDocument();
+  });
 
-test("it should handle tab onClick", () => {
-  const { getByText } = render(omelet);
-  count = 0;
+  it("should handle tab onClick", () => {
+    const { getByText } = render(omelet);
+    count = 0;
 
-  fireEvent.click(getByText("Cheese"));
-  expect(count).toBe(1);
-  fireEvent.click(getByText("Cheese"));
-  expect(count).toBe(2);
+    fireEvent.click(getByText("Cheese"));
+    expect(count).toBe(1);
+    fireEvent.click(getByText("Cheese"));
+    expect(count).toBe(2);
+  });
+
+  it("calls the onTabChange callback after a tab is clicked", () => {
+    const onTabChange = jest.fn();
+    const { getByText } = render(
+      <Tabs onTabChange={onTabChange}>
+        <Tab label="Eggs">
+          <p>🍳</p>
+          <p>Eggs</p>
+        </Tab>
+        <Tab label="Cheese">
+          <p>🧀</p>
+        </Tab>
+      </Tabs>,
+    );
+
+    fireEvent.click(getByText("Cheese"));
+    expect(onTabChange).toHaveBeenCalledWith(1);
+  });
+
+  it("sets the active tab on mount", () => {
+    const { queryByText } = render(
+      <Tabs defaultTab={1}>
+        <Tab label="Eggs">
+          <p>🍳</p>
+          <p>Eggs</p>
+        </Tab>
+        <Tab label="Cheese">
+          <p>🧀</p>
+        </Tab>
+      </Tabs>,
+    );
+
+    expect(queryByText("🍳")).not.toBeInTheDocument();
+    expect(queryByText("🧀")).toBeInTheDocument();
+  });
+
+  it("sets the active tab to 0 if the defaultTab is out of bounds", () => {
+    const { queryByText } = render(
+      <Tabs defaultTab={2}>
+        <Tab label="Eggs">
+          <p>🍳</p>
+          <p>Eggs</p>
+        </Tab>
+        <Tab label="Cheese">
+          <p>🧀</p>
+        </Tab>
+      </Tabs>,
+    );
+
+    expect(queryByText("🍳")).toBeInTheDocument();
+    expect(queryByText("🧀")).not.toBeInTheDocument();
+  });
 });
