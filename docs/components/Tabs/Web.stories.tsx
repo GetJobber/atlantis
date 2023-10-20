9cfcc03fd88ba224fcf008d69ff0674681fd5ba2
Matt Anderson <matt.a@getjobber.com>
2023-10-20 08:49:31 -0400

    add web stories for default tab and callback

--- docs/components/Tabs/Web.stories.tsx
+++ docs/components/Tabs/Web.stories.tsx
@@ -39,7 +39,72 @@ const BasicTemplate: ComponentStory<typeof Tab> = args => (
   </Tabs>
 );
 
+const WithDefaultTabTemplate: ComponentStory<typeof Tabs> = args => (
+  <Tabs {...args}>
+    <Tab label="Eggs">
+      🍳 Some eggs are laid by female animals of many different species,
+      including birds, reptiles, amphibians, mammals, and fish, and have been
+      eaten by humans for thousands of years.
+    </Tab>
+    <Tab label="Cheese">
+      🧀 Cheese is a dairy product derived from milk that is produced in a wide
+      range of flavors, textures, and forms by coagulation of the milk protein
+      casein.
+    </Tab>
+    <Tab label="Berries">
+      🍓 A berry is a small, pulpy, and often edible fruit. Typically, berries
+      are juicy, rounded, brightly colored, sweet, sour or tart, and do not have
+      a stone or pit.
+    </Tab>
+  </Tabs>
+);
+
+const WithTabChangeCallbackTemplate: ComponentStory<typeof Tabs> = args => {
+  const [tab, setTab] = React.useState(args.defaultTab ?? 0);
+  const [prevTab, setPrevTab] = React.useState<number>();
+
+  const handleTabChange = (tabIndex: number, prevTabIndex: number) => {
+    setTab(tabIndex);
+    setPrevTab(prevTabIndex);
+  };
+
+  return (
+    <div>
+      <p>Active tab index: {tab}</p>
+      <p>Previous tab index: {prevTab !== undefined ? prevTab : "none"}</p>
+      <Tabs {...args} onTabChange={handleTabChange}>
+        <Tab label="Eggs">
+          🍳 Some eggs are laid by female animals of many different species,
+          including birds, reptiles, amphibians, mammals, and fish, and have
+          been eaten by humans for thousands of years.
+        </Tab>
+        <Tab label="Cheese">
+          🧀 Cheese is a dairy product derived from milk that is produced in a
+          wide range of flavors, textures, and forms by coagulation of the milk
+          protein casein.
+        </Tab>
+        <Tab label="Berries">
+          🍓 A berry is a small, pulpy, and often edible fruit. Typically,
+          berries are juicy, rounded, brightly colored, sweet, sour or tart, and
+          do not have a stone or pit.
+        </Tab>
+      </Tabs>
+    </div>
+  );
+};
+
 export const Basic = BasicTemplate.bind({});
+export const WithDefaultTab = WithDefaultTabTemplate.bind({});
+export const WithTabChangeCallback = WithTabChangeCallbackTemplate.bind({});
+
 Basic.args = {
   label: "Eggs",
+};
+
+WithDefaultTab.args = {
+  defaultTab: 1,
+};
+
+WithTabChangeCallback.args = {
+  defaultTab: 1,
 };
