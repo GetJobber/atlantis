555afc91a6818e0016f3808171fbb7f055af4e9f
Taylor Noj <taylor.n@getjobber.com>
2023-10-27 11:43:32 -0400

    reorganize Select tests and remove duplicate tests

--- packages/components-native/src/Select/Select.test.tsx
+++ packages/components-native/src/Select/Select.test.tsx
@@ -25,7 +25,6 @@ afterEach(() => {
 
 const defaultPlaceholder = "Select an option";
 
-// eslint-disable-next-line max-statements
 describe("Select", () => {
   it("renders a Select", () => {
     const component = render(
@@ -79,48 +78,6 @@ describe("Select", () => {
     expect(
       getByText(assistiveText, { includeHiddenElements: true }),
     ).toBeDefined();
-  });
-
-  describe("when invalid", () => {
-    const labelText = "labelText";
-
-    it("renders an invalid Select", () => {
-      const { getByText } = render(
-        <Select onChange={onChange} invalid={true} label={labelText}>
-          <Option value={"1"}>1</Option>
-          <Option value={"2"}>2</Option>
-        </Select>,
-      );
-      expect(
-        getByText(labelText, { includeHiddenElements: true }).props.style,
-      ).toContainEqual({
-        color: tokens["color-critical"],
-      });
-    });
-
-    it("renders an invalid Select with placeholder", () => {
-      const placeholder = "Place me in the holder";
-      const { getByText } = render(
-        <Select
-          label={labelText}
-          onChange={onChange}
-          invalid={true}
-          placeholder={placeholder}
-        >
-          <Option value={"1"}>1</Option>
-          <Option value={"2"}>2</Option>
-        </Select>,
-      );
-
-      expect(
-        getByText(placeholder, { includeHiddenElements: true }),
-      ).toBeDefined();
-      expect(
-        getByText(labelText, { includeHiddenElements: true }).props.style,
-      ).toContainEqual({
-        color: tokens["color-critical"],
-      });
-    });
   });
 
   it("renders a disabled Select", () => {
@@ -179,156 +136,160 @@ describe("Select", () => {
     expect(getByTestId(`ATL-${testID}-Select`)).toBeDefined();
   });
 
-  describe("fires the onChange callback", () => {
-    it("fires", () => {
-      const { getByTestId } = render(
-        <Select onChange={onChange} value={"2"}>
-          <Option value={"1"}>1</Option>
-          <Option value={"2"}>2</Option>
+  it("renders an accessibilityLabel if provided", () => {
+    const { getByLabelText } = render(
+      <Select
+        onChange={onChange}
+        label="label"
+        accessibilityLabel="accessibilityLabel"
+      >
+        <Option value={"1"}>1</Option>
+        <Option value={"2"}>2</Option>
+      </Select>,
+    );
+
+    expect(getByLabelText("accessibilityLabel")).toBeTruthy();
+  });
+  it("fires the onChange callback", () => {
+    const { getByTestId } = render(
+      <Select onChange={onChange} value={"2"}>
+        <Option value={"1"}>1</Option>
+        <Option value={"2"}>2</Option>
+      </Select>,
+    );
+
+    const select = getByTestId("ATL-Select").findByType(SelectInternalPicker);
+    expect(select).toBeTruthy();
+    fireEvent(select, "onChange", "1");
+    expect(onChange).toHaveBeenCalledWith("1");
+  });
+});
+
+describe("when Select is invalid", () => {
+  const labelText = "labelText";
+
+  it("renders an invalid Select", () => {
+    const { getByText } = render(
+      <Select onChange={onChange} invalid={true} label={labelText}>
+        <Option value={"1"}>1</Option>
+        <Option value={"2"}>2</Option>
+      </Select>,
+    );
+    expect(
+      getByText(labelText, { includeHiddenElements: true }).props.style,
+    ).toContainEqual({
+      color: tokens["color-critical"],
+    });
+  });
+
+  it("renders an invalid Select with placeholder", () => {
+    const placeholder = "Place me in the holder";
+    const { getByText } = render(
+      <Select
+        label={labelText}
+        onChange={onChange}
+        invalid={true}
+        placeholder={placeholder}
+      >
+        <Option value={"1"}>1</Option>
+        <Option value={"2"}>2</Option>
+      </Select>,
+    );
+
+    expect(
+      getByText(placeholder, { includeHiddenElements: true }),
+    ).toBeDefined();
+    expect(
+      getByText(labelText, { includeHiddenElements: true }).props.style,
+    ).toContainEqual({
+      color: tokens["color-critical"],
+    });
+  });
+});
+
+describe("when validations are passed to the component", () => {
+  describe("validations fail", () => {
+    let tree: RenderAPI;
+    const labelText = "labelText";
+    const errorMsg = "Too short";
+    beforeEach(() => {
+      tree = render(
+        <Select
+          label={labelText}
+          onChange={onChange}
+          value={"Watermelon"}
+          validations={{
+            minLength: { value: 60, message: errorMsg },
+          }}
+        >
+          <Option value={"Apple"}>Apple</Option>
+          <Option value={"Watermelon"}>Watermelon</Option>
         </Select>,
       );
+    });
 
-      const select = getByTestId("ATL-Select").findByType(SelectInternalPicker);
+    it("renders the error message when there is an error", async () => {
+      const select = tree
+        .getByTestId("ATL-Select")
+        .findByType(SelectInternalPicker);
+      fireEvent(select, "onChange", "Apple");
+      expect(
+        await tree.findByText(errorMsg, { includeHiddenElements: true }),
+      ).toBeTruthy();
+    });
+
+    it("shows the invalid colours", async () => {
+      const select = tree
+        .getByTestId("ATL-Select")
+        .findByType(SelectInternalPicker);
+      fireEvent(select, "onChange", "Apple");
+      expect(
+        (await tree.findByText(labelText, { includeHiddenElements: true }))
+          .props.style,
+      ).toContainEqual({
+        color: tokens["color-critical"],
+      });
+    });
+  });
+
+  describe("validations passes", () => {
+    let tree: RenderAPI;
+    const labelText = "labelText";
+    const errorMsg = "Not too short";
+    beforeEach(() => {
+      tree = render(
+        <Select
+          label={labelText}
+          onChange={onChange}
+          value={"Watermelon"}
+          validations={{
+            minLength: { value: 4, message: errorMsg },
+          }}
+        >
+          <Option value={"Apple"}>Apple</Option>
+          <Option value={"Watermelon"}>Watermelon</Option>
+        </Select>,
+      );
+    });
+
+    it("does not render any error messages", () => {
+      const select = tree
+        .getByTestId("ATL-Select")
+        .findByType(SelectInternalPicker);
       expect(select).toBeTruthy();
-      fireEvent(select, "onChange", "1");
-      expect(onChange).toHaveBeenCalledWith("1");
+      fireEvent(select, "onChange", "Apple");
+      expect(tree.queryByText(errorMsg)).toBeNull();
     });
-  });
 
-  describe("Invalid value", () => {
-    it("renders with the empty value option", () => {
-      const { getByText } = render(
-        <Select onChange={onChange} value={"invalid"}>
-          <Option value={"first"}>first</Option>
-          <Option value={"2"}>2</Option>
-        </Select>,
-      );
-
+    it("has non-critical colours", () => {
+      const select = tree
+        .getByTestId("ATL-Select")
+        .findByType(SelectInternalPicker);
+      fireEvent(select, "onChange", "Apple");
       expect(
-        getByText(defaultPlaceholder, { includeHiddenElements: true }),
-      ).toBeDefined();
-    });
-
-    it("renders with the placeholder", () => {
-      const { getByText } = render(
-        <Select
-          onChange={onChange}
-          value={"invalid"}
-          placeholder={"Make a selection"}
-        >
-          <Option value={"1"}>1</Option>
-          <Option value={"2"}>2</Option>
-        </Select>,
-      );
-
-      expect(
-        getByText("Make a selection", { includeHiddenElements: true }),
-      ).toBeDefined();
-    });
-  });
-
-  describe("accessibilityLabel", () => {
-    it("uses accessibilityLabel if specified", () => {
-      const { getByLabelText } = render(
-        <Select
-          onChange={onChange}
-          label="label"
-          accessibilityLabel="accessibilityLabel"
-        >
-          <Option value={"1"}>1</Option>
-          <Option value={"2"}>2</Option>
-        </Select>,
-      );
-
-      expect(getByLabelText("accessibilityLabel")).toBeTruthy();
-    });
-  });
-
-  describe("when validations are passed to the component", () => {
-    describe("validations fail", () => {
-      let tree: RenderAPI;
-      const labelText = "labelText";
-      const errorMsg = "Too short";
-      beforeEach(() => {
-        tree = render(
-          <Select
-            label={labelText}
-            onChange={onChange}
-            value={"Watermelon"}
-            validations={{
-              minLength: { value: 60, message: errorMsg },
-            }}
-          >
-            <Option value={"Apple"}>Apple</Option>
-            <Option value={"Watermelon"}>Watermelon</Option>
-          </Select>,
-        );
-      });
-
-      it("renders the error message when there is an error", async () => {
-        const select = tree
-          .getByTestId("ATL-Select")
-          .findByType(SelectInternalPicker);
-        fireEvent(select, "onChange", "Apple");
-        expect(
-          await tree.findByText(errorMsg, { includeHiddenElements: true }),
-        ).toBeTruthy();
-      });
-
-      it("shows the invalid colours", async () => {
-        const select = tree
-          .getByTestId("ATL-Select")
-          .findByType(SelectInternalPicker);
-        fireEvent(select, "onChange", "Apple");
-        expect(
-          (await tree.findByText(labelText, { includeHiddenElements: true }))
-            .props.style,
-        ).toContainEqual({
-          color: tokens["color-critical"],
-        });
-      });
-    });
-
-    describe("validations passes", () => {
-      let tree: RenderAPI;
-      const labelText = "labelText";
-      const errorMsg = "Not too short";
-      beforeEach(() => {
-        tree = render(
-          <Select
-            label={labelText}
-            onChange={onChange}
-            value={"Watermelon"}
-            validations={{
-              minLength: { value: 4, message: errorMsg },
-            }}
-          >
-            <Option value={"Apple"}>Apple</Option>
-            <Option value={"Watermelon"}>Watermelon</Option>
-          </Select>,
-        );
-      });
-
-      it("does not render any error messages", () => {
-        const select = tree
-          .getByTestId("ATL-Select")
-          .findByType(SelectInternalPicker);
-        expect(select).toBeTruthy();
-        fireEvent(select, "onChange", "Apple");
-        expect(tree.queryByText(errorMsg)).toBeNull();
-      });
-
-      it("has non-critical colours", () => {
-        const select = tree
-          .getByTestId("ATL-Select")
-          .findByType(SelectInternalPicker);
-        fireEvent(select, "onChange", "Apple");
-        expect(
-          tree.getByText(labelText, { includeHiddenElements: true }).props
-            .style,
-        ).toContainEqual({
-          color: tokens["color-text--secondary"],
-        });
+        tree.getByText(labelText, { includeHiddenElements: true }).props.style,
+      ).toContainEqual({
+        color: tokens["color-text--secondary"],
       });
     });
   });
