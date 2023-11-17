e6d2187e4fd7e969b0e3b17de370656756dfbc3f
jcw- <jc.w@getjobber.com>
2023-11-17 05:08:48 -0700

    Remove assert usage from Chip

--- packages/components/src/Chips/tests/Chip.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { Chip } from "..";
-
-it("should throw error when not wrapped inside `<Chips>`", () => {
-  expect(() => render(<Chip value={""} label={""} />)).toThrow(
-    "`<Chip>` component can only be used inside `<Chips>`",
-  );
-});
