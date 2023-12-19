beee9ffd13b2283fd7cf70a6e50ac64629dbd6b2
Darryl Tec <darryl.t@getjobber.com>
2023-12-19 13:57:12 -0700

    first pass

--- packages/components/src/InputTime/InputTime.tsx
+++ packages/components/src/InputTime/InputTime.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 // eslint-disable-next-line import/no-internal-modules
 import supportsTime from "time-input-polyfill/supportsTime";
+import debounce from "lodash/debounce";
 import { InputTimeSafari } from "./InputTimeSafari";
 import {
   civilTimeToHTMLTime,
@@ -9,14 +10,28 @@ import {
 import { InputTimeProps } from "./InputTimeProps";
 import { FormField, FormFieldProps } from "../FormField";
 
+const hour12to24Hours: Record<string, number | undefined> = {
+  "1": 13,
+  "2": 14,
+  "3": 15,
+  "4": 16,
+  "5": 17,
+  "6": 18,
+  "7": 19,
+  "8": 20,
+};
+
 export function InputTime({
   defaultValue,
   value,
   onChange,
   ...params
 }: InputTimeProps) {
+  const [typedTime, setTypedTime] = React.useState<string>("");
+
   const handleChange = (newValue: string) => {
     onChange && onChange(htmlTimeToCivilTime(newValue));
+    setTypedTime("");
   };
 
   if (supportsTime) {
@@ -27,7 +42,42 @@ export function InputTime({
       ...params,
     };
 
-    return <FormField type="time" {...fieldProps} />;
+    const handleKeyup = debounce(() => {
+      if (value) {
+        return;
+      }
+
+      const today = new Date();
+      const currentHour = today.getHours();
+      let predictedHour: number;
+
+      if (typedTime === "1") {
+        predictedHour = currentHour + 1;
+      } else {
+        predictedHour =
+          hour12to24Hours[parseInt(typedTime, 10)] || parseInt(typedTime, 10);
+      }
+
+      console.log(typedTime);
+
+      handleChange(`${predictedHour}:00`);
+    }, 300);
+
+    useEffect(() => {
+      handleKeyup();
+
+      return handleKeyup.cancel;
+    }, [typedTime]);
+
+    return (
+      <FormField
+        type="time"
+        {...fieldProps}
+        onKeyUp={e => {
+          setTypedTime(prev => prev + e.key);
+        }}
+      />
+    );
   } else {
     return (
       <InputTimeSafari
