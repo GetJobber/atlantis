101f3a8bcfecbd08beb7409f9533e3a2d9350daf
Darryl Tec <darryl.t@getjobber.com>
2023-12-20 08:41:51 -0700

    guard config

--- packages/components/src/utils/getAtlantisConfig.ts
+++ packages/components/src/utils/getAtlantisConfig.ts
@@ -1,4 +1,7 @@
-// This is a workaround for the lack of context and its limitations in Jobber.
+// For retheme purposes only.
+// This will be removed once the retheme is out of the experiment.
 export function getAtlantisConfig(): typeof window.ATLANTIS_CONFIG {
-  return window.ATLANTIS_CONFIG;
+  if (typeof window !== "undefined") {
+    return window.ATLANTIS_CONFIG;
+  }
 }
