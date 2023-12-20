4fd22e5cbaceb91901ee7e4e31d348b6c5a0138a
Darryl Tec <darryl.t@getjobber.com>
2023-12-20 16:35:49 -0700

    chore: prep banner for retheme (#1701)
    * add icon and banner for retheme
    
    * banner background
    
    * adjust css for retheme
    
    * use small icons
    
    * remove align center
    
    * interactive subtle close icon

--- packages/components/src/Banner/components/BannerIcon/BannerIcon.tsx
+++ packages/components/src/Banner/components/BannerIcon/BannerIcon.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { IconNames } from "@jobber/design";
+import classNames from "classnames";
+import { getAtlantisConfig } from "@jobber/components/utils/getAtlantisConfig";
+import styles from "./BannerIcon.css";
 import { Icon } from "../../../Icon";
+import { BannerType } from "../../Banner.types";
 
 export interface BannerIconProps {
-  icon: IconNames;
+  readonly icon: IconNames;
+  readonly type: BannerType;
 }
 
-export function BannerIcon({ icon }: BannerIconProps): JSX.Element {
+export function BannerIcon({ icon, type }: BannerIconProps) {
+  const { JOBBER_RETHEME } = getAtlantisConfig();
+
+  if (JOBBER_RETHEME) {
+    return (
+      <span className={classNames(styles.wrapper, styles[type])}>
+        <Icon name={icon} color={"white"} size="small" />
+      </span>
+    );
+  }
+
   return <Icon name={icon} color={"greyBlueDark"} />;
 }
