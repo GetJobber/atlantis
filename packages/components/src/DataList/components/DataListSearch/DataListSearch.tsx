f8ab9fcbce746f079acd1080ba3ad982110f75f3
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2023-12-20 14:09:40 -0300

    Address PR review comments

--- packages/components/src/DataList/components/DataListSearch/DataListSearch.tsx
+++ packages/components/src/DataList/components/DataListSearch/DataListSearch.tsx
@@ -59,7 +59,6 @@ export function InternalDataListSearch() {
           placeholder={getPlaceholder()}
           onChange={debouncedSearch}
           prefix={{ icon: "search" }}
-          size="small"
           clearable="always"
         />
       </div>
