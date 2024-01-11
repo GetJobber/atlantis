16d8cc86d15a3a35c9b1041fe9cee6e914cdbd94
Darryl Tec <darryl.t@getjobber.com>
2024-01-11 12:28:29 -0700

    cleanup code and document

--- packages/components/src/InputTime/hooks/useTimePredict.ts
+++ packages/components/src/InputTime/hooks/useTimePredict.ts
@@ -28,14 +28,18 @@ export function useTimePredict({ value, handleChange }: UseTimePredictProps) {
   );
 
   /**
-   * Predict time when user types
+   * Predict the hour when user types a number
    */
   useEffect(() => {
+    /**
+     * Don't try to predict if the user types 0 as the user would almost always
+     * type another number after 0.
+     */
     if (typedTime && typedTime !== "0") {
       predictTime();
 
-      // Immediately predict if user types any number except for 1 and it's in a 12 hour format
       if ((IS_12_HOUR_FORMAT && typedTime !== "1") || typedTime.length === 2) {
+        // Immediately predict the hour
         predictTime.flush();
       }
     }
@@ -55,30 +59,75 @@ export function useTimePredict({ value, handleChange }: UseTimePredictProps) {
   };
 }
 
-// eslint-disable-next-line max-statements
 function predictHours(time: string, is12HourFormat = false) {
   const today = new Date();
   const currentHour = today.getHours();
   const parsedTime = parseInt(time, 10);
 
-  if (is12HourFormat && parsedTime === 1 && currentHour < 12) {
-    return currentHour < 10 ? 10 : currentHour + 1;
+  if (is12HourFormat) {
+    return predict12Hours(parsedTime, currentHour);
   }
 
-  if (!is12HourFormat && (parsedTime === 1 || parsedTime === 2)) {
-    if (parsedTime === 1) {
-      return currentHour < 10 || currentHour >= 20 ? 10 : currentHour + 1;
+  return predict24Hours(time, parsedTime, currentHour);
+}
+
+function predict12Hours(parsedTime: number, currentHour: number) {
+  /**
+   * We need to predict what the user wants when they type 1 since it could be
+   * 10, 11, 12, or 1.
+   *
+   * If the current hour is over 12PM, we can skip this and go to the next logic.
+   */
+  if (parsedTime === 1 && currentHour < 12) {
+    if (currentHour < 10) {
+      return 10;
     } else {
-      return currentHour < 20 ? 20 : currentHour + 1;
+      return currentHour + 1;
     }
   }
 
-  if (parsedTime === 24 || time === "00") {
-    return 0;
+  /**
+   * Typing 1-6 predicts that the user want that exact hour on the afternoon.
+   */
+  if (parsedTime <= 6) {
+    return parsedTime + 12;
   }
 
-  if (is12HourFormat && parsedTime < 12 && !(parsedTime > 6)) {
-    return parsedTime + 12;
+  /**
+   * Anything after 6 will be predicted to be set as AM.
+   */
+  return parsedTime;
+}
+
+function predict24Hours(time: string, parsedTime: number, currentHour: number) {
+  /**
+   * Typing 1 could be predicted from 10 to 19.
+   */
+  if (parsedTime === 1) {
+    if (currentHour < 10 || currentHour > 19) {
+      return 10;
+    } else {
+      return currentHour + 1;
+    }
+  }
+
+  /**
+   * Typing 2 could be predicted from 20 to 24.
+   */
+  if (parsedTime === 2) {
+    if (currentHour < 20) {
+      return 20;
+    } else {
+      return currentHour + 1;
+    }
+  }
+
+  /**
+   * Typing 24 or 00 is set to midnight. This ensures the time doesn't get set
+   * to 24 as that would be invalid.
+   */
+  if (parsedTime === 24 || time === "00") {
+    return 0;
   }
 
   return parsedTime;
