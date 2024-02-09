c57d5801448077dcfc407383c6c264aa092c5c48
Scott Thompson <145565743+scotttjob@users.noreply.github.com>
2024-02-09 06:48:19 -0400

    guarding use of document.navigator in formattime

--- packages/components/src/FormatTime/FormatTime.tsx
+++ packages/components/src/FormatTime/FormatTime.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CivilTime } from "@std-proposal/temporal";
 
 interface FormatTimeProps {
@@ -19,35 +19,39 @@ export function FormatTime({
   time: inputTime,
   use24HourClock,
 }: FormatTimeProps) {
-  let dateObject: Date;
+  const [civilTime, setCivilTime] = React.useState("");
 
-  if (inputTime instanceof Date) {
-    dateObject = inputTime;
-  } else if (typeof inputTime === "string") {
-    dateObject = new Date(inputTime);
-  } else {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const currentMonth = currentDate.getMonth();
-    const currentDay = currentDate.getDay();
-    dateObject = new Date(
-      currentYear,
-      currentMonth,
-      currentDay,
-      inputTime.hour,
-      inputTime.minute,
-      inputTime.second,
-      inputTime.millisecond,
+  useEffect(() => {
+    let dateObject: Date;
+
+    if (inputTime instanceof Date) {
+      dateObject = inputTime;
+    } else if (typeof inputTime === "string") {
+      dateObject = new Date(inputTime);
+    } else {
+      const currentDate = new Date();
+      const currentYear = currentDate.getFullYear();
+      const currentMonth = currentDate.getMonth();
+      const currentDay = currentDate.getDay();
+      dateObject = new Date(
+        currentYear,
+        currentMonth,
+        currentDay,
+        inputTime.hour,
+        inputTime.minute,
+        inputTime.second,
+        inputTime.millisecond,
+      );
+    }
+
+    setCivilTime(
+      dateObject.toLocaleTimeString(navigator?.language, {
+        hourCycle: use24HourClock ? "h23" : "h12",
+        minute: "2-digit",
+        hour: "numeric",
+      }),
     );
-  }
+  }, [inputTime]);
 
-  return <>{formatCivilTime(dateObject, use24HourClock)}</>;
-}
-
-function formatCivilTime(date: Date, use24HourClock?: boolean) {
-  return date.toLocaleTimeString(navigator.language, {
-    hourCycle: use24HourClock ? "h23" : "h12",
-    minute: "2-digit",
-    hour: "numeric",
-  });
+  return <>{civilTime}</>;
 }
