6a3428745d97f983ac3fd9e7c06ce41c75e85b51
Rob Ring <rob.r@getjobber.com>
2024-02-20 12:21:50 -0800

    * allow duplicate 'terms' to be passed into data tuple
    * add optional parameter to data tuple to manually define a unique key on each data element

--- packages/components/src/DescriptionList/DescriptionList.tsx
+++ packages/components/src/DescriptionList/DescriptionList.tsx
@@ -4,17 +4,18 @@ import { Typography } from "../Typography";
 
 interface DescriptionListProps {
   /**
-   * A tuple where the first item is the string to display as the term
-   * and the second value is the string to display as the definition
+   * A tuple where the first item is the string to display as the term,
+   * the second value is the string to display as the definition,
+   * and the third value is an optional key to use as the React key in scenarios where the display value is not unique for all elements in the data set.
    */
-  data: [string, string | ReactNode][];
+  readonly data: [string, string | ReactNode, string?][];
 }
 
 export function DescriptionList({ data }: DescriptionListProps) {
   return (
     <dl className={styles.descriptionList}>
-      {data.map(([term, description]) => (
-        <div key={term} className={styles.termGroup}>
+      {data.map(([term, description, key]) => (
+        <div key={key || term} className={styles.termGroup}>
           <Typography element="dt" textColor="heading" size="base">
             {term}
           </Typography>
