40b71b16dcdd187942a1b70feecdaf820bb66ea4
Ryan Berry <59584307+ryanb-jobber@users.noreply.github.com>
2024-02-22 09:04:09 -0700

    fix(components): Always show clear when there are selected options in ComboBox (#1768)
    * Always show header when there is a selection
    
    * Fix conditional logic
    
    * Fix conditional logic

--- packages/components/src/Combobox/components/ComboboxContent/ComboboxContent.tsx
+++ packages/components/src/Combobox/components/ComboboxContent/ComboboxContent.tsx
@@ -39,7 +39,7 @@ export function ComboboxContent(props: ComboboxContentProps): JSX.Element {
         handleSearchChange={props.handleSearchChange}
       />
 
-      {props.multiselect && optionsExist && (
+      {props.multiselect && (optionsExist || props.selected.length > 0) && (
         <ComboboxContentHeader
           hasOptionsVisible={optionsExist}
           subjectNoun={props.subjectNoun}
