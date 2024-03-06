5859dabecd371df182c569c0ca4118827b61dba8
Scott Hampton <felgamedev@gmail.com>
2024-03-06 13:28:05 -0400

    feat(components): Allow a prefix element on ComboBox.Option. Promote internal StatusLabel to StatusIndicator, top level component  (#1788)
    * Add StatusIndicator as Atlantis top level component from StatusLabel
    
    * Update ComboBox to accept a prefix element
    
    * Reuse existing getOption method
    
    * Add web stories for StatusIndicator
    
    * Remove double comment
    
    * Add related components note for StatusLabel
    
    * Ensure flex accounts for no prefix in template
    
    * Remove prefix from some options to show composition
    
    * Convert status indicator to inline-block span with tests
    
    * Move the margin top css rule back to the StatusLabel
    
    * Update StatusIndicatorType location and naming
    
    * Add more stern recommendation for StatusLabel usage

---
+++ docs/components/StatusIndicator/Web.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+import { StatusIndicator } from "@jobber/components/StatusIndicator";
+
+export default {
+  title: "Components/Status and Feedback/StatusIndicator/Web",
+  component: StatusIndicator,
+  parameters: {
+    viewMode: "story",
+    previewTabs: {
+      code: {
+        hidden: false,
+      },
+    },
+  },
+} as ComponentMeta<typeof StatusIndicator>;
+
+const BasicTemplate: ComponentStory<typeof StatusIndicator> = args => {
+  return <StatusIndicator {...args} />;
+};
+
+export const Basic = BasicTemplate.bind({});
+
+Basic.args = {
+  status: "success",
+};
--- packages/components/src/StatusIndicator/StatusIndicator.css.d.ts
+++ packages/components/src/StatusIndicator/StatusIndicator.css.d.ts
@@ -1,6 +1,4 @@
 declare const styles: {
-  readonly "statusLabelRow": string;
-  readonly "labelTextEndAligned": string;
   readonly "statusIndicator": string;
 };
 export = styles;
