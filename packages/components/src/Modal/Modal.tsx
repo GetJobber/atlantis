bff08b83d38cf5c44f2b90d948a567afefb09b81
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2024-03-11 10:20:36 -0300

    Cleanup

--- packages/components/src/Modal/Modal.tsx
+++ packages/components/src/Modal/Modal.tsx
@@ -5,9 +5,9 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useRefocusOnActivator } from "@jobber/hooks/useRefocusOnActivator";
 import { useOnKeyDown } from "@jobber/hooks/useOnKeyDown";
 import { useFocusTrap } from "@jobber/hooks/useFocusTrap";
+import { useIsMounted } from "@jobber/hooks/useIsMounted";
 import styles from "./Modal.css";
 import sizes from "./Sizes.css";
-import { ClientOnly } from "./ClientOnly";
 import { Heading } from "../Heading";
 import { Button, ButtonProps } from "../Button";
 import { ButtonDismiss } from "../ButtonDismiss";
@@ -45,6 +45,7 @@ export function Modal({
   useRefocusOnActivator(open);
   const modalRef = useFocusTrap<HTMLDivElement>(open);
   useOnKeyDown(handleRequestClose, "Escape");
+  const mounted = useIsMounted();
 
   const template = (
     <AnimatePresence>
@@ -95,11 +96,7 @@ export function Modal({
     </AnimatePresence>
   );
 
-  return (
-    <ClientOnly>
-      {typeof document !== "undefined" && createPortal(template, document.body)}
-    </ClientOnly>
-  );
+  return mounted.current ? createPortal(template, document.body) : template;
 
   function handleRequestClose() {
     if (open && onRequestClose) {
