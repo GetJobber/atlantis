590515abe880442ffd270c5c21a7f3841b6b8931
Scott Thompson <145565743+scotttjob@users.noreply.github.com>
2024-03-18 08:36:53 -0300

    stubbing process for react-markdown

--- packages/components/src/Markdown/index.ts
+++ packages/components/src/Markdown/index.ts
@@ -1,1 +1,10 @@
+if (typeof process === "undefined") {
+  globalThis.process = {
+    cwd: () => "/",
+    // react-markdown relies on the nodejs `process` package
+    // to determine if it's running in the browser or the server.
+    // This is the only method they use so we stub it for them.
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } as any;
+}
 export { Markdown } from "./Markdown";
