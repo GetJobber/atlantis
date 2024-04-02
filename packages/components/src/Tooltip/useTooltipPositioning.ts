b6cb777ac7605368208d80681dc81840c7a59b63
Eric Chernuka <ericchernuka@gmail.com>
2024-04-02 13:24:05 -0600

    feat: adds ability to provide preferred placement for tooltip
    Mimic's behaviour of popover to allow influencing the desired placement of the tooltip.
    
    Signed-off-by: Eric Chernuka <ericchernuka@gmail.com>

--- packages/components/src/Tooltip/useTooltipPositioning.ts
+++ packages/components/src/Tooltip/useTooltipPositioning.ts
@@ -1,7 +1,16 @@
 import { useRef, useState } from "react";
 import { usePopper } from "react-popper";
+import { Placement } from "./Tooltip.types";
 
-export function useTooltipPositioning() {
+const DEFAULT_PLACEMENT: Placement = "top";
+
+interface ToolTipPositionOptions {
+  readonly preferredPlacement?: Placement;
+}
+
+export function useTooltipPositioning({
+  preferredPlacement,
+}: ToolTipPositionOptions) {
   const shadowRef = useRef<HTMLSpanElement>(null);
   const [positionElement, setTooltipRef] = useState<HTMLDivElement | null>();
   const [arrowElement, setArrowRef] = useState<HTMLDivElement | null>();
@@ -10,7 +19,7 @@ export function useTooltipPositioning() {
     shadowRef.current?.nextElementSibling,
     positionElement,
     {
-      placement: "top",
+      placement: preferredPlacement,
       modifiers: [
         { name: "flip", options: { fallbackPlacements: ["bottom"] } },
         {
@@ -23,7 +32,7 @@ export function useTooltipPositioning() {
 
   return {
     ...popper,
-    placement: popper.state?.placement,
+    placement: popper.state?.placement || DEFAULT_PLACEMENT,
     shadowRef,
     setArrowRef,
     setTooltipRef,
