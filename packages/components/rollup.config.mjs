17dc9cff1e4b27f05df905bbafa390015ef25738
Scott Thompson <145565743+scotttjob@users.noreply.github.com>
2024-04-04 09:08:57 -0300

    hooks was not being exported properly

--- packages/components/rollup.config.mjs
+++ packages/components/rollup.config.mjs
@@ -156,6 +156,7 @@ export default {
     "framer-motion",
     "classnames",
     "@std-proposal/temporal",
+    "@jobber/hooks",
     "@jobber/design",
     "@jobber/design/foundation",
     "@jobber/formatters",
