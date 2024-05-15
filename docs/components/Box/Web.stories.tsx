a3d0d239788f6c8b22221e4aa56c9807f1da457f
Scott Thompson <145565743+scotttjob@users.noreply.github.com>
2024-05-15 09:38:29 -0300

    adding specific box documentation

--- docs/components/Box/Web.stories.tsx
+++ docs/components/Box/Web.stories.tsx
@@ -1,11 +1,19 @@
-import { ComponentMeta } from "@storybook/react";
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Box } from "@jobber/components/Box";
 
 export default {
-  title: "Components/Layout and Structure/Box/Web",
+  title: "Components/Layouts and Structure/Box/Web",
   component: Box,
   parameters: {
     viewMode: "story",
     previewTabs: { code: { hidden: false } },
   },
 } as ComponentMeta<typeof Box>;
+
+const BasicTemplate: ComponentStory<typeof Box> = args => (
+  <Box {...args}>Box Content</Box>
+);
+
+export const Basic = BasicTemplate.bind({});
+Basic.args = {};
