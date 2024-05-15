882990e67dec1bae8abb35e24ab7d4c182eecf1a
Taylor Noj <104797704+taylorvnoj@users.noreply.github.com>
2024-05-15 13:54:57 -0400

    fix(components): Remove hardcoded labels in DataListSort (#1883)
    * cleanup: fix DataListSort label issue
    
    * add toggle check on else if
    
    * shouldn't need that logic on the else
    
    * fix test
    
    * get label from sortable options, not header label
    
    * small cleanup
    
    * make DataListSortable options required and add prop comments
    
    * add documentation for Sorting in DataList story

--- docs/components/DataList/Web.stories.tsx
+++ docs/components/DataList/Web.stories.tsx
@@ -110,6 +110,7 @@ const Template: ComponentStory<typeof DataList> = args => {
         sortable: [
           {
             key: "label",
+            sortType: "dropdown",
             options: [
               {
                 id: "firstName",
@@ -131,6 +132,7 @@ const Template: ComponentStory<typeof DataList> = args => {
           },
           {
             key: "home",
+            sortType: "dropdown",
             options: [
               {
                 id: "homeWorld",
@@ -154,7 +156,22 @@ const Template: ComponentStory<typeof DataList> = args => {
               },
             ],
           },
-          { key: "lastActivity" },
+          {
+            key: "lastActivity",
+            sortType: "toggle",
+            options: [
+              {
+                id: "lastActivity",
+                label: "Last activity (Newest)",
+                order: "desc",
+              },
+              {
+                id: "lastActivity",
+                label: "Last activity (Oldest)",
+                order: "asc",
+              },
+            ],
+          },
         ],
       }}
     >
--- packages/components/src/DataList/DataList.types.ts
+++ packages/components/src/DataList/DataList.types.ts
@@ -57,8 +57,22 @@ export interface SortableOptions {
 }
 
 export interface DataListSortable {
+  /**
+   * The key of the sortable column.
+   */
   readonly key: string;
-  readonly options?: SortableOptions[];
+
+  /**
+   * The type of sorting on the column. Toggle will show the sorting arrows
+   * on the column header. Dropdown will show a menu with the sorting
+   * options.
+   */
+  readonly sortType: "toggle" | "dropdown";
+
+  /**
+   * The sorting options, containing id, label and order.
+   */
+  readonly options: SortableOptions[];
 }
 
 export interface DataListProps<T extends DataListObject> {
