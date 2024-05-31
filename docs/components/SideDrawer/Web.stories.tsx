8dd69d55231d5e66fe72d56efa350f0dcbf4a19a
Darryl Tec <darryl.t@getjobber.com>
2024-05-31 12:17:29 -0600

    add more examples

--- docs/components/SideDrawer/Web.stories.tsx
+++ docs/components/SideDrawer/Web.stories.tsx
@@ -7,6 +7,10 @@ import { Text } from "@jobber/components/Text";
 import { List } from "@jobber/components/List";
 import { Heading } from "@jobber/components/Heading";
 import { Tab, Tabs } from "@jobber/components/Tabs";
+import { InlineLabel } from "@jobber/components/InlineLabel";
+import { Flex } from "@jobber/components/Flex";
+import { Emphasis } from "@jobber/components/Emphasis";
+import { InputText } from "@jobber/components/InputText";
 
 export default {
   title: "Components/Overlays/SideDrawer/Web",
@@ -103,7 +107,115 @@ const BasicTemplate: ComponentStory<typeof SideDrawer> = args => {
   );
 };
 
-export const Basic = BasicTemplate.bind({});
-Basic.args = {
-  open: true,
+const ActionTemplate: ComponentStory<typeof SideDrawer> = args => {
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(args.open);
+
+  return (
+    <>
+      <Button
+        onClick={() => setSideDrawerOpen(true)}
+        label="Open Side Drawer"
+      />
+      <SideDrawer
+        open={sideDrawerOpen}
+        onRequestClose={() => setSideDrawerOpen(false)}
+      >
+        <SideDrawer.Title>Actions</SideDrawer.Title>
+        <SideDrawer.Actions>
+          <Button
+            ariaLabel="New"
+            icon="compose"
+            type="secondary"
+            variation="subtle"
+          />
+          <Button
+            ariaLabel="New"
+            icon="cog"
+            type="secondary"
+            variation="subtle"
+          />
+        </SideDrawer.Actions>
+
+        <Content>
+          <Text>Actions shows up before the close button.</Text>
+        </Content>
+      </SideDrawer>
+    </>
+  );
 };
+
+const CustomTitleTemplate: ComponentStory<typeof SideDrawer> = args => {
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(args.open);
+
+  return (
+    <>
+      <Button
+        onClick={() => setSideDrawerOpen(true)}
+        label="Open Side Drawer"
+      />
+      <SideDrawer
+        open={sideDrawerOpen}
+        onRequestClose={() => setSideDrawerOpen(false)}
+      >
+        <SideDrawer.Title>
+          <Flex template={["shrink", "shrink"]}>
+            <Heading level={2}>
+              <Emphasis variation="highlight">Custom Title</Emphasis>
+            </Heading>
+            <InlineLabel color="lightBlue">Lead</InlineLabel>
+          </Flex>
+        </SideDrawer.Title>
+
+        <Content>
+          <Text>
+            If you need more control over what shows up on the heading, you can
+            pass in a custom element in SideDrawer.Title
+          </Text>
+        </Content>
+      </SideDrawer>
+    </>
+  );
+};
+
+const ToolbarTemplate: ComponentStory<typeof SideDrawer> = args => {
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(args.open);
+
+  return (
+    <>
+      <Button
+        onClick={() => setSideDrawerOpen(true)}
+        label="Open Side Drawer"
+      />
+      <SideDrawer
+        open={sideDrawerOpen}
+        onRequestClose={() => setSideDrawerOpen(false)}
+      >
+        <SideDrawer.Title>Toolbar</SideDrawer.Title>
+        <SideDrawer.Toolbar>
+          <InputText
+            placeholder="Search for something"
+            prefix={{ icon: "search" }}
+            clearable="always"
+          />
+        </SideDrawer.Toolbar>
+
+        <Content>
+          <Heading level={3}>No results found</Heading>
+          <Text>Try searching again</Text>
+        </Content>
+      </SideDrawer>
+    </>
+  );
+};
+
+export const Basic = BasicTemplate.bind({});
+Basic.args = { open: true };
+
+export const Action = ActionTemplate.bind({});
+Action.args = { open: true };
+
+export const CustomTitle = CustomTitleTemplate.bind({});
+CustomTitle.args = { open: true };
+
+export const Toolbar = ToolbarTemplate.bind({});
+Toolbar.args = { open: true };
