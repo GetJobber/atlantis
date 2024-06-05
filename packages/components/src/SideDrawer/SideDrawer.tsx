6f55a35f56ec0569d827d929678ad7fe70a93cbe
Darryl Tec <darryl.t@getjobber.com>
2024-06-05 09:29:29 -0600

    feat(components): add side drawer (#1909)
    * side drawer first pass
    
    * add docs example
    
    * animation baby!
    
    * add overlay to close
    
    * add an expansive example
    
    * add more examples
    
    * add basic docs
    
    * add jsdoc
    
    * add tests and a11y tweaks
    
    * add subtle variation
    
    * add shadow on scroll
    
    * support a real back button now
    
    * add docs
    
    * view and show are the same
    
    * refactor portal ID
    
    * CSS magic when the header title is not there
    
    * reset content css
    
    * fix css
    
    * add footer slot
    
    * add footer example and docs
    
    * fix the footer to the bottom
    
    * use long arrow back
    
    * alpha it up

---
+++ packages/components/src/SideDrawer/SideDrawer.tsx
@@ -0,0 +1,172 @@
+import React, { useId, useState } from "react";
+import type { KeyboardEvent, PropsWithChildren } from "react";
+import { createPortal } from "react-dom";
+import { AnimatePresence, Variants, motion } from "framer-motion";
+import { tokens } from "@jobber/design";
+import { useRefocusOnActivator } from "@jobber/hooks/useRefocusOnActivator";
+import { useFocusTrap } from "@jobber/hooks/useFocusTrap";
+import classNames from "classnames";
+import { useInView } from "@jobber/hooks/useInView";
+import { SideDrawerActions } from "./SideDrawerActions";
+import { SideDrawerContext } from "./SideDrawerContext";
+import { SideDrawerTitle } from "./SideDrawerTitle";
+import { SideDrawerToolbar } from "./SideDrawerToolbar";
+import styles from "./SideDrawer.css";
+import { SideDrawerBackButton } from "./SideDrawerBackButton";
+import { SideDrawerFooter } from "./SideDrawerFooter";
+import { Button } from "../Button";
+import { Flex } from "../Flex";
+
+interface SideDrawerProps extends PropsWithChildren {
+  /**
+   * Whether or not the drawer is open.
+   */
+  readonly open: boolean;
+
+  /**
+   * Callback function to close the drawer.
+   */
+  readonly onRequestClose: () => void;
+
+  /**
+   * Change the appearance of the drawer.
+   */
+  readonly variation?: "base" | "subtle";
+}
+
+const variants: Variants = {
+  hidden: { x: "100%" },
+  visible: { x: 0, transitionEnd: { x: 0 } },
+};
+
+// eslint-disable-next-line max-statements
+export function SideDrawer({
+  children,
+  onRequestClose,
+  open,
+  variation = "base",
+}: SideDrawerProps) {
+  const [ref, setRef] = useState<HTMLDivElement | null>(null);
+  const toolbar = useSlotID();
+  const title = useSlotID("title");
+  const actions = useSlotID();
+  const backButton = useSlotID("back");
+  const footer = useSlotID();
+
+  useRefocusOnActivator(open);
+  const sideDrawerRef = useFocusTrap<HTMLDivElement>(open);
+  const [headerShadowRef, noHeaderShadow] = useInView<HTMLDivElement>();
+  const [footerShadowRef, noFooterShadow] = useInView<HTMLDivElement>();
+
+  const container = globalThis.document?.body || null;
+
+  if (!container) return null;
+
+  return createPortal(
+    <SideDrawerContext.Provider
+      value={{
+        actionPortal: ref?.querySelector(actions.selector),
+        titlePortal: ref?.querySelector(title.selector),
+        toolbarPortal: ref?.querySelector(toolbar.selector),
+        backPortal: ref?.querySelector(backButton.selector),
+        footerPortal: ref?.querySelector(footer.selector),
+      }}
+    >
+      {open && (
+        <button
+          className={styles.overlay}
+          aria-label="Close"
+          onClick={onRequestClose}
+          type="button"
+        />
+      )}
+      <AnimatePresence initial={false}>
+        {open && (
+          <motion.div
+            className={styles.drawer}
+            ref={setRef}
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            transition={{
+              duration: tokens["timing-base"] / 1000,
+            }}
+          >
+            <div
+              ref={sideDrawerRef}
+              role="dialog"
+              className={classNames(styles.container, styles.hasShadow, {
+                [styles.subtle]: variation === "subtle",
+              })}
+              tabIndex={0}
+              onKeyUp={handleKeyUp}
+            >
+              <div ref={headerShadowRef} />
+              <div
+                className={classNames(styles.header, {
+                  [styles.hasShadow]:
+                    headerShadowRef.current && !noHeaderShadow,
+                })}
+              >
+                <Flex template={["grow", "shrink"]}>
+                  <Flex template={["shrink", "grow"]} gap="none">
+                    <div {...backButton.attr} />
+                    <div {...title.attr} />
+                  </Flex>
+
+                  <div className={styles.headerActions}>
+                    <div className={styles.hideWhenEmpty} {...actions.attr} />
+                    <Button
+                      ariaLabel="Close"
+                      icon="cross"
+                      onClick={onRequestClose}
+                      type={"tertiary"}
+                      variation="subtle"
+                    />
+                  </div>
+                </Flex>
+
+                <div className={styles.hideWhenEmpty} {...toolbar.attr} />
+              </div>
+
+              <div className={styles.content}>{children}</div>
+
+              <div
+                className={classNames(styles.footer, styles.hideWhenEmpty, {
+                  [styles.hasShadow]:
+                    footerShadowRef.current && !noFooterShadow,
+                })}
+                {...footer.attr}
+              />
+              <div ref={footerShadowRef} />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </SideDrawerContext.Provider>,
+    container,
+  );
+
+  function handleKeyUp(event: KeyboardEvent<HTMLDivElement>): void {
+    if (event.key === "Escape") {
+      onRequestClose();
+    }
+  }
+}
+
+function useSlotID(prefix?: string) {
+  const id = useId();
+  const prefixedId = prefix ? `${prefix}-${id}` : id;
+  const attrKey = "data-side-drawer-slot";
+  const selector = `[${attrKey}="${prefixedId}"]`;
+  const attr = { [attrKey]: prefixedId };
+
+  return { id: prefixedId, selector, attr };
+}
+
+SideDrawer.Title = SideDrawerTitle;
+SideDrawer.Toolbar = SideDrawerToolbar;
+SideDrawer.Actions = SideDrawerActions;
+SideDrawer.BackButton = SideDrawerBackButton;
+SideDrawer.Footer = SideDrawerFooter;
--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -63,6 +63,7 @@ export * from "./ProgressBar";
 export * from "./RadioGroup";
 export * from "./RecurringSelect";
 export * from "./Select";
+export * from "./SideDrawer";
 export * from "./Spinner";
 export * from "./StatusIndicator";
 export * from "./StatusLabel";
