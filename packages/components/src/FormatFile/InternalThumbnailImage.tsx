de28f43a50ad8ba58d802b8b7c7d79ad52511b2c
Tyler Santos <tyler.s@getjobber.com>
2024-06-10 15:53:09 -0600

    update the InternalThumbnailImage component to retry on error 404s

--- packages/components/src/FormatFile/InternalThumbnailImage.tsx
+++ packages/components/src/FormatFile/InternalThumbnailImage.tsx
@@ -4,6 +4,7 @@ import styles from "./InternalThumbnailImage.css";
 import { FileUpload } from "../InputFile";
 import { Glimmer } from "../Glimmer";
 
+const RETRY_COUNT = 4;
 interface InternalThumbnailImageProps {
   readonly file: FileUpload;
 }
@@ -12,6 +13,7 @@ export function InternalThumbnailImage({ file }: InternalThumbnailImageProps) {
   const { name, src } = file;
   const [imageSource, setImageSource] = useState<string>();
   const [imageLoaded, setImageLoaded] = useState<boolean>(false);
+  const [retryCount, setRetryCount] = useState<number>(0);
 
   useEffect(() => {
     src().then(url => setImageSource(url));
@@ -22,8 +24,10 @@ export function InternalThumbnailImage({ file }: InternalThumbnailImageProps) {
       {!imageLoaded && <Glimmer size="auto" />}
 
       <img
+        key={retryCount}
         src={imageSource}
         onLoad={handleImageLoad}
+        onError={handleImageError}
         alt={name}
         data-testid="internalThumbnailImage"
         className={classNames(styles.image, { [styles.hidden]: !imageLoaded })}
@@ -33,5 +37,11 @@ export function InternalThumbnailImage({ file }: InternalThumbnailImageProps) {
 
   function handleImageLoad() {
     setImageLoaded(true);
+  }
+
+  function handleImageError() {
+    if (retryCount < RETRY_COUNT) {
+      setRetryCount(retryCount + 1);
+    }
   }
 }
