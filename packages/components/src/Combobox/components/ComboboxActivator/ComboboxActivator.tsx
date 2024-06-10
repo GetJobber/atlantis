b3114e2c10f34333d9c503521de5cfb7935cd98a
Michael Paradis <michael.p@getjobber.com>
2024-06-10 07:03:28 -0700

    Add CustomActivator to Combobox Activator

--- packages/components/src/Combobox/components/ComboboxActivator/ComboboxActivator.tsx
+++ packages/components/src/Combobox/components/ComboboxActivator/ComboboxActivator.tsx
@@ -3,11 +3,20 @@ import { Button } from "@jobber/components/Button";
 import { Chip } from "@jobber/components/Chip";
 import { ComboboxContext } from "../../ComboboxProvider";
 import { ComboboxActivatorProps } from "../../Combobox.types";
+import { useComboboxActivatorAccessibility } from "../../hooks/useComboboxActivatorAccessibility";
 
 export function ComboboxActivator(props: ComboboxActivatorProps) {
   const { handleClose, open, setOpen } = React.useContext(ComboboxContext);
+  const {
+    ariaControls,
+    ariaExpanded,
+    ariaLabel = "",
+  } = useComboboxActivatorAccessibility();
 
-  if (props.children?.type === Button || props.children?.type === Chip) {
+  if (
+    typeof props.children !== "function" &&
+    (props.children?.type === Button || props.children?.type === Chip)
+  ) {
     return React.cloneElement(props.children, {
       role: "combobox",
       onClick: () => {
@@ -18,19 +27,21 @@ export function ComboboxActivator(props: ComboboxActivatorProps) {
         }
       },
     });
-  } else if (props.customActivator) {
+    // Custom Activator component
+  } else if (typeof props.children === "function") {
     return (
-      <div role="combobox" aria-expanded={open}>
-        {props.customActivator({ setOpen, open, handleClose })}
+      <div role="combobox">
+        {props.children({
+          setOpen,
+          open,
+          handleClose,
+          ariaControls,
+          ariaExpanded,
+          ariaLabel,
+        })}
       </div>
     );
   }
 
-  return null;
-}
-
-export interface ComboboxCustomActivatorProps {
-  setOpen: (open: boolean) => void;
-  open: boolean;
-  handleClose: () => void;
+  return props.children || null;
 }
