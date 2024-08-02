59eda621560f223640ab64c93b021b2b98fe650b
Taylor Noj <104797704+taylorvnoj@users.noreply.github.com>
2024-08-02 11:35:02 -0400

    feat(components): Add onClick to Chip Suffix (#1963)
    * working-ish
    
    * dismissible suffix is working and looks great
    
    * linting things
    
    * linting things
    
    * refactor css and add more accessibility to suffix
    
    * add tests for Chip suffix
    
    * update Chip stories
    
    * add test for testID presence
    
    * refactor return logic
    
    * add outline: none to clickableSuffix
    
    * update clickableSuffix hover color
    
    * update outline value
    
    Co-authored-by: Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>
    
    * refactor Chip.Suffix to only render span to avoid button children, add tests
    
    * update last test to queryByTestId
    
    * update onClick test to use userEvent
    
    Co-authored-by: Nazarii L <nazarii.l@getjobber.com>
    
    * replace not.toBeNull with .toBeInTheDocument
    
    Co-authored-by: Nazarii L <nazarii.l@getjobber.com>
    
    * remove if statement from keypress test and switch to userEvent.tab
    
    * return props within the span instead of spreading them
    
    ---------
    
    Co-authored-by: Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>
    Co-authored-by: Nazarii L <nazarii.l@getjobber.com>

--- packages/components/src/Chip/components/ChipSuffix/Chip.Suffix.tsx
+++ packages/components/src/Chip/components/ChipSuffix/Chip.Suffix.tsx
@@ -4,8 +4,27 @@ import { Icon } from "@jobber/components/Icon";
 import { useChildComponent } from "../../hooks";
 import styles from "../../Chip.css";
 
-export function ChipSuffix({ children, className }: ChipSuffixProps) {
+export function ChipSuffix({
+  children,
+  className,
+  onClick,
+  testID,
+  ariaLabel,
+}: ChipSuffixProps) {
   let singleChild = useChildComponent(children, d => d.type === Icon);
+
+  const handleClick = (
+    event:
+      | React.MouseEvent<HTMLSpanElement, MouseEvent>
+      | React.KeyboardEvent<HTMLSpanElement>,
+  ) => {
+    if (!onClick) {
+      return;
+    }
+    event.preventDefault();
+    event.stopPropagation();
+    onClick(event);
+  };
 
   if (!allowedSuffixIcons.includes(singleChild?.props?.name)) {
     singleChild = undefined;
@@ -14,10 +33,16 @@ export function ChipSuffix({ children, className }: ChipSuffixProps) {
   return (
     <span
       className={classNames(
-        styles.suffix,
+        onClick ? styles.clickableSuffix : styles.suffix,
         className,
         !singleChild && styles.empty,
       )}
+      onClick={handleClick}
+      onKeyPress={handleClick}
+      role={onClick ? "button" : undefined}
+      tabIndex={onClick ? 0 : undefined}
+      data-testid={testID}
+      aria-label={ariaLabel}
     >
       {singleChild}
     </span>
@@ -26,6 +51,13 @@ export function ChipSuffix({ children, className }: ChipSuffixProps) {
 
 export interface ChipSuffixProps extends PropsWithChildren {
   readonly className?: string;
+  readonly testID?: string;
+  readonly ariaLabel?: string;
+  readonly onClick?: (
+    event:
+      | React.MouseEvent<HTMLSpanElement, MouseEvent>
+      | React.KeyboardEvent<HTMLSpanElement>,
+  ) => void;
 }
 
 export const allowedSuffixIcons = ["cross", "add", "checkmark", "arrowDown"];
