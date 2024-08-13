3cd85636e3eb827ef5497e8c55219e9cf8eb6376
Jacob Deichert <jacob.d@getjobber.com>
2024-08-13 11:26:05 -0700

    Restore another test to original on master

--- packages/components-native/src/Menu/Menu.test.tsx
+++ packages/components-native/src/Menu/Menu.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { fireEvent, render } from "@testing-library/react-native";
 import { Host } from "react-native-portalize";
 import { View } from "react-native";
+import { tokens } from "@jobber/design/foundation";
 import { Menu, MenuOptionProps, MenuProps } from ".";
 import { Icon } from "../Icon";
 import { Button } from "../Button";
@@ -129,17 +130,13 @@ describe("Menu", () => {
       });
 
       fireEvent.press(getByLabelText(menuLabel));
-      expect(getByTestId("add").props.style).toStrictEqual([
-        { backgroundColor: "transparent", borderWidth: 0 },
-        {
-          display: "flex",
-          fill: "hsl(6, 64%, 51%)",
-          height: 24,
-          verticalAlign: "middle",
-          width: 24,
-        },
-        { flex: 0, height: 24, width: 24 },
-      ]);
+      expect(getByTestId("add").props.style).toContainEqual({
+        display: "flex",
+        fill: tokens["color-critical"],
+        height: 24,
+        verticalAlign: "middle",
+        width: 24,
+      });
     });
 
     it("closes the menu after clicking on a menu option", () => {
