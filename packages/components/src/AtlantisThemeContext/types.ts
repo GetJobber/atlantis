18c7c7f8715fcc915a605a57b7bc9073a2abad74
Michael Paradis <michael.p@getjobber.com>
2024-08-22 08:43:04 -0700

    Update comment for dangerouslyOverrideTheme

--- packages/components/src/AtlantisThemeContext/types.ts
+++ packages/components/src/AtlantisThemeContext/types.ts
@@ -20,7 +20,8 @@ export interface AtlantisThemeContextProviderProps extends PropsWithChildren {
   readonly children: React.ReactNode;
 
   /**
-   * Force the theme for this provider to always be the same as the provided defaultTheme.
+   * Force the theme for this provider to always be the same as the provided theme. Useful for sections that should remain the same theme regardless of the rest of the application's theme.
+   * This is dangerous because the children in this provider will not be able to change the theme.
    */
   readonly dangerouslyOverrideTheme?: Theme;
 }
