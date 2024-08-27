3b186879cc9463945d9cc7ca1c2842d7ff1b4617
Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>
2024-08-27 17:32:57 -0600

    update swatches and fix broken base colors

--- .storybook/components/ColorSwatches/ColorSwatches.module.css.d.ts
+++ .storybook/components/ColorSwatches/ColorSwatches.module.css.d.ts
@@ -1,8 +1,5 @@
 declare const styles: {
-  readonly "colorbar": string;
-  readonly "color": string;
   readonly "swatch": string;
-  readonly "button": string;
   readonly "pre": string;
 };
 export = styles;
