31fa06e32b3c3d47b19aa4f2cbc61a0ec0ae6bcb
Michael Paradis <michael.p@getjobber.com>
2024-08-30 08:45:01 -0700

    Fix usage of hooks in callback rules of hooks error in InputDate

--- packages/components/src/InputDate/InputDate.tsx
+++ packages/components/src/InputDate/InputDate.tsx
@@ -8,6 +8,7 @@ import {
   Suffix,
 } from "../FormField";
 import { DatePicker } from "../DatePicker";
+import { DatePickerActivatorProps } from "../DatePicker/DatePickerActivator";
 
 interface InputDateProps
   extends Omit<CommonFormFieldProps, "clearable">,
@@ -52,8 +53,6 @@ interface InputDateProps
 }
 
 export function InputDate(inputProps: InputDateProps) {
-  const formFieldActionsRef = useRef<FieldActionsRef>(null);
-
   return (
     <DatePicker
       selected={inputProps.value}
@@ -64,63 +63,73 @@ export function InputDate(inputProps: InputDateProps) {
       minDate={inputProps.minDate}
       maxDate={inputProps.maxDate}
       smartAutofocus={false}
-      activator={activatorProps => {
-        const { onChange, onClick, value } = activatorProps;
-        const newActivatorProps = omit(activatorProps, ["activator"]);
-        const [isFocused, setIsFocused] = useState(false);
-        const suffix =
-          inputProps.showIcon !== false
-            ? ({
-                icon: "calendar",
-                ariaLabel: "Show calendar",
-                onClick: onClick && onClick,
-              } as Suffix)
-            : undefined;
-
-        // Set form field to formatted date string immediately, to avoid validations
-        //  triggering incorrectly when it blurs (to handle the datepicker UI click)
-        useEffect(() => {
-          value && formFieldActionsRef.current?.setValue(value);
-        }, [value]);
-        const showEmptyValueLabel = !value && !isFocused;
-
-        return (
-          // We prevent the picker from opening on focus for keyboard navigation, so to maintain a good UX for mouse users we want to open the picker on click
-          <div onClick={onClick}>
-            <FormField
-              {...newActivatorProps}
-              {...inputProps}
-              value={
-                showEmptyValueLabel ? inputProps.emptyValueLabel || "" : value
-              }
-              placeholder={inputProps.placeholder}
-              onChange={(_, event) => {
-                onChange && onChange(event);
-              }}
-              onBlur={() => {
-                inputProps.onBlur && inputProps.onBlur();
-                activatorProps.onBlur && activatorProps.onBlur();
-                setIsFocused(false);
-              }}
-              onFocus={() => {
-                inputProps.onFocus && inputProps.onFocus();
-                activatorProps.onFocus && activatorProps.onFocus();
-                setIsFocused(true);
-              }}
-              onKeyUp={event => {
-                if (
-                  inputProps.showIcon === false &&
-                  event.key === "ArrowDown"
-                ) {
-                  activatorProps.onClick?.();
-                }
-              }}
-              actionsRef={formFieldActionsRef}
-              suffix={suffix}
-            />
-          </div>
-        );
-      }}
+      activator={activatorProps => (
+        <DatePickerActivator
+          activatorProps={activatorProps}
+          inputProps={inputProps}
+        />
+      )}
     />
+  );
+}
+
+function DatePickerActivator({
+  activatorProps,
+  inputProps,
+}: {
+  readonly activatorProps: DatePickerActivatorProps;
+  readonly inputProps: InputDateProps;
+}) {
+  const formFieldActionsRef = useRef<FieldActionsRef>(null);
+
+  const { onChange, onClick, value } = activatorProps;
+  const newActivatorProps = omit(activatorProps, ["activator"]);
+  const [isFocused, setIsFocused] = useState(false);
+  const suffix =
+    inputProps.showIcon !== false
+      ? ({
+          icon: "calendar",
+          ariaLabel: "Show calendar",
+          onClick: onClick && onClick,
+        } as Suffix)
+      : undefined;
+
+  // Set form field to formatted date string immediately, to avoid validations
+  //  triggering incorrectly when it blurs (to handle the datepicker UI click)
+  useEffect(() => {
+    value && formFieldActionsRef.current?.setValue(value);
+  }, [value]);
+  const showEmptyValueLabel = !value && !isFocused;
+
+  return (
+    // We prevent the picker from opening on focus for keyboard navigation, so to maintain a good UX for mouse users we want to open the picker on click
+    <div onClick={onClick}>
+      <FormField
+        {...newActivatorProps}
+        {...inputProps}
+        value={showEmptyValueLabel ? inputProps.emptyValueLabel || "" : value}
+        placeholder={inputProps.placeholder}
+        onChange={(_, event) => {
+          onChange && onChange(event);
+        }}
+        onBlur={() => {
+          inputProps.onBlur && inputProps.onBlur();
+          activatorProps.onBlur && activatorProps.onBlur();
+          setIsFocused(false);
+        }}
+        onFocus={() => {
+          inputProps.onFocus && inputProps.onFocus();
+          activatorProps.onFocus && activatorProps.onFocus();
+          setIsFocused(true);
+        }}
+        onKeyUp={event => {
+          if (inputProps.showIcon === false && event.key === "ArrowDown") {
+            activatorProps.onClick?.();
+          }
+        }}
+        actionsRef={formFieldActionsRef}
+        suffix={suffix}
+      />
+    </div>
   );
 }
