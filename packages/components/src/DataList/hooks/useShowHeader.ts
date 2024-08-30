1b50bf8a49adaa9fe80dfcb162870ab5f1489245
Michael Paradis <michael.p@getjobber.com>
2024-08-30 11:43:16 -0700

    Fix useResponsiveSizing being called conditional in DataList in useShowHeader

--- packages/components/src/DataList/hooks/useShowHeader.ts
+++ packages/components/src/DataList/hooks/useShowHeader.ts
@@ -3,9 +3,10 @@ import { useDataListContext } from "../context/DataListContext";
 
 export function useShowHeader(): boolean {
   const { headerVisibility } = useDataListContext();
+  const sizes = useResponsiveSizing();
+
   if (headerVisibility === undefined) return true;
 
-  const sizes = useResponsiveSizing();
   const sizeKeys = Object.keys(sizes) as (keyof typeof sizes)[];
 
   const showHeader = sizeKeys.reduce((previous, breakpoint) => {
