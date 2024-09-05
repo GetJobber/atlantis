699a0a99a6aaacff0c7f7dec547de607ce871d81
Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>
2024-09-05 10:25:42 -0600

    declare explicit stopOpacitys

--- packages/components-native/src/Glimmer/Glimmer.tsx
+++ packages/components-native/src/Glimmer/Glimmer.tsx
@@ -83,9 +83,21 @@ export function Glimmer({
         <Svg>
           <Defs>
             <LinearGradient id="gradientShine" x1={0} y1={0.5} x2={1} y2={0.5}>
-              <Stop offset="0%" stopColor="rgba(255,255,255,0)" />
-              <Stop offset="50%" stopColor={tokens["color-surface"]} />
-              <Stop offset="100%" stopColor="rgba(255,255,255,0)" />
+              <Stop
+                offset="0%"
+                stopColor="rgba(255,255,255,0)"
+                stopOpacity="0"
+              />
+              <Stop
+                offset="50%"
+                stopColor="rgba(255,255,255,1)"
+                stopOpacity="1"
+              />
+              <Stop
+                offset="100%"
+                stopColor="rgba(255,255,255,0)"
+                stopOpacity="0"
+              />
             </LinearGradient>
           </Defs>
           <Rect fill="url(#gradientShine)" height="100%" width="100%" />
