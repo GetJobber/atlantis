2bf1814dd60853be481ce253f91446cd1e075b9f
Jeff Marvin <jeff.m@getjobber.com>
2024-09-08 22:15:36 -0600

    annotate civiltime conversions
    includes notes on naming influenced by x-mui adapter layer

--- packages/components/src/utils/civilTimeConversions.tsx
+++ packages/components/src/utils/civilTimeConversions.tsx
@@ -1,5 +1,15 @@
+// If this is the operations, the three adapters are maybe String, Date and CivilTime
+//  i.e. three "libraries" that people might use... string/date might collapse into one
+//    so could just have a default for Date/String and then a manual adapter for CivilTime
+//    -- so boil the civiltime operations down to the same general API/operations that we
+//       would use for string / Date
+
+// there will be a hook loading the set of utils that match the adapter
 import { CivilDate, CivilDateTime, CivilTime } from "@std-proposal/temporal";
 
+// "format" is a good name for this
+// "formatCivilTime"
+// timeToStr
 export function civilTimeToTimeString(civilTime?: CivilTime): string {
   if (civilTime == undefined) {
     return "";
@@ -11,6 +21,17 @@ export function civilTimeToTimeString(civilTime?: CivilTime): string {
   return timeString.slice(0, 5);
 }
 
+// "parse" is a good name for this
+// "parseToCivilTime"
+// strToTime
+/**
+ * Parse a string date in a specific format.
+ * @template TDate
+ * @param {string} value The string date to parse.
+ * @param {string} format The format in which the string date is.
+ * @returns {TDate | null} The parsed date.
+ */
+// parse(value: string, format: string): TDate | null;
 export function timeStringToCivilTime(
   timeString: string,
 ): CivilTime | undefined {
@@ -21,6 +42,9 @@ export function timeStringToCivilTime(
   }
 }
 
+// toJSDate
+//   toJSDateFromCivilTime
+// ** look at Material adapter API to see how they are naming things
 export function getTodayDateAtCivilTime(inputTime: CivilTime) {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
@@ -38,10 +62,31 @@ export function getTodayDateAtCivilTime(inputTime: CivilTime) {
   );
 }
 
+// toJSDate
+//  toJSDateFromCivilDate
+//  (this usage is a bit unique because this function is not using the time from the civildate
 export function getDateFromCivilDate(inputDate: CivilDate) {
   return new Date(inputDate.year, inputDate.month - 1, inputDate.day);
 }
 
+// toJSDate?
+// toJSDateFromCivilDateTime
+// between this and the one above, CivilDateTime is the only one that uses the time
 export function getDateTimeFromCivilDateTime(inputDateTime: CivilDateTime) {
   return new Date(inputDateTime.toJSON());
 }
+
+/**
+ * Get the hours of the given date.
+ * @template TDate
+ * @param {TDate} value The given date.
+ * @returns {number} The hours of the given date.
+ */
+// getHours(value: TDate): number;
+/**
+ * Get the minutes of the given date.
+ * @template TDate
+ * @param {TDate} value The given date.
+ * @returns {number} The minutes of the given date.
+ */
+// getMinutes(value: TDate): number;
