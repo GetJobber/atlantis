aea28aaa0e3a2cbfe3321f8df7fb7d09a9e83aa6
Guilherme Vieira de Moraes <guilherme.dm@getjobber.com>
2024-09-10 16:47:17 -0300

    feat(components): Update Chip.Prefix to accept more than Icon and Avatar as children

--- packages/components/src/Chip/components/ChipPrefix/Chip.Prefix.tsx
+++ packages/components/src/Chip/components/ChipPrefix/Chip.Prefix.tsx
@@ -6,14 +6,12 @@ import { useChildComponent } from "../../hooks/index";
 import styles from "../../Chip.css";
 
 export function ChipPrefix({ children }: PropsWithChildren) {
-  const singleChild = useChildComponent(
+  const avatarOrIcon = useChildComponent(
     children,
     d => d.type === Avatar || d.type === Icon,
   );
 
-  return (
-    <span className={classNames(styles.prefix, !singleChild && styles.empty)}>
-      {singleChild}
-    </span>
-  );
+  if (!avatarOrIcon) return children as React.ReactElement;
+
+  return <span className={classNames(styles.prefix)}>{avatarOrIcon}</span>;
 }
