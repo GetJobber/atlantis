e9e21c075260c9ecec022668eb832768c178f978
Taylor Noj <taylor.n@getjobber.com>
2024-09-17 11:38:26 -0400

    create a new fileSize story

--- docs/components/InputFile/Web.stories.tsx
+++ docs/components/InputFile/Web.stories.tsx
@@ -51,9 +51,9 @@ const StatefulTemplate: ComponentStory<typeof InputFile> = args => {
   }
 };
 
-const MaxFilesTemplate: ComponentStory<typeof InputFile> = args => {
+const FileSizeValidatorTemplate: ComponentStory<typeof InputFile> = args => {
   const [files, setFiles] = useState<FileUpload[]>([]);
-  const maxFileSize = 5 * 1024 * 1024; // 5 MB
+  const maxFileSize = 2 * 1024 * 1024;
   const fileSizeValidator = useCallback(
     (file: File) => {
       if (file.size > maxFileSize) {
@@ -72,6 +72,32 @@ const MaxFilesTemplate: ComponentStory<typeof InputFile> = args => {
 
   return (
     <>
+      <Heading level={4}>Attempt to upload an image larger than 2MB</Heading>
+      <InputFile
+        {...args}
+        onUploadStart={handleUpload}
+        onUploadProgress={handleUpload}
+        onUploadComplete={handleUpload}
+        validator={fileSizeValidator}
+        allowedTypes="basicImages"
+        description="JPEG, JPG or PNG up to 2MB each"
+      />
+      {files.map(file => (
+        <FormatFile file={file} key={file.key} />
+      ))}
+    </>
+  );
+
+  function handleUpload(file: FileUpload) {
+    setFiles(oldFiles => updateFiles(file, oldFiles));
+  }
+};
+
+const MaxFilesTemplate: ComponentStory<typeof InputFile> = args => {
+  const [files, setFiles] = useState<FileUpload[]>([]);
+
+  return (
+    <>
       <Heading level={4}>Upload more than 3 files at one time</Heading>
       <InputFile
         {...args}
@@ -79,7 +105,6 @@ const MaxFilesTemplate: ComponentStory<typeof InputFile> = args => {
         onUploadProgress={handleUpload}
         onUploadComplete={handleUpload}
         maxFilesValidation={{ maxFiles: 3, numberOfCurrentFiles: files.length }}
-        validator={fileSizeValidator}
       />
       {files.map(file => (
         <FormatFile
@@ -174,4 +199,10 @@ WithDescription.args = {
   allowMultiple: true,
   getUploadParams: () => Promise.resolve({ url: "https://httpbin.org/post" }),
   description: "JPEG, HEIC, PNG up to 5MB each",
+};
+
+export const FileSizeValidator = FileSizeValidatorTemplate.bind({});
+FileSizeValidator.args = {
+  allowMultiple: true,
+  getUploadParams: () => Promise.resolve({ url: "https://httpbin.org/post" }),
 };
