9a0a6bee0f78fed0e734e30201f54a87b18d5b1e
Scott Thompson <145565743+scotttjob@users.noreply.github.com>
2024-10-15 09:17:35 -0300

    missed two css d ts files

--- packages/components/src/DataList/components/DataListLayoutActions/DataListLayoutActions.css.d.ts
+++
@@ -1,5 +0,0 @@
-declare const styles: {
-  readonly "hidden": string;
-};
-export = styles;
-
--- packages/components/src/DataList/components/DataListLoadMore/DataListLoadMore.css.d.ts
+++
@@ -1,8 +0,0 @@
-declare const styles: {
-  readonly "trigger": string;
-  readonly "loadingMore": string;
-  readonly "backToTop": string;
-  readonly "fadeIn": string;
-};
-export = styles;
-
