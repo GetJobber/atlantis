69db152edcc09a3d572432125ce7dc6f091e10a5
Aiden-Brine <aidenbrine@gmail.com>
2024-10-18 14:08:36 -0700

    Add render prop and stories

--- packages/components/src/List/List.tsx
+++ packages/components/src/List/List.tsx
@@ -2,20 +2,40 @@ import React from "react";
 import classnames from "classnames";
 import get from "lodash/get";
 import groupBy from "lodash/groupBy";
-import styles from "./List.module.css";
+import { Typography } from "@jobber/components/Typography";
+import {
+  BaseListItemProps,
+  ListItem,
+  ListItemProps,
+} from "@jobber/components/List/ListItem";
 import sectionStyles from "./SectionHeader.module.css";
-import { ListItem, ListItemProps } from "./ListItem";
-import { Typography } from "../Typography";
+import styles from "./List.module.css";
 
-interface ListProps {
+interface ListProps<T extends BaseListItemProps = ListItemProps> {
   /**
    * Array of the list items.
    */
-  readonly items: ListItemProps[];
+  readonly items: T[];
+  /**
+   * A custom render function
+   */
+  readonly customRenderItem?: (item: T) => React.ReactNode;
 }
 
-export function List({ items }: ListProps) {
-  const isSectioned = items.some(item => item.section);
+export function List<T extends BaseListItemProps = ListItemProps>({
+  items,
+  customRenderItem,
+}: ListProps<T>) {
+  if (customRenderItem) {
+    return (
+      <ul className={styles.list}>
+        {items.map(item => (
+          <li key={item.id}>{customRenderItem(item)}</li>
+        ))}
+      </ul>
+    );
+  }
+  const isSectioned = items.some(item => "section" in item && item.section);
 
   if (isSectioned) {
     return <SectionedList items={items} />;
