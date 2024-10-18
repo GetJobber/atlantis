e93cc8757e4a0a1c01c0deffc4190ce0b9ecc8f7
Aiden-Brine <aidenbrine@gmail.com>
2024-10-18 14:08:36 -0700

    Add render prop and stories

--- docs/components/List/Web.stories.tsx
+++ docs/components/List/Web.stories.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { List, ListItem } from "@jobber/components/List";
+import groupBy from "lodash/groupBy";
+import {
+  BaseListItemProps,
+  List,
+  ListItem,
+  ListItemProps,
+} from "@jobber/components/List";
 import { Card } from "@jobber/components/Card";
+import { Text } from "@jobber/components/Text";
+import { Flex } from "@jobber/components/Flex";
+import { Avatar } from "@jobber/components/Avatar";
+import { Heading } from "@jobber/components/Heading";
+import { Icon } from "@jobber/components/Icon";
+import { FileUpload } from "@jobber/components/InputFile";
+import { FormatFile } from "@jobber/components/FormatFile";
 
 export default {
   title: "Components/Lists and Tables/List/Web",
@@ -20,63 +33,271 @@ const BasicTemplate: ComponentStory<typeof List> = args => (
 );
 
 export const Basic = BasicTemplate.bind({});
-Basic.args = {
-  items: [
-    {
-      id: 1,
-      icon: "wallet",
-      iconColor: "orange",
-      content: "Payment for Invoice #39",
-      value: "-$300.00",
-      caption: "Sep 25, 2019",
-      onClick: () => alert("TODO: Implement onClick"),
-    },
-    {
-      id: 3,
-      icon: "paidInvoice",
-      content: "Invoice #39",
-      value: "$300.00",
-      caption: "Sep 24, 2019",
-      onClick: () => alert("TODO: Implement onClick"),
-    },
-  ],
-};
+const basicItems: ListItemProps[] = [
+  {
+    id: 1,
+    icon: "wallet",
+    iconColor: "orange",
+    content: "Payment for Invoice #39",
+    value: "-$300.00",
+    caption: "Sep 25, 2019",
+    onClick: () => alert("TODO: Implement onClick"),
+  },
+  {
+    id: 3,
+    icon: "paidInvoice",
+    content: "Invoice #39",
+    value: "$300.00",
+    caption: "Sep 24, 2019",
+    onClick: () => alert("TODO: Implement onClick"),
+  },
+];
+Basic.args = { items: basicItems };
 
 export const SectionedListItems = BasicTemplate.bind({});
-SectionedListItems.args = {
-  items: [
-    {
-      id: 1,
-      icon: "addNote",
-      title: "Darryl Tec added a note",
-      content: [
-        "_'Called the client. Asked if they want the luxury package, they said yes!'_",
-        "Deck Build",
-      ],
-      caption: "1 minute ago",
-      section: "Today",
-      isActive: true,
-      onClick: () => alert("TODO: Implement onClick"),
-    },
-    {
-      id: 2,
-      icon: "checkmark",
-      iconColor: "green",
-      title: "Josh Elford completed a visit",
-      content: "Annual Maintenance",
-      caption: "2 hours ago",
-      section: "Today",
-      onClick: () => alert("TODO: Implement onClick"),
-    },
-    {
-      id: 3,
-      icon: "badInvoice",
-      title: "Payment failed",
-      content: "For services rendered",
-      value: "$300.00",
-      caption: "1 day ago",
-      onClick: () => alert("TODO: Implement onClick"),
-      section: "Yesterday",
-    },
-  ],
+const sectionedListItems: ListItemProps[] = [
+  {
+    id: 1,
+    icon: "addNote",
+    title: "Darryl Tec added a note",
+    content: [
+      "_'Called the client. Asked if they want the luxury package, they said yes!'_",
+      "Deck Build",
+    ],
+    caption: "1 minute ago",
+    section: "Today",
+    isActive: true,
+    onClick: () => alert("TODO: Implement onClick"),
+  },
+  {
+    id: 2,
+    icon: "checkmark",
+    iconColor: "green",
+    title: "Josh Elford completed a visit",
+    content: "Annual Maintenance",
+    caption: "2 hours ago",
+    section: "Today",
+    onClick: () => alert("TODO: Implement onClick"),
+  },
+  {
+    id: 3,
+    icon: "badInvoice",
+    title: "Payment failed",
+    content: "For services rendered",
+    value: "$300.00",
+    caption: "1 day ago",
+    onClick: () => alert("TODO: Implement onClick"),
+    section: "Yesterday",
+  },
+];
+SectionedListItems.args = { items: sectionedListItems };
+
+interface SPListItemProps extends BaseListItemProps {
+  readonly name: string;
+  readonly avatarUrl: string;
+}
+
+const serviceProviderListItems: SPListItemProps[] = [
+  {
+    id: 1,
+    name: "Rob Lane",
+    avatarUrl: "https://randomuser.me/api/portraits/men/1.jpg",
+  },
+  {
+    id: 2,
+    name: "Marlene Hamilton",
+    avatarUrl: "https://randomuser.me/api/portraits/women/2.jpg",
+  },
+  {
+    id: 3,
+    name: "Miriam Graves",
+    avatarUrl: "https://randomuser.me/api/portraits/women/3.jpg",
+  },
+  {
+    id: 4,
+    name: "Milton Payne",
+    avatarUrl: "https://randomuser.me/api/portraits/men/3.jpg",
+  },
+  {
+    id: 5,
+    name: "Madison Simmons",
+    avatarUrl: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+];
+
+function RenderServiceProviderList({
+  listItem,
+}: {
+  readonly listItem: SPListItemProps;
+}) {
+  return (
+    <div
+      style={{
+        padding: "16px",
+        // If this was outside of Storybook :not(:last-child) would be used
+        borderBottom: listItem.id !== 5 ? "1px solid 	#dadfe2" : "none",
+      }}
+    >
+      <Flex template={["shrink", "grow", "shrink"]} align="center">
+        <Avatar imageUrl={listItem.avatarUrl} />
+        <Heading level={4}>{listItem.name}</Heading>
+        <Icon name="arrowRight" size="base" color="interactive" />
+      </Flex>
+    </div>
+  );
+}
+
+export const ListWithCustomRenderer = BasicTemplate.bind({});
+ListWithCustomRenderer.args = {
+  items: serviceProviderListItems,
+  customRenderItem: (item: SPListItemProps) => (
+    <RenderServiceProviderList listItem={item} />
+  ),
+};
+
+interface ProductListItemProps extends BaseListItemProps {
+  readonly name: string;
+  readonly description: string;
+  readonly image: FileUpload;
+  readonly price: string;
+  readonly section: string;
+}
+
+const imageFile: FileUpload = {
+  key: "abc",
+  name: "image_of_something.png",
+  type: "image/png",
+  size: 213402324,
+  progress: 1,
+  src: () => Promise.resolve("https://picsum.photos/250"),
+};
+
+const productsList: ProductListItemProps[] = [
+  {
+    id: 1,
+    name: "Lawn Mower",
+    description: "A reliable lawn mower for home or commercial use.",
+    image: imageFile,
+    price: "$250.00",
+    section: "Products",
+  },
+  {
+    id: 2,
+    name: "Hedge Trimmer",
+    description: "Electric hedge trimmer for precise shaping.",
+    image: imageFile,
+    price: "$85.00",
+    section: "Products",
+  },
+  {
+    id: 3,
+    name: "Garden Fertilizer",
+    description: "Premium fertilizer for healthy lawn growth.",
+    image: imageFile,
+    price: "$30.00",
+    section: "Products",
+  },
+  {
+    id: 4,
+    name: "Lawn Mowing Service",
+    description: "Keep your lawn tidy with regular mowing.",
+    image: imageFile,
+    price: "$50.00",
+    section: "Services",
+  },
+  {
+    id: 5,
+    name: "Weed Control Service",
+    description: "Prevent and eliminate unwanted weeds.",
+    image: imageFile,
+    price: "$40.00",
+    section: "Services",
+  },
+  {
+    id: 6,
+    name: "Hedge Trimming Service",
+    description: "Shape your hedges to perfection.",
+    image: imageFile,
+    price: "$75.00",
+    section: "Services",
+  },
+];
+
+function RenderProductList({
+  listItem,
+}: {
+  readonly listItem: ProductListItemProps;
+}) {
+  return (
+    <div
+      style={{
+        padding: "16px",
+        // If this was outside of Storybook :not(:last-child) would be used
+        borderBottom: listItem.id !== 5 ? "1px solid 	#dadfe2" : "none",
+      }}
+    >
+      <Flex template={["shrink", "grow"]} align="center">
+        <FormatFile
+          file={listItem.image}
+          display="compact"
+          displaySize="base"
+        />
+        <div style={{ marginBottom: "8px" }}>
+          <Heading level={4}>{listItem.name}</Heading>
+          <Flex template={["grow", "shrink"]} align="start">
+            <Text>{listItem.description}</Text>
+            <Text>{listItem.price}</Text>
+          </Flex>
+        </div>
+      </Flex>
+    </div>
+  );
+}
+
+interface ProductSectionProps extends BaseListItemProps {
+  readonly products: ProductListItemProps[];
+}
+
+function groupProductsBySection(
+  items: ProductListItemProps[],
+): ProductSectionProps[] {
+  const grouped = groupBy(items, "section");
+
+  return Object.entries(grouped).map(([section, products]) => ({
+    id: section,
+    products,
+  }));
+}
+
+function RenderProductSection({
+  listItem,
+}: {
+  readonly listItem: ProductSectionProps;
+}) {
+  return (
+    <div>
+      <div
+        style={{
+          padding: "10px 0px 10px 16px",
+          borderBottom: "1px solid black",
+        }}
+      >
+        <Heading level={4}>{listItem.id}</Heading>
+      </div>
+      <List
+        items={listItem.products}
+        customRenderItem={(item: ProductListItemProps) => (
+          <RenderProductList listItem={item} />
+        )}
+      />
+    </div>
+  );
+}
+
+export const SectionedListWithCustomRenderer = BasicTemplate.bind({});
+SectionedListWithCustomRenderer.args = {
+  items: groupProductsBySection(productsList),
+  customRenderItem: (item: ProductSectionProps) => (
+    <RenderProductSection listItem={item} />
+  ),
 };
