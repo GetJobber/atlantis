bfac31d85bc847ce0fc080890cc9818bea9f5838
Taylor Noj <taylor.n@getjobber.com>
2024-10-23 14:01:04 -0400

    remove checked prop from SegmentedControlOption

--- packages/components/src/SegmentedControl/SegmentedControlOption.tsx
+++ packages/components/src/SegmentedControl/SegmentedControlOption.tsx
@@ -6,25 +6,21 @@ interface SegmentedControlOptionProps extends PropsWithChildren {
    * The value of the option.
    */
   readonly value: string | number;
-  /**
-   * The label to appear beside the radio button.
-   */
-  readonly checked?: boolean;
   readonly label?: string;
   readonly onChange?: (valueIn: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export function SegmentedControlOption({
   value,
-  checked,
   children,
   label,
   onChange,
 }: SegmentedControlOptionProps) {
   const { selectedOption, handleChange } = useSegmentedControl();
-  const localChecked = useMemo(() => {
-    return checked ?? selectedOption === value;
-  }, [selectedOption, checked]);
+  const localChecked = useMemo(
+    () => selectedOption === value,
+    [selectedOption, value],
+  );
   const inputId = `${value.toString()}_${useId()}`;
 
   return (
