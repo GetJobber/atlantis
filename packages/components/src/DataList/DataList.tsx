fb1133c6d66be956efb94acdcbb5e2d96c6608e1
Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>
2024-10-25 13:21:43 -0600

    re-position search to be primary element

--- packages/components/src/DataList/DataList.tsx
+++ packages/components/src/DataList/DataList.tsx
@@ -160,8 +160,8 @@ function InternalDataList({
       {shouldRenderStickyHeader && (
         <DataListStickyHeader>
           <div className={styles.headerFilters}>
-            <InternalDataListFilters />
             <InternalDataListSearch />
+            <InternalDataListFilters />
           </div>
 
           <InternalDataListStatusBar />
