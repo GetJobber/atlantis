e01ab3594544d692cb57c563f341792c3df6bffc
Rebecca Anderson <rebecca.a@getjobber.com>
2024-11-06 12:13:24 -0500

    msaving place

--- packages/components-native/src/Divider/Divider.style.ts
+++ packages/components-native/src/Divider/Divider.style.ts
@@ -12,8 +12,25 @@ export const styles = StyleSheet.create({
     borderBottomWidth: tokens["border-thick"],
     opacity: 0.875,
   },
+  larger: {
+    borderBottomWidth: tokens["border-thicker"],
+    opacity: 0.625,
+  },
   largest: {
     borderBottomWidth: tokens["space-small"],
     opacity: 0.375,
+  },
+  horizontal: {
+    height: tokens["space-minuscule"],
+    borderBottomWidth: tokens["divider-width"],
+    borderBottom: "solid",
+    borderBottomColor: tokens["divider-color"],
+  },
+  vertical: {
+    width: tokens["space-minuscule"],
+    height: "auto",
+    borderRightWidth: tokens["divider-width"],
+    borderRight: "solid",
+    borderRightColor: tokens["divider-color"],
   },
 });
--- packages/components-native/src/Divider/Divider.tsx
+++ packages/components-native/src/Divider/Divider.tsx
@@ -9,14 +9,30 @@ interface DividerProps {
    * @default "base"
    */
   readonly size?: keyof typeof styles;
+  /**
+   * The direction of the divider
+   *
+   * @default "horizontal"
+   */
+  readonly direction?: "horizontal" | "vertical";
+  /**
+   * Used to locate this view in end-to-end tests.
+   */
+  readonly testID?: string;
 }
 
-export function Divider({ size = "base" }: DividerProps): JSX.Element {
+export function Divider({
+  size = "base",
+  direction = "horizontal",
+  testID = "Divider",
+}: DividerProps): JSX.Element {
   const style = [
     styles.base,
     size === "large" && styles.large,
+    size === "larger" && styles.larger,
     size === "largest" && styles.largest,
+    direction === "horizontal" ? styles.horizontal : styles.vertical,
   ];
 
-  return <View testID="Divider" style={style} />;
+  return <View testID={testID} style={style} />;
 }
