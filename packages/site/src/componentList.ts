26e3d4fca48065ae7058d7821fe5c801a69eee33
Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>
2024-11-07 16:52:35 -0700

    add disambiguation to a few components

--- packages/site/src/componentList.ts
+++ packages/site/src/componentList.ts
@@ -3,12 +3,13 @@ export const componentList = [
     title: "AnimatedPresence",
     to: "/components/AnimatedPresence",
     imageURL: "/Animation.png",
-    additionalMatches: ["Motion"],
+    additionalMatches: ["Motion", "Animation"],
   },
   {
     title: "AnimatedSwitcher",
     to: "/components/AnimatedSwitcher",
     imageURL: "/Animation.png",
+    additionalMatches: ["Motion", "Animation"],
   },
   {
     title: "Autocomplete",
@@ -29,20 +30,24 @@ export const componentList = [
     title: "Chip",
     to: "/components/Chip",
     imageURL: "/Chip.png",
+    additionalMatches: ["Pill"],
   },
   {
     title: "Disclosure",
     to: "/components/Disclosure",
     imageURL: "/Disclosure.png",
+    additionalMatches: ["Accordion", "Collapsable"],
   },
   {
     title: "StatusLabel",
     to: "/components/StatusLabel",
     imageURL: "/StatusLabel.png",
+    additionalMatches: ["Badge", "Pill", "InlineLabel"],
   },
   {
     title: "Switch",
     to: "/components/Switch",
     imageURL: "/Switch.png",
+    additionalMatches: ["Toggle"],
   },
 ];
