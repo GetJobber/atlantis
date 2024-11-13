4130491e32a135f4ccd1cce34dd635568192ecb4
Taylor Noj <taylor.n@getjobber.com>
2024-11-13 17:31:11 -0500

    Update setorybook examples; change the values and have a more descriptive controlled story

--- docs/components/SegmentedControl/Web.stories.tsx
+++ docs/components/SegmentedControl/Web.stories.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Meta, StoryFn } from "@storybook/react";
 import { SegmentedControl } from "@jobber/components/SegmentedControl";
+import { Button } from "@jobber/components/Button";
+import { Text } from "@jobber/components/Text";
+import { Typography } from "@jobber/components/Typography";
 
 const meta: Meta = {
   title: "Components/Selections/SegmentedControl/Web",
@@ -17,33 +20,44 @@ const meta: Meta = {
 export default meta;
 
 type Story = StoryFn<typeof SegmentedControl>;
+
 const options = [
-  { value: "pizza", label: "Pizza" },
-  { value: "tacos", label: "Tacos" },
-  { value: "sushi", label: "Sushi" },
-  { value: "burgers", label: "Burgers" },
+  { value: "day", label: "Day" },
+  { value: "week", label: "Week" },
+  { value: "month", label: "Month" },
+  { value: "year", label: "Year" },
 ];
 
 export const Controlled: Story = () => {
-  const [activeOption, setActiveOption] = useState("tacos");
+  const [activeOption, setActiveOption] = useState("week");
 
   return (
-    <SegmentedControl
-      selectedValue={activeOption}
-      onSelectValue={setActiveOption}
-    >
-      {options.map(option => (
-        <SegmentedControl.Option key={option.value} value={option.value}>
-          {option.label}
-        </SegmentedControl.Option>
-      ))}
-    </SegmentedControl>
+    <div>
+      <Text>
+        Control the state without interacting with the SegmentedControl
+        directly. Navigate through the options and reset state via the button.
+      </Text>
+      <br />
+      <Typography>Current activeOption: {activeOption}</Typography>
+      <SegmentedControl
+        selectedValue={activeOption}
+        onSelectValue={setActiveOption}
+      >
+        {options.map(option => (
+          <SegmentedControl.Option key={option.value} value={option.value}>
+            {option.label}
+          </SegmentedControl.Option>
+        ))}
+      </SegmentedControl>
+      <br />
+      <Button label="Reset to Week" onClick={() => setActiveOption("week")} />
+    </div>
   );
 };
 
 export const Uncontrolled: Story = () => {
   return (
-    <SegmentedControl defaultValue="pizza">
+    <SegmentedControl defaultValue="day">
       {options.map(option => (
         <SegmentedControl.Option key={option.value} value={option.value}>
           {option.label}
