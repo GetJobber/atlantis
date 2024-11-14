c403bd11b6eebfcaa361e5158770b0c8bb178b96
Scott Thompson <145565743+scotttjob@users.noreply.github.com>
2024-11-14 17:16:17 -0400

    feat(components): SegmentedControl (#1966)
    * segmented control v0.1
    
    * fix webstory import and location
    
    * add keyboard functionality, tab in and arrow key navigation
    
    * rename css files to use module
    
    * update index file and meta.json
    
    * add descriptions to the SegmentedControl props
    
    * match css to Triton's SegmentedControl
    
    * Remove box-shadow and replace with border
    
    Co-authored-by: Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>
    
    * fix linting issue with css order
    
    * component container is now 32px in height, removed double border from left selected border
    
    * SegmentedControl container respects parent maxWidth
    
    * remove the dependency array from useEffect in SegmentedControl
    
    * update storybook example to show emojis changing with focus
    
    * remove checked prop from SegmentedControlOption
    
    * remove useEffect from SegementedControl and set css variable directly in style prop to reflect count
    
    * handleChange and setSelectedOption are using useCallback
    
    * use 32px instead of spacing token
    
    * add aria-label to the option label and update prop descriptions
    
    * move aria-label to recommended input
    
    * code tab renders something and no longer completely fails
    
    * add uncontrolled option via SegmentedControlBase
    
    * breing SegmentedControlBase into it's own file
    
    * update Docs page for SemgnetedControl
    
    * change onChange to onSelected in the option
    
    * update story examples
    
    * match implementation to POC with updated prop names and story examples
    
    * ensure onSelectValue callback is called with initial value
    
    * add tabIndex to option input so you don't have to double key press to go to the next option
    
    * add tests for SegmentedControl and SegmentedControlProvider
    
    * update docs page with info on controlled and uncontrolled
    
    * update prop descriptions and clean up storybook examples
    
    * update switch link in docs
    
    * add name prop to segmentedcontrol parent, pass through options per provider
    
    * add tests for name prop
    
    * move uncontrolled paragraph before controlled paragraph in docs
    
    * remove console.logs
    
    * Update setorybook examples; change the values and have a more descriptive controlled story
    
    * update prop descriptions in SegmentedControl to provide more info
    
    * update broken links for controlled and uncontrolled stories
    
    * provide more details on the docs page about controlled and uncontrolled
    
    * use outline variable in span to get rid of double border on left
    
    ---------
    
    Co-authored-by: ZakaryH <zakary.h@getjobber.com>
    Co-authored-by: Taylor Noj <taylor.n@getjobber.com>
    Co-authored-by: Taylor Noj <104797704+taylorvnoj@users.noreply.github.com>
    Co-authored-by: Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>

---
+++ packages/components/src/SegmentedControl/SegmentedControlProvider.tsx
@@ -0,0 +1,85 @@
+import React, {
+  ChangeEventHandler,
+  PropsWithChildren,
+  createContext,
+  useCallback,
+  useContext,
+  useRef,
+  useState,
+} from "react";
+
+interface SegmentedControlProviderContext<T = unknown> {
+  handleChange: ChangeEventHandler<HTMLInputElement>;
+  selectedValue: T;
+  segmentedControlName?: string;
+}
+
+export const SegmentedControlContext = createContext<
+  SegmentedControlProviderContext<unknown>
+>({
+  handleChange: () => ({}),
+  selectedValue: null,
+  segmentedControlName: "",
+});
+
+interface SegmentedControlProviderProps<T> extends PropsWithChildren {
+  readonly selectedValue?: T;
+  readonly onSelectValue?: (value: T) => void;
+  readonly defaultValue?: T;
+  readonly name?: string;
+}
+
+export const useSegmentedControl = <T,>() => {
+  return useContext(
+    SegmentedControlContext,
+  ) as SegmentedControlProviderContext<T>;
+};
+
+export function SegmentedControlProvider<T>({
+  children,
+  selectedValue,
+  onSelectValue,
+  defaultValue,
+  name,
+}: SegmentedControlProviderProps<T>) {
+  const isControlled =
+    selectedValue !== undefined && onSelectValue !== undefined;
+  const [internalSelectedValue, setInternalSelectedValue] = useState<
+    T | undefined
+  >(defaultValue);
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback(
+    event => {
+      const value = event.target.value as T;
+
+      if (!isControlled) {
+        setInternalSelectedValue(value);
+      }
+      onSelectValue?.(value);
+    },
+    [onSelectValue, isControlled],
+  );
+
+  const currentSelectedOption = isControlled
+    ? selectedValue
+    : internalSelectedValue;
+
+  const hasInitialized = useRef(false);
+
+  if (!hasInitialized.current && currentSelectedOption !== undefined) {
+    onSelectValue?.(currentSelectedOption);
+    hasInitialized.current = true;
+  }
+
+  return (
+    <SegmentedControlContext.Provider
+      value={{
+        handleChange,
+        selectedValue: currentSelectedOption,
+        segmentedControlName: name,
+      }}
+    >
+      {children}
+    </SegmentedControlContext.Provider>
+  );
+}
