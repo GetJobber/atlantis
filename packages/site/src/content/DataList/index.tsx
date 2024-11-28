cd30b89a163cc7ec6602cb124a1d29719a2b298e
Scott Thompson <145565743+scotttjob@users.noreply.github.com>
2024-11-28 09:16:32 -0400

    adding DataList

---
+++ packages/site/src/content/DataList/index.tsx
@@ -0,0 +1,50 @@
+import { Button, DataList, Grid } from "@jobber/components";
+import Content from "@atlantis/docs/components/DataList/DataList.stories.mdx";
+import Props from "./DataList.props.json";
+import { ContentExport } from "../../types/content";
+
+function DataListRoot() {
+  const data = [
+    { id: 1, name: "Test Person", email: "sample@example.com" },
+    { id: 2, name: "Second Person", email: "second@example.com" },
+    { id: 3, name: "Third Person", email: "third@example.com" },
+  ];
+
+  const headers = {
+    name: "Name",
+    email: "Email",
+  };
+
+  return (
+    <DataList data={data} headers={headers}>
+      <DataList.Layout>
+        {(item: { name: string; email: string }) => (
+          <Grid>
+            <Grid.Cell size={{ xs: 12, md: 6 }}>{item.name}</Grid.Cell>
+            <Grid.Cell size={{ xs: 12, md: 6 }}>{item.email}</Grid.Cell>
+          </Grid>
+        )}
+      </DataList.Layout>
+      <DataList.EmptyState
+        message="No items to display"
+        action={<Button label="Add item" onClick={() => alert("Add item")} />}
+      />
+    </DataList>
+  );
+}
+
+export default {
+  content: () => <Content />,
+  props: Props,
+  component: {
+    element: DataListRoot,
+    defaultProps: {},
+  },
+  title: "DataList",
+  links: [
+    {
+      label: "Storybook",
+      url: "http://localhost:6006/?path=/docs/components-lists-and-tables-datalist--docs",
+    },
+  ],
+} as const satisfies ContentExport;
