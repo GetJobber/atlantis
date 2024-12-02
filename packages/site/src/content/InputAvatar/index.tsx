563072c089e839c1ef52cf5c241da1654a6d72bc
Taylor Noj <taylor.n@getjobber.com>
2024-12-02 13:53:17 -0500

    docs(components): Add InputAvatar to New Docs Site

--- packages/site/src/content/InputAvatar/index.tsx
+++ packages/site/src/content/InputAvatar/index.tsx
@@ -1,14 +1,30 @@
-import { InputAvatar } from "@jobber/components";
-import Content from "@atlantis/docs/components/InputAvatar/InputAvatar.stories.mdx";
+import InputAvatarContent from "@atlantis/docs/components/InputAvatar/InputAvatar.stories.mdx";
 import Props from "./InputAvatar.props.json";
 import { ContentExport } from "../../types/content";
 
 export default {
-  content: () => <Content />,
+  content: () => <InputAvatarContent />,
   props: Props,
   component: {
-    element: InputAvatar,
-    defaultProps: {},
+    element: `
+      const [avatarUrl, setAvatarUrl] = useState("https://picsum.photos/250");
+
+      async function handleChange(newAvatar) {
+        if (newAvatar) {
+          setAvatarUrl(await newAvatar.src());
+        } else {
+          setAvatarUrl(undefined);
+        }
+      }
+
+      return (
+        <InputAvatar
+          imageUrl={avatarUrl}
+          getUploadParams={() => Promise.resolve({ url: "https://httpbin.org/post" })}
+          onChange={handleChange}
+        />
+      );
+    `,
   },
   title: "InputAvatar",
   links: [
