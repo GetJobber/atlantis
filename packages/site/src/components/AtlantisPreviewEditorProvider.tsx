8ae2e0e0d4e82c70f339725548ca866bcffae8af
Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>
2024-12-03 15:49:39 -0700

    docs: standardize styling of code snippets and props table (#2181)
    * code rendering
    
    * live editor
    
    * comment
    
    * removing unneeded code
    
    * give description more room
    
    * rename Key to Property
    
    * use surface--background and text for all the code
    
    * addressing layout issue
    
    * bundler
    
    * fixing build errros
    
    * fixing build issues
    
    * fixing eslint issue
    
    * fix props list
    
    * call the list "properties"
    
    * changes to editor
    
    * more room for description
    
    * Move required into the name column
    
    * Update PropsList.tsx
    
    * better looking show code segments
    
    * prettier fixes
    
    * az sort
    
    * adjust styling, get rid of unecessary important
    
    * use a pre element for prop names
    
    * remove headers on small screens
    
    ---------
    
    Co-authored-by: Scott Thompson <145565743+scotttjob@users.noreply.github.com>

--- packages/site/src/components/AtlantisPreviewEditorProvider.tsx
+++ packages/site/src/components/AtlantisPreviewEditorProvider.tsx
@@ -228,17 +228,26 @@ const myTheme = EditorView.theme(
     "&": {
       color: "var(--color-text)",
       backgroundColor: "var(--color-surface--background)",
+      border: "var(--border-base) solid var(--color-border)",
+      borderRadius: "var(--radius-base)",
+      padding: "var(--space-small)",
     },
     ".cm-content": {
-      caretColor: "var(--color-brand--highlight)",
+      caretColor: "var(--color-interactive)",
     },
-
+    ".cm-type": {
+      color: "var(--color-text)",
+    },
+    "&.cm-focused": {
+      outline: "transparent",
+      boxShadow: "var(--shadow-focus)",
+    },
     "&.cm-focused .cm-selectionBackground, ::selection": {
-      backgroundColor: "var(--color-white)",
+      backgroundColor: "var(--color-surface)",
     },
     ".cm-gutters": {
-      backgroundColor: "#045",
-      color: "#ddd",
+      backgroundColor: "inherit",
+      color: "var(--color-text--secondary)",
       border: "none",
     },
   },
