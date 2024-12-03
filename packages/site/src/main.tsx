6087d65f51a7949d75e22a6d46bc4d9b3c8ee5c8
Jacob Deichert <jacob.d@getjobber.com>
2024-12-03 12:07:50 -0800

    Initialize atlantis theme provider

--- packages/site/src/main.tsx
+++ packages/site/src/main.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@jobber/design/foundation.css";
+import "@jobber/design/dist/dark.mode.css";
 import "@jobber/components/dist/styles.css";
 import "./main.css";
 import { BrowserRouter } from "react-router-dom";
+import { AtlantisThemeContextProvider } from "@jobber/components";
 import { Layout } from "./layout/Layout";
 import { AtlantisPreviewEditorProvider } from "./providers/AtlantisPreviewEditorProvider";
 import { AtlantisSiteProvider } from "./providers/AtlantisSiteProvider";
+import { initAtlantisTheme } from "./utils/theme";
 
 const urlParams = new URLSearchParams(window.location.search);
 const minimalMode = urlParams.has("minimal");
+
+initAtlantisTheme();
 
 function renderApp() {
   const root = document.getElementById("root");
@@ -18,13 +23,15 @@ function renderApp() {
     ReactDOM.createRoot(root).render(
       <React.StrictMode>
         <BrowserRouter>
-          <AtlantisSiteProvider
-            minimal={{ requested: minimalMode, enabled: false }}
-          >
-            <AtlantisPreviewEditorProvider>
-              <Layout />
-            </AtlantisPreviewEditorProvider>
-          </AtlantisSiteProvider>
+          <AtlantisThemeContextProvider>
+            <AtlantisSiteProvider
+              minimal={{ requested: minimalMode, enabled: false }}
+            >
+              <AtlantisPreviewEditorProvider>
+                <Layout />
+              </AtlantisPreviewEditorProvider>
+            </AtlantisSiteProvider>
+          </AtlantisThemeContextProvider>
         </BrowserRouter>
       </React.StrictMode>,
     );
---
+++ packages/site/src/utils/theme.ts
@@ -0,0 +1,28 @@
+import { Theme } from "@jobber/components";
+
+const FALLBACK_THEME: Theme = "light";
+
+/**
+ * Initialize the site's theme. This checks local storage for a previously set theme,
+ * else it falls back to the default "light" theme. It then sets the data-theme attribute,
+ * which is what AtlantisThemeContextProvider reads upon startup.
+ */
+export function initAtlantisTheme() {
+  const theme = getTheme() || FALLBACK_THEME;
+  document.documentElement.dataset.theme = theme;
+}
+
+/**
+ * Get the theme from local storage.
+ */
+function getTheme(): Theme | null {
+  try {
+    const theme = localStorage.getItem("theme");
+
+    return theme as Theme;
+  } catch (error) {
+    // noop
+  }
+
+  return null;
+}
