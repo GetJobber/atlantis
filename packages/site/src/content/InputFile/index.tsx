35c61283dc6e9fe413aa64421a02290daa6f488c
Rebecca Anderson <rebecca.a@getjobber.com>
2024-12-03 08:33:23 -0500

    simplifying inputfile default example

--- packages/site/src/content/InputFile/index.tsx
+++ packages/site/src/content/InputFile/index.tsx
@@ -6,46 +6,7 @@ export default {
   content: () => <Content />,
   props: Props,
   component: {
-    element: `
-     const fetchUploadParams = () => Promise.resolve({ url: "https://httpbin.org/post" });
-
-    return (
-    <Content>
-      <Heading level={2}>Default</Heading>
-      <InputFile />
-
-      <Heading level={2}>Dropzone</Heading>
-      <InputFile
-        buttonLabel="Base Dropzone Uploader"
-        getUploadParams={fetchUploadParams}
-      />
-      <InputFile
-        buttonLabel="Dropzone Uploader with description"
-        getUploadParams={fetchUploadParams}
-        allowMultiple
-        allowedTypes="images"
-        description="JPEG, HEIC, PNG up to 5MB each"
-      />
-      <InputFile
-        size="small"
-        getUploadParams={fetchUploadParams}
-        buttonLabel="Small Dropzone Uploader"
-      />
-
-      <Heading level={2}>Button</Heading>
-      <InputFile
-        variation="button"
-        size="base"
-        buttonLabel="Base Button Uploader"
-        getUploadParams={fetchUploadParams}
-      />
-      <InputFile
-        variation="button"
-        size="small"
-        buttonLabel="Small Button Uploader"
-        getUploadParams={fetchUploadParams}
-      />
-    </Content>)`,
+    element: `<InputFile />`,
     defaultProps: {},
   },
   title: "InputFile",
