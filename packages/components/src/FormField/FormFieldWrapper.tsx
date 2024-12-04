d0c4be178d5e84d8a20a4daa42969adfc96e7532
Michael Paradis <michael.p@getjobber.com>
2024-12-04 12:26:58 -0800

    refactor(components): Expose Reusable hooks and components used in FormField [JOB-110812] (#2184)
    ---------
    
    Co-authored-by: Scott Thompson <145565743+scotttjob@users.noreply.github.com>

--- packages/components/src/FormField/FormFieldWrapper.tsx
+++ packages/components/src/FormField/FormFieldWrapper.tsx
@@ -1,11 +1,4 @@
-import React, {
-  PropsWithChildren,
-  RefObject,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import classnames from "classnames";
+import React, { PropsWithChildren, ReactNode, RefObject, useRef } from "react";
 import { Clearable, useShowClear } from "@jobber/hooks/useShowClear";
 import { AnimatePresence, motion } from "framer-motion";
 import { tokens } from "@jobber/design";
@@ -16,10 +9,10 @@ import { FormFieldDescription } from "./FormFieldDescription";
 import { ClearAction } from "./components/ClearAction";
 import { useToolbar } from "./hooks/useToolbar";
 import { useFormFieldFocus } from "./hooks/useFormFieldFocus";
-import { useIsSafari } from "./hooks/useIsSafari";
+import { useFormFieldWrapperStyles } from "./hooks/useFormFieldWrapperStyles";
 import { InputValidation } from "../InputValidation";
 
-interface FormFieldWrapperProps extends FormFieldProps {
+export interface FormFieldWrapperProps extends FormFieldProps {
   readonly error: string;
   readonly identifier: string;
   readonly descriptionIdentifier: string;
@@ -27,12 +20,6 @@ interface FormFieldWrapperProps extends FormFieldProps {
   readonly onClear: () => void;
 }
 
-interface LabelPadding {
-  paddingLeft: number | string | undefined;
-  paddingRight: number | string | undefined;
-}
-
-// eslint-disable-next-line max-statements
 export function FormFieldWrapper({
   align,
   description,
@@ -57,47 +44,25 @@ export function FormFieldWrapper({
   toolbarVisibility = "while-editing",
   wrapperRef,
 }: PropsWithChildren<FormFieldWrapperProps>) {
-  const isSafari = useIsSafari();
-  const wrapperClasses = classnames(
-    styles.wrapper,
-    size && styles[size],
-    align && styles[align],
-    {
-      [styles.miniLabel]:
-        (placeholder && value !== "") ||
-        (placeholder && type === "select") ||
-        // Naively assume that if the the type is tel, it is the InputPhoneNumber
-        (placeholder && type === "tel"),
-      [styles.text]: type === "textarea" || type === "text",
-      [styles.textarea]: type === "textarea",
-      [styles.safari]: isSafari && type === "textarea",
-      [styles.select]: type === "select",
-      [styles.invalid]: invalid ?? error,
-      [styles.disabled]: disabled,
-      [styles.maxLength]: maxLength,
-      [styles.timeInputLabel]:
-        placeholder && type === "time" && placeholder && value === "",
-    },
-  );
-  const containerClasses = classnames(styles.container, {
-    [styles.inline]: inline,
-  });
-
-  const wrapperInlineStyle = {
-    ["--formField-maxLength" as string]: maxLength || max,
-  };
-
   const prefixRef = useRef() as RefObject<HTMLDivElement>;
   const suffixRef = useRef() as RefObject<HTMLDivElement>;
 
-  const [labelStyle, setLabelStyle] = useState<LabelPadding>({
-    paddingLeft: undefined,
-    paddingRight: undefined,
-  });
-
-  useEffect(() => {
-    setLabelStyle(getAffixPaddding);
-  }, [value]);
+  const { wrapperClasses, containerClasses, wrapperInlineStyle, labelStyle } =
+    useFormFieldWrapperStyles({
+      align,
+      max,
+      maxLength,
+      prefixRef,
+      suffixRef,
+      placeholder,
+      value,
+      invalid,
+      error,
+      type,
+      disabled,
+      inline,
+      size,
+    });
 
   const { focused } = useFormFieldFocus({ wrapperRef });
 
@@ -124,97 +89,123 @@ export function FormFieldWrapper({
         data-testid="Form-Field-Wrapper"
         ref={wrapperRef}
       >
-        <div className={styles.horizontalWrapper}>
-          {prefix?.icon && <AffixIcon {...prefix} size={size} />}
-          <div className={styles.inputWrapper}>
-            {placeholder && (
-              <label
-                className={styles.label}
-                htmlFor={identifier}
-                style={
-                  prefixRef?.current || suffixRef?.current
-                    ? labelStyle
-                    : undefined
-                }
-              >
-                {placeholder}
-              </label>
-            )}
+        <FormFieldInputHorizontalWrapper>
+          <AffixIcon {...prefix} size={size} />
+          <FormFieldInputWrapperStyles>
+            <FormFieldLabel
+              placeholder={placeholder}
+              identifier={identifier}
+              style={
+                prefixRef?.current || suffixRef?.current
+                  ? labelStyle
+                  : undefined
+              }
+            />
+            <AffixLabel {...prefix} labelRef={prefixRef} />
 
-            {prefix?.label && <AffixLabel {...prefix} labelRef={prefixRef} />}
+            <FormFieldWrapperMain>{children}</FormFieldWrapperMain>
 
-            <div className={styles.childrenWrapper} tabIndex={-1}>
-              {children}
-            </div>
-
-            {suffix?.label && (
-              <AffixLabel {...suffix} labelRef={suffixRef} variation="suffix" />
-            )}
-          </div>
-          {showClear && <ClearAction onClick={onClear} />}
-          {suffix?.icon && (
-            <AffixIcon {...suffix} variation="suffix" size={size} />
-          )}
-        </div>
-        <AnimatePresence
-          initial={toolbarVisibility === "always" ? false : true}
-        >
-          {isToolbarVisible && (
-            <motion.div
-              key="toolbar"
-              initial={toolbarAnimationEnd}
-              animate={toolbarAnimationStart}
-              exit={toolbarAnimationEnd}
-              transition={{
-                duration: tokens["timing-base"] / 1000,
-                ease: "easeInOut",
-              }}
-              tabIndex={-1}
-            >
-              <div
-                className={styles.toolbar}
-                data-testid="ATL-InputText-Toolbar"
-              >
-                {toolbar}
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </div>
-      {description && !inline && (
-        <FormFieldDescription
-          id={descriptionIdentifier}
-          description={description}
+            <AffixLabel {...suffix} labelRef={suffixRef} variation="suffix" />
+          </FormFieldInputWrapperStyles>
+          <ClearAction onClick={onClear} visible={showClear} />
+          <AffixIcon {...suffix} variation="suffix" size={size} />
+        </FormFieldInputHorizontalWrapper>
+        <FormFieldWrapperToolbar
+          toolbarVisibility={toolbarVisibility}
+          isToolbarVisible={isToolbarVisible}
+          toolbarAnimationEnd={toolbarAnimationEnd}
+          toolbarAnimationStart={toolbarAnimationStart}
+          toolbar={toolbar}
         />
-      )}
-      {error && !inline && <InputValidation message={error} />}
+      </div>
+      <FormFieldDescription
+        visible={!!description && !inline}
+        id={descriptionIdentifier}
+        description={description}
+      />
+      <InputValidation message={error} visible={!!error && !inline} />
     </div>
   );
+}
 
-  function getAffixPaddding() {
-    const hasValue = value !== "";
-    const newPadding: LabelPadding = {
-      paddingLeft: undefined,
-      paddingRight: undefined,
-    };
+export function FormFieldInputHorizontalWrapper({
+  children,
+}: PropsWithChildren) {
+  return <div className={styles.horizontalWrapper}>{children}</div>;
+}
 
-    // Naively assume that if the the type is tel, it is the InputPhoneNumber
-    if (type === "tel") return newPadding;
+export function FormFieldInputWrapperStyles({
+  children,
+}: {
+  readonly children: React.ReactNode;
+}) {
+  return <div className={styles.inputWrapper}>{children}</div>;
+}
 
-    if (prefixRef?.current && !hasValue) {
-      const { offsetWidth } = prefixRef.current;
-      newPadding.paddingLeft = offset(offsetWidth);
-    }
+export function FormFieldWrapperMain({
+  children,
+  tabIndex = -1,
+}: {
+  readonly children: React.ReactNode;
+  readonly tabIndex?: number;
+}) {
+  return (
+    <div className={styles.childrenWrapper} tabIndex={tabIndex}>
+      {children}
+    </div>
+  );
+}
 
-    if (suffixRef?.current && !hasValue) {
-      const { offsetWidth } = suffixRef.current;
-      newPadding.paddingRight = offset(offsetWidth);
-    }
+export function FormFieldLabel({
+  placeholder,
+  identifier,
+  style,
+}: {
+  readonly placeholder?: string;
+  readonly identifier?: string;
+  readonly style?: React.CSSProperties;
+}) {
+  if (!placeholder) return null;
 
-    function offset(width: number) {
-      return `calc(${width}px + var(--space-smallest)`;
-    }
+  return (
+    <label className={styles.label} htmlFor={identifier} style={style}>
+      {placeholder}
+    </label>
+  );
+}
 
-    return newPadding;
-  }
+export function FormFieldWrapperToolbar({
+  toolbar,
+  isToolbarVisible,
+  toolbarAnimationEnd,
+  toolbarAnimationStart,
+  toolbarVisibility,
+}: {
+  readonly toolbarVisibility: string;
+  readonly isToolbarVisible: boolean;
+  readonly toolbarAnimationEnd: { opacity: number; height: number };
+  readonly toolbarAnimationStart: { opacity: number; height: string | number };
+  readonly toolbar: ReactNode;
+}) {
+  return (
+    <AnimatePresence initial={toolbarVisibility === "always" ? false : true}>
+      {isToolbarVisible && (
+        <motion.div
+          key="toolbar"
+          initial={toolbarAnimationEnd}
+          animate={toolbarAnimationStart}
+          exit={toolbarAnimationEnd}
+          transition={{
+            duration: tokens["timing-base"] / 1000,
+            ease: "easeInOut",
+          }}
+          tabIndex={-1}
+        >
+          <div className={styles.toolbar} data-testid="ATL-InputText-Toolbar">
+            {toolbar}
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 }
