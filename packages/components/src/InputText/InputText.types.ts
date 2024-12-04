1641a34d2f4fc5c571668b2a28aac9c7913ef074
Michael Paradis <michael.p@getjobber.com>
2024-12-04 13:28:49 -0800

    Move InputTextRebuiltProps to types file

---
+++ packages/components/src/InputText/InputText.types.ts
@@ -0,0 +1,54 @@
+import { Clearable } from "@jobber/hooks/src";
+import {
+  AutocompleteTypes,
+  FormFieldProps,
+  FormFieldTypes,
+} from "../FormField";
+
+export interface RowRange {
+  min: number;
+  max: number;
+}
+
+export type InputTextVersion = 1 | 2 | undefined;
+
+export interface InputTextRebuiltProps
+  extends Omit<
+    React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement>,
+    "onChange" | "onBlur" | "max" | "min" | "maxLength" | "rows" | "size"
+  > {
+  readonly error?: string;
+  readonly identifier?: string;
+  readonly autocomplete?: boolean | AutocompleteTypes;
+  readonly loading?: boolean;
+  readonly onKeyUp?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  readonly children?: React.ReactNode;
+  readonly clearable?: Clearable;
+  /**
+   * Use this when you're expecting a long answer.
+   */
+  readonly multiline?: boolean;
+
+  /**
+   * Specifies the visible height of a long answer form field. Can be in the
+   * form of a single number to set a static height, or an object with a min
+   * and max keys indicating the minimum number of visible rows, and the
+   * maximum number of visible rows.
+   */
+  readonly rows?: number | RowRange;
+  readonly version: Extract<InputTextVersion, 2>;
+  readonly type?: FormFieldTypes;
+
+  readonly onChange?: FormFieldProps["onChange"];
+  readonly onEnter?: FormFieldProps["onEnter"];
+
+  readonly onBlur?: FormFieldProps["onBlur"];
+  readonly value: string;
+
+  readonly maxLength?: number;
+  readonly max?: number;
+  readonly min?: number;
+
+  readonly size?: FormFieldProps["size"];
+  readonly inline?: FormFieldProps["inline"];
+}
