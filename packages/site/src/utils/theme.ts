0f28390d334e5e440c4d5393c92dcbaf1ddde7e7
Jacob Deichert <jacob.d@getjobber.com>
2024-12-04 07:57:46 -0800

    Change theme init to pull prioritize localstorage
    The VS Code extension renders the theme toggle button and actually remembers the value in localstorage. I want to support that UX, so I'm prioritizing the stored value over the initial theme provided by the extension.

--- packages/site/src/utils/theme.ts
+++ packages/site/src/utils/theme.ts
@@ -3,12 +3,15 @@ import { Theme } from "@jobber/components";
 const FALLBACK_THEME: Theme = "light";
 
 /**
- * Initialize the site's theme. This checks local storage for a previously set theme,
- * else it falls back to the default "light" theme. It then sets the data-theme attribute,
- * which is what AtlantisThemeContextProvider reads upon startup.
+ * Initializes the site's theme.
+ *
+ * This checks local storage for a previously set theme preference. If that's not set, it'll
+ * apply the initial theme if provided, else it falls back to the default "light" theme.
+ * It then sets the data-theme attribute, which is what AtlantisThemeContextProvider reads
+ * upon startup.
  */
-export function initAtlantisTheme(themeOverride: string | null) {
-  const theme = themeOverride || getTheme() || FALLBACK_THEME;
+export function initAtlantisTheme(initialTheme: string | null) {
+  const theme = getTheme() || initialTheme || FALLBACK_THEME;
   document.documentElement.dataset.theme = theme;
 }
 
