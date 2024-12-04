2b863fe758c639c45ccca3389e9f74e31876db61
Jacob Deichert <jacob.d@getjobber.com>
2024-12-04 14:16:52 -0800

    Update mobile iframe when theme changes

--- packages/site/src/providers/AtlantisPreviewEditorProvider.tsx
+++ packages/site/src/providers/AtlantisPreviewEditorProvider.tsx
@@ -139,6 +139,11 @@ export const AtlantisPreviewEditorProvider = ({
       const iframeWindow = iframe.current.contentWindow;
       iframeWindow?.postMessage({ type: "updateTheme", theme }, "*");
     }
+
+    if (iframeMobile.current) {
+      const iframeWindow = iframeMobile.current.contentWindow;
+      iframeWindow?.postMessage({ type: "updateTheme", theme }, "*");
+    }
   }, [theme]);
 
   return (
