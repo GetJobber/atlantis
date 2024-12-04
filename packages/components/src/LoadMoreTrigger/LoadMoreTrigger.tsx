ce59ac746b09148451d849557f2028909bc5dfd8
ZakaryH <zakary.h@getjobber.com>
2024-12-03 19:23:36 -0800

    add testId to loading trigger

--- packages/components/src/LoadMoreTrigger/LoadMoreTrigger.tsx
+++ packages/components/src/LoadMoreTrigger/LoadMoreTrigger.tsx
@@ -4,22 +4,23 @@ import styles from "./LoadMoreTrigger.module.css";
 
 interface LoadMoreTriggerProps {
   readonly onLoadMore: () => void;
+  readonly testId?: string;
 }
 
 export const LOAD_MORE_TEST_ID = "ATL-LoadMore-Trigger";
 
-export function LoadMoreTrigger({ onLoadMore }: LoadMoreTriggerProps) {
+export function LoadMoreTrigger({ onLoadMore, testId }: LoadMoreTriggerProps) {
   const [inViewRef, isInView] = useInView<HTMLDivElement>();
 
   useEffect(() => {
-    isInView && onLoadMore?.();
+    isInView && onLoadMore();
   }, [isInView]);
 
   return (
     <div
       ref={inViewRef}
       className={styles.trigger}
-      data-testid={LOAD_MORE_TEST_ID}
+      data-testid={testId || LOAD_MORE_TEST_ID}
     />
   );
 }
