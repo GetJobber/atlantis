d9427331f6b9a9d8c0daea41569f012579074f2c
Rebecca Anderson <rebecca.a@getjobber.com>
2024-12-05 08:51:22 -0500

    docs(components): Adding SideDrawer to New Docs Site [JOB-110722] (#2197)
    * adding SideDrawer
    
    * fixing the import thing

--- packages/site/src/content/index.ts
+++ packages/site/src/content/index.ts
@@ -63,6 +63,7 @@ import RadioGroupContent from "./RadioGroup";
 import RecurringSelectContent from "./RecurringSelect";
 import SegmentedControlContent from "./SegmentedControl";
 import SelectContent from "./Select";
+import SideDrawerContent from "./SideDrawer";
 import SpinnerContent from "./Spinner";
 import StatusIndicatorContent from "./StatusIndicator";
 import StatusLabelContent from "./StatusLabel";
@@ -270,6 +271,9 @@ export const SiteContent: Record<string, ContentExport> = {
   },
   Select: {
     ...SelectContent,
+  },
+  SideDrawer: {
+    ...SideDrawerContent,
   },
   Spinner: {
     ...SpinnerContent,
