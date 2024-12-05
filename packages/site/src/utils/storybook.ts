f39c74ab4916955c6579b2979cb1a2d5842f9d5c
Jacob Deichert <jacob.d@getjobber.com>
2024-12-05 08:56:14 -0800

    Only redirect the root index to storybook unless we force it with ?new

--- packages/site/src/utils/storybook.ts
+++ packages/site/src/utils/storybook.ts
@@ -3,8 +3,10 @@
  *
  * If the url contains `?path`, we redirect to `/storybook/?path=___`.
  *
- * If not, by default we still redirect to `/storybook` unless the url contains
- * `?new` which forces the new docs site to render.
+ * If the url is the root page, we redirect to `/storybook` unless the `?new`
+ * param is set, which forces the new docs site to render.
+ *
+ * Any other path is assumed to be owned by the new docs site, and will render.
  */
 export function handleStorybookRedirect() {
   // When running locally, don't check for storybook redirects.
@@ -18,7 +20,7 @@ export function handleStorybookRedirect() {
 
   if (isStorybookPath) {
     window.location.href = `/storybook/${window.location.search}`;
-  } else if (!forceNewDocsSite) {
+  } else if (window.location.pathname === "/" && !forceNewDocsSite) {
     // NOTE: by default we redirect to storybook for now.
     // TODO: when the new site is ready, we just need to remove this case.
     window.location.href = `/storybook`;
