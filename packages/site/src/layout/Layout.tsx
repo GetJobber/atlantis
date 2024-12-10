0344ab4426a6a6d09a5ac0f8e9d2fc931d32ab97
Rebecca Anderson <rebecca.a@getjobber.com>
2024-12-10 12:23:14 -0500

    fixing routes

--- packages/site/src/layout/Layout.tsx
+++ packages/site/src/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren, useEffect } from "react";
 import { Route, Switch, useLocation } from "react-router";
 import { NavMenu } from "./NavMenu";
+import { ComponentView } from "./ComponentView";
 import { AtlantisRoute, routes } from "../routes";
 import "./code-theme.css";
 import { ToggleThemeButton } from "../components/ToggleThemeButton";
@@ -9,6 +10,7 @@ import { ToggleThemeButton } from "../components/ToggleThemeButton";
  * Layout for whole application. This will display the NavMenu and the content of the page.
  * @returns ReactNode
  */
+
 export const Layout = () => {
   const location = useLocation();
 
@@ -21,67 +23,60 @@ export const Layout = () => {
       <NavMenu />
       <div style={{ overflow: "auto", width: "100%", minHeight: "100%" }}>
         <Switch>
-          {routes?.map((route, routeIndex) => {
-            console.log("route", route);
-            if (route.inNav === false) return null;
+          <>
+            {routes?.map((route, routeIndex) => {
+              if (route.inNav === false) return null;
 
-            const iterateSubSubMenu = (childchildroutes: AtlantisRoute[]) => {
-              console.log("iteratesubsubsmenu", routes);
+              const iterateSubMenu = (childroutes: AtlantisRoute[]) => {
+                return childroutes.map((child, childIndex) => {
+                  // We don't want to loop through the components
+                  if (!child.children) {
+                    return (
+                      <Route
+                        key={childIndex}
+                        exact={child.exact ?? false}
+                        path={child.path}
+                        component={child.component}
+                      />
+                    );
+                  }
+                });
+              };
 
-              return childchildroutes.map((childchild, childchildIndex) => {
+              // Top level items with children (Changelog)
+              if (route.children) {
                 return (
-                  <Route
-                    key={childchildIndex}
-                    exact={childchild.exact ?? false}
-                    path={childchild.path}
-                    component={childchild.component}
-                  />
+                  <>
+                    <Route
+                      key={routeIndex}
+                      exact={route.exact ?? false}
+                      path={route.path}
+                      component={route.component}
+                    />
+                    {iterateSubMenu(route.children)}
+                  </>
                 );
-              });
-            };
+              }
 
-            const iterateSubMenu = (childroutes: AtlantisRoute[]) => {
-              console.log("iteratesubmenu", routes);
-
-              return childroutes.map((child, childIndex) => {
-                if (child.children) {
-                  return <>{iterateSubSubMenu(child.children)}</>;
-                }
-
-                return (
-                  <Route
-                    key={childIndex}
-                    exact={child.exact ?? false}
-                    path={child.path}
-                    component={child.component}
-                  />
-                );
-              });
-            };
-
-            if (route.children) {
+              // Top level items with no children
               return (
-                <>
-                  <Route
-                    key={routeIndex}
-                    exact={route.exact ?? false}
-                    path={route.path}
-                    component={route.component}
-                  />
-                  {iterateSubMenu(route.children)}
-                </>
+                <Route
+                  exact={route.exact ?? false}
+                  key={routeIndex}
+                  path={route.path}
+                  component={route.component}
+                />
               );
-            }
+            })}
 
-            return (
-              <Route
-                exact={route.exact ?? false}
-                key={routeIndex}
-                path={route.path}
-                component={route.component}
-              />
-            );
-          })}
+            {/* The component page */}
+            <Route
+              key={"component"}
+              exact={true}
+              path={"/components/:name"}
+              component={ComponentView}
+            />
+          </>
         </Switch>
       </div>
 
