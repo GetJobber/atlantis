17546822db489d5f3bab16f96c1b35e9e31ee3f4
Rebecca Anderson <rebecca.a@getjobber.com>
2024-12-11 08:56:05 -0500

    replacing thing i should not have removeD

--- packages/site/src/layout/Layout.tsx
+++ packages/site/src/layout/Layout.tsx
@@ -23,58 +23,60 @@ export const Layout = () => {
       <NavMenu />
       <div style={{ overflow: "auto", width: "100%", minHeight: "100%" }}>
         <Switch>
-          {routes?.map((route, routeIndex) => {
-            if (route.inNav === false) return null;
+          <>
+            {routes?.map((route, routeIndex) => {
+              if (route.inNav === false) return null;
 
-            const iterateSubMenu = (childroutes: AtlantisRoute[]) => {
-              return childroutes.map((child, childIndex) => {
-                // We don't want to loop through the components
-                if (!child.children) {
-                  return (
+              const iterateSubMenu = (childroutes: AtlantisRoute[]) => {
+                return childroutes.map((child, childIndex) => {
+                  // We don't want to loop through the components
+                  if (!child.children) {
+                    return (
+                      <Route
+                        key={childIndex}
+                        exact={child.exact ?? false}
+                        path={child.path}
+                        component={child.component}
+                      />
+                    );
+                  }
+                });
+              };
+
+              // Top level items with children (Changelog)
+              if (route.children) {
+                return (
+                  <>
                     <Route
-                      key={childIndex}
-                      exact={child.exact ?? false}
-                      path={child.path}
-                      component={child.component}
+                      key={routeIndex}
+                      exact={route.exact ?? false}
+                      path={route.path}
+                      component={route.component}
                     />
-                  );
-                }
-              });
-            };
+                    {iterateSubMenu(route.children)}
+                  </>
+                );
+              }
 
-            // Top level items with children (Changelog)
-            if (route.children) {
+              // Top level items with no children
               return (
-                <>
-                  <Route
-                    key={routeIndex}
-                    exact={route.exact ?? false}
-                    path={route.path}
-                    component={route.component}
-                  />
-                  {iterateSubMenu(route.children)}
-                </>
+                <Route
+                  exact={route.exact ?? false}
+                  key={routeIndex}
+                  path={route.path}
+                  component={route.component}
+                />
               );
-            }
+            })}
 
-            // Top level items with no children
-            return (
-              <Route
-                exact={route.exact ?? false}
-                key={routeIndex}
-                path={route.path}
-                component={route.component}
-              />
-            );
-          })}
-
-          {/* The component page */}
-          <Route
-            key={"component"}
-            exact={true}
-            path={"/components/:name"}
-            component={ComponentView}
-          />
+            {/* The component page */}
+            <Route
+              key={"component"}
+              exact={true}
+              path={"/components/:name"}
+              component={ComponentView}
+            />
+          </>
         </Switch>
       </div>
 
