966caf233f7b4cd2bd302436752e385d6d74a0d6
Jacob Deichert <jacob.d@getjobber.com>
2024-12-11 09:07:02 -0800

    Limit analytics to prod

--- packages/site/src/components/Analytics.ts
+++ packages/site/src/components/Analytics.ts
@@ -3,7 +3,7 @@ import { useLocation } from "react-router";
 import { useOnMount } from "@jobber/hooks";
 import ReactGA from "react-ga4";
 
-const IS_LOCAL = window.location.hostname.includes("localhost");
+const IS_PROD = window.location.hostname.includes("atlantis.getjobber.com");
 
 /**
  * Analytics tracks page navigations and reports them to our analytics provider.
@@ -12,12 +12,12 @@ export function Analytics() {
   const location = useLocation();
 
   useOnMount(() => {
-    if (IS_LOCAL) return;
+    if (!IS_PROD) return;
     ReactGA.initialize("G-V1N3TQVQB5");
   });
 
   useEffect(() => {
-    if (IS_LOCAL) return;
+    if (!IS_PROD) return;
     ReactGA.send({
       hitType: "pageview",
       page: location.pathname,
