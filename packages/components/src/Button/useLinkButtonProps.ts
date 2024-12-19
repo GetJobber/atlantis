e920aadb897781b0acb2fffc4bbdf6cb5178cdba
Michael Paradis <michael.p@getjobber.com>
2024-12-19 06:56:28 -0800

    Add useLinkButtonProps for event handlers on Buttons that are links

---
+++ packages/components/src/Button/useLinkButtonProps.ts
@@ -0,0 +1,30 @@
+import React, { useCallback } from "react";
+import { useButtonNavigationProvider } from "./ButtonNavigationProvider";
+import { ButtonProps } from "./Button.types";
+
+export function useLinkButtonProps(props: ButtonProps) {
+  const { routerOptions, onClick, url, useClientSideRouting } = props;
+  const { openLink, buildLocationHref } = useButtonNavigationProvider();
+  const routerHref = buildLocationHref(url, routerOptions);
+
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement>) => {
+      useClientSideRouting && event.preventDefault();
+
+      onClick?.(event);
+      useClientSideRouting &&
+        url &&
+        openLink(
+          url,
+          routerOptions,
+          event as React.MouseEvent<HTMLAnchorElement>,
+        );
+    },
+    [onClick, open, buildLocationHref, routerOptions],
+  );
+
+  return {
+    handleClick,
+    href: useClientSideRouting ? routerHref : url,
+  };
+}
