b45783f56cc595c8984074e43df859d53390105e
Taylor Noj <taylor.n@getjobber.com>
2025-01-07 00:21:44 -0500

    add tests for UNSAFE props

--- packages/components/src/Popover/Popover.test.tsx
+++ packages/components/src/Popover/Popover.test.tsx
@@ -19,23 +19,52 @@ const PopoverTestComponent = (props: Omit<PopoverProps, "attachTo">) => {
   );
 };
 
-it("should render a Popover with the content and dismiss button", async () => {
-  const handleClose = jest.fn();
+describe("Popover", () => {
+  it("should render a Popover with the content and dismiss button", async () => {
+    const handleClose = jest.fn();
 
-  render(
-    <PopoverTestComponent open={true} onRequestClose={handleClose}>
-      {content}
-    </PopoverTestComponent>,
-  );
+    render(
+      <PopoverTestComponent open={true} onRequestClose={handleClose}>
+        {content}
+      </PopoverTestComponent>,
+    );
 
-  expect(screen.queryByText(content)).toBeInstanceOf(HTMLElement);
+    expect(screen.queryByText(content)).toBeInstanceOf(HTMLElement);
 
-  await userEvent.click(screen.getByLabelText("Close dialog"));
-  expect(handleClose).toHaveBeenCalledTimes(1);
-});
+    await userEvent.click(screen.getByLabelText("Close dialog"));
+    expect(handleClose).toHaveBeenCalledTimes(1);
+  });
 
-it("shouldn't render a popover when open is false", async () => {
-  render(<PopoverTestComponent open={false}>{content}</PopoverTestComponent>);
+  it("shouldn't render a popover when open is false", async () => {
+    render(<PopoverTestComponent open={false}>{content}</PopoverTestComponent>);
 
-  expect(screen.queryByText(content)).toBeNull();
+    expect(screen.queryByText(content)).toBeNull();
+  });
+
+  describe("UNSAFE_ props", () => {
+    it("should apply the UNSAFE_className to the Popover", () => {
+      render(
+        <PopoverTestComponent open={true} UNSAFE_className="custom-class">
+          {content}
+        </PopoverTestComponent>,
+      );
+
+      const popover = screen.getByRole("dialog");
+      expect(popover).toHaveClass("custom-class");
+    });
+
+    it("should apply the UNSAFE_style to the Popover", () => {
+      render(
+        <PopoverTestComponent
+          open={true}
+          UNSAFE_style={{ backgroundColor: "lightblue" }}
+        >
+          {content}
+        </PopoverTestComponent>,
+      );
+
+      const popover = screen.getByRole("dialog");
+      expect(getComputedStyle(popover).backgroundColor).toBe("lightblue");
+    });
+  });
 });
