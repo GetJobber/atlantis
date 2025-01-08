dad4ebd62ac1eb66ef91b711dc366fed422861a0
Taylor Noj <taylor.n@getjobber.com>
2025-01-07 20:55:59 -0500

    UNSAFE_ props now target each popover element and tests are updated

--- packages/components/src/Popover/Popover.test.tsx
+++ packages/components/src/Popover/Popover.test.tsx
@@ -7,7 +7,7 @@ import { PopoverProps } from "./Popover";
 const content = "Test Content";
 
 const PopoverTestComponent = (props: Omit<PopoverProps, "attachTo">) => {
-  const divRef = useRef(null);
+  const divRef = useRef<HTMLDivElement>(null);
 
   return (
     <>
@@ -19,15 +19,21 @@ const PopoverTestComponent = (props: Omit<PopoverProps, "attachTo">) => {
   );
 };
 
+const renderPopover = (props: Omit<PopoverProps, "attachTo">) => {
+  return render(
+    <PopoverTestComponent {...props}>{content}</PopoverTestComponent>,
+  );
+};
+
 describe("Popover", () => {
   it("should render a Popover with the content and dismiss button", async () => {
     const handleClose = jest.fn();
 
-    render(
-      <PopoverTestComponent open={true} onRequestClose={handleClose}>
-        {content}
-      </PopoverTestComponent>,
-    );
+    renderPopover({
+      open: true,
+      onRequestClose: handleClose,
+      children: content,
+    });
 
     expect(screen.queryByText(content)).toBeInstanceOf(HTMLElement);
 
@@ -36,35 +42,84 @@ describe("Popover", () => {
   });
 
   it("shouldn't render a popover when open is false", async () => {
-    render(<PopoverTestComponent open={false}>{content}</PopoverTestComponent>);
+    renderPopover({ open: false, children: content });
 
     expect(screen.queryByText(content)).toBeNull();
   });
 
   describe("UNSAFE_ props", () => {
-    it("should apply the UNSAFE_className to the Popover", () => {
-      render(
-        <PopoverTestComponent open={true} UNSAFE_className="custom-class">
-          {content}
-        </PopoverTestComponent>,
-      );
+    it("should apply the UNSAFE_className to the container", () => {
+      renderPopover({
+        open: true,
+        UNSAFE_className: { container: "custom-container-class" },
+        children: content,
+      });
 
-      const popover = screen.getByRole("dialog");
-      expect(popover).toHaveClass("custom-class");
+      const popover = screen.getByTestId("popover-container");
+      expect(popover).toHaveClass("custom-container-class");
     });
 
-    it("should apply the UNSAFE_style to the Popover", () => {
-      render(
-        <PopoverTestComponent
-          open={true}
-          UNSAFE_style={{ backgroundColor: "lightblue" }}
-        >
-          {content}
-        </PopoverTestComponent>,
-      );
+    it("should apply the UNSAFE_className to the dismiss button container", () => {
+      renderPopover({
+        open: true,
+        UNSAFE_className: {
+          dismissButtonContainer: "custom-dismiss-button-class",
+        },
+        children: content,
+      });
 
-      const popover = screen.getByRole("dialog");
+      const dismissButtonContainer = screen.getByTestId(
+        "popover-dismiss-button-container",
+      );
+      expect(dismissButtonContainer).toHaveClass("custom-dismiss-button-class");
+    });
+
+    it("should apply the UNSAFE_className to the arrow", () => {
+      renderPopover({
+        open: true,
+        UNSAFE_className: { arrow: "custom-arrow-class" },
+        children: content,
+      });
+
+      const arrow = screen.getByTestId("popover-arrow");
+      expect(arrow).toHaveClass("custom-arrow-class");
+    });
+
+    it("should apply the UNSAFE_style to the container", () => {
+      renderPopover({
+        open: true,
+        UNSAFE_style: { container: { backgroundColor: "lightblue" } },
+        children: content,
+      });
+
+      const popover = screen.getByTestId("popover-container");
       expect(getComputedStyle(popover).backgroundColor).toBe("lightblue");
+    });
+
+    it("should apply the UNSAFE_style to the dismiss button container", () => {
+      renderPopover({
+        open: true,
+        UNSAFE_style: { dismissButtonContainer: { backgroundColor: "red" } },
+        children: content,
+      });
+
+      const dismissButtonContainer = screen.getByTestId(
+        "popover-dismiss-button-container",
+      );
+      expect(getComputedStyle(dismissButtonContainer).backgroundColor).toBe(
+        "red",
+      );
+    });
+
+    it("should apply the UNSAFE_style to the arrow", () => {
+      renderPopover({
+        open: true,
+        UNSAFE_style: { arrow: { backgroundColor: "green" } },
+        children: content,
+      });
+
+      const arrow = screen.getByTestId("popover-arrow");
+      expect(getComputedStyle(arrow).backgroundColor).toBe("green");
     });
   });
 });
--- packages/components/src/Popover/Popover.tsx
+++ packages/components/src/Popover/Popover.tsx
@@ -34,18 +34,26 @@ export interface PopoverProps {
   readonly preferredPlacement?: "top" | "bottom" | "left" | "right" | "auto";
 
   /**
-   * **Use at your own risk:** Custom className for the wrapping container. This should only be used as a
+   * **Use at your own risk:** Custom class names for specific elements. This should only be used as a
    * **last resort**. Using this may result in unexpected side effects.
    * More information [here](../?path=/docs/guides-customizing-components--docs#unsafe_-props).
    */
-  readonly UNSAFE_className?: string;
+  readonly UNSAFE_className?: {
+    container?: string;
+    dismissButtonContainer?: string;
+    arrow?: string;
+  };
 
   /**
-   * **Use at your own risk:** Custom style for the wrapping container. This should only be used as a
+   * **Use at your own risk:** Custom style for specific elements. This should only be used as a
    * **last resort**. Using this may result in unexpected side effects.
    * More information [here](../?path=/docs/guides-customizing-components--docs#unsafe_-props).
    */
-  readonly UNSAFE_style?: CSSProperties;
+  readonly UNSAFE_style?: {
+    container?: CSSProperties;
+    dismissButtonContainer?: CSSProperties;
+    arrow?: CSSProperties;
+  };
 }
 
 export function Popover({
@@ -54,8 +62,8 @@ export function Popover({
   attachTo,
   open,
   preferredPlacement = "auto",
-  UNSAFE_className,
-  UNSAFE_style,
+  UNSAFE_className = {},
+  UNSAFE_style = {},
 }: PopoverProps) {
   const [popperElement, setPopperElement] = useState<HTMLElement | null>();
   const [arrowElement, setArrowElement] = useState<HTMLElement | null>();
@@ -69,7 +77,15 @@ export function Popover({
   );
   useRefocusOnActivator(open);
 
-  const popoverClassNames = classnames(classes.popover, UNSAFE_className);
+  const popoverClassNames = classnames(
+    classes.popover,
+    UNSAFE_className.container,
+  );
+  const dismissButtonClassNames = classnames(
+    classes.dismissButton,
+    UNSAFE_className.dismissButtonContainer,
+  );
+  const arrowClassNames = classnames(classes.arrow, UNSAFE_className.arrow);
 
   return (
     <>
@@ -78,18 +94,24 @@ export function Popover({
           role="dialog"
           data-elevation={"elevated"}
           ref={setPopperElement}
-          style={{ ...popperStyles.popper, ...UNSAFE_style }}
+          style={{ ...popperStyles.popper, ...(UNSAFE_style.container ?? {}) }}
           className={popoverClassNames}
           {...attributes.popper}
+          data-testid="popover-container"
         >
-          <div className={classes.dismissButton}>
+          <div
+            className={dismissButtonClassNames}
+            style={UNSAFE_style.dismissButtonContainer ?? {}}
+            data-testid="popover-dismiss-button-container"
+          >
             <ButtonDismiss onClick={onRequestClose} ariaLabel="Close dialog" />
           </div>
           {children}
           <div
             ref={setArrowElement}
-            className={classes.arrow}
-            style={popperStyles.arrow}
+            className={arrowClassNames}
+            style={{ ...popperStyles.arrow, ...(UNSAFE_style.arrow ?? {}) }}
+            data-testid="popover-arrow"
           />
         </div>
       )}
