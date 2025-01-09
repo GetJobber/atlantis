cc27d17260991642e960389b0b1e608d4905cb8d
Michael Paradis <michael.p@getjobber.com>
2025-01-09 11:49:29 -0800

    Add rebuilt InputDate

--- packages/components/src/InputDate/InputDate.types.ts
+++ packages/components/src/InputDate/InputDate.types.ts
@@ -1,4 +1,54 @@
 import { CommonFormFieldProps, FormFieldProps } from "../FormField";
+import { InputTextRebuiltProps } from "../InputText/InputText.types";
+
+export interface InputDateRebuiltProps
+  extends Omit<
+    InputTextRebuiltProps,
+    | "clearable"
+    | "onChange"
+    | "value"
+    | "prefix"
+    | "suffix"
+    | "multiline"
+    | "rows"
+    | "type"
+  > {
+  /**
+   * A Date object value
+   * (e.g., `new Date("11/11/2011")`)
+   * */
+  readonly value?: Date;
+  /**
+   * The maximum selectable date.
+   */
+  readonly maxDate?: Date;
+
+  /**
+   * The minimum selectable date.
+   */
+  readonly minDate?: Date;
+
+  /**
+   * Whether to show the calendar icon
+   * @default true
+   */
+  readonly showIcon?: boolean;
+
+  /**
+   * Text to display instead of a date value
+   */
+  readonly emptyValueLabel?: string;
+
+  /**
+   * Version 2 is highly experimental. Avoid using it unless you have talked with Atlantis first.
+   */
+  readonly version: 2;
+
+  readonly onChange: (
+    newValue: Date,
+    event?: React.ChangeEvent<HTMLInputElement>,
+  ) => void;
+}
 
 export interface InputDateProps
   extends Omit<CommonFormFieldProps, "clearable">,
