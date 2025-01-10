faa621d3ef76d05f03becf87321111aafeff756e
Chris Murray <39704901+chris-at-jobber@users.noreply.github.com>
2025-01-10 10:07:08 -0700

    docs: improvements to the state of thumbnails [JOB-112069] (#2300)
    * remove images from items that don't need thumbnails
    
    * remove more thumbnails
    
    * dont render an image if no URL is provided
    
    * Add thumbs for Content
    
    * make the imageURL truly optional

--- packages/site/src/guidesList.ts
+++ packages/site/src/guidesList.ts
@@ -2,32 +2,26 @@ export const guidesList = [
   {
     title: "Create a React component",
     to: "/guides/create-a-react-component",
-    imageURL: "/Placeholder.png",
   },
   {
     title: "Customizing components",
     to: "/guides/customizing-components",
-    imageURL: "/Placeholder.png",
-    additionalMatches: ["Custom"],
+    additionalMatches: ["Custom", "Composability"],
   },
   {
     title: "Documentation styleguide",
     to: "/guides/documentation-styleguide",
-    imageURL: "/Placeholder.png",
   },
   {
     title: "Frontend styleguide",
     to: "/guides/frontend-styleguide",
-    imageURL: "/Placeholder.png",
   },
   {
     title: "Getting started with React",
     to: "/guides/getting-started-with-react",
-    imageURL: "/Placeholder.png",
   },
   {
     title: "Pull request title generator",
     to: "/guides/pull-request-title-generator",
-    imageURL: "/Placeholder.png",
   },
 ];
