086f8e81ece3e0adcb20bebaf7656b6f6232fe3c
Michael Paradis <michael.p@getjobber.com>
2025-01-23 13:38:56 -0800

    Update useKeyboardNavigation to use a new API that can be reused in CustomMenus

--- packages/components/src/Autocomplete/useKeyboardNavigation.ts
+++ packages/components/src/Autocomplete/useKeyboardNavigation.ts
@@ -1,69 +1,49 @@
 import { useOnKeyDown } from "@jobber/hooks/useOnKeyDown";
-import { useEffect, useState } from "react";
-import { AnyOption, Option } from "./Autocomplete.types";
+import { AnyOption } from "./Autocomplete.types";
 import { isGroup } from "./Autocomplete.utils";
 
-enum IndexChange {
+export enum KeyboardAction {
   Previous = -1,
   Next = 1,
+  Select = 0,
 }
 
 export function useKeyboardNavigation({
-  options,
-  visible,
-  onOptionSelect,
-  onHighlightChange,
+  onRequestHighlightChange,
 }: {
-  options: AnyOption[];
-  visible: boolean;
-  onOptionSelect?: (option: Option) => void;
-  onHighlightChange?: (index: number) => void;
+  onRequestHighlightChange?: (
+    event: KeyboardEvent,
+    direction: KeyboardAction,
+  ) => void;
 }) {
-  const [highlightedIndex, setHighlightedIndex] = useState(0);
-  const detectGroups = (option: AnyOption) => option.options;
-
-  const initialHighlight = options.some(detectGroups) ? 1 : 0;
-  useEffect(() => setHighlightedIndex(initialHighlight), [options]);
-
-  useEffect(() => {
-    onHighlightChange?.(highlightedIndex);
-  }, [highlightedIndex]);
-
   useOnKeyDown((event: KeyboardEvent) => {
-    const indexChange = arrowKeyPress(event, IndexChange.Next);
-
-    if (indexChange) {
-      setHighlightedIndex(
-        Math.min(options.length - 1, highlightedIndex + indexChange),
-      );
-    }
+    onRequestHighlightChange?.(event, KeyboardAction.Next);
   }, "ArrowDown");
 
   useOnKeyDown((event: KeyboardEvent) => {
-    const indexChange = arrowKeyPress(event, IndexChange.Previous);
-
-    if (indexChange) {
-      setHighlightedIndex(Math.max(0, highlightedIndex + indexChange));
-    }
+    onRequestHighlightChange?.(event, KeyboardAction.Previous);
   }, "ArrowUp");
 
   useOnKeyDown((event: KeyboardEvent) => {
-    if (!visible) return;
-    if (isGroup(options[highlightedIndex])) return;
-
-    event.preventDefault();
-    onOptionSelect?.(options[highlightedIndex]);
+    onRequestHighlightChange?.(event, KeyboardAction.Select);
   }, "Enter");
+}
 
-  function arrowKeyPress(event: KeyboardEvent, direction: number) {
-    if (!visible) return;
-    event.preventDefault();
-    const requestedIndex = options[highlightedIndex + direction];
+export function getRequestedIndexChange<T extends AnyOption>({
+  event,
+  options,
+  direction,
+  highlightedIndex,
+}: {
+  event: KeyboardEvent;
+  direction: KeyboardAction;
+  options: T[];
+  highlightedIndex: number;
+}) {
+  event.preventDefault();
+  const requestedIndex = options[highlightedIndex + direction];
 
-    return requestedIndex && isGroup(requestedIndex)
-      ? direction + direction
-      : direction;
-  }
-
-  return { highlightedIndex, setHighlightedIndex };
+  return requestedIndex && isGroup(requestedIndex)
+    ? direction + direction
+    : direction;
 }
