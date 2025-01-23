f848c39b32469a4100e59d7ec326690cc020dc5a
Michael Paradis <michael.p@getjobber.com>
2025-01-23 14:11:11 -0800

    Update exports from autocomplete with the new functionality

--- packages/components/src/Autocomplete/index.ts
+++ packages/components/src/Autocomplete/index.ts
@@ -1,2 +1,24 @@
 export { Autocomplete } from "./Autocomplete";
-export * from "./Option";
+export {
+  MenuOptionProps,
+  BaseMenuOption,
+  BaseMenuOptionProps,
+  MenuOption,
+} from "./Option";
+export {
+  AnyOption,
+  AutocompleteProps,
+  BaseOption,
+  CustomOptionsMenuProp,
+  GroupOption,
+  OptionCollection,
+  Option,
+} from "./Autocomplete.types";
+export {
+  KeyboardAction,
+  getRequestedIndexChange as getRequestedIndex,
+  useKeyboardNavigation,
+} from "./useKeyboardNavigation";
+export { useRepositionMenu } from "./useRepositionMenu";
+export { MenuPopper } from "./Menu/MenuPopper";
+export { isOptionSelected, isGroup } from "./Autocomplete.utils";
