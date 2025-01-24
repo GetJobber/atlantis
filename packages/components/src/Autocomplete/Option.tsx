2c95667f9120d456078da04eadd18f71725d66b0
Michael Paradis <michael.p@getjobber.com>
2025-01-24 08:53:35 -0800

    Expose UNSAFE styling props in Options

--- packages/components/src/Autocomplete/Option.tsx
+++ packages/components/src/Autocomplete/Option.tsx
@@ -13,6 +13,16 @@ export interface MenuOptionProps {
   readonly onOptionSelect: (option: AnyOption) => void;
   readonly addSeparators: boolean;
   readonly isSelected: boolean;
+  readonly UNSAFE_className?: {
+    option?: BaseMenuOptionProps["UNSAFE_className"];
+    content?: MenuOptionContentProps["UNSAFE_className"];
+    groupOption?: MenuOptionGroupProps["UNSAFE_className"];
+  };
+  readonly UNSAFE_style?: {
+    option?: BaseMenuOptionProps["UNSAFE_style"];
+    content?: MenuOptionContentProps["UNSAFE_style"];
+    groupOption?: MenuOptionGroupProps["UNSAFE_style"];
+  };
 }
 
 export function MenuOption({
@@ -21,44 +31,79 @@ export function MenuOption({
   onOptionSelect,
   isSelected,
   addSeparators,
+  UNSAFE_className = {},
+  UNSAFE_style = {},
 }: MenuOptionProps) {
   if (isGroup(option)) {
-    return <MenuOptionGroup option={option} />;
+    return (
+      <MenuOptionGroup
+        UNSAFE_className={UNSAFE_className.groupOption}
+        option={option}
+        UNSAFE_style={UNSAFE_style.groupOption}
+      />
+    );
   }
 
   return (
     <BaseMenuOption
+      UNSAFE_className={UNSAFE_className.option}
+      UNSAFE_style={UNSAFE_style.option}
       option={option}
       isHighlighted={isHighlighted}
       onOptionSelect={onOptionSelect}
       addSeparators={addSeparators}
     >
-      <MenuOptionContent option={option} isSelected={isSelected} />
+      <MenuOptionContent
+        option={option}
+        isSelected={isSelected}
+        UNSAFE_className={UNSAFE_className.content}
+        UNSAFE_style={UNSAFE_style.content}
+      />
     </BaseMenuOption>
   );
+}
+interface MenuOptionContentProps {
+  readonly UNSAFE_className?: {
+    readonly icon?: string;
+    readonly text?: string;
+    readonly details?: string;
+    readonly label?: string;
+  };
+  readonly UNSAFE_style?: {
+    readonly icon?: React.CSSProperties;
+    readonly text?: React.CSSProperties;
+    readonly label?: React.CSSProperties;
+    readonly details?: React.CSSProperties;
+  };
+  readonly option: Option;
+  readonly isSelected: boolean;
 }
 
 export function MenuOptionContent({
   option,
   isSelected,
-}: {
-  readonly option: Option;
-  readonly isSelected: boolean;
-}) {
+  UNSAFE_className = {},
+  UNSAFE_style = {},
+}: MenuOptionContentProps) {
+  const iconClassName = classnames(styles.icon, UNSAFE_className.icon);
+  const textClassName = classnames(styles.text, UNSAFE_className.text);
+  const labelClassName = classnames(styles.label, UNSAFE_className.label);
+  const detailsClassName = classnames(styles.details, UNSAFE_className.details);
+
   return (
     <>
-      <div className={styles.icon}>
+      <div className={iconClassName} style={UNSAFE_style.icon}>
         {isSelected && <Icon name="checkmark" size="small" />}
       </div>
-      <div className={styles.text}>
-        <div className={styles.label}>
+      <div className={textClassName} style={UNSAFE_style.text}>
+        <div className={labelClassName} style={UNSAFE_style.label}>
           <Text>{option.label}</Text>
           {option.description !== undefined && (
             <Text variation="subdued">{option.description}</Text>
           )}
         </div>
         {option.details !== undefined && (
-          <div className={styles.details}>
+          <div className={detailsClassName} style={UNSAFE_style.details}>
             <Text>{option.details}</Text>
           </div>
         )}
@@ -67,9 +112,21 @@ export function MenuOptionContent({
   );
 }
 
-export function MenuOptionGroup({ option }: { readonly option: AnyOption }) {
+export interface MenuOptionGroupProps {
+  readonly option: AnyOption;
+  readonly UNSAFE_className?: { heading?: string };
+  readonly UNSAFE_style?: { heading?: React.CSSProperties };
+}
+
+export function MenuOptionGroup({
+  option,
+  UNSAFE_className = {},
+  UNSAFE_style = {},
+}: MenuOptionGroupProps) {
+  const headingClassName = classnames(styles.heading, UNSAFE_className.heading);
+
   return (
-    <div className={styles.heading}>
+    <div className={headingClassName} style={UNSAFE_style.heading}>
       <Heading level={5}>{option.label}</Heading>
     </div>
   );
@@ -82,6 +139,8 @@ export interface BaseMenuOptionProps<
   readonly onOptionSelect: (option: GenericOption) => void;
   readonly isHighlighted: boolean;
   readonly addSeparators: boolean;
+  readonly UNSAFE_className?: string;
+  readonly UNSAFE_style?: React.CSSProperties;
 }
 
 /**
@@ -93,16 +152,23 @@ export function BaseMenuOption<GenericOption extends AnyOption = AnyOption>({
   onOptionSelect,
   addSeparators,
   children,
+  UNSAFE_className = "",
+  UNSAFE_style = {},
 }: BaseMenuOptionProps<GenericOption>) {
-  const optionClass = classnames(styles.option, {
-    [styles.active]: isHighlighted,
-    [styles.separator]: addSeparators,
-  });
+  const optionClass = classnames(
+    styles.option,
+    {
+      [styles.active]: isHighlighted,
+      [styles.separator]: addSeparators,
+    },
+    UNSAFE_className,
+  );
 
   return (
     <button
       className={optionClass}
       onMouseDown={onOptionSelect.bind(undefined, option)}
+      style={UNSAFE_style}
     >
       {children}
     </button>
