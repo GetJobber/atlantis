311af58d46cbf73a87e2745ada027607ce2e8fe1
Taylor Noj <taylor.n@getjobber.com>
2025-01-28 20:21:47 -0500

    cna be done via stylesheet also

--- docs/components/Text/Mobile.stories.tsx
+++ docs/components/Text/Mobile.stories.tsx
@@ -2,6 +2,7 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
 import { Text } from "@jobber/components-native";
 import { tokens } from "@jobber/design";
+import { styles } from "./Text.style";
 
 export default {
   title: "Components/Text and Typography/Text/Mobile",
@@ -24,6 +25,7 @@ export const Basic = BasicTemplate.bind({});
 
 Basic.args = {
   children: "640K is more memory than anyone will ever need on a computer",
+  // inline styles
   UNSAFE_style: {
     textStyle: {
       color: tokens["color-purple--light"],
@@ -31,6 +33,8 @@ Basic.args = {
       letterSpacing: 3,
     },
   },
+  // stylesheet
+  // UNSAFE_style: { textStyle: styles.customText },
 };
 
 export const SupportingText = BasicTemplate.bind({});
