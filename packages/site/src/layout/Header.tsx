0f9d93ad3f3eced57d27026cfd0d32c78076fbbb
Edi Yang <edison.cy.yang@gmail.com>
2025-02-10 09:38:53 -0800

    make header responsive in minimal mode

--- packages/site/src/layout/Header.tsx
+++ packages/site/src/layout/Header.tsx
@@ -7,24 +7,36 @@ export const Header = () => {
   const { onOpenTriton } = useTritonChat();
 
   return (
-    <div
-      style={{
-        backgroundColor: "var(--color-surface--background)",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        padding: "var(--space-small)",
-      }}
+    <Box
+      padding="small"
+      direction="row"
+      alignItems="center"
+      justifyContent="space-between"
+      gap="base"
     >
-      <div style={{ flex: 1, display: "flex", justifyContent: "center" }}>
-        <Box width={385} direction="row" alignItems="center" gap={"base"}>
-          <Box width="100%">
-            <SearchButton />
+      <div
+        style={{
+          flex: "1 1 0%",
+          minWidth: 0,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            maxWidth: "385px",
+            width: "100%",
+          }}
+        >
+          <Box width="100%" direction="row" alignItems="center" gap="base">
+            <Box width="100%">
+              <SearchButton />
+            </Box>
+            <Button onClick={onOpenTriton} icon="sparkles" ariaLabel="triton" />
           </Box>
-          <Button onClick={onOpenTriton} icon="sparkles" ariaLabel="triton" />
-        </Box>
+        </div>
       </div>
       <ToggleThemeButton />
-    </div>
+    </Box>
   );
 };
