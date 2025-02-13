441b24180bd4bd0c7a43a98e9087088447dc41e0
Edi Yang <edison.cy.yang@gmail.com>
2025-02-13 14:42:07 -0800

    menu button and jobber logo in top nav below 768

--- packages/site/src/layout/TopNav.tsx
+++ packages/site/src/layout/TopNav.tsx
@@ -1,19 +1,46 @@
 import { Box, Button, Tooltip } from "@jobber/components";
+import { useBreakpoints } from "@jobber/hooks";
+import { Link } from "react-router-dom";
 import { SearchButton } from "./SearchButton";
+import styles from "./TopNav.module.css";
 import { ToggleThemeButton } from "../components/ToggleThemeButton";
 import { useTritonChat } from "../providers/TritonProvider";
+import { JobberLogo } from "../assets/JobberLogo.svg";
 
 export const TopNav = () => {
   const { onOpenTriton } = useTritonChat();
+  const { mediumAndUp } = useBreakpoints();
 
   return (
     <Box padding="base" direction="row" alignItems="center" gap="small">
+      <div className={styles.mobileNavContainer}>
+        <Box
+          direction="row"
+          gap="small"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Button
+            ariaLabel="Menu"
+            type="tertiary"
+            variation="subtle"
+            size="base"
+            onClick={() => alert("open menu")}
+            icon="menu"
+          />
+          <Box padding={{ top: "smaller" }}>
+            <Link to="/">
+              <JobberLogo />
+            </Link>
+          </Box>
+        </Box>
+      </div>
       <Box
         direction="row"
         gap="small"
         justifyContent="center"
         width="grow"
-        padding={{ left: "extravagant" }}
+        padding={mediumAndUp ? { left: "extravagant" } : {}}
       >
         <Box width={200}>
           <SearchButton />
