1c3085ff2fda058fc0e2f6438194638ba1352c27
Jacob Deichert <82835373+jdeichert@users.noreply.github.com>
2025-02-24 10:28:28 -0800

    feat(components-native): AtlantisThemeContext for mobile (#2387)
    * Add AtlantisThemeContext for components-native
    
    * Make Button component use theme tokens
    
    * Add todos
    
    * Handle changing theme globally
    
    * Add mobile story for AtlantisThemeContext
    
    * Add tests for mobile AtlantisThemeContextProvider
    
    * Update meta.json
    
    * Cleanup types
    
    * Undo Button changes
    
    * Update comment
    
    * Generate mobile props for AtlantisThemeContext
    
    * Show mobile props
    
    * Theme the mobile preview iframe
    
    * Add mobile example for AtlantisThemeContext
    
    * Update mobile snippet to override the theme
    
    * Fix lint
    
    * Rename function
    
    * Regen props files
    
    * Revert "Regen props files"
    
    This reverts commit 556789c2664ac4a1f9e767fc9dbbedae4264e447.
    
    * Note differences between web and mobile
    
    * Tweak wording

--- packages/components-native/src/utils/design/index.ts
+++ packages/components-native/src/utils/design/index.ts
@@ -1,6 +1,11 @@
 import { Platform } from "react-native";
 import { androidTokens, iosTokens } from "@jobber/design";
 
+/**
+ * These design tokens don't react to theme changes. Only use these if you need to access
+ * tokens that are not affected by the current theme, otherwise you should be using our
+ * useAtlantisTheme() hook to access the current theme's tokens.
+ */
 export const tokens: typeof iosTokens = Platform.select({
   ios: () => iosTokens,
   android: () => androidTokens,
