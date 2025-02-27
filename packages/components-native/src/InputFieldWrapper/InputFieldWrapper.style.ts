3b483b40401d09fccc7488c42c9131c88fe742ac
Aiden-Brine <aidenbrine@gmail.com>
2025-02-26 18:14:06 -0800

    Add back newline

--- packages/components-native/src/InputFieldWrapper/InputFieldWrapper.style.ts
+++ packages/components-native/src/InputFieldWrapper/InputFieldWrapper.style.ts
@@ -14,6 +14,7 @@ export const useStyles = buildThemedStyles(tokens => {
         flexDirection: "column",
       },
     ]),
+
     field: {
       flexDirection: "row",
       alignItems: "center",
