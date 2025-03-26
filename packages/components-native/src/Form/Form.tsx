f11b248bb96d19073e12714b352ab37b4f67d260
David Riediger <david.r@getjobber.com>
2025-03-26 11:13:11 -0600

    Move on submit success to after the form is successfully submited

--- packages/components-native/src/Form/Form.tsx
+++ packages/components-native/src/Form/Form.tsx
@@ -152,7 +152,7 @@ function InternalForm<T extends FieldValues, S>({
 
         <FormBody
           keyboardHeight={calculateSaveButtonOffset()}
-          submit={handleSubmit(internalSubmit)}
+          submit={callHandleSubmit}
           isFormSubmitting={isSubmitting}
           saveButtonLabel={saveButtonLabel}
           shouldRenderActionBar={saveButtonPosition === "sticky"}
@@ -195,13 +195,13 @@ function InternalForm<T extends FieldValues, S>({
                     <View style={styles.fixedSaveButton}>
                       {renderStickySection ? (
                         renderStickySection(
-                          handleSubmit(internalSubmit),
+                          callHandleSubmit,
                           saveButtonLabel,
                           isSubmitting,
                         )
                       ) : (
                         <FormSaveButton
-                          primaryAction={handleSubmit(internalSubmit)}
+                          primaryAction={callHandleSubmit}
                           label={saveButtonLabel}
                           loading={isSubmitting}
                           secondaryActions={secondaryActions}
@@ -247,6 +247,19 @@ function InternalForm<T extends FieldValues, S>({
     setKeyboardScreenY(0);
   }
 
+  function callHandleSubmit() {
+    let result: S;
+
+    handleSubmit(data => {
+      internalSubmit(data).then(saveResult => {
+        result = saveResult as S;
+      })
+    })().then(() => {
+          removeListenerRef.current?.();
+          onSubmitSuccess(result);
+        });
+  }
+
   async function internalSubmit(data: FormValues<T>) {
     let performSubmit = true;
 
@@ -258,10 +271,6 @@ function InternalForm<T extends FieldValues, S>({
       Keyboard.dismiss();
 
       return onSubmit(data)
-        .then((result: S) => {
-          removeListenerRef.current?.();
-          onSubmitSuccess(result);
-        })
         .catch(handleSubmitCatch);
     }
   }
@@ -289,7 +298,7 @@ function InternalForm<T extends FieldValues, S>({
   function handleRetry() {
     clearFormErrors();
 
-    return handleSubmit(internalSubmit)();
+    return callHandleSubmit();
   }
 
   function calculateSaveButtonOffset() {
