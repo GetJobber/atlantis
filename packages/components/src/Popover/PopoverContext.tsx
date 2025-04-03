81709e8468234bab8c9148a8f31a20194cc22d33
Jacob Deichert <82835373+jdeichert@users.noreply.github.com>
2025-04-03 11:35:17 -0700

    feat(components): Popover composability (#2451)
    * provider sort of working but not really
    
    * props functional
    
    * provider working mostly. hover state and click not functional on action. I think just some props to be passed?
    
    * provider
    
    * Regen meta.json
    
    * Restore recently added portal behaviour
    
    * Add AtlantisThemedPortal helper component
    
    * Use AtlantisThemedPortal inside Popover.Wrapper
    
    * Move popover context to its own file
    
    * Don't need to check for open
    
    Popover.Provider returns null if not open
    
    * Add PopoverDismissWrapperProps
    
    * Remove Popover.Wrapper
    
    Moved logic into Popover.Provider
    
    * Adjust UNSAFE props and usePopoverStyles
    
    Reduces stuff being passed around in context
    
    * Change Popover.DismissWrapper to Popover.DismissButton
    
    By default it renders ButtonDismiss, but can also be configured with a custom child the consumer supplies.
    
    * Don't export unnecessary things
    
    * Make type more explicit
    
    * Regen files
    
    * Split into PopoverWrapper
    
    This allows `usePopoverStyles` to inherit properties from context in the future at the `PopoverWrapper` has access to `PopoverContext` values.
    
    * Rename AtlantisThemedPortal to AtlantisPortalContent
    
    This gives us the ability to extend this wrapper in the future with other context if necessary.
    
    Also gives back control of ReactDOM.createPortal to the consumer.
    
    * Add composed story for Popover
    
    * Remove theme provider test
    
    * Simplify story
    
    * Add composable usage notes for Popover
    
    * Export prop types
    
    * Add tests for composable Popover
    
    * Change buildModifiers to useMemo
    
    * Update test ids to follow our pattern
    
    * Rename type files
    
    * Regen props
    
    * Add force theme story
    
    * Simplify dismiss button props
    
    * Regen props
    
    ---------
    
    Co-authored-by: Scott Thompson <145565743+scotttjob@users.noreply.github.com>

---
+++ packages/components/src/Popover/PopoverContext.tsx
@@ -0,0 +1,97 @@
+import React, { CSSProperties, createContext, useContext } from "react";
+import classnames from "classnames";
+import ReactDOM from "react-dom";
+import { PopoverProviderProps } from "./Popover.types";
+import { usePopover } from "./usePopover";
+import { usePopoverStyles } from "./usePopoverStyles";
+import { AtlantisPortalContent } from "../AtlantisPortalContent";
+
+interface PopoverContextProps {
+  setArrowElement: (element: HTMLElement | null) => void;
+  popperStyles: { [key: string]: CSSProperties };
+}
+
+const PopoverContext = createContext<PopoverContextProps>({
+  popperStyles: {},
+  setArrowElement: () => {
+    // noop
+  },
+});
+
+export function usePopoverContext() {
+  return useContext(PopoverContext);
+}
+
+export function PopoverProvider({
+  children,
+  preferredPlacement,
+  attachTo,
+  open,
+  UNSAFE_className,
+  UNSAFE_style,
+}: PopoverProviderProps) {
+  const { setPopperElement, setArrowElement, popperStyles, attributes } =
+    usePopover({
+      preferredPlacement,
+      attachTo,
+      open,
+    });
+
+  if (!open) return null;
+
+  return (
+    <PopoverContext.Provider
+      value={{
+        setArrowElement,
+        popperStyles,
+      }}
+    >
+      <PopoverWrapper
+        attributes={attributes}
+        UNSAFE_className={UNSAFE_className}
+        UNSAFE_style={UNSAFE_style}
+        setPopperElement={setPopperElement}
+      >
+        {children}
+      </PopoverWrapper>
+    </PopoverContext.Provider>
+  );
+}
+
+function PopoverWrapper({
+  children,
+  attributes,
+  setPopperElement,
+  UNSAFE_className,
+  UNSAFE_style,
+}: {
+  readonly children: React.ReactNode;
+  readonly attributes: Record<string, { [key: string]: string } | undefined>;
+  readonly setPopperElement: (element: HTMLElement | null) => void;
+} & Pick<PopoverProviderProps, "UNSAFE_className" | "UNSAFE_style">) {
+  const popoverStyles = usePopoverStyles();
+  const { popperStyles } = usePopoverContext();
+
+  const classes = classnames(
+    popoverStyles.container,
+    UNSAFE_className?.container,
+  );
+
+  const content = (
+    <AtlantisPortalContent>
+      <div
+        role="dialog"
+        data-elevation="elevated"
+        ref={setPopperElement}
+        style={{ ...popperStyles.popper, ...UNSAFE_style?.container }}
+        className={classes}
+        {...attributes.popper}
+        data-testid="ATL-Popover-Container"
+      >
+        {children}
+      </div>
+    </AtlantisPortalContent>
+  );
+
+  return ReactDOM.createPortal(content, document.body);
+}
