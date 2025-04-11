ed4052085beabb04b84856a6987764ccb36cfc81
Edi Yang <edison.cy.yang@gmail.com>
2025-04-11 16:50:53 -0700

    props for Select rebuilt

---
+++ packages/components/src/Select/Select.types.ts
@@ -0,0 +1,50 @@
+import { Clearable } from "@jobber/hooks";
+import { FormFieldProps } from "../FormField";
+
+/**
+ * Rebuilt version of the Select component without React Hook Form dependency.
+ */
+export interface SelectRebuiltProps
+  extends Omit<
+    React.SelectHTMLAttributes<HTMLSelectElement>,
+    | "onChange"
+    | "onBlur"
+    | "size"
+    | "value"
+    | "defaultValue"
+    | "prefix"
+    | "suffix"
+  > {
+  readonly error?: string;
+  readonly invalid?: boolean;
+  readonly identifier?: string;
+  readonly loading?: boolean;
+  readonly children?: React.ReactNode;
+  readonly clearable?: Clearable;
+
+  /**
+   * Version 2 designates the rebuilt component without RHF dependency
+   */
+  readonly version: 2;
+
+  readonly onChange?: (
+    newValue: string,
+    event?: React.ChangeEvent<HTMLSelectElement>,
+  ) => void;
+
+  readonly onBlur?: FormFieldProps["onBlur"];
+  readonly onFocus?: FormFieldProps["onFocus"];
+  readonly value?: string;
+  readonly defaultValue?: string;
+
+  readonly size?: FormFieldProps["size"];
+  readonly inline?: FormFieldProps["inline"];
+  readonly align?: FormFieldProps["align"];
+
+  readonly prefix?: FormFieldProps["prefix"];
+  readonly suffix?: FormFieldProps["suffix"];
+  readonly description?: FormFieldProps["description"];
+  readonly disabled?: boolean;
+  readonly readonly?: boolean;
+  readonly autofocus?: boolean;
+}
