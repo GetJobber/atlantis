2711c8da9afb53f33a3f900cf5b9764e26f52aba
Edi Yang <edison.cy.yang@gmail.com>
2025-04-11 16:51:39 -0700

    extract select actions out from FormField

---
+++ packages/components/src/Select/hooks/useSelectActions.ts
@@ -0,0 +1,47 @@
+import { ChangeEvent, RefObject } from "react";
+
+interface UseSelectActionsProps {
+  readonly onChange?: (
+    newValue: string,
+    event?: ChangeEvent<HTMLSelectElement>,
+  ) => void;
+  readonly onBlur?: () => void;
+  readonly onFocus?: () => void;
+  readonly selectRef: RefObject<HTMLSelectElement>;
+}
+
+/**
+ * Hook with actions on the Select component.
+ */
+export function useSelectActions({
+  onChange,
+  onBlur,
+  onFocus,
+  selectRef,
+}: UseSelectActionsProps) {
+  function handleChange(event: ChangeEvent<HTMLSelectElement>) {
+    const newValue = event.currentTarget.value;
+    onChange?.(newValue, event);
+  }
+
+  function handleBlur() {
+    onBlur?.();
+  }
+
+  function handleFocus() {
+    onFocus?.();
+  }
+
+  function handleClear() {
+    handleBlur();
+    onChange?.("");
+    selectRef?.current?.focus();
+  }
+
+  return {
+    handleChange,
+    handleBlur,
+    handleFocus,
+    handleClear,
+  };
+}
