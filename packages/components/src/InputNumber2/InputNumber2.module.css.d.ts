30aeb2c2b7aa682f0f93d164d995bd27f0f20716
Jeff Marvin <jeff.m@getjobber.com>
2025-04-15 06:46:41 -0600

    power input number with RAC

--- packages/components/src/InputNumber2/InputNumber2.module.css.d.ts
+++
@@ -1,6 +0,0 @@
-declare const styles: {
-  readonly "inputNumber2": string;
-  readonly "bold": string;
-};
-export = styles;
-
--- packages/components/src/InputNumber2/InputNumber2.tsx
+++ packages/components/src/InputNumber2/InputNumber2.tsx
@@ -1,35 +1,40 @@
 import React from "react";
-import classnames from "classnames";
-import styles from "./InputNumber2.module.css";
+import {
+  NumberField as AriaNumberField,
+  // NumberFieldProps as AriaNumberFieldProps,
+  // FieldError,
+  // Group,
+  Input,
+  // Label,
+  // Text,
+  // ValidationResult,
+} from "react-aria-components";
+// import classnames from "classnames";
+import { InputNumberRebuiltProps } from "./InputNumber2.types";
+import styles from "../FormField/FormField.module.css";
+// import { useIsSafari } from "../FormField/hooks/useIsSafari";
+// import {
+//   LabelPadding,
+//   useFormFieldWrapperStylesProps,
+// } from "../FormField/hooks/useFormFieldWrapperStyles";
 
-export interface InputNumber2Props {
-  /**
-   * Styles the text bold and uppercased
-   * @default false
-   */
-  readonly loud?: boolean;
-
-  /**
-   * Text to display.
-   */
-  readonly text: string;
-
-  /**
-   * Click handler.
-   */
-  onClick?(event: React.MouseEvent<HTMLDivElement>): void;
-}
-
-export function InputNumber2({
-  loud = false,
-  text,
-  onClick,
-}: InputNumber2Props) {
-  const className = classnames(styles.inputNumber2, { [styles.bold]: loud });
+export function InputNumber2(props: InputNumberRebuiltProps) {
+  const ariaNumberFieldProps = {
+    ...props,
+  };
 
   return (
-    <div className={className} onClick={onClick}>
-      {text}
+    <div className={styles.container}>
+      <AriaNumberField {...ariaNumberFieldProps} className={styles.wrapper}>
+        {/* <Label>{placeholder}</Label> */}
+        {/* <Group> */}
+        <Input className={styles.input} />
+        {/* <Button slot="decrement">-</Button> */}
+        {/* <Button slot="increment">+</Button> */}
+        {/* </Group> */}
+        {/* {description && <Text slot="description">{description}</Text>} */}
+        {/* <FieldError>{error}</FieldError> */}
+      </AriaNumberField>
     </div>
   );
 }
