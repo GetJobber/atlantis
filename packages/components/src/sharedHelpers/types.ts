837885b7a8f2cce3f1359c86a30929d087e08f7a
Scott Thompson <145565743+scotttjob@users.noreply.github.com>
2025-04-15 10:37:31 -0300

    making types a bit more specific and adding js doc comments for them

--- packages/components/src/sharedHelpers/types.ts
+++ packages/components/src/sharedHelpers/types.ts
@@ -1,7 +1,11 @@
 export interface CommonAtlantisProps {
+  /** Standard HTML data attributes. Accepts anything in a {{"data-key":"value"}} format. */
   data?: { [key: `data-${string}`]: string };
+  /** Standard HTML aria attributes. Accepts all standard HTML aria attributes. */
   aria?: React.AriaAttributes;
-  role?: string;
+  /** Standard HTML role attribute. */
+  role?: React.AriaRole;
+  /** Standard HTML id attribute. */
   id?: string;
 }
 export type CommonAllowedElements =
