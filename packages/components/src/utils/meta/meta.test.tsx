64f37090e7730043fa3a2c39fc06df32c6454716
Zak <ZakaryH@users.noreply.github.com>
2025-04-17 09:43:19 -0700

    Revert "feat(components): Add ref to Button and Chip (#2470)"
    This reverts commit 81dbed31f37e85a5d0450ae8e94e52c8e4844d0d.

--- packages/components/src/utils/meta/meta.test.tsx
+++ packages/components/src/utils/meta/meta.test.tsx
@@ -5,7 +5,6 @@ describe("meta", () => {
   // If this test fails, please update meta.json accordingly.
   it("verifies that the meta.json file is up to date", async () => {
     const meta = await fs.readFile(`${__dirname}/meta.json`, "utf-8");
-
     const allNames = findComponentNamesDeep(allExports);
     allNames.sort();
 
@@ -74,10 +73,6 @@ function findComponentNamesDeep(objectOrFunction: any, name?: string) {
     } else if (isForwardedRef(k, v) || isMemoizedComponent(k, v)) {
       const thisName = name ? `${name}.${k}` : k;
       allNames.push(thisName);
-
-      // Also check for child components in forwardRef and memoized components
-      const childComponents = findComponentNamesDeep(v, k);
-      allNames.push(...childComponents);
     } else if (isComponent(k, v)) {
       const thisName = name ? `${name}.${k}` : k;
       allNames.push(thisName);
