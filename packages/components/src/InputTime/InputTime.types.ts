7955db3b698276e8a7ce10cc91032cbaac91c822
ZakaryH <zakary.h@getjobber.com>
2025-04-22 13:59:04 -0700

    exclude defaultValue properly from v2

--- packages/components/src/InputTime/InputTime.types.ts
+++ packages/components/src/InputTime/InputTime.types.ts
@@ -43,10 +43,10 @@ export interface InputTimeProps
    * Function called when user changes input value.
    */
   onChange?(newValue?: Date): void;
-  /**
-   * The version of the component.
-   */
-  version?: 1 | 2;
+}
+
+export interface InputTimeLegacyProps extends InputTimeProps {
+  version?: 1;
 }
 
 export interface InputTimeRebuiltProps
