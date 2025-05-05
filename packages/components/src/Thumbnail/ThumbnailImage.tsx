b7f3a74532257100d2117218a2421e14601695c8
Timotei Albu <timotei.albu@gmail.com>
2025-05-05 14:29:22 -0600

    pass along the unsafe style and className to the ThumbnailImage

--- packages/components/src/Thumbnail/ThumbnailImage.tsx
+++ packages/components/src/Thumbnail/ThumbnailImage.tsx
@@ -6,9 +6,31 @@ import { Glimmer } from "../Glimmer";
 
 interface InternalThumbnailImageProps {
   readonly file: FileUpload;
+
+  /**
+   * **Use at your own risk:** Custom classNames for specific elements. This should only be used as a
+   * **last resort**. Using this may result in unexpected side effects.
+   * More information in the [Customizing components Guide](https://atlantis.getjobber.com/guides/customizing-components).
+   */
+  readonly UNSAFE_className?: {
+    container?: string;
+  };
+
+  /**
+   * **Use at your own risk:** Custom classNames for specific elements. This should only be used as a
+   * **last resort**. Using this may result in unexpected side effects.
+   * More information in the [Customizing components Guide](https://atlantis.getjobber.com/guides/customizing-components).
+   */
+  readonly UNSAFE_style?: {
+    container?: React.CSSProperties;
+  };
 }
 
-export function InternalThumbnailImage({ file }: InternalThumbnailImageProps) {
+export function InternalThumbnailImage({
+  file,
+  UNSAFE_className,
+  UNSAFE_style,
+}: InternalThumbnailImageProps) {
   const { name, src } = file;
   const [imageSource, setImageSource] = useState<string>();
   const [imageLoaded, setImageLoaded] = useState<boolean>(false);
@@ -27,7 +49,12 @@ export function InternalThumbnailImage({ file }: InternalThumbnailImageProps) {
         onLoad={handleImageLoad}
         alt={name}
         data-testid="internalThumbnailImage"
-        className={classNames(styles.image, { [styles.hidden]: !imageLoaded })}
+        className={classNames(
+          styles.image,
+          { [styles.hidden]: !imageLoaded },
+          UNSAFE_className?.container,
+        )}
+        style={UNSAFE_style?.container}
       />
     </>
   );
