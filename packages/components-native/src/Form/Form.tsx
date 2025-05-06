288745068c78774ec580f968bb469a5b1790ce46
ZakaryH <zakary.h@getjobber.com>
2025-05-06 14:16:34 -0700

    throwaway commits to trigger builds

--- packages/components-native/src/Form/Form.tsx
+++ packages/components-native/src/Form/Form.tsx
@@ -138,7 +138,7 @@ function InternalForm<T extends FieldValues, S>({
 
   const styles = useStyles();
 
-  // Comment to trigger build once more
+  // Comment to trigger build once more and again
   return (
     <FormProvider {...formMethods}>
       <>
--- packages/components/src/ConfirmationModal/ConfirmationModal.tsx
+++ packages/components/src/ConfirmationModal/ConfirmationModal.tsx
@@ -163,7 +163,7 @@ type ConfirmationModalProps =
   | (SimpleConfirmationModalProps & (StringMessage | ChildrenMessage))
   | (ComplexConfirmationModalProps & (StringMessage | ChildrenMessage));
 
-// Comment to trigger build once more
+// Comment to trigger build once more and again
 export const ConfirmationModal = forwardRef(function ConfirmationModalInternal(
   {
     title,
