94ed824b95f30e4f0943efa6bf951097bb6cd97b
Jeff Marvin <jeff.m@getjobber.com>
2025-05-12 08:36:32 -0600

    add and demo formatting options

--- packages/components/src/InputNumber2/InputNumber2.types.tsx
+++ packages/components/src/InputNumber2/InputNumber2.types.tsx
@@ -7,20 +7,7 @@ export interface InputNumber2Props
   extends CommonFormFieldProps,
     Pick<
       FormFieldProps,
-      // | "maxLength"
-      // | "autocomplete"
-      // | "max"
-      // | "min"
-      | "onFocus"
-      | "onBlur"
-      | "inputRef"
-      // | "validations"
-      | "readonly"
-      | "size"
-      // | "defaultValue"
-      // | "keyboard"
-      // | "prefix"
-      // | "suffix"
+      "onFocus" | "onBlur" | "inputRef" | "readonly" | "size"
     > {
   readonly align?: "center" | "right"; // todo add left and make it default
   readonly autocomplete?: boolean;
@@ -28,6 +15,7 @@ export interface InputNumber2Props
   readonly defaultValue?: number;
   readonly description?: string;
   readonly error?: string;
+  readonly formatOptions?: Intl.NumberFormatOptions;
   readonly identifier?: string;
   readonly inline?: boolean;
   readonly invalid?: boolean;
@@ -43,60 +31,3 @@ export interface InputNumber2Ref {
   focus: () => void;
   blur: () => void;
 }
-
-/**
- * Experimental version 2 of the InputNumber component.
- * Do not use unless you have talked with Atlantis first.
- */
-
-// export interface InputNumberRebuiltProps
-//   extends Omit<
-//     React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement>,
-//     | "onChange"
-//     | "onBlur"
-//     | "maxLength"
-//     | "rows"
-//     | "size"
-//     | "suffix"
-//     | "prefix"
-//     | "value"
-//     | "max"
-//     | "min"
-//     | "defaultValue"
-//   > {
-//   readonly error?: string;
-
-//   readonly invalid?: boolean;
-//   readonly identifier?: string;
-//   // readonly autocomplete?: boolean | AutocompleteTypes;
-//   readonly loading?: boolean;
-//   readonly onKeyUp?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
-//   readonly children?: React.ReactNode;
-//   // readonly clearable?: Clearable;
-//   /**
-//    * Version 2 is highly experimental. Avoid using it unless you have talked with Atlantis first.
-//    */
-//   readonly version: 2;
-
-//   readonly onChange?: (
-//     newValue: number,
-//     event?: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-//   ) => void;
-//   readonly onEnter?: FormFieldProps["onEnter"];
-
-//   readonly onBlur?: FormFieldProps["onBlur"];
-//   readonly value: number;
-
-//   readonly maxLength?: number;
-
-//   readonly size?: FormFieldProps["size"];
-//   readonly inline?: FormFieldProps["inline"];
-//   readonly align?: FormFieldProps["align"];
-
-//   readonly toolbar?: FormFieldProps["toolbar"];
-//   readonly toolbarVisibility?: FormFieldProps["toolbarVisibility"];
-
-//   readonly prefix?: FormFieldProps["prefix"];
-//   readonly suffix?: FormFieldProps["suffix"];
-//   readonly description?: FormFieldProps["description"];
-// }
