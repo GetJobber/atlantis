d2d2a32eb93501ca2783fe86b940b5901aa9858d
Jeff Marvin <jeff.m@getjobber.com>
2025-05-12 07:52:36 -0600

    minor css cleanup

--- packages/components/src/InputNumber2/InputNumber2.module.css.d.ts
+++ packages/components/src/InputNumber2/InputNumber2.module.css.d.ts
@@ -1,21 +1,20 @@
 declare const styles: {
-  readonly "container": string;
-  readonly "inline": string;
-  readonly "wrapper": string;
-  readonly "horizontalWrapper": string;
-  readonly "invalid": string;
-  readonly "disabled": string;
-  readonly "small": string;
-  readonly "large": string;
   readonly "center": string;
-  readonly "right": string;
-  readonly "inputWrapper": string;
   readonly "childrenWrapper": string;
-  readonly "input": string;
-  readonly "label": string;
-  readonly "hideLabel": string;
+  readonly "container": string;
   readonly "description": string;
+  readonly "disabled": string;
   readonly "fieldError": string;
+  readonly "hideLabel": string;
+  readonly "horizontalWrapper": string;
+  readonly "inline": string;
+  readonly "input": string;
+  readonly "inputWrapper": string;
+  readonly "invalid": string;
+  readonly "label": string;
+  readonly "large": string;
+  readonly "right": string;
+  readonly "small": string;
+  readonly "wrapper": string;
 };
 export = styles;
-
