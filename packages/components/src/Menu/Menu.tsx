227ff4421156ad8237c2482556ebf9fb99160ae9
Zak <ZakaryH@users.noreply.github.com>
2025-07-04 15:31:35 -0700

    feat(components): Add UNSAFE Props to Menu (#2611)
    * add UNSAFE props to Menu
    
    * update site types, and create implement tab content
    
    * improve appearance of custom example
    
    * use actual name of property, remove webstory

--- packages/components/src/Menu/Menu.tsx
+++ packages/components/src/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import React, {
+  CSSProperties,
   MouseEvent,
   ReactElement,
   RefObject,
@@ -47,6 +48,28 @@ export interface MenuProps {
    * Collection of action items.
    */
   readonly items: SectionProps[];
+
+  /**
+   * **Use at your own risk:** Custom class names for specific elements. This should only be used as a
+   * **last resort**. Using this may result in unexpected side effects.
+   * More information in the [Customizing components Guide](https://atlantis.getjobber.com/guides/customizing-components).
+   */
+  readonly UNSAFE_className?: {
+    menu?: string;
+    header?: string;
+    action?: string;
+  };
+
+  /**
+   * **Use at your own risk:** Custom style for specific elements. This should only be used as a
+   * **last resort**. Using this may result in unexpected side effects.
+   * More information in the [Customizing components Guide](https://atlantis.getjobber.com/guides/customizing-components).
+   */
+  readonly UNSAFE_style?: {
+    menu?: CSSProperties;
+    header?: CSSProperties;
+    action?: CSSProperties;
+  };
 }
 
 export interface SectionProps {
@@ -62,7 +85,12 @@ export interface SectionProps {
 }
 
 // eslint-disable-next-line max-statements
-export function Menu({ activator, items }: MenuProps) {
+export function Menu({
+  activator,
+  items,
+  UNSAFE_className,
+  UNSAFE_style,
+}: MenuProps) {
   const [visible, setVisible] = useState(false);
   const popperRef = useRef<HTMLDivElement>(null);
 
@@ -160,7 +188,7 @@ export function Menu({ activator, items }: MenuProps) {
               >
                 {items.length > 0 && (
                   <motion.div
-                    className={styles.menu}
+                    className={classnames(styles.menu, UNSAFE_className?.menu)}
                     role="menu"
                     data-elevation={"elevated"}
                     aria-labelledby={buttonID}
@@ -176,13 +204,22 @@ export function Menu({ activator, items }: MenuProps) {
                       type: "tween",
                       duration: 0.25,
                     }}
+                    style={UNSAFE_style?.menu}
                   >
                     {items.map((item, key: number) => (
                       <div key={key} className={styles.section}>
-                        {item.header && <SectionHeader text={item.header} />}
+                        {item.header && (
+                          <SectionHeader
+                            text={item.header}
+                            UNSAFE_style={UNSAFE_style?.header}
+                            UNSAFE_className={UNSAFE_className?.header}
+                          />
+                        )}
 
                         {item.actions.map(action => (
                           <Action
+                            UNSAFE_style={UNSAFE_style?.action}
+                            UNSAFE_className={UNSAFE_className?.action}
                             sectionLabel={item.header}
                             key={action.label}
                             {...action}
@@ -231,11 +268,21 @@ export function Menu({ activator, items }: MenuProps) {
 
 interface SectionHeaderProps {
   readonly text: string;
+  readonly UNSAFE_style?: CSSProperties;
+  readonly UNSAFE_className?: string;
 }
 
-function SectionHeader({ text }: SectionHeaderProps) {
+function SectionHeader({
+  text,
+  UNSAFE_style,
+  UNSAFE_className,
+}: SectionHeaderProps) {
   return (
-    <div className={styles.sectionHeader} aria-hidden={true}>
+    <div
+      className={classnames(styles.sectionHeader, UNSAFE_className)}
+      aria-hidden={true}
+      style={UNSAFE_style}
+    >
       <Typography
         element="h6"
         size="base"
@@ -276,6 +323,16 @@ export interface ActionProps {
   readonly destructive?: boolean;
 
   /**
+   * Inline style overrides for the action button
+   */
+  readonly UNSAFE_style?: CSSProperties;
+
+  /**
+   * Style class overrides for the action button
+   */
+  readonly UNSAFE_className?: string;
+
+  /**
    * Callback when an action gets clicked
    */
   onClick?(event: React.MouseEvent<HTMLButtonElement>): void;
@@ -287,6 +344,8 @@ function Action({
   icon,
   iconColor,
   destructive,
+  UNSAFE_style,
+  UNSAFE_className,
   onClick,
 }: ActionProps) {
   const actionButtonRef = useRef() as RefObject<HTMLButtonElement>;
@@ -298,10 +357,11 @@ function Action({
     <button
       role="menuitem"
       type="button"
-      className={buttonClasses}
+      className={classnames(buttonClasses, UNSAFE_className)}
       key={label}
       onClick={onClick}
       ref={actionButtonRef}
+      style={UNSAFE_style}
     >
       {icon && (
         <div>
