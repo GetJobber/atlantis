4ba0f4accac8565b24c5e372e956b653de034377
ZakaryH <zakary.h@getjobber.com>
2025-07-14 14:11:27 -0700

    adjust function memoization to avoid setting ref in render path

--- packages/hooks/src/useDebounce/useDebounce.ts
+++ packages/hooks/src/useDebounce/useDebounce.ts
@@ -1,5 +1,6 @@
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useMemo } from "react";
 import debounce from "lodash/debounce";
+import { useCallbackRef } from "../useCallbackRef";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type AnyFunction = (...args: any[]) => any;
@@ -16,27 +17,15 @@ export function useDebounce<T extends AnyFunction>(
   wait: number,
   options?: Parameters<typeof debounce>[2],
 ) {
-  const funcRef = useRef(func);
-
-  // We're keeping the provided func wrapped in a ref to avoid forcing the consumer to memoize it.
-  // This is a defense against consumers who aren't memoizing their functions.. or if they are
-  // memoized but invalidating too frequently due to possible bugs.
-  if (funcRef.current !== func) {
-    funcRef.current = func;
-  }
+  const funcRef = useCallbackRef(func);
 
   const debounced = useMemo(() => {
     return debounce(
-      (...args: Parameters<T>) => funcRef.current(...args),
+      (...args: Parameters<T>) => funcRef(...args),
       wait,
       options,
     );
-    // Note: do not add any dependencies! There is currently no use case where we would change
-    // the wait delay or options between renders. By not tracking as dependencies, this allows
-    // consumers of useDebounce to provide a raw object for options rather than forcing them to
-    // memoize that param. Same with the func they provide, we're using a ref to keep that up
-    // to date and to avoid forcing the consumer to memoize it.
-  }, []);
+  }, [funcRef, wait, options]);
 
   useEffect(() => {
     // If the debounced function is recreated (or unmounted), cancel the last call.
