d6c7659d75574b3f7b1d838fe5a2c504ff9e83bb
Jacob Deichert <jacob.d@getjobber.com>
2025-07-24 15:04:52 -0700

    Update Banner Provider to include BannerWrapper internally

--- packages/components/src/Banner/BannerProvider.tsx
+++ packages/components/src/Banner/BannerProvider.tsx
@@ -1,13 +1,40 @@
-import React, { createContext, useContext, useState } from "react";
+import React, {
+  PropsWithChildren,
+  createContext,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
+import { useResizeObserver } from "@jobber/hooks/useResizeObserver";
+import classnames from "classnames";
 import { BannerType } from "./Banner.types";
+import styles from "./Banner.module.css";
 
 interface BannerProviderProps {
+  /**
+   * Sets the status-based theme of the Banner
+   */
   readonly type: BannerType;
+
+  /**
+   * When provided, Banner's visibility is controlled by this value
+   */
+  readonly visible?: boolean;
 }
 
 interface BannerContextValue {
+  /**
+   * The status-based theme of the Banner
+   */
   readonly type: BannerType;
+  /**
+   * Whether the Banner is currently visible
+   */
   readonly isVisible: boolean;
+
+  /**
+   * Sets the visibility of the Banner
+   */
   readonly setIsVisible: (visible: boolean) => void;
 }
 
@@ -25,21 +52,63 @@ export function useBanner() {
 
 export function BannerProvider({
   children,
-  ...providerProps
+  visible,
+  type,
 }: {
   readonly children: React.ReactNode;
 } & BannerProviderProps) {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, _setIsVisible] = useState(true);
+  const showBanner = visible ?? isVisible;
+
+  const setIsVisible = useCallback(
+    (newValue: boolean) => {
+      // Only update internal visibility if it's not controlled by the parent.
+      if (typeof visible === "undefined") {
+        _setIsVisible(newValue);
+      }
+    },
+    [visible],
+  );
 
   return (
     <BannerContext.Provider
       value={{
-        ...providerProps,
-        isVisible,
+        type,
+        isVisible: showBanner,
         setIsVisible,
       }}
     >
-      {children}
+      <BannerWrapper>{children}</BannerWrapper>
     </BannerContext.Provider>
+  );
+}
+
+function BannerWrapper({ children }: PropsWithChildren) {
+  const { isVisible, type } = useBanner();
+
+  const bannerWidths = {
+    small: 320,
+    medium: 480,
+  };
+
+  const [bannerRef, { width: bannerWidth = bannerWidths.small }] =
+    useResizeObserver<HTMLDivElement>({
+      widths: bannerWidths,
+    });
+
+  const bannerClassNames = classnames(styles.banner, [styles[type]], {
+    [styles.medium]: bannerWidth >= bannerWidths.medium,
+  });
+
+  if (!isVisible) return null;
+
+  return (
+    <div
+      ref={bannerRef}
+      className={bannerClassNames}
+      role={type === "error" ? "alert" : "status"}
+    >
+      {children}
+    </div>
   );
 }
