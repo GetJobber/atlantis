df17108036ff4cbc542143fe6f063df8dfe187bd
Jacob Deichert <jacob.d@getjobber.com>
2025-07-28 12:46:44 -0700

    Increase test timeout
    Long page, maybe slow to render icons?

--- packages/site/tests/visual/site.visual.ts
+++ packages/site/tests/visual/site.visual.ts
@@ -88,25 +88,25 @@ test.describe("Atlantis Visual Tests", () => {
   test.describe("banner components", () => {
     test("banner components", async ({ page }) => {
       await page.goto("/visual-tests/banner");
-      await page.waitForTimeout(500);
+      await page.waitForTimeout(1000);
       await expect(page).toHaveScreenshot("visual-test-banner-page.png", {
         fullPage: true,
       });
     });
 
     test("banner components (small window)", async ({ page }) => {
-      await page.goto("/visual-tests/banner");
       await page.setViewportSize({ width: 320, height: 1000 });
-      await page.waitForTimeout(500);
+      await page.goto("/visual-tests/banner");
+      await page.waitForTimeout(1000);
       await expect(page).toHaveScreenshot("visual-test-banner-small-page.png", {
         fullPage: true,
       });
     });
 
     test("banner components (medium window)", async ({ page }) => {
-      await page.goto("/visual-tests/banner");
       await page.setViewportSize({ width: 550, height: 1000 });
-      await page.waitForTimeout(500);
+      await page.goto("/visual-tests/banner");
+      await page.waitForTimeout(1000);
       await expect(page).toHaveScreenshot(
         "visual-test-banner-medium-page.png",
         {
