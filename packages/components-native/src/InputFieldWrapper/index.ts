14f08ddbabb3532d02deb6007e0f4e5380003578
Michael Paradis <michael.p@getjobber.com>
2025-08-08 10:57:00 -0700

    Export clear action in components-native

--- packages/components-native/src/InputFieldWrapper/index.ts
+++ packages/components-native/src/InputFieldWrapper/index.ts
@@ -4,3 +4,5 @@ export {
 } from "./CommonInputStyles.style";
 export { InputFieldWrapper } from "./InputFieldWrapper";
 export type { InputFieldWrapperProps } from "./InputFieldWrapper";
+export { ClearAction } from "./components/ClearAction";
+export type { ClearActionProps } from "./components/ClearAction";
