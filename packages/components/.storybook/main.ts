e8a5d09067df4cd3e7a9699c9eb9d46e3355755a
Jacob Deichert <jacob.d@getjobber.com>
2025-08-11 08:59:09 -0700

    New storybook doesn't need to look for mdx files
    Docs will only exist on the docs site, not in the new storybook instances.

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -2,7 +2,6 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
