4768d6caefba7a32ac40fb15df2346425d56c6ce
Jacob Deichert <jacob.d@getjobber.com>
2025-08-12 08:09:59 -0700

    Remove docs addon
    We aren't using docs in storybook v9. Our docs site handles that.

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-docs",
     "@storybook/addon-a11y",
   ],
   "framework": {
