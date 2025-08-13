bb873f74bc1dd54a9068441c45254156dbf5f58a
Michael Paradis <michael.p@getjobber.com>
2025-08-13 10:12:54 -0700

    Fix only exporting 2 hooks in editorMobileBundle instead of all of them

--- packages/site/src/editorMobileBundle.js
+++ packages/site/src/editorMobileBundle.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import { View } from "react-native-web";
 import { Modalize } from "react-native-modalize";
 import { IntlProvider } from "react-intl";
-import { useFormState } from "@jobber/hooks/useFormState";
-import { useIsMounted } from "@jobber/hooks/useIsMounted";
 
 window.React = React;
 window.ReactDOM = ReactDOM;
@@ -12,6 +10,7 @@ window.IntlProvider = IntlProvider;
 
 export * from "@jobber/components-native";
 export * from "react-native-portalize";
+export * from "@jobber/hooks";
 
 export {
   ReactDOM,
@@ -19,8 +18,6 @@ export {
   React,
   Modalize,
   useRef,
-  useFormState,
-  useIsMounted,
   useEffect,
   useState,
   View,
