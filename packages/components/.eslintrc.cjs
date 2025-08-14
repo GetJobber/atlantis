627bcd282584be9426fa8b55a229309c16a8778d
Jacob Deichert <jacob.d@getjobber.com>
2025-08-14 07:33:19 -0700

    Use eslint to enforce consistent import type specifiers

--- packages/components/.eslintrc.cjs
+++ packages/components/.eslintrc.cjs
@@ -1,5 +1,12 @@
 module.exports = {
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
   rules: {
+    "@typescript-eslint/consistent-type-exports": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
     "import/no-internal-modules": [
       "error",
       {
