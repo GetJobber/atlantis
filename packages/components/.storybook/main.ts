9d8a84bebe212881794a57e00268ddcb80486bcc
Jacob Deichert <jacob.d@getjobber.com>
2025-08-14 15:13:34 -0700

    Update storybook config for components

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -1,15 +1,27 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
 
-const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-a11y",
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
-};
-export default config;
+export default {
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-a11y"],
+  typescript: {
+    // Correctly parses our prop types and generates controls for stories
+    reactDocgen: "react-docgen-typescript",
+  },
+  async viteFinal(config) {
+    const { mergeConfig } = await import("vite");
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          "@jobber/components": path.resolve(__dirname, "../../components/src"),
+          "@jobber/design": path.resolve(__dirname, "../../design/src"),
+          "@jobber/hooks": path.resolve(__dirname, "../../hooks/src"),
+        },
+      },
+    });
+  },
+} satisfies StorybookConfig;
