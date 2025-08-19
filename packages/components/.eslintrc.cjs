d26df28ed21b96e5d75f3ab8eda77a006fd5204d
Jacob Deichert <jacob.d@getjobber.com>
2025-08-19 15:01:53 -0700

    Configure eslint for web v9 .storybook directory

--- packages/components/.eslintrc.cjs
+++ packages/components/.eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       files: ["*.ts", "*.tsx"],
       parserOptions: {
-        project: "tsconfig.json",
+        project: "tsconfig.eslint.json",
         tsconfigRootDir: __dirname,
         sourceType: "module",
       },
--- packages/components/.storybook/preview.tsx
+++ packages/components/.storybook/preview.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import type { Preview } from '@storybook/react-vite'
+import type { Preview } from "@storybook/react-vite";
 import "../../design/dist/foundation.css";
 import "../../design/dist/dark.mode.css";
-import { SBProvider } from './components/SBProvider';
+import { SBProvider } from "./components/SBProvider";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
       expanded: true,
       sort: "alpha",
@@ -17,31 +17,34 @@ const preview: Preview = {
   },
   globalTypes: {
     theme: {
-      description: 'Toggle the Atlantis theme',
+      description: "Toggle the Atlantis theme",
       toolbar: {
         icon: "sun",
-        items: [{
-          title: 'Light',
-          value: 'light',
-          icon: 'sun',
-        }, {
-          title: 'Dark',
-          value: 'dark',
-          icon: 'moon',
-        }],
+        items: [
+          {
+            title: "Light",
+            value: "light",
+            icon: "sun",
+          },
+          {
+            title: "Dark",
+            value: "dark",
+            icon: "moon",
+          },
+        ],
       },
     },
   },
   initialGlobals: {
-    theme: 'light',
+    theme: "light",
   },
   decorators: [
     (Story, context) => (
       <SBProvider theme={context.globals.theme}>
         <Story />
       </SBProvider>
-    )
-  ]
+    ),
+  ],
 };
 
 export default preview;
