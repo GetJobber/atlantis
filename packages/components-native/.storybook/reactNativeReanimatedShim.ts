32d8af482714afaae3ab1fd013145d1ef0533a3e
Jacob Deichert <jacob.d@getjobber.com>
2025-08-21 12:34:40 -0700

    Fix comment

--- packages/components-native/.storybook/reactNativeReanimatedShim.ts
+++ packages/components-native/.storybook/reactNativeReanimatedShim.ts
@@ -1,7 +1,7 @@
 /**
  * react-native-reanimated has a few internal APIs that throw errors when
  * used within web environments (react-native-web). I'm unsure how many
- * conditions may lead to these errors to be thrown, but using fn() from storybook
+ * conditions may lead to these errors being thrown, but using fn() from storybook
  * is one case which causes it.
  */
 setTimeout(() => {
