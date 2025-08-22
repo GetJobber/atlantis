cc5b1046b7b916328287430f00121cf11f134587
Jacob Deichert <jacob.d@getjobber.com>
2025-08-22 13:14:45 -0700

    Fix web and mobile iframe editors
    They were loading multiple instances of react 19 from various directories I guess.

--- packages/site/editorBundle.rollup.config.js
+++ packages/site/editorBundle.rollup.config.js
@@ -16,7 +16,9 @@ export default {
     commonjs(),
     nodePolyfills({ util: true, process: true }),
     json(),
-    resolve(),
+    resolve({
+      dedupe: ["react", "react-dom"],
+    }),
     replace({
       "process.env.NODE_ENV": JSON.stringify("production"),
       preventAssignment: true,
