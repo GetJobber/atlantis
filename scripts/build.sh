fe8e7b198d638862464dc77b63f30174e3a5a7d5
Jacob Deichert <82835373+jdeichert@users.noreply.github.com>
2025-08-28 09:40:12 -0700

    build: Deploy web and mobile v9 storybooks (#2690)
    * Build v9 storybooks and merge into output directory
    
    * Render mobile and web storybook links in docs sidebar
    
    * Delete v7 Button stories
    
    * Set up redirects for v7 Button stories
    
    * Remove unused import
    
    * Don't redirect for valid web/mobile v9 links
    
    * Move redirect to storybook v7 index
    
    I realized that these redirects don't solve the problem. Storybook v7 loads on `/storybook/` and takes over, so I need to move the redirect there.
    
    * Build storybook v9 in CI
    
    * Cleanup long npm cmds into a single script

---
+++ scripts/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e # Exit on error
+
+# This script builds the production docs site, including storybook.
+#
+# The final output looks like this:
+# /                 -> the docs site is the root
+# /storybook        -> storybook v7 (going away soon)
+# /storybook/web    -> storybook v9 web (components)
+# /storybook/mobile -> storybook v9 mobile (components-native)
+
+# Build storybook v7 and v9 instances
+npm run storybook:build
+npm run --prefix packages/components storybook:build
+npm run --prefix packages/components-native storybook:build
+
+# Build the docs site
+npm run --prefix packages/site build
+
+# Merge all outputs into the final dist directory
+mv storybook-static packages/site/dist/storybook
+mv packages/components/storybook-static packages/site/dist/storybook/web
+mv packages/components-native/storybook-static packages/site/dist/storybook/mobile
+mv packages/site/dist storybook-static
+
+# Generate the sitemap
+npm run generate:sitemap
