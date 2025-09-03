2b6caba4a0394ec7af7f21d9e764616659365a70
Jacob Deichert <jacob.d@getjobber.com>
2025-09-03 08:41:47 -0700

    Build hooks with rollup

---
+++ packages/hooks/rollup.config.mjs
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-default-export */
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import multiInput from "rollup-plugin-multi-input";
+
+export default {
+  input: `src/**/index.{ts,tsx}`,
+  output: {
+    dir: "dist",
+    format: "esm",
+    entryFileNames: "[name].js",
+  },
+  plugins: [
+    multiInput.default(),
+    nodeResolve(),
+    commonjs(),
+    typescript({
+      tsconfig: "./tsconfig.rollup.json",
+      noEmitOnError: true,
+      outputToFilesystem: true,
+    }),
+  ],
+  external: [
+    "@apollo/client",
+    "@jobber/formatters",
+    "react",
+    "react-dom",
+    "tslib",
+  ],
+};
