fa0a5f276b5a2ceed7d09d5ff92b31f0bd7bc7c2
ZakaryH <zakary.h@getjobber.com>
2025-09-04 10:35:07 -0700

    add separate entry for v2 autocomplete for now

--- packages/site/src/componentList.ts
+++ packages/site/src/componentList.ts
@@ -54,6 +54,13 @@ export const componentList = [
     additionalMatches: ["Search", "Typeahead", "Suggest"],
   },
   {
+    title: "Autocomplete (v2)",
+    to: "/components/Autocomplete (v2)",
+    imageURL: "/Autocomplete.png",
+    sections: ["Forms & Inputs"],
+    additionalMatches: ["Search", "Typeahead", "Suggest", "Rebuilt"],
+  },
+  {
     title: "AutoLink",
     to: "/components/AutoLink",
     imageURL: "/AutoLink.png",
---
+++ packages/site/src/content/AutocompleteV2/index.tsx
@@ -0,0 +1,39 @@
+import Content from "./AutocompleteV2.stories.mdx";
+import Props from "./AutocompleteV2.props.json";
+import Notes from "./AutocompleteV2Notes.mdx";
+import { ContentExport } from "../../types/content";
+import { getStorybookUrl } from "../../layout/getStorybookUrl";
+
+export default {
+  content: () => <Content />,
+  props: Props,
+  component: {
+    element: `const [value, setValue] = useState();
+const [inputValue, setInputValue] = useState("");
+const menu = [{ type: "section", label: "Services", options: [{ label: "Drain Cleaning" }, { label: "Pipe Replacement" }, { label: "Sewer Line Repair" }, { label: "Seasonal Refreshment" }, { label: "Window Cleaning" }, { label: "Roof Inspection" }, { label: "Flooring Installation" }, { label: "Baseboard Installation" }, { label: "HVAC Repair" }, { label: "HVAC Installation" }] }];
+
+return (
+<div style={{width: "100%"}}>
+  <Autocomplete
+    version={2}
+    placeholder="Search"
+    value={value}
+    onChange={setValue}
+    inputValue={inputValue}
+    onInputChange={setInputValue}
+    menu={menu}
+  />
+</div>
+);`,
+  },
+  title: "Autocomplete (v2)",
+  links: [
+    {
+      label: "Storybook",
+      url: getStorybookUrl(
+        "?path=/docs/components-forms-and-inputs-autocomplete-web-v2--docs",
+      ),
+    },
+  ],
+  notes: () => <Notes />,
+} as const satisfies ContentExport;
