43ed068acb572881354ffc72f08cee41f4fd7f8b
George Miles <george.m@getjobber.com>
2025-09-03 17:58:48 -0700

    Update contentContent.tsx
    Update content map to point to grammar.stories.mdx

--- packages/site/src/maps/contentContent.tsx
+++ packages/site/src/maps/contentContent.tsx
@@ -1,13 +1,13 @@
-import FormattingDocs from "@atlantis/docs/content/formatting.stories.mdx";
+import GrammarDocs from "@atlantis/docs/content/grammar.stories.mdx";
 import ProductVocabularyDocs from "@atlantis/docs/content/product-vocabulary.stories.mdx";
 import VoiceAndToneDocs from "@atlantis/docs/content/voice-and-tone.stories.mdx";
 import { ContentMapItems } from "../types/maps";
 
 export const contentContentMap: ContentMapItems = {
-  formatting: {
-    intro: "Formatting",
-    title: "Formatting",
-    content: () => <FormattingDocs />,
+  grammar: {
+    intro: "Grammar",
+    title: "Grammar",
+    content: () => <GrammarDocs />,
   },
   "product-vocabulary": {
     intro: "Product Vocabulary",
