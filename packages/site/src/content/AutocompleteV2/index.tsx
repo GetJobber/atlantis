93c777c6618ad55d865fc1c91938b4d14e859d46
ZakaryH <zakary.h@getjobber.com>
2025-09-05 13:09:12 -0700

    add more documentation for v2

--- packages/site/src/content/AutocompleteV2/index.tsx
+++ packages/site/src/content/AutocompleteV2/index.tsx
@@ -10,7 +10,12 @@ export default {
   component: {
     element: `const [value, setValue] = useState();
 const [inputValue, setInputValue] = useState("");
-const menu = [{ type: "section", label: "Services", options: [{ label: "Drain Cleaning" }, { label: "Pipe Replacement" }, { label: "Sewer Line Repair" }, { label: "Seasonal Refreshment" }, { label: "Window Cleaning" }, { label: "Roof Inspection" }, { label: "Flooring Installation" }, { label: "Baseboard Installation" }, { label: "HVAC Repair" }, { label: "HVAC Installation" }] }];
+const menu = [{ 
+  type: "section", 
+  label: "Services", 
+  options: [
+    { label: "Drain Cleaning" }, { label: "Pipe Replacement" }, { label: "Sewer Line Repair" }, { label: "Seasonal Refreshment" }, { label: "Window Cleaning" }, { label: "Roof Inspection" }, { label: "Flooring Installation" }, { label: "Baseboard Installation" }, { label: "HVAC Repair" }, { label: "HVAC Installation" }] 
+}];
 
 return (
 <div style={{width: "100%"}}>
