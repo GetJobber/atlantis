467c37cbbccc514a120a5446b6c27a47c9f366d7
Jacob Deichert <jacob.d@getjobber.com>
2025-09-12 13:28:29 -0700

    Remove unnecessary jest config

--- jest.web.config.js
+++ jest.web.config.js
@@ -10,8 +10,6 @@ module.exports = {
     "\\.css$": "identity-obj-proxy",
     "react-markdown":
       "<rootDir>/node_modules/react-markdown/react-markdown.min.js",
-    "^@jobber/hooks$": "<rootDir>/packages/hooks/dist/index.cjs",
-    "^@jobber/hooks/(.*)$": "<rootDir>/packages/hooks/dist/$1",
     "^@jobber/components/(.*)$": "<rootDir>/packages/components/src/$1",
   },
   // Automatically clear mock calls and instances between every test
