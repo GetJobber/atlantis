a0bd5e09fcdec0b49e7d90110c9303c0dab893d9
Jacob Deichert <jacob.d@getjobber.com>
2025-09-18 07:54:31 -0700

    Update prod build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,18 +10,18 @@ set -e # Exit on error
 # /storybook/mobile -> storybook v9 mobile (components-native)
 
 # Build storybook v7 and v9 instances
-npm run storybook:build
+npm run --prefix packages/storybook-v7 storybook:build
 npm run --prefix packages/components storybook:build
 npm run --prefix packages/components-native storybook:build
+
+# Generate the sitemap
+npm run generate:sitemap
 
 # Build the docs site
 npm run --prefix packages/site build
 
 # Merge all outputs into the final dist directory
-mv storybook-static packages/site/dist/storybook
+mv packages/storybook-v7/storybook-static packages/site/dist/storybook
 mv packages/components/storybook-static packages/site/dist/storybook/web
 mv packages/components-native/storybook-static packages/site/dist/storybook/mobile
 mv packages/site/dist storybook-static
-
-# Generate the sitemap
-npm run generate:sitemap
