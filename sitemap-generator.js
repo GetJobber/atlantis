aaffdd2cde5816bb299303d8328b52eaae5d162a
Jacob Deichert <jacob.d@getjobber.com>
2025-09-18 07:51:26 -0700

    Fix sitemap generation path

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -86,7 +86,7 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 </urlset>`;
 
 // Ensure storybook-static directory exists
-const outputDir = path.join(__dirname, "storybook-static");
+const outputDir = path.join(__dirname, "packages/storybook-v7/storybook-static");
 
 if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir, { recursive: true });
