549f328628dd01581f604aad90c7cbe3ac8365de
Jacob Deichert <jacob.d@getjobber.com>
2025-09-19 14:45:33 -0700

    Replace deprecated waitForNextUpdate with alternative solution

--- packages/hooks/src/useCollectionQuery/useCollectionQuery.test.tsx
+++ packages/hooks/src/useCollectionQuery/useCollectionQuery.test.tsx
@@ -377,7 +377,7 @@ describe("useCollectionQuery", () => {
     describe("#subscribeToMore", () => {
       describe("when hook receives update from item not in collection", () => {
         it("should add new item to collection", async () => {
-          const { result, waitForNextUpdate } = renderHook(
+          const { result } = renderHook(
             () => useCollectionQueryHookWithSubscription(query),
             {
               wrapper: wrapper([
@@ -387,21 +387,17 @@ describe("useCollectionQuery", () => {
             },
           );
 
-          // Wait for initial load
-          await act(waitForNextUpdate);
-
-          // Wait for subscription
-          await act(waitForNextUpdate);
-
-          expect(
-            result?.current?.data?.conversation?.smsMessages?.edges?.length,
-          ).toBe(2);
+          await waitFor(() => {
+            expect(
+              result?.current?.data?.conversation?.smsMessages?.edges?.length,
+            ).toBe(2);
+          });
         });
       });
 
       describe("when hook receives update from item already in collection", () => {
         it("should return the existing collection", async () => {
-          const { result, waitForNextUpdate } = renderHook(
+          const { result } = renderHook(
             () => useCollectionQueryHookWithSubscription(query),
             {
               wrapper: wrapper([
@@ -411,22 +407,18 @@ describe("useCollectionQuery", () => {
             },
           );
 
-          // Wait for initial load
-          await act(waitForNextUpdate);
-
-          // Wait for subscription
-          await act(() => wait(200));
-
-          expect(
-            result?.current?.data?.conversation?.smsMessages?.edges?.length,
-          ).toBe(1);
+          await waitFor(() => {
+            expect(
+              result?.current?.data?.conversation?.smsMessages?.edges?.length,
+            ).toBe(1);
+          });
         });
       });
 
       describe("when hook receives `update` but is currently searching a collection", () => {
         it("should return the existing collection without adding the subscribed content", async () => {
           const searchTerm = "FooBar";
-          const { result, waitForNextUpdate } = renderHook(
+          const { result } = renderHook(
             () =>
               useCollectionQueryHookWithSubscriptionAndSearch(
                 query,
@@ -440,15 +432,11 @@ describe("useCollectionQuery", () => {
             },
           );
 
-          // Wait for initial load
-          await act(waitForNextUpdate);
-
-          // Wait for subscription
-          await act(() => wait(200));
-
-          expect(
-            result?.current?.data?.conversation?.smsMessages?.edges?.length,
-          ).toBe(1);
+          await waitFor(() => {
+            expect(
+              result?.current?.data?.conversation?.smsMessages?.edges?.length,
+            ).toBe(1);
+          });
         });
       });
     });
