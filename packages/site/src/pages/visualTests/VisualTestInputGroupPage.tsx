4fbc8ca21997069d1374d33d39d99afd4f024271
nad182 <nazarii.l@getjobber.com>
2025-09-22 13:58:17 -0400

    feat(visualTests): Add vertical InputGroup with horizontal rows to VisualTestInputGroupPage

--- packages/site/src/pages/visualTests/VisualTestInputGroupPage.tsx
+++ packages/site/src/pages/visualTests/VisualTestInputGroupPage.tsx
@@ -29,6 +29,27 @@ export const VisualTestInputGroupPage = () => {
             </Grid>
           </section>
 
+          {/* Vertical InputGroup with Horizontal Rows */}
+          <section>
+            <Text size="large">Vertical InputGroup with Horizontal Rows</Text>
+            <Grid>
+              <Grid.Cell size={{ xs: 12, md: 8 }}>
+                <InputGroup flowDirection="vertical">
+                  <InputText placeholder="Street 1" />
+                  <InputText placeholder="Street 2" />
+                  <InputGroup flowDirection="horizontal">
+                    <InputText placeholder="City" />
+                    <InputText placeholder="Province" />
+                  </InputGroup>
+                  <InputGroup flowDirection="horizontal">
+                    <InputText placeholder="Postal Code" />
+                    <InputText placeholder="Country" />
+                  </InputGroup>
+                </InputGroup>
+              </Grid.Cell>
+            </Grid>
+          </section>
+
           {/* Horizontal InputGroup */}
           <section>
             <Text size="large">Horizontal InputGroup</Text>
