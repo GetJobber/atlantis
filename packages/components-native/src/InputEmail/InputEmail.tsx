b835e8b0c00dfb785446be4de3aff4f54109de36
nad182 <nazarii.l@getjobber.com>
2025-09-30 14:04:03 -0400

    feat(InputEmail): localize email validation message in English and Spanish

--- packages/components-native/src/InputEmail/InputEmail.tsx
+++ packages/components-native/src/InputEmail/InputEmail.tsx
@@ -2,16 +2,19 @@ import type { Ref } from "react";
 import React, { forwardRef } from "react";
 import type { InputTextProps, InputTextRef } from "../InputText";
 import { InputText } from "../InputText";
+import { useAtlantisI18n } from "../hooks/useAtlantisI18n";
 
 export const InputEmail = forwardRef(InputEmailInternal);
 type InputEmailProps = Omit<InputTextProps, "keyboard">;
 
 function InputEmailInternal(props: InputEmailProps, ref: Ref<InputTextRef>) {
+  const { t } = useAtlantisI18n();
+
   const defaultValidations = {
     pattern: {
       value:
         /[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?/,
-      message: "Enter a valid email address (email@example.com)",
+      message: t("InputEmail.enterEmail"),
     },
   } as const;
   const mergedValidations = props.validations
