85c616b7ee5646fac15659c5aed9b8dd09b72dff
ZakaryH <zakary.h@getjobber.com>
2025-10-07 09:44:32 -0700

    Revert "set up conditions for issue on webstory"
    This reverts commit 755801140e58c1e80940ca98d0dc23223b385ba5.

--- docs/components/Menu/Web.stories.tsx
+++ docs/components/Menu/Web.stories.tsx
@@ -12,25 +12,7 @@ export default {
   },
 } as ComponentMeta<typeof Menu>;
 
-const BasicTemplate: ComponentStory<typeof Menu> = args => {
-  return (
-    <div
-      style={{
-        height: "100vh",
-        position: "relative",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-end",
-      }}
-    >
-      <div style={{ display: "inline-flex" }}>
-        <div style={{ position: "sticky", bottom: 0 }}>
-          <Menu {...args} />
-        </div>
-      </div>
-    </div>
-  );
-};
+const BasicTemplate: ComponentStory<typeof Menu> = args => <Menu {...args} />;
 
 export const Horizontal = BasicTemplate.bind({});
 Horizontal.args = {
