867b2024ba297e44bcb930601d57cc6cc6b3439d
Jacob Deichert <jacob.d@getjobber.com>
2025-10-28 09:09:07 -0700

    Fix UseResizeObserver example

--- packages/site/src/content/hooks/UseResizeObserver.tsx
+++ packages/site/src/content/hooks/UseResizeObserver.tsx
@@ -2,30 +2,33 @@ import { Text } from "@jobber/components/Text";
 import { Card } from "@jobber/components/Card";
 import { Content } from "@jobber/components/Content";
 import { useResizeObserver } from "@jobber/hooks/useResizeObserver";
-import { useBreakpoints } from "@jobber/hooks/useBreakpoints";
+import { BREAKPOINT_SIZES } from "@jobber/hooks/useBreakpoints";
+import { Box } from "@jobber/components/Box";
 
 export function UseResizeObserver() {
-  const [ref, { width, exactWidth, exactHeight, height }] = useResizeObserver();
+  const [ref, { width, exactWidth, exactHeight, height }] =
+    useResizeObserver<HTMLDivElement>();
 
   return (
-    <div ref={ref}>
-      <Card title={`Check out my àçƈéñŦ`} accent={getAccent()}>
-        <Content>
-          <Text>Width Step: {width}</Text>
-          <Text>Height Step: {height}</Text>
-          <Text>Exact Width: {exactWidth}</Text>
-          <Text>Exact Height: {exactHeight}</Text>
-        </Content>
-      </Card>
-    </div>
+    <Box width="100%">
+      <div ref={ref}>
+        <Card title={`Check out my àçƈéñŦ`} accent={getAccent()}>
+          <Content>
+            <Text>Width Step: {width}</Text>
+            <Text>Height Step: {height}</Text>
+            <Text>Exact Width: {exactWidth}</Text>
+            <Text>Exact Height: {exactHeight}</Text>
+          </Content>
+        </Card>
+      </div>
+    </Box>
   );
 
   function getAccent() {
-    if (exactWidth < useBreakpoints.smaller) return "red";
-    if (width < useBreakpoints.small) return "orange";
-    if (width < useBreakpoints.base) return "yellow";
-    if (width < useBreakpoints.large) return "green";
-    if (width < useBreakpoints.larger) return "lightBlue";
+    if (exactWidth && exactWidth < BREAKPOINT_SIZES.sm) return "red";
+    if (width && width < BREAKPOINT_SIZES.md) return "orange";
+    if (width && width < BREAKPOINT_SIZES.lg) return "green";
+    if (width && width < BREAKPOINT_SIZES.xl) return "lightBlue";
 
     return "purple";
   }
