a398c1a3a3f1def378b00e5dd42668ec872b90dd
ZakaryH <zakary.h@getjobber.com>
2025-10-29 16:46:31 -0700

    add full width button example

--- docs/components/Menu/Web.stories.tsx
+++ docs/components/Menu/Web.stories.tsx
@@ -378,7 +378,7 @@ export const Composable = () => {
         <h1>Composable with Custom Content</h1>
         <Menu>
           <Menu.Trigger>
-            <Button>
+            <Button fullWidth={true}>
               <Button.Label>Custom</Button.Label>
             </Button>
           </Menu.Trigger>
