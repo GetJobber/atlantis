3aaae0bea7ae7e45b4a47ffdee4d7fdc6b2559d7
ZakaryH <zakary.h@getjobber.com>
2025-11-07 15:19:37 -0800

    fix time rebuilt props

--- packages/components/src/InputTime/InputTime.types.ts
+++ packages/components/src/InputTime/InputTime.types.ts
@@ -69,6 +69,9 @@ export interface InputTimeRebuiltProps
       | "onFocus"
       | "onBlur"
       | "onEnter"
+      | "max"
+      | "min"
+      | "maxLength"
     >,
     AriaInputProps,
     FocusEvents<HTMLInputElement>,
