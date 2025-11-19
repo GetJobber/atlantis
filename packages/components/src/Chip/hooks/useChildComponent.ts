01cbf7f4cb287c6e239598f6fd91bc91418b16cb
Jacob Deichert <jacob.d@getjobber.com>
2025-11-19 14:51:34 -0800

    Fix mention of JSX Element

--- packages/components/src/Chip/hooks/useChildComponent.ts
+++ packages/components/src/Chip/hooks/useChildComponent.ts
@@ -4,7 +4,7 @@ import React, { isValidElement, useMemo } from "react";
 /**
  * Returns the first React Child that receives `true` from isCorrectComponent
  * @param children React Children
- * @param isCorrectComponent a function that accepts a JSX Element and returns a boolean.
+ * @param isCorrectComponent a function that accepts a ReactElement and returns a boolean.
  * @returns The first child component that returns true on isCorrectComponent
  */
 export function useChildComponent(
