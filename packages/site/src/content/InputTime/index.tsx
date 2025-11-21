90b82913d6ac2c2822bd4acb694d44be6e1d6d72
nad182 <nazarii.l@getjobber.com>
2025-11-21 15:31:12 -0500

    fix: removed redundant file extensions

--- packages/site/src/content/InputTime/index.tsx
+++ packages/site/src/content/InputTime/index.tsx
@@ -2,8 +2,8 @@ import Content from "./InputTime.stories.mdx";
 import Props from "./InputTime.props.json";
 import RebuiltProps from "./InputTimeV2.props.json";
 import Notes from "./InputTimeNotes.mdx";
-import originalExample from "./exampleV1.ts";
-import rebuiltExample from "./exampleV2.ts";
+import originalExample from "./exampleV1";
+import rebuiltExample from "./exampleV2";
 import { ContentExport } from "../../types/content";
 import { getStorybookUrl } from "../../layout/getStorybookUrl";
 
