acfcddc57c91d179ef43e2ed44fd25bd3cae6405
Michael Paradis <michael.p@getjobber.com>
2026-01-07 14:06:29 -0800

    Pushing for a test build

--- packages/components-native/src/InputText/InputText.tsx
+++ packages/components-native/src/InputText/InputText.tsx
@@ -9,6 +9,7 @@ import React, {
 } from "react";
 import type {
   FocusEvent,
+  LayoutChangeEvent,
   ReturnKeyTypeOptions,
   StyleProp,
   TextInputProps,
@@ -29,6 +30,15 @@ import type {
 } from "../InputFieldWrapper/InputFieldWrapper";
 import { InputFieldWrapper } from "../InputFieldWrapper";
 import { useCommonInputStyles } from "../InputFieldWrapper/CommonInputStyles.style";
+import { useScreenInformation } from "../Form/hooks/useScreenInformation";
+
+/**
+ * Buffer zone in pixels for offscreen detection.
+ * This makes the detection more sensitive by marking the component as offscreen
+ * even if it's technically still visible but within this buffer distance from the edge.
+ */
+// 44 (accessory bar height) + 20 (buffer)
+const KEYBOARD_AWARE_DETECTION_BUFFER = 64;
 
 export interface InputTextProps
   extends Pick<
@@ -373,6 +383,10 @@ function InputTextInternal(
 
   const styles = useStyles();
   const commonInputStyles = useCommonInputStyles();
+  const { headerHeight, windowHeight } = useScreenInformation();
+  // State to track if the InputText component can fully fit on screen
+  // (i.e., it's completely visible). Use this state to handle visibility issues.
+  const [canFullyFitOnScreen, setCanFullyFitOnScreen] = useState(true);
 
   return (
     <InputFieldWrapper
@@ -395,6 +409,20 @@ function InputTextInternal(
       loadingType={loadingType}
     >
       <TextInput
+        onLayout={(event: LayoutChangeEvent) => {
+          event.target?.measureInWindow((_, y, __, height) => {
+            // Check if component can't fully fit on screen (height only)
+            // Account for headerHeight at the top of the screen and buffer zone
+            const visibleTop = headerHeight + KEYBOARD_AWARE_DETECTION_BUFFER; // Top of visible area (below header) with buffer
+            const visibleBottom =
+              windowHeight - KEYBOARD_AWARE_DETECTION_BUFFER; // Bottom of visible area with buffer
+            const isOffScreen =
+              y < visibleTop || // Top edge is behind or above the header (with buffer)
+              y + height > visibleBottom; // Bottom edge is below the window (with buffer)
+
+            setCanFullyFitOnScreen(!isOffScreen);
+          });
+        }}
         inputAccessoryViewID={inputAccessoryID || undefined}
         testID={testID}
         autoCapitalize={autoCapitalize}
@@ -417,13 +445,18 @@ function InputTextInternal(
         // Prevent focus during scroll for multiline inputs to avoid
         // the input focusing when the user is trying to scroll the form
         readOnly={readonly || (multiline && isScrolling && !focused)}
+        // readOnly={readonly}
         editable={!disabled}
         keyboardType={keyboard}
         value={inputTransform(internalValue)}
         autoFocus={autoFocus}
         autoComplete={autoComplete}
         multiline={multiline}
-        scrollEnabled={multiline}
+        // Makes sure it doesn't jump to the top of the screen when the keyboard is shown and a new line is added.
+        // State for tracking if the input should be scrollable.
+        // This is tech debt related to an issue where keyboard aware scrollview doesn't work if `scrollEnabled` is true. However,
+        // when `scrollEnabled` is false it causes an issue where super long text inputs will jump to the top when a new line is added to the bottom of the input.
+        scrollEnabled={Platform.OS === "ios" && !canFullyFitOnScreen}
         textContentType={textContentType}
         onChangeText={handleChangeText}
         onSubmitEditing={handleOnSubmitEditing}
@@ -447,10 +480,6 @@ function InputTextInternal(
           trimWhitespace(inputTransform(field.value), updateFormAndState);
         }}
         ref={(instance: TextInput) => {
-          // if (autoFocus && instance) {
-          //   console.log("autoFocus", { ...instance });
-          //   instance.focus();
-          // }
           // RHF wants us to do it this way
           // https://react-hook-form.com/faqs#Howtosharerefusage
           textInputRef.current = instance;
