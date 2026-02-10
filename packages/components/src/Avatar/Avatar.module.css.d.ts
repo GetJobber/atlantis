7fe012eaf0f12bccd3d7030199e04ec57ce9d0a2
Taylor <taylor.n@getjobber.com>
2026-02-09 21:14:56 -0500

    remove isDark variable

--- packages/components/src/Avatar/Avatar.module.css.d.ts
+++ packages/components/src/Avatar/Avatar.module.css.d.ts
@@ -2,7 +2,6 @@ declare const styles: {
   readonly "avatar": string;
   readonly "large": string;
   readonly "small": string;
-  readonly "isDark": string;
   readonly "initials": string;
   readonly "smallInitials": string;
   readonly "hasBorder": string;
