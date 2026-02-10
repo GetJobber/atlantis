40a88a8d627ae21993a6005ecc424597b01887d7
Taylor <taylor.n@getjobber.com>
2026-02-10 12:11:18 -0500

    bring back isDark

--- packages/components/src/Avatar/Avatar.module.css.d.ts
+++ packages/components/src/Avatar/Avatar.module.css.d.ts
@@ -2,6 +2,7 @@ declare const styles: {
   readonly "avatar": string;
   readonly "large": string;
   readonly "small": string;
+  readonly "isDark": string;
   readonly "initials": string;
   readonly "smallInitials": string;
   readonly "hasBorder": string;
