6a014d0d0d409bf0e87f1bc9d1a2455d7e6946b2
Lawrence Lin <lawrence.l@getjobber.com>
2026-02-25 22:06:16 -0800

    fix style

--- packages/components/src/LightBox/LightBox.tsx
+++ packages/components/src/LightBox/LightBox.tsx
@@ -83,7 +83,7 @@ export function LightBoxContent() {
           <LightBoxBackground />
           <LightBoxOverlay />
           <LightBoxToolbar />
-          <LightBoxSlides className={styles.imageArea} />
+          <LightBoxSlides />
           <LightBoxNavigation />
           <LightBoxCaption />
           <LightBoxThumbnails />
