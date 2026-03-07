2246a92f5573702673534f3dbd0b48faca714381
Taylor <taylor.n@getjobber.com>
2026-03-06 21:53:29 -0500

    CLEANUP Switch Base-UI

--- jestWebSetupConfig.js
+++ jestWebSetupConfig.js
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom";
+
+if (typeof window !== "undefined" && window.PointerEvent == null) {
+  class PointerEvent extends MouseEvent {}
+  window.PointerEvent = PointerEvent;
+  global.PointerEvent = PointerEvent;
+}
