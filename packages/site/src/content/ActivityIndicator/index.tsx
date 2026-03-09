6d45262d38fc6738e521bccad0e83ee61ee68ee3
Scott Thompson <145565743+scotttjob@users.noreply.github.com>
2026-03-09 11:08:09 -0300

    activityindicator

--- packages/site/src/content/ActivityIndicator/index.tsx
+++ packages/site/src/content/ActivityIndicator/index.tsx
@@ -12,9 +12,11 @@ export default {
   title: "ActivityIndicator",
   links: [
     {
-      label: "Storybook",
+      label: "Mobile Storybook",
+      type: "mobile",
       url: getStorybookUrl(
-        `?path=/docs/components-status-and-feedback-activityindicator--docs`,
+        "?path=/story/components-status-and-feedback-activityindicator--basic",
+        "mobile",
       ),
     },
   ],
