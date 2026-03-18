9fbfd94921c4b62d48a0f8c4aaf3b58c4900d79e
Scott Thompson <145565743+scotttjob@users.noreply.github.com>
2026-03-18 10:45:21 -0300

    removing type that is no longer there

--- packages/components-native/src/InputFieldWrapper/index.ts
+++ packages/components-native/src/InputFieldWrapper/index.ts
@@ -3,6 +3,6 @@ export {
   useCommonInputStyles,
   commonInputStyles,
 } from "./CommonInputStyles.style";
-export { InputFieldWrapper, type Clearable } from "./InputFieldWrapper";
+export { InputFieldWrapper } from "./InputFieldWrapper";
 export type { InputFieldWrapperProps } from "./InputFieldWrapper";
 export { ClearAction } from "./components/ClearAction/ClearAction";
